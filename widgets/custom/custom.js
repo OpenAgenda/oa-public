4b91bf4d74dc4c88b1c6c9a144a0009d282f7033
Kari Olafsson <kaoreolafsson@gmail.com>
2014-10-27 07:37:01 +0100

    map file update

---
+++ widgets/custom/custom.js
@@ -0,0 +1,112 @@
+var UID = 0, VALUES = 1, ACTIVECLASS = 2,
+
+cn = require(  '../../js/lib/common/common.mod.js' ),
+
+wLib = require(  '../lib/widgetLib' ),
+
+debug = require( 'debug' );
+
+if ( ['tpl', 'dev'].indexOf( window.env ) !== -1 ) debug.enable( '*' );
+
+
+var widget = function( elem, options ) {
+
+  var log,
+
+  controller,
+
+  enabled = false,
+
+  selected = false,
+
+  activeClass,
+
+  values,
+
+  init = function() {
+
+    var uid = options.anchorConfig[ UID ];
+
+    log = debug( 'custom widget ' + uid );
+
+    log( 'initing' );
+
+    try {
+
+      values = JSON.parse( options.anchorConfig[ VALUES ] );  
+
+    } catch( e ) {
+
+      log( 'could not parse values' );
+
+      return;
+
+    }
+
+    activeClass = ( typeof options.anchorConfig[ ACTIVECLASS ] !== 'undefined' ) ?  options.anchorConfig[ ACTIVECLASS ] : 'active';
+
+    controller = options.register( wLib.interface( 'custom', uid, {
+      enable: enable,
+      disable: disable,
+      clear: clear
+    } ));
+
+    cn.addEvent( elem, 'click', _onClick );
+
+  },
+
+  enable = function( reqParams ) {
+
+    var same = true;
+
+    for ( var v in values ) {
+
+      if ( ( typeof reqParams[v] !== 'undefined' ) && ( reqParams[v] !== values[v] ) ) {
+
+        same = false;
+
+      }
+
+    }
+
+    cn.addClass( elem, activeClass );
+
+    enabled = true;
+
+  },
+
+  disable = function( ) {
+
+    cn.removeClass( elem, activeClass );
+
+  },
+
+  clear = function( ) {
+
+    cn.removeClass( elem, activeClass );
+
+  },
+
+  _onClick = function() {
+
+    if ( !enabled ) {
+
+      log( 'widget is disabled' );
+
+      return;
+
+    }
+
+    controller.update( 'custom', values );
+
+  }
+
+  init();
+
+};
+
+require( '../lib/controllerLoader' )( function( register ) {
+
+  wLib.forEachAnchor( '.cbpgcstm', { register: register }, widget );
+
+} );
