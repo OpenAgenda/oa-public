372266b66cbf08bc1e8e9d7f15deb69553e41355
Kari Olafsson <kaoreolafsson@gmail.com>
2014-10-28 16:12:54 +0100

    handling of module types

--- packages/cibul-node/server.js
+++ packages/cibul-node/server.js
@@ -1,6 +1,12 @@
 var supervisor = require( './lib/supervisor' ),
 
-log = require( './lib/logger' )('server');
+log = require( './lib/logger' )('server'),
+
+enabledTypes = ( process.argv ? process.argv : [] ).filter( function( argItem ) {
+
+  return [ 'web', 'admin', 'task' ].indexOf( argItem ) !== -1;
+
+});
 
 supervisor( function( loadTasks ) {
 
@@ -23,23 +29,37 @@ supervisor( function( loadTasks ) {
   app.use( require('cookie-parser')() );
 
 
-  // all web app modules are defined here
+  // run 'web' type modules
+  if ( enabledTypes.indexOf( 'web' ) !== -1 ) {
 
-  require( './newsletter/back' )( '/:slug/admin/newsletters' ).load( app );
-  require( './newsletter/front' )( '/:slug/newsletters' ).load( app );
-  require( './general/front' )( '' ).load( app );
-  require( './search/front' )( '' ).load( app );
+    require( './newsletter/back' )( '/:slug/admin/newsletters' ).load( app );
+    require( './newsletter/front' )( '/:slug/newsletters' ).load( app );
+    require( './general/front' )( '' ).load( app );
+    require( './search/front' )( '' ).load( app );
+    
+  }
 
+  // run 'admin' type modules
+  
+  if ( enabledTypes.indexOf( 'admin' ) !== -1 ) {
+
+    log( 'no admin module is defined yet' );
+
+  }
 
   app.listen( config.port );
 
 
   // only one process runs background tasks. supervisor handles that.
+  // only 'task' types run tasks
 
-  if ( !loadTasks ) return;
+  if ( !loadTasks || ( enabledTypes.indexOf( 'admin' ) == -1 ) ) return;
+
 
   require( './newsletter/task' ).load( { period: 60000, bootOffset: 15000 } );
+  
   require( './mailer/task' ).load( { bootOffset: 14909 } );
+  
   require( './search/task' ).load( { bootOffset: 12483 } );
 
 });
