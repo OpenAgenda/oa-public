7c1f92fc455fe37481c3d57eebbc9c9975dcb56c
Kari Olafsson <kaoreolafsson@gmail.com>
2015-04-20 13:17:31 +0200

    make language toggle work in node first layout

--- layout/js/main.js
+++ layout/js/main.js
@@ -58,8 +58,6 @@ cn.addEvent( window, 'load', function() {
 
   cibulMessage();
 
-  //_languageMenu( options.langHeadMenu );
-
   headerProfile( options.profile );
 
   cn.forEach( hooks, function( hook ) {
@@ -88,48 +86,3 @@ window.hook = function( cb ) {
  */
 
 window.getSession = handleSession();
-
-
-
-/**
- * toggle language menu display
- */
-
-function _languageMenu( options ) {
-
-  var params = cn.extend( {
-    selectors: {
-      main: '.js_top_nav',
-      langMenu: '.js_language_menu',
-    }
-  }, options ? options : {} ),
-
-  langOn = false,
-
-  headElem = cn.el( params.selectors.main ),
-
-  langMenuElem = cn.el( headElem, params.selectors.langMenu );
-
-  if ( !langMenuElem ) return;
-
-  var langList = cn.el( langMenuElem, 'ul' );
-
-  cn.addEvent( langMenuElem, 'click', function( e ) {
-
-    cn.preventDefault( e );
-
-    if ( !langOn ) {
-
-      langList.style.display = 'block';
-
-    } else {
-
-      langList.removeAttribute( 'style' );
-
-    }
-
-    langOn = !langOn;
-
-  });
-
-}
