463bda28f5e36e9cbf7634b711fbd3a17f9d1a6a
Kari Olafsson <kaoreolafsson@gmail.com>
2015-09-09 10:52:14 +0200

    removed comment over CDATA decoration

--- packages/emailstrategie/lib/interface.js
+++ packages/emailstrategie/lib/interface.js
@@ -284,7 +284,7 @@ function _parse( key, item, leaf ) {
       // emailStrategie does not like &
       clean = clean.replace( /\&/g, '&amp;' );
 
-      //clean = '<![CDATA[' + clean + ']]>';
+      clean = '<![CDATA[' + clean + ']]>';
 
     }
 
