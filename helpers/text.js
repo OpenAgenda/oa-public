c8dfbc155cbaca5c37d7f5114c41ae07f1279a35
Kari Olafsson <kaoreolafsson@gmail.com>
2015-12-02 09:58:23 +0100

    transactional email tweaks

--- helpers/text.js
+++ helpers/text.js
@@ -40,7 +40,7 @@ function nl2br( str, is_xhtml ) {
 
   var breakTag = (is_xhtml || typeof is_xhtml === 'undefined') ? '<br ' + '/>' : '<br>'; // Adjust comment to avoid issue on phpjs.org display
 
-  return (str + '').replace(/([^>\r\n]?)(\r\n|\n\r|\r|\n)/g, '$1' + breakTag + '$2');
+  return ( escape( str + '' ) ).replace(/([^>\r\n]?)(\r\n|\n\r|\r|\n)/g, '$1' + breakTag + '$2');
 
 }
 
