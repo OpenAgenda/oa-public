ca0d73f4d59264561be0d825f101fa24c08a1d43
Kari Olafsson <kaoreolafsson@gmail.com>
2016-02-01 10:15:44 +0100

    limited ics generation

--- packages/cibul-node/services/agenda/ics.js
+++ packages/cibul-node/services/agenda/ics.js
@@ -6,13 +6,19 @@ eventSvc = require( '../event' ),
 
 genUrl = require( '../genUrl' ),
 
-utils = require( 'utils' );
+utils = require( 'utils' ),
+
+i18n = require( '../../i18n/i18n' ),
+
+limit = 2000000;
 
 module.exports = function( req, res, next ) {
 
   var stream = req.agenda.searchStream( req.query.oaq ),
 
-  renderedHeader = false;
+  renderedHeader = false,
+
+  size = 0;
 
   res.writeHead( 200, {
     'Content-Type': 'text/calendar'
@@ -30,47 +36,75 @@ module.exports = function( req, res, next ) {
 
     }
 
-    chunk += '\n' + _renderEvent( req.agenda, data, req.lang );
+    chunk += '\n' + _renderEvent( req.agenda, data, req.lang, req.query.oaq || {} );
+
+    size += Buffer.byteLength( chunk, 'utf8');
 
     res.write( chunk );
+
+    if ( size > limit ) {
+     
+      stream.pause();
+
+      _end( stream, res );
+
+    }
 
   } );
 
   stream.on( 'end', () => {
 
-    res.write( '\nEND:VCALENDAR' );
-
-    res.end();
+    _end( stream, res );
 
   } );
 
 }
 
-function _renderEvent( agenda, eData, lang, cb ) {
+function _end( stream, res ) {
+
+  stream = undefined;
+
+  res.write( '\nEND:VCALENDAR' );
+
+  res.end();
+
+}
+
+function _renderEvent( agenda, eData, lang, query ) {
 
   var ev = eventSvc.instanciate( eData ),
 
   l = ev.getLocationDetails(),
 
-  repeated, icaled = [];
+  repeated, icaled = [],
+
+  today = new Date(),
+
+  url = genUrl( 'agendaEventShow', { 
+    slug: agenda.slug,
+    eventSlug: ev.slug
+  }, { protocol: 'https://' } ),
+
+  truncatedDescription = _esc( utils.truncate( ev.getFreeText(), 30, '...' ) ),
+
+  timings = ev.getTimings();
 
   ev.switchLanguage( lang );
 
   repeated = [
     'DTSTAMP:' + _date(),
     'SUMMARY:' + _esc( ev.getDescription() ),
-    'DESCRIPTION:' + _esc( ev.getFreeText() ),
+    'DESCRIPTION:' + truncatedDescription + ' ' + i18n( 'see more', lang ) + ': ' + url,
     'STATUS:CONFIRMED',
     'LOCATION:' + l.name + '\\n' + l.address,
     'GEO:' + l.latitude + ';' + l.longitude,
-    'URL:' + genUrl( 'agendaEventShow', { 
-      slug: agenda.slug,
-      eventSlug: ev.slug
-    }, { protocol: 'https://' } ),
+    'URL:' + url,
     'LAST-MODIFIED:' + _date( ev.updatedAt )
   ];
 
-  ev.getTimings().forEach( t => {
+  today = today.getFullYear() + '-' + utils.fZ( today.getMonth() + 1 ) + '-' + utils.fZ( today.getDate() );
+
+  timings.filter( t => timings.length <= 10 || t.start.split( 'T' )[ 0 ] >= today ).forEach( t => {
 
     icaled = icaled.concat( [
       'BEGIN:VEVENT',
