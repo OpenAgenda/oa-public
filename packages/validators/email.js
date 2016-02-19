b6c2d36652f2c1d5ebb5be15731f882ab9138420
Kari Olafsson <kaoreolafsson@gmail.com>
2016-02-19 12:28:00 +0100

    have email validator reject spaced strings

--- packages/validators/email.js
+++ packages/validators/email.js
@@ -1,18 +1,41 @@
 "use strict";
 
-var rgx = require( './regex' );
+var utils = require( 'utils' );
 
 module.exports = function( config ) {
 
-  return rgx( {
-    optional: config ? config.optional : false,
-    field: config ? config.field : undefined,
-    regex: /[a-z0-9!#$%&'*+\/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+\/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/i,
+  var params = utils.extend( {
+    field: undefined,
     error: {
       code: 'email.invalid',
       message: 'email is not valid'
     },
     type: 'email'
-  } );
+  }, config || {} ),
+
+  emaiRgx = /[a-z0-9!#$%&'*+\/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+\/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/i;
+
+  validate.type = 'email';
+
+  return validate;
+
+  function validate( value ) {
+
+    var clean = typeof value === 'string' ? value.trim() : '';
+
+    if ( clean.indexOf( ' ' ) !== -1 || !emaiRgx.test( clean ) ) {
+
+      throw [ {
+        field: params.field,
+        code: params.error.code,
+        message: params.error.message,
+        origin: value
+      } ];      
+
+    } 
+
+    return clean;
+
+  }
 
 }
--- packages/validators/test/email.js
+++ packages/validators/test/email.js
@@ -34,4 +34,24 @@ describe( 'email validator', () => {
 
   } );
 
+  it( 'some long random string with an email does not validate', () => {
+
+    let caught = false;
+
+    try {
+
+      validate( 'Amandine Plas ( chargéés de la com et du spons ) contacts: protable 06 33 09 49 72 email: usmelunhand@wanadoo.fr' );
+
+    } catch( e ) {
+
+      caught = true;
+
+      e[ 0 ].code.should.equal( 'email.invalid' );
+
+    }
+
+    caught.should.equal( true );
+
+  } );
+
 } );
