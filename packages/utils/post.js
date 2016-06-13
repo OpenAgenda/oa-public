7c291744de52ce4a15e70b62987728522141e8cc
Kari Olafsson <kaoreolafsson@gmail.com>
2016-06-13 15:11:36 +0200

    added a post file

---
+++ packages/utils/post.js
@@ -0,0 +1,33 @@
+"use strict";
+
+var xhr = require( 'xhr' );
+
+module.exports = function( res, data, cb ) {
+
+  xhr( {
+    uri: res,
+    method: 'post',
+    headers: {
+      'X-Requested-With': 'XMLHttpRequest',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify( data )
+  }, function( err, result ) {
+
+    if ( err ) {
+
+      return cb( err );
+
+    }
+
+    if ( result.statusCode !== 200 ) {
+
+      return cb( { statusCode: result.statusCode } );
+
+    }
+
+    cb( null, result.body );
+
+  } );
+
+}
