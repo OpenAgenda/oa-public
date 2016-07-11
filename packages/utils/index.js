16a457375c826212941b263b0914eb370dd20359
Kari Olafsson <kaoreolafsson@gmail.com>
2016-07-11 17:04:26 +0200

    added ENQ to cleanString function

--- packages/utils/index.js
+++ packages/utils/index.js
@@ -235,11 +235,14 @@ function cleanString( str ) {
   return str.replace( new RegExp( [ '[',
     String.fromCharCode( 8233 ),
     String.fromCharCode( 8232 ),
-    String.fromCharCode( 8 ),
     String.fromCharCode( 1 ),
     String.fromCharCode( 2 ),
     String.fromCharCode( 3 ),
     String.fromCharCode( 4 ),
+    String.fromCharCode( 5 ),
+    String.fromCharCode( 6 ),
+    String.fromCharCode( 7 ),
+    String.fromCharCode( 8 ),
   ']' ].join( '' ), 'g' ), ' ' );
 
 }
