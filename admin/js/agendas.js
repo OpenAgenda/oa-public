ce848e742e4a85ff6d73ace2dacabb7c7a98a88d
Kari Olafsson <kaoreolafsson@gmail.com>
2016-08-11 20:30:31 +0200

    fix admin agenda routes

--- admin/js/agendas.js
+++ admin/js/agendas.js
@@ -6,7 +6,9 @@ var App = require( 'admin-agendas/components/lib/main' ),
     lang: 'fr',
     res: {
       agendas: '/admin/agendas/search',
-      stakeholders : '/admin/agendas/stakeholders/search'
+      stakeholders : '/admin/agendas/stakeholders/search',
+      agendaSet: '/admin/agendas',
+      agendaGet: '/admin/agendas/get'
     },
     selectors: {
       canvas: '.js_canvas'
@@ -20,6 +22,8 @@ window.hook( function( options ) {
   App( {
     searchRes: params.res.agendas,
     stakeholdersRes: params.res.stakeholders,
+    setAgendaRes: params.res.agendaSet,
+    agendaRes: params.res.agendaGet,
     canvas: params.selectors.canvas
   } );
 
