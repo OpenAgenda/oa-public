85fcee5c81ccadb59db201d3b48baac09f5977ca
Kari Olafsson <kaoreolafsson@gmail.com>
2016-09-06 17:46:41 +0200

    missed a file

---
+++ packages/react-components/components/bodyScroll.js
@@ -0,0 +1,64 @@
+"use strict";
+
+const du = require( 'dom-utils' );
+
+module.exports = {
+  enable,
+  disable
+}
+
+// remove overflow:hidden from body
+function enable() {
+
+  let bodyElem = du.el( 'body' );
+
+  if ( !bodyElem ) return;
+
+  let style = _parseStyle( bodyElem.getAttribute( 'style' ) );
+
+  style.overflow = undefined;
+
+  bodyElem.setAttribute( 'style', _stringifyStyle( style ) );
+
+}
+
+// add overflow:hidden to body
+function disable() {
+
+  let bodyElem = du.el( 'body' );
+
+  if ( !bodyElem ) return;
+
+  let style = _parseStyle( bodyElem.getAttribute( 'style' ) );
+
+  style.overflow = 'hidden';
+
+  bodyElem.setAttribute( 'style', _stringifyStyle( style ) );
+
+}
+
+function _stringifyStyle( style ) {
+
+  console.log( style );
+
+  return Object.keys( style ).filter( k => !!k.length ).map( k => k + ':' + style[ k ] ).join( ';' );
+
+}
+
+function _parseStyle( style ) {
+
+  let parsed = {};
+
+  ( style || '' ).split( ';' ).forEach( part => {
+
+    if ( !part.length ) return;
+
+    let bits = part.split( '=' );
+
+    parsed[ bits[ 0 ] ] = bits[ 1 ];
+
+  } );
+
+  return parsed;
+
+}
