a437b2dfc4eedf092e728f24f6c51b49b1dc7353
Kari Olafsson <kaoreolafsson@gmail.com>
2016-10-24 09:52:34 +0200

    added some readme text

--- packages/home/testconfig.sample.js
+++ packages/home/testconfig.sample.js
@@ -1,1 +1,11 @@
-module.exports = {};
+module.exports = {
+  mysql: {},
+  schemas: {
+    agendas: 'review',
+    stakeholders: 'reviewer'
+  },
+  image: {
+    path: '//cibul.s3.amazonaws.com/',
+    default: '//s3.eu-central-1.amazonaws.com/oastatic/graylogo140.png'
+  }
+};
