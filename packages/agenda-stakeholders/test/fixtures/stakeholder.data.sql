a6afca6c540dfb17640bdb6305a24af6d7e172bd
Kari Olafsson <kaoreolafsson@gmail.com>
2016-11-09 13:58:09 +0100

    fixtures(chores): set id as autoincrement to please knex

--- packages/agenda-stakeholders/test/fixtures/stakeholder.data.sql
+++ packages/agenda-stakeholders/test/fixtures/stakeholder.data.sql
@@ -27,14 +27,15 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE IF NOT EXISTS ${schema} (
-`id` bigint(20) NOT NULL,
+  `id` bigint(20) AUTO_INCREMENT,
   `user_id` bigint(20) NOT NULL,
   `review_id` bigint(20) NOT NULL,
   `credential` bigint(20) NOT NULL,
   `created_at` datetime NOT NULL,
   `updated_at` datetime NOT NULL,
   `store` longtext,
-  `organization` varchar(255) DEFAULT NULL
+  `organization` varchar(255) DEFAULT NULL,
+  PRIMARY KEY(id)
 ) ENGINE=InnoDB AUTO_INCREMENT=10746 DEFAULT CHARSET=latin1;
 
 --
--- packages/agenda-stakeholders/test/fixtures/stakeholder_empty.data.sql
+++ packages/agenda-stakeholders/test/fixtures/stakeholder_empty.data.sql
@@ -1,10 +1,11 @@
 CREATE TABLE IF NOT EXISTS ${schema} (
-`id` bigint(20) NOT NULL,
+`id` bigint(20) AUTO_INCREMENT,
   `user_id` bigint(20) NOT NULL,
   `review_id` bigint(20) NOT NULL,
   `credential` bigint(20) NOT NULL,
   `created_at` datetime NOT NULL,
   `updated_at` datetime NOT NULL,
   `store` longtext,
-  `organization` varchar(255) DEFAULT NULL
+  `organization` varchar(255) DEFAULT NULL,
+  PRIMARY KEY(id)
 ) ENGINE=InnoDB AUTO_INCREMENT=10746 DEFAULT CHARSET=latin1;
