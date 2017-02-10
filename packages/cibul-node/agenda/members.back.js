86c839cbc0735b5a510280fd20280aaf717b1e9e
Bertho <kevin.bertho@gmail.com>
2017-02-10 11:17:51 +0100

    feat(members) implement first release of members-app

--- packages/cibul-node/agenda/members.back.js
+++ packages/cibul-node/agenda/members.back.js
@@ -6,42 +6,124 @@ const config = require( '../config' );
 const modLib = require( '../lib/moduleLib.js' );
 const cmn = require( '../lib/commons-app' );
 const bodyParser = require( 'body-parser' );
+const { middleware: agendasMw, instanciate: instanciateAgenda } = require( 'agendas' );
+const usersSvc = require( 'users' );
 const stakeholdersSvc = require( 'agenda-stakeholders' );
-const mw = require( 'member-apps/middleware' )( stakeholdersSvc, { limit: 20 } );
-const agendaSvc = require( '../services/agenda' );
+const stakeholdersMw = require( 'agenda-stakeholders/middleware' );
+const mw = require( 'member-apps/middleware' );
+const { mw: { loadAdminLayout, load: oldAgendaLoad } } = require( '../services/agenda' );
 const sessions = require( 'sessions' );
 
 
+const appMw = [
+  loadAdminLayout,
+  cmn.loadBaseData( 'oasfmain.css' ),
+  matchApp
+];
+
 const routes = {
 
-  membersApp: [ 'get', '', [
-    agendaSvc.mw.load( 'slug' ),
-    cmn.checkAdministrator(),
-    agendaSvc.mw.loadAdminLayout,
-    cmn.loadBaseData( 'oasfmain.css' ),
-    matchApp
-  ] ],
-
-  membersSub: [ 'get', '/?*?', [
-    agendaSvc.mw.load( 'slug' ),
-    cmn.checkAdministrator(),
-    agendaSvc.mw.loadAdminLayout,
-    cmn.loadBaseData( 'oasfmain.css' ),
-    matchApp
-  ] ],
+  membersApp: [ 'get', '', appMw ],
+  membersSub: [ 'get', '/?*?', appMw ],
 
   /**********/
 
   membersList: [ 'get', '/stakeholders.json', [
-    agendaSvc.mw.load( 'slug' ),
-    cmn.checkAdministrator(),
-    mw.list
+    stakeholdersMw.agenda( 'agendaInstance.data' ).list( { detailed: true } ),
+    ( { stakeholders, total }, res ) => res.json( { stakeholders, total } )
   ] ],
 
   membersStats: [ 'get', '/stats', [
-    agendaSvc.mw.load( 'slug' ),
-    cmn.checkAdministrator(),
-    mw.stats
+    stakeholdersMw.agenda( 'agendaInstance.data' ).stats(),
+    ( { stats }, res ) => res.json( { stats } )
+  ] ],
+
+  membersRemove: [ 'get', '/remove/:uid', [
+    usersSvc.mw.load( 'params.uid', 'stakeholderUser' ),
+    stakeholdersMw.agenda( 'agendaInstance.data' ).get( {
+      namespaces: {
+        user: 'stakeholderUser',
+        stakeholder: 'stakeholderToUse',
+        instance: 'stakeholderInstanceToUse'
+      }
+    } ),
+    ( req, res, next ) => {
+      if ( req.stakeholder.credential === 3 && [ 2, 3 ].includes( req.stakeholderToUse.credential ) ) {
+        return next( new Error( 'You don\'t have right to remove this stakeholder' ) );
+      }
+      next();
+    },
+    stakeholdersMw.agenda( 'agendaInstance.data' ).remove( {
+      namespaces: {
+        user: 'stakeholderUser'
+      }
+    } ),
+    ( { result }, res ) => res.status( !result.success ? 400 : 200 ).json( result )
+  ] ],
+
+  membersUpdate: [ 'post', '/update/:uid', [
+    usersSvc.mw.load( 'params.uid', 'stakeholderUser' ),
+    stakeholdersMw.agenda( 'agendaInstance.data' ).get( {
+      namespaces: {
+        user: 'stakeholderUser',
+        stakeholder: 'stakeholderToUse',
+        instance: 'stakeholderInstanceToUse'
+      }
+    } ),
+    ( req, res, next ) => {
+      if ( req.stakeholder.credential !== 3 || ![ 2, 3 ].includes( req.stakeholderToUse.credential ) ) {
+        return next();
+      }
+      next( new Error( 'You don\'t have right to update this stakeholder' ) );
+    },
+    stakeholdersMw.agenda( 'agendaInstance.data' ).update( {
+      namespaces: {
+        user: 'stakeholderUser',
+        data: 'body'
+      }
+    } ),
+    ( { result }, res ) => res.status( result.errors.length ? 400 : 200 ).json( result )
+  ] ],
+
+  membersInvite: [ 'post', '/invite', [
+    ( req, res, next ) => {
+      if ( req.stakeholder.credential !== 3 || ![ 2, 3 ].includes( req.body.credential ) ) {
+        return next();
+      }
+      next( new Error( 'You don\'t have right to invite members with this role' ) );
+    },
+    stakeholdersMw.agenda( 'agendaInstance.data' ).bulkCreate( {
+      namespaces: {
+        data: 'body'
+      },
+      allowPartial: true
+    } ),
+    ( req, res, next ) => {
+
+      const { queued } = req.result;
+      const [ errors, results ] = _.unzip( req.result.results ).map( _.compact );
+
+      if ( errors.length ) return next( { errors } );
+
+      const emailsRejected = _.compact( results.reduce( ( prev, next ) => {
+        let emailRejected;
+        if ( next.errors && next.errors.length ) {
+          emailRejected = next.errors.reduce( ( prev, next ) => {
+            return (next.code && next.code.includes( 'email' ) && next.origin) || null;
+          }, null );
+        }
+        return prev.concat( emailRejected );
+      }, [] ) );
+
+      req.result = { queued, emailsRejected, success: !emailsRejected.length };
+
+      next();
+
+    },
+    ( { result }, res ) => {
+      const status = (result.errors && result.errors.length) || !result.success ? 400 : 200;
+      res.status( status ).json( result )
+    }
   ] ]
 
 };
@@ -52,8 +134,34 @@ module.exports = path => {
 
   router.pre( [
     cmn.loadLogger( 'members' ),
+    bodyParser.json(),
+    sessions.middleware.load(),
     sessions.middleware.ifUnlogged( cmn.redirectTo() ),
-    bodyParser.json()
+    agendasMw.load( {
+      namespaces: {
+        identifiers: {
+          slug: 'params.slug'
+        },
+        result: 'agendaInstance'
+      },
+      instanciate: true,
+      internal: true
+    } ),
+    stakeholdersMw.agenda( 'agendaInstance.data' ).get( {
+      namespaces: {
+        user: 'user',
+        stakeholder: 'stakeholder',
+        instance: 'stakeholderInstance'
+      }
+    } ),
+    agendasMw.loadRoles( {
+      namespaces: {
+        agenda: 'agendaInstance', // slug with req.params.slug in real world
+        result: 'agendaRoles'
+      }
+    } ),
+    oldAgendaLoad( 'slug' ),
+    cmn.checkAdministrator()
   ] );
 
   return {
@@ -91,14 +199,20 @@ function matchApp( req, res, next ) {
           perPageLimit: 20
         },
         res: {
+          app: req.genUrl( 'membersApp', { slug: req.agenda.slug } ),
           list: req.genUrl( 'membersList', { slug: req.agenda.slug } ),
+          update: req.genUrl( 'membersUpdate', { slug: req.agenda.slug, uid: ':uid' } ),
+          remove: req.genUrl( 'membersRemove', { slug: req.agenda.slug, uid: ':uid' } ),
+          invite: req.genUrl( 'membersInvite', { slug: req.agenda.slug } ),
           stats: req.genUrl( 'membersStats', { slug: req.agenda.slug } ),
-          showContributor: req.genUrl( 'agendaAdminShow', { slug: req.agenda.slug }) + '?contributorUid=:contributorUid'
+          showContributor: req.genUrl( 'agendaAdminShow', { slug: req.agenda.slug } ) + '?contributorUid=:contributorUid',
+          writeToMember: '#'
         },
         agenda: {
           uid: req.agenda.uid,
           slug: req.agenda.slug,
-          title: req.agenda.title
+          title: req.agenda.title,
+          roles: req.agendaRoles
         }
       }
     },
