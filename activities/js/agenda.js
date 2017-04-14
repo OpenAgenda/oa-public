fec6c161a8e6ea5e65b007fbcccd743538835102
Bertho <kevin.bertho@gmail.com>
2017-04-14 14:43:08 +0200

    feat(activities,admin) add activities apps

---
+++ activities/js/agenda.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+import App from 'activity-apps/react/dist/apps/agenda';
+import deepExtend from 'deep-extend';
+import du from 'dom-utils';
+
+
+const params = {
+  state: {
+    settings: {
+      lang: 'fr',
+      prefix: '/activities',
+      apiRoot: `localhost:${process.env.PORT || 3000}`,
+      perPageLimit: 20
+    },
+    res: {
+      list: '/:uid/list'
+    },
+    agenda: {
+      title: 'La gargouille',
+      slug: 'la-gargouille',
+      uid: 88888888
+    }
+  }
+};
+
+window.hook( options => {
+
+  deepExtend( params, options );
+
+  ReactDom.render( App( params ), du.el( '.js_canvas' ) );
+
+} );
---
+++ admin/js/activities.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+import App from 'activity-apps/react/dist/apps/admin';
+import deepExtend from 'deep-extend';
+import du from 'dom-utils';
+
+
+const params = {
+  state: {
+    settings: {
+      lang: 'fr',
+      prefix: '/admin/activities',
+      apiRoot: `localhost:${process.env.PORT || 3000}`,
+      perPageLimit: 20
+    },
+    res: {
+      list: '/list'
+    }
+  }
+};
+
+window.hook( options => {
+
+  deepExtend( params, options );
+
+  ReactDom.render( App( params ), du.el( '.js_canvas' ) );
+
+} );
