5cb6b15795c6ece758843ac88add393654d757e6
Kari Olafsson <kaoreolafsson@gmail.com>
2017-06-26 13:20:00 +0200

    missing files

---
+++ packages/react-form-components/components/MarkdownComponentQuill.jsx
@@ -0,0 +1,25 @@
+"use strict";
+
+import React, { Component, PropTypes } from 'react';
+import toMarkdown from 'to-markdown';
+import marked from 'marked';
+
+export default class MarkdownComponentQuill extends Component {
+
+  static propTypes = {
+
+  };
+
+  constructor( props ) {
+
+    super( props );
+
+  }
+
+  render() {
+
+    return <p>fdsffdq</p>
+
+  }
+
+}
