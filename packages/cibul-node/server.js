8d77b6a2406d05a4b14c01adab43abfa62028b10
Bertho <kevin.bertho@gmail.com>
2017-06-27 14:52:10 +0200

    add globals for SSR

--- packages/cibul-node/server.js
+++ packages/cibul-node/server.js
@@ -4,10 +4,13 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
 if ( process.env.NODE_ENV === 'production' ) require( 'newrelic' );
 
-var enabledTypes = ( process.argv ? process.argv : [] ).filter( function( argItem ) {
+var enabledTypes = ( process.argv ? process.argv : [] ).filter( function ( argItem ) {
 
   return [ 'web', 'admin', 'task' ].indexOf( argItem ) !== -1;
 
 } );
+
+global.__CLIENT__ = false;
+global.__SERVER__ = true;
 
 require( './app' )( enabledTypes );
