773d4aa628e174bae46510c4ccd1b1d33ecb40c5
Bertho <kevin.bertho@gmail.com>
2017-07-03 14:23:22 +0200

    add user activity template

---
+++ activities/js/user.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+import App from 'activity-apps/react/dist/apps/user';
+import deepExtend from 'deep-extend';
+import du from 'dom-utils';
+
+
+const params = {
+  state: {
+    settings: {
+      lang: 'fr',
+      prefix: '/home',
+      apiRoot: `localhost:${process.env.PORT || 3000}`,
+      perPageLimit: 20
+    },
+    res: {
+      list: '/:uid/list'
+    }
+  }
+};
+
+window.hook( options => {
+
+  deepExtend( params, options );
+
+  ReactDom.render( App( params ), du.el( '.js_canvas' ) );
+
+} );
