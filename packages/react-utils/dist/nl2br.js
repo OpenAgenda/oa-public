8935236911f2a5c55f20bcc2204fbec7a88566d1
Bertho <kevin.bertho@gmail.com>
2017-07-20 11:37:24 +0200

    add precommit build

---
+++ packages/react-utils/dist/nl2br.js
@@ -0,0 +1,28 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = nl2br;
+
+var _react = require('react');
+
+var _react2 = _interopRequireDefault(_react);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function nl2br(str) {
+
+  var newlineRegex = /(\r\n|\r|\n)/g;
+
+  if (typeof str === 'number') {
+    return str;
+  } else if (typeof str !== 'string') {
+    return '';
+  }
+
+  return str.split(newlineRegex).map(function (line, index) {
+    return line.match(newlineRegex ? _react2.default.createElement('br', { key: index }) : line);
+  });
+}
+module.exports = exports['default'];
