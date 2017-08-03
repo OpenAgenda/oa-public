efd1aedfc9341d264f7016c4679aed575b549b4c
Kari Olafsson <kaoreolafsson@gmail.com>
2017-08-03 17:51:55 +0200

    tweak(search): dev elasticsearch port setup

--- packages/cibul-node/services/eventSearch/index.js
+++ packages/cibul-node/services/eventSearch/index.js
@@ -9,7 +9,7 @@ module.exports.init = config => {
   eventSearch.init( {
 
     elasticsearch: {
-      host: 'http://ns397902.ip-151-80-41.eu:9200/',
+      host: `http://ns397902.ip-151-80-41.eu:${process.env.NODE_ENV==='production' ? '9200' : '9205'}/`,
       apiVersion: '5.3'
     },
 
