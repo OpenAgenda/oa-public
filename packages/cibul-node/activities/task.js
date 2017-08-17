9699ff605e6dfe02dcd7943bdf796a8de96941b2
Bertho <kevin.bertho@gmail.com>
2017-08-17 16:44:39 +0200

    give activities service to activity-apps with config

--- packages/cibul-node/activities/task.js
+++ packages/cibul-node/activities/task.js
@@ -1,6 +1,4 @@
 const logger = require( 'logger' );
-const utils = require( 'utils' );
-const async = require( 'async' );
 const activitiesSvc = require( 'activities' );
 const agendasSvc = require( 'agendas' );
 const usersSvc = require( 'users' );
--- packages/cibul-node/services/activityApps.js
+++ packages/cibul-node/services/activityApps.js
@@ -3,5 +3,12 @@
 const logger = require( 'logger' );
 
 const activityAppsMw = require( 'activity-apps/middleware' );
+const activitiesSvc = require( 'activities' );
 
-module.exports.init = config => activityAppsMw.init( { limit: 20, logger } );
+module.exports.init = config => activityAppsMw.init( {
+  limit: 20,
+  services: {
+    activities: activitiesSvc
+  },
+  logger
+} );
