195d634e7899151a1ae5a87b9d0dbd0ab7eb6cab
Kari Olafsson <kaoreolafsson@gmail.com>
2017-08-28 11:52:40 +0200

    fix(event interfaces): removed extra call to agendaEvents.remove in events.onRemove interface

--- packages/cibul-node/services/events/onRemove.js
+++ packages/cibul-node/services/events/onRemove.js
@@ -1,7 +1,5 @@
 "use strict";
 
-const agendaEvents = require( 'agenda-events' );
-
 const eventSearch = require( '../eventSearch' );
 
 let log = console.log;
@@ -9,8 +7,6 @@ let log = console.log;
 module.exports = ( event, context ) => {
 
   log( 'removed event %s with context %s', event.uid, JSON.stringify( context ) );
-  
-  agendaEvents.remove( event.uid, { context } );
 
   eventSearch.events.batch.remove( event, context ); // context should have agendaUid
 
