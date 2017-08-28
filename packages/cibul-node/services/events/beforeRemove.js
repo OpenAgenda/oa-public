c85e8fbf0afc233c8e3a4df9f11b60f587733549
Kari Olafsson <kaoreolafsson@gmail.com>
2017-08-28 11:41:27 +0200

    tweak(interfaces): call agendaEvents.remove in events service beforeRemove

--- packages/cibul-node/services/events/beforeRemove.js
+++ packages/cibul-node/services/events/beforeRemove.js
@@ -1,10 +1,22 @@
 "use strict";
 
+const agendaEvents = require( 'agenda-events' );
+
 let log = console.log;
 
-module.exports = ( event, cb ) => {
+module.exports = async ( event, context, cb ) => {
 
   log( 'will remove event %s', event.uid );
+
+  try {
+
+    await agendaEvents.remove( event.uid, { context } );
+
+  } catch( e ) {
+
+    log( 'error', 'failed to remove all agenda event references for event uid %s, error: %s', event.uid, e );
+
+  }
 
   cb();
 
