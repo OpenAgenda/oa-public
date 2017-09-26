881321388e1f12d921929f2f61ea15b571b77fbf
Kari Olafsson <kaoreolafsson@gmail.com>
2017-09-26 10:21:54 +0200

    tweak(agendaTags): using app knex pool for querying

--- packages/cibul-node/services/agendaTags.js
+++ packages/cibul-node/services/agendaTags.js
@@ -4,13 +4,37 @@ const agendaTags = require( 'agenda-tags' );
 
 const appServiceAgendas = require( './agenda' ),
 
+  _ = require( 'lodash' ),
+
   logger = require( 'logger' );
 
 module.exports.init = ( config, cb ) => {
 
+  function _query( queryStr, values, cb ) {
+
+    const query = config.knex.raw( queryStr, values );
+
+    query.then( result => result[ 0 ] ).then( rows => {
+
+      process.nextTick( () => cb( null, rows ) );
+
+    } );
+
+    query.catch( err => {
+
+      process.nextTick( () => cb( err ) );
+
+    } );
+
+  }
+
   agendaTags.init( {
-    store: config.db,
-    legacy: config.db,
+    store: {
+      query: _query
+    },
+    legacy: {
+      query: _query
+    },
     logger,
     interfaces: appServiceAgendas.tagsAndCategories
   }, cb );
