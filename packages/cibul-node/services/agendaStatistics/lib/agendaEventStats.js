7013da415c4d5054e867bed6565c3b4c0d8a1c53
Kari Olafsson <kaoreolafsson@gmail.com>
2017-10-09 11:20:20 +0200

    tweak(stats): added checksum for agenda-events

--- packages/cibul-node/services/agendaStatistics/lib/agendaEventStats.js
+++ packages/cibul-node/services/agendaStatistics/lib/agendaEventStats.js
@@ -1,14 +1,19 @@
 "use strict";
 
 const agendaEvents = require( 'agenda-events' );
+const _ = require( 'lodash' );
 
 module.exports = async agendaUid => {
 
-  return {
+  const totals = {
     total: ( await agendaEvents( agendaUid ).list( 0, 0 ) ).total,
     published: ( await agendaEvents( agendaUid ).list( { state: 'published' }, 0, 0 ) ).total,
     ready: ( await agendaEvents( agendaUid ).list( { state: 'controlled' }, 0, 0 ) ).total,
     toBeCompleted: ( await agendaEvents( agendaUid ).list( { state: 'tocontrol' }, 0, 0 ) ).total
-  }
+  };
+
+  return _.extend( totals, {
+    checksum: total === totals.published + totals.ready + totals.toBeCompleted
+  } );
 
 }
