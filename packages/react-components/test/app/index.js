3b263fe12c758fdb4d94b30f7f92aa6ef488592d
Bertho <kevin.bertho@gmail.com>
2017-10-27 16:07:47 +0200

    npm version tags with next tag instead of pre

--- packages/react-components/test/app/index.js
+++ packages/react-components/test/app/index.js
@@ -1,9 +1,9 @@
 "use strict";
 
-var app = require( 'test-app' )( {
+var app = require( '@openagenda/test-app' )( {
   frontWrapper: __dirname + '/front.jsx',
   styles: [
-    __dirname + '/../../node_modules/bs-templates/compiled/main.css'
+    __dirname + '/../../node_modules/@openagenda/bs-templates/compiled/main.css'
   ],
   webpack: true
 } );
