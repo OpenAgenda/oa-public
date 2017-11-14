fc89f936677e4c8ce727931a4cf4b165dfa316fb
Bertho <kevin.bertho@gmail.com>
2017-11-14 11:29:05 +0100

    remove useless dependencies

--- packages/agendas/service/Agenda/image.js
+++ packages/agendas/service/Agenda/image.js
@@ -1,9 +1,6 @@
 "use strict";
 
-const files = require( '@openagenda/files' );
-const images = require( '@openagenda/images' );
 const imageFiles = require( '@openagenda/image-files' );
-const w = require( 'when' );
 
 module.exports = {
   setImage,
