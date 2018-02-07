b335bcc46aed5adf2974fcade0e353a77b089fd8
Bertho <kevin.bertho@gmail.com>
2018-02-07 14:47:17 +0100

    fix(build): add transform-async-to-generator plugin

--- packages/member-apps/react/dist/containers/Dashboard/Dashboard.js
+++ packages/member-apps/react/dist/containers/Dashboard/Dashboard.js
@@ -4,6 +4,14 @@ Object.defineProperty(exports, "__esModule", {
   value: true
 });
 
+var _regenerator = require('babel-runtime/regenerator');
+
+var _regenerator2 = _interopRequireDefault(_regenerator);
+
+var _asyncToGenerator2 = require('babel-runtime/helpers/asyncToGenerator');
+
+var _asyncToGenerator3 = _interopRequireDefault(_asyncToGenerator2);
+
 var _getPrototypeOf = require('babel-runtime/core-js/object/get-prototype-of');
 
 var _getPrototypeOf2 = _interopRequireDefault(_getPrototypeOf);
@@ -824,14 +832,38 @@ var Dashboard = _wrapComponent('Dashboard')((_dec = (0, _reduxConnect.asyncConne
               getLabel('membersInvited')
             )
           ) : _react3.default.createElement(_InviteMembersForm2.default, { onSubmit: function onSubmit(data) {
-              return invite(data).then(async function (result) {
-                if (result.error && result.error instanceof _reduxForm.SubmissionError) {
-                  throw new _reduxForm.SubmissionError(result.error.errors);
-                }
+              return invite(data).then(function () {
+                var _ref3 = (0, _asyncToGenerator3.default)( /*#__PURE__*/_regenerator2.default.mark(function _callee(result) {
+                  return _regenerator2.default.wrap(function _callee$(_context2) {
+                    while (1) {
+                      switch (_context2.prev = _context2.next) {
+                        case 0:
+                          if (!(result.error && result.error instanceof _reduxForm.SubmissionError)) {
+                            _context2.next = 2;
+                            break;
+                          }
 
-                await getStats();
-                return _this5.search({ search: search });
-              });
+                          throw new _reduxForm.SubmissionError(result.error.errors);
+
+                        case 2:
+                          _context2.next = 4;
+                          return getStats();
+
+                        case 4:
+                          return _context2.abrupt('return', _this5.search({ search: search }));
+
+                        case 5:
+                        case 'end':
+                          return _context2.stop();
+                      }
+                    }
+                  }, _callee, _this5);
+                }));
+
+                return function (_x) {
+                  return _ref3.apply(this, arguments);
+                };
+              }());
             } })
         ),
         memberReinvitedModal.visible && _react3.default.createElement(
