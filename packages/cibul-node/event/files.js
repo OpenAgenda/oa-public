9300199c533cb6de75c27c2405e13a8284aa1527
Kari Olafsson <kaoreolafsson@gmail.com>
2018-03-19 13:33:28 +0400

    feature(custom fields): sub app

---
+++ packages/cibul-node/event/files.js
@@ -0,0 +1,45 @@
+"use strict";
+
+const _ = require( 'lodash' );
+const https = require( 'https' );
+
+const app = require( 'express' )();
+
+const config = require( '../config' );
+const legacyEventSvc = require( '../services/event' );
+const legacyAgendaSvc = require( '../services/agenda' );
+
+module.exports = ( parentApp, path ) => {
+
+  parentApp.use( path, app );
+
+}
+
+app.param( 'slug', legacyAgendaSvc.mw.load( 'slug', { basicLoad: true, cache: true } ) );
+
+app.param( 'eventSlug', legacyEventSvc.mw.load( 'eventSlug', 'slug' ) );
+
+app.param( 'eventSlug', legacyEventSvc.mw.format );
+
+app.param( 'eventSlug', legacyAgendaSvc.mw.decorateEvent( true ) );
+
+app.get( '/:slug/events/:eventSlug/files/:file', ( req, res, next ) => {
+
+  const file = _.head( req.formatted.custom.filter( c => c.fieldType === 'file' && c.name === req.params.file ) )
+
+  if ( !file ) return res.sendStatus( 404 );
+
+  const s3FilePath = config.aws.imageBucketPath + file.value.uploaded;
+
+  https.get( s3FilePath, s3Res => {
+
+    res.writeHead( 200, {
+      'Content-Type' : config.authorizedMimeTypes[ file.value.name.split( '.' ).pop() ],
+      'content-disposition' : `attachment; filename="${file.value.name}"`
+    } );
+
+    s3Res.pipe( res );
+
+  } );
+
+} );
