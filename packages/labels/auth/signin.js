5f4da9c2392fe2bbb19f902dceb20daee8f6fc8d
Kari Olafsson <kaoreolafsson@gmail.com>
2018-05-02 18:55:37 +0200

    chore(labels): added signin & signup labels

---
+++ packages/labels/auth/signin.js
@@ -0,0 +1,40 @@
+"use strict";
+
+module.exports = {
+  connection: {
+    fr: 'Connexion',
+    en: 'Connection'
+  },
+  incorrectPassword: {
+    fr: 'L\'email ou le mot de passe sont incorrects',
+    en: 'Your email or password is incorrect.'
+  },
+  resetPassword: {
+    fr: 'Réinitialiser le mot de passe',
+    en: 'Reset your password'
+  },
+  email: {
+    fr: 'Email',
+    en: 'Email'
+  },
+  password: {
+    fr: 'Mot de passe',
+    en: 'Password'
+  },
+  signin: {
+    fr: 'Se connecter',
+    en: 'Sign in'
+  },
+  lostPassword: {
+    fr: 'J\'ai perdu mon mot de passe',
+    en: 'I lost my password'
+  },
+  noAccount: {
+    fr: 'Vous n\'avez pas encore de compte?',
+    en: 'Don\'t have an account yet?'
+  },
+  createAccount: {
+    fr: 'Créez-en un!',
+    en: 'Create one!'
+  }
+}
---
+++ packages/labels/auth/signup.js
@@ -0,0 +1,48 @@
+"use strict";
+
+module.exports = {
+  createAccount: {
+    fr: 'Créez un compte',
+    en: 'Create an account'
+  },
+  hasAccount: {
+    fr: 'Vous avez déjà utilisé OpenAgenda ?',
+    en: 'Have you already used OpenAgenda ?'
+  },
+  connect: {
+    fr: 'Connectez-vous',
+    en: 'Connect'
+  },
+  nameSurname: {
+    fr: 'Prénom Nom',
+    en: 'Name Surname'
+  },
+  email: {
+    fr: 'Email',
+    en: 'Email'
+  },
+  password: {
+    fr: 'Mot de passe',
+    en: 'Password'
+  },
+  repeatPassword: {
+    fr: 'Répéter le mot de passe',
+    en: 'Repeat Password'
+  },
+  signup: {
+    fr: 'S\'inscrire',
+    en: 'Sign Up'
+  },
+  agree: {
+    fr: 'En créant un compte vous acceptez les %terms% ainsi que la %policy%.',
+    en: 'By creating an account you agree to the %terms% and to the %policy%.'
+  },
+  termsOfUse: {
+    fr: 'Conditions d\'Utilisation',
+    en: 'Terms of Use'
+  },
+  policy: {
+    fr: 'Politique de confidentialité',
+    en: 'Privacy Policy'
+  }
+}
