eaff883ca2fd8043aaf2a03c47c90f4b4792392f
Kari Olafsson <kaoreolafsson@gmail.com>
2018-06-12 10:18:37 +0200

    chore(surveys): put compressed js files on s3 at dist

--- packages/surveys/webpack.dist.js
+++ packages/surveys/webpack.dist.js
@@ -1,5 +1,9 @@
 "use strict";
 
+const CompressionPlugin = require( 'compression-webpack-plugin' );
+const LodashModuleReplacementPlugin = require( 'lodash-webpack-plugin' );
+const S3Plugin = require( 'webpack-s3-plugin' );
+
 module.exports = {
   mode: 'production',
   context: __dirname,
@@ -8,6 +12,26 @@ module.exports = {
     filename: 'index.js',
     path: __dirname + '/client/dist'
   },
+  plugins: [ new LodashModuleReplacementPlugin ].concat(
+    ( process.env.NODE_ENV === 'production' && parseInt( process.env.CI ) ) ? [
+      new CompressionPlugin( {
+        test: /\.js/,
+        filename ( asset ) {
+          return asset.replace( '.gz', '' );
+        }
+      } ), new S3Plugin( {
+        s3Options: {
+          accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+          secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+          region: 'eu-west-1'
+        },
+        s3UploadOptions: {
+          Bucket: 'oasvc',
+          ContentEncoding: 'gzip'
+        },
+        basePathTransform: f => 'surveys/' + f
+      } )
+  ] : [] ),
   module: {
     rules: [ {
       test: /\.js$/,
