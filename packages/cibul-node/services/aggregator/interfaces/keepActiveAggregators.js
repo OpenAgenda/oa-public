4d0025eb1c51f8b83fb2516f7f73e4ef6f815ef3
Kari Olafsson <kaoreolafsson@gmail.com>
2018-06-26 07:08:17 +0200

    fix(aggregators): active aggregator control limits filtering to 20 agendas

--- packages/cibul-node/services/aggregator/interfaces/keepActiveAggregators.js
+++ packages/cibul-node/services/aggregator/interfaces/keepActiveAggregators.js
@@ -6,7 +6,7 @@ const wn = require( 'when/node' );
 
 module.exports = async agendaIds => {
 
-  const agendas = ( await wn.call( agendaSvc.list, { id: agendaIds }, { private: null, includeFields: [ 'credentials' ] } ) )[ 0 ];
+  const agendas = ( await wn.call( agendaSvc.list, { id: agendaIds }, 0, 200, { private: null, includeFields: [ 'credentials' ] } ) )[ 0 ];
 
   return agendas.filter( a => a.credentials && a.credentials.aggregator );
 
