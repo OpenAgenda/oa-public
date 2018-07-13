6caab9484a0e788d89f3bd7c1a1bcac8666ebe52
Bertho <kevin.bertho@gmail.com>
2018-07-13 18:41:59 +0200

    tweak(agenda-stakeholders): allow .message to take 'invited' query fix(cibul-node): fix mails.transport config

--- packages/cibul-node/config/index.js
+++ packages/cibul-node/config/index.js
@@ -228,8 +228,7 @@ const config = {
       defaults: {
         from: 'no-reply@openagenda.com',
         replyTo: 'admin@openagenda.com'
-      },
-      transport: prod.mails.transport
+      }
     },
     mailer: {
       service: 'mailgun',
@@ -764,7 +763,11 @@ const config = {
     }
   },
 
-  production: {}
+  production: {
+    mails: {
+      transport: prod.mails.transport
+    }
+  }
 };
 
 currentConfig = _loadEnv( process.env.NODE_ENV || 'development' );
