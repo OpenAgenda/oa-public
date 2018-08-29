8b93cd1622b984acfde738905a93602a81fc8998
Kari Olafsson <kaoreolafsson@gmail.com>
2018-08-29 10:12:57 +0200

    tweak(agenda-contribute): normalize app width

--- packages/agenda-contribute/client/src/components/Canvas.js
+++ packages/agenda-contribute/client/src/components/Canvas.js
@@ -24,7 +24,7 @@ export default class Canvas extends Component {
 
     return <div className="container">
       <div className="row">
-        <div className="col-lg-offset-3 col-lg-6 margin-bottom-lg">
+        <div className="col-sm-offset-2 col-sm-8 col-lg-offset-3 col-lg-6 margin-bottom-lg">
           <div className="text-center">
             <h2 className="margin-top-lg margin-bottom-lg">{labels.addEvent[lang]}</h2>
             <div className="wsq padding-top-md padding-h-md">
