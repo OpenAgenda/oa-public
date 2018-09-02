c05514395d123c2d12db0ce2e23cb2dea07e60e8
Kari Olafsson <kaoreolafsson@gmail.com>
2018-09-02 19:46:12 +0200

    chore(cibul-templates): minor change in config.prod

--- packages/cibul-templates/prodify/config.prod.js
+++ packages/cibul-templates/prodify/config.prod.js
@@ -4,73 +4,69 @@ const path = require( 'path' );
 const webpack = require( 'webpack' );
 const ourOwnModules = require( './ourOwnModules.json' );
 
-module.exports = paths => {
-
-  return {
-    entry: path.join( __dirname, paths.src.path, paths.src.name ),
-    output: {
-      path: paths.dest.path,
-      filename: paths.dest.name
-    },
-    module: {
-      rules: [
-        {
-          test: /\.jsx?$/,
-          enforce: 'pre',
-          loader: 'source-map-loader'
-        },
-        {
-          test: /\.jsx?$/,
-          exclude: new RegExp( 'node_modules\\/(?!' + ourOwnModules.join( '|' ) + ')' ),
-          use: {
-            loader: 'babel-loader',
-            options: {
-              cacheDirectory: true,
-              forceEnv: 'production'
-            }
+module.exports = paths => ( {
+  entry: path.join( __dirname, paths.src.path, paths.src.name ),
+  output: {
+    path: paths.dest.path,
+    filename: paths.dest.name
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        enforce: 'pre',
+        loader: 'source-map-loader'
+      },
+      {
+        test: /\.jsx?$/,
+        exclude: new RegExp( 'node_modules\\/(?!' + ourOwnModules.join( '|' ) + ')' ),
+        use: {
+          loader: 'babel-loader',
+          options: {
+            cacheDirectory: true,
+            forceEnv: 'production'
           }
-        },
-        {
-          test: /\.ejs$/,
-          loader: 'ejs-compiled-loader',
-        },
-        {
-          test: /\.(css|html|tblr)$/,
-          loader: 'raw-loader',
         }
-      ]
-    },
-    resolve: {
-      symlinks: false,
-      extensions: [ '.js', '.jsx' ],
-      alias: {
-        'react': require.resolve( 'react' ),
+      },
+      {
+        test: /\.ejs$/,
+        loader: 'ejs-compiled-loader',
+      },
+      {
+        test: /\.(css|html|tblr)$/,
+        loader: 'raw-loader',
       }
-    },
-    performance: {
-      hints: false,
-      maxAssetSize: 2000000
-    },
-    plugins: [
-      new webpack.DefinePlugin( {
-        'process.env.NODE_ENV': '"production"',
-        __CLIENT__: true,
-        __SERVER__: false,
-        __DEVELOPMENT__: false,
-        __DEVTOOLS__: false
-      } ),
-      new webpack.optimize.DedupePlugin(),
-      new webpack.optimize.UglifyJsPlugin( {
-        compress: {
-          warnings: false
-        },
-        mangle: true,
-        fromString: true
-      } )
-    ],
-    node: {
-      fs: 'empty'
+    ]
+  },
+  resolve: {
+    symlinks: false,
+    extensions: [ '.js', '.jsx' ],
+    alias: {
+      'react': require.resolve( 'react' ),
     }
-  };
-
-};
+  },
+  performance: {
+    hints: false,
+    maxAssetSize: 2000000
+  },
+  plugins: [
+    new webpack.DefinePlugin( {
+      'process.env.NODE_ENV': '"production"',
+      __CLIENT__: true,
+      __SERVER__: false,
+      __DEVELOPMENT__: false,
+      __DEVTOOLS__: false
+    } ),
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.UglifyJsPlugin( {
+      compress: {
+        warnings: false
+      },
+      mangle: true,
+      fromString: true
+    } )
+  ],
+  node: {
+    fs: 'empty'
+  }
+} );
