88e8dc399fbb5d7a2c984ff7d586ff5a8d7d9503
Kari Olafsson <kaoreolafsson@gmail.com>
2018-09-05 16:04:43 +0200

    fix(labels): file upload tweaks

---
+++ packages/labels/form-schemas/imageUpload.js
@@ -0,0 +1,40 @@
+"use strict";
+
+module.exports = {
+  upload: {
+    en: 'Upload an image',
+    fr: 'Charger une image'
+  },
+  update: {
+    en: 'Update the image',
+    fr: 'Mettre à jour l\'image'
+  },
+  error: {
+    en: 'A problem occurred during the upload. Please try again',
+    fr: 'Un problème est survenu pendant le chargement. Veuillez retenter.'
+  },
+  tip: {
+    en: 'Drag and drop an image in the grayed area.',
+    fr: 'Glissez une image dans la zone grisée.'
+  },
+  replace: {
+    en: 'Replace the file',
+    fr: 'Remplacer le fichier'
+  },
+  loading: {
+    en: 'Upload ongoing',
+    fr: 'Chargement en cours'
+  },
+  remove: {
+    en: 'Remove the file',
+    fr: 'Supprimer le fichier'
+  },
+  or: {
+    en: 'OR',
+    fr: 'OU'
+  },
+  acceptedExtensions: {
+    fr: 'Fichiers acceptés',
+    en: 'Accepted files'
+  }
+}
