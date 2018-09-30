933044c58231a67971d20b24497e4f3e554fbeb6
Kari Olafsson <kaoreolafsson@gmail.com>
2018-09-30 09:51:32 +0200

    feature(form-schemas): schemas merge function

---
+++ packages/form-schemas/test/merge.js
@@ -0,0 +1,168 @@
+"use strict";
+
+const fs = require( 'fs' );
+const should = require( 'should' );
+
+const merge = require( '../iso/merge' );
+
+describe( 'unit - assigning schema properties to another schema', function() {
+
+  // ids will need to be prefixed by formschema id before they can make sense
+  
+  it( 'order of fields is dictated by outer-most schema', () => {
+
+    const eventSchema = {
+      fields: [ {
+        field: 'title',
+        fieldType: 'text',
+        label: 'Le titre'
+      }, {
+        field: 'description',
+        fieldType: 'text',
+        label: 'La description'
+      } ]
+    };
+
+    const networkSchema = {
+      fields: [ {
+        field: 'theme',
+        fieldType: 'text',
+        label: 'Thème'
+      } ]
+    }
+
+    const agendaSchema = {
+      fields: [ {
+        field: 'title',
+        fieldType: 'abstract',
+        label: 'Nom de la manifestation'
+      }, {
+        field: 'participants',
+        fieldType: 'integer',
+        label: 'Participants'
+      }, {
+        field: 'description',
+        fieldType: 'abstract'
+      } ]
+    }
+
+    merge( eventSchema, networkSchema, agendaSchema ).fields
+      .map( f => f.field ).should.eql( [
+        'theme', 'title', 'participants', 'description'
+      ] );
+
+  } );
+
+
+  it( 'merge extends schemas', () => {
+
+    const s1 = {
+      fields: [ {
+        field: "participants",
+        optional: false,
+        fieldType: "integer",
+        label: "Participants",
+        info: "Combien de participants"
+      } ]
+    };
+
+    const s2 = {
+      fields: [ {
+        field: 'organizer',
+        optional: false,
+        fieldType: 'text',
+        label: 'Organizer'
+      } ]
+    };
+
+    const s3 = {
+      fields: [ {
+        field: 'budget',
+        optional: false,
+        fieldType: 'text',
+        label: 'Budget'
+      } ]
+    };
+
+    merge( s1, s2, s3 ).should.eql( { 
+      fields: [ { 
+         field: 'participants',
+         optional: false,
+         fieldType: 'integer',
+         label: 'Participants',
+         info: 'Combien de participants' 
+      }, { 
+         field: 'organizer',
+         optional: false,
+         fieldType: 'text',
+         label: 'Organizer' 
+      }, { 
+        field: 'budget',
+        optional: false,
+        fieldType: 'text',
+        label: 'Budget' 
+      } ] 
+    } );
+
+  } ); 
+
+  it( 'merge can relabel fields', () => {
+
+    const schema = {
+      fields: [ {
+        "field": "participants",
+        "optional": true,
+        "fieldType": "integer",
+        "min": null,
+        "max": null,
+        "label": {
+          "fr": "Participants",
+          "en": "Participants"
+        },
+        "info": {
+          "fr": "Combien de participants",
+          "en": "How many participants"
+        },
+        "placeholder" : null,
+        "sub" : null
+      } ]
+    }
+
+    const abstract = {
+      fields: [ {
+        field: 'participants',
+        "fieldType": "abstract",
+        label: {
+          fr: 'Les gens',
+          en: 'People'
+        },
+        info: {
+          fr: 'Combien de gens',
+          en: 'How many people'
+        }
+      } ]
+    };
+
+    merge( schema, abstract ).should.eql( {
+      fields: [ {
+        "field": "participants",
+        "optional": true,
+        "fieldType": "integer",
+        "min": null,
+        "max": null,
+        label: {
+          fr: 'Les gens',
+          en: 'People'
+        },
+        info: {
+          fr: 'Combien de gens',
+          en: 'How many people'
+        },
+        "placeholder" : null,
+        "sub" : null
+      } ]
+    } );
+
+  } );
+
+} );
