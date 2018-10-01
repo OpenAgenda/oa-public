44987f65c5f370e4395bf2f8df0d55764b8262b8
Bertho <kevin.bertho@gmail.com>
2018-10-01 09:08:24 +0200

    update testconfig.sample.js

--- packages/abilities/testconfig.sample.js
+++ packages/abilities/testconfig.sample.js
@@ -2,6 +2,12 @@
 
 const abilities = require( './' );
 
+const getEntity = {
+  agenda: uid => ( { uid } ),
+  member: id => ( { id } ),
+  user: uid => ( { uid } )
+};
+
 module.exports = {
   mysql: {
     host: '127.0.0.1',
@@ -13,33 +19,92 @@ module.exports = {
     rule: 'rule'
   },
   interfaces: {
-    defineFor: {
-      async user( identifier, { can, cannot, rules } ) {
-        /* rules already contains the default rules */
-
+    getEntity,
+    defaultFor: {
+      user( { can, cannot, rules } ) {
         can( 'create', 'event' );
         can( 'receive', 'activity' );
         cannot( 'receive', 'activity', { verb: 'spam' } );
 
-        /* Define default rules for a user */
-        // if ( isAdmin( identifier ) {
+        return rules;
+      }
+    },
+    defineFor: {
+      // agenda = agenda
+      // member = agenda + user + member
+      // user = agendas + user + members
+
+      async agenda( agenda, builder ) {
+        const { rules } = builder;
+
+        return rules.concat( await abilities.rules.list( 'agenda', agenda.uid ) );
+      },
+      async member( member, builder, options = {} ) {
+        const { specificOnly } = options;
+        const { rules } = builder;
+
+        const memberRules = await abilities.rules.list( 'member', member.id );
+
+        if ( specificOnly ) {
+          return rules.concat( memberRules );
+        }
+
+        const defaultRules = {
+          agenda: abilities.rules.getDefaultFor( 'agenda' ),
+          user: abilities.rules.getDefaultFor( 'user' ),
+          member: abilities.rules.getDefaultFor( 'member' )
+        };
+
+        const agendaRules = await abilities.get( 'agenda', member.agendaUid ).rules;
+        const userRules = await abilities.get( 'user', member.userUid, { specificOnly: true } ).rules;
+
+        return defaultRules.agenda
+          .concat( agendaRules )
+          .concat( defaultRules.user )
+          .concat( userRules )
+          .concat( defaultRules.member )
+          .concat( rules ) // the rules defined with can/cannot in this block
+          .concat( memberRules );
+      },
+      async user( user, builder, options = {} ) {
+        const { specificOnly } = options;
+        const { rules } = builder;
+
+        const userRules = await abilities.rules.list( 'user', user.uid );
+
+        // if ( isAdmin( user ) {
         //   can( ... );
         // } else {
         //   cannot( ... );
         // }
 
-        return rules.concat( await abilities.rules.list( 'user', identifier ) );
-      },
-      async member( identifier, { can, cannot, rules } ) {
-        //
-      }
-    },
-    getParents: {
-      user() {
-        return [
-          { entity_name: 'member', identifier: 12345 },
-          { entity_name: 'member', identifier: 23456 }
-        ];
+        if ( specificOnly ) {
+          return rules.concat( userRules );
+        }
+
+        const defaultRules = {
+          agenda: abilities.rules.getDefaultFor( 'agenda' ),
+          user: abilities.rules.getDefaultFor( 'user' ),
+          member: abilities.rules.getDefaultFor( 'member' )
+        };
+
+        const members = await membersSvc.list( {} );
+        const agendas = await agendasSvc.list( {} );
+
+        const agendasRules = await abilities.rules.list( 'agenda', _.map( agendas, 'uid' ) );
+        const membersRules = await abilities.rules.list( 'member', _.map( members, 'id' ) );
+
+        // TODO define agendasRules & membersRules
+
+        return defaultRules.user.concat( rules ).concat( userRules );
+
+        return defaultRules.agenda
+          .concat( agendasRules )
+          .concat( defaultRules.user )
+          .concat( rules ) // the rules defined with can/cannot in this block
+          .concat( userRules )
+          .concat( defaultRules.member )
+          .concat( membersRules );
       }
     }
   }
