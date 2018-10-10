937268c1b2e27cb01ec72c6dbabff8b86cc96564
Kari Olafsson <kaoreolafsson@gmail.com>
2018-10-10 09:51:18 +0200

    fix(validators): adding renamed file

---
+++ packages/validators/src/schema/utils.js
@@ -0,0 +1,91 @@
+"use strict";
+
+const _ = {
+  extend: require( 'lodash/extend' ),
+  keys: require( 'lodash/keys' )
+}
+
+module.exports = {
+  registerValidators,
+  getFlat,
+  getDefault
+}
+
+const registeredValidators = {};
+
+
+function getFlat( fields, values ) {
+
+  return _.keys( fields ).map( f => {
+
+    const fieldOptions = fields[ f ];
+
+    const type = _extractType( fieldOptions );
+
+    return {
+      field: f,
+      validator: _makeValidator( type, f, fieldOptions ),
+      value: ( values || {} )[ f ]
+    }
+
+  } );
+
+}
+
+
+function _makeValidator( type, field, options ) {
+
+  let validatorOptions = _.extend( { field }, options );
+
+  if ( type === 'list' ) {
+
+    validatorOptions.validators = registeredValidators;
+
+  }
+
+  return registeredValidators[ type ]( validatorOptions );
+
+}
+
+
+function getDefault( fields ) {
+
+  let clean = {};
+
+  Object.keys( fields ).forEach( k => {
+
+    if ( fields[ k ].type === 'schema' ) {
+
+      clean[ k ] = fields[ k ].list ? [] : getDefault( fields[ k ].fields );
+
+    } else {
+
+      clean[ k ] = fields[ k ].default === undefined ? null : fields[ k ].default;
+
+    }
+
+  } );
+
+  return clean;
+
+}
+
+
+function _extractType( fieldOptions ) {
+
+  if ( typeof registeredValidators[ fieldOptions.type ] === 'undefined' ) {
+
+    throw new Error( 'Unregistered type: ' + fieldOptions.type );
+
+  }
+
+  return fieldOptions.type;
+
+}
+
+
+function registerValidators( validators ) {
+
+  _.extend( registeredValidators, validators );
+
+}
