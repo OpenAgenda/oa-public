eb86e4c93303f4bdbc0d218166d17b0061288f07
Kari Olafsson <kaoreolafsson@gmail.com>
2018-11-08 10:05:21 +0100

    tweak(react-timings-picker): removed minify css call in html gulp task

--- packages/react-timings-picker/gulpfile.babel.js
+++ packages/react-timings-picker/gulpfile.babel.js
@@ -72,7 +72,6 @@ gulp.task('html', ['styles'], () =>
 	return gulp.src('app/*.html')
 		.pipe(assets)
 		.pipe($.if('*.js', $.uglify()))
-		.pipe($.if('*.css', $.minifyCss({ compatibility: '*' })))
 		.pipe(assets.restore())
 		.pipe($.useref())
 		.pipe($.if('*.html', $.minifyHtml({ conditionals: true, loose: true })))
