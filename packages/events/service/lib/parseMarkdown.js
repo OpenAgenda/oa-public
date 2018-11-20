34628a4088c2355b4b5e255fee584da25260b9f4
Kari Olafsson <kaoreolafsson@gmail.com>
2018-11-20 10:01:17 +0100

    fix(events): parseMarkdown helper does not handle undefined values

--- packages/events/service/lib/parseMarkdown.js
+++ packages/events/service/lib/parseMarkdown.js
@@ -4,4 +4,4 @@ const marked = require( 'marked' );
 
 const renderer = new marked.Renderer();
 
-module.exports = str => marked( str, { renderer } );
+module.exports = str => marked( str || '', { renderer } );
