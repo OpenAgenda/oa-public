986cbb0cd0f0834e83934334cfb34f1cd69ac0e5
Kari Olafsson <kaoreolafsson@gmail.com>
2018-11-24 19:46:43 +0100

    tweak(labels): added labels for active filters

--- packages/labels/agendas/activeFilters.js
+++ packages/labels/agendas/activeFilters.js
@@ -1,84 +1,165 @@
 "use strict";
 
 module.exports = {
+  "map" : {
+    "en" : "map",
+    "fr" : "carte",
+    "es" : "mapa",
+    "it" : "carta",
+    "de" : "karte"
+  },
+  "passed" : {
+    "en" : "past events",
+    "fr" : "événements passés",
+    "es" : "pasado",
+    "it" : "passato",
+    "de" : "vergangenheit"
+  },
+  "location" : {
+    "en" : "place",
+    "fr" : "lieu",
+    "es" : "lugar",
+    "it" : "luogo",
+    "de" : "platz"
+  },
   "fromTo": {
     "en" : "from %start% to %end%",
-    "fr" : "du %start% au %end%"
+    "fr" : "du %start% au %end%",
+    "es" : "desde el %start% hasta %end%",
+    "it" : "dal %from% al %to%",
+    "de" : "von %from% bis %to%"
   },
   "january": {
     "en" : "january",
-    "fr" : "janvier"
+    "fr" : "janvier",
+    "es" : "enero",
+    "de" : "januar",
+    "it" : "gennaio"
   },
   "february" : {
     "en" : "february",
-    "fr" : "février"
+    "fr" : "février",
+    "es" : "febrero",
+    "de" : "februar",
+    "it" : "febbraio"
   },
   "march" : {
     "en" : "march",
-    "fr" : "mars"
+    "fr" : "mars",
+    "es" : "marzo",
+    "de" : "märz",
+    "it" : "marzo"
   }, 
   "april" : {
     "en" : "april",
-    "fr" : "avril"
+    "fr" : "avril",
+    "es" : "abril",
+    "de" : "april",
+    "it" : "aprile"
   }, 
   "may" : {
     "en" : "may",
-    "fr" : "mai"
+    "fr" : "mai",
+    "es" : "mayo",
+    "de" : "mai",
+    "it" : "maggio"
   }, 
   "june" : {
     "en" : "june",
-    "fr" : "juin"
+    "fr" : "juin",
+    "es" : "junio",
+    "de" : "juni",
+    "it" : "giugno"
   }, 
   "july" : {
     "en" : "july",
-    "fr" : "juillet"
+    "fr" : "juillet",
+    "es" : "julio",
+    "de" : "juli",
+    "it" : "luglio"
   }, 
   "august" : {
     "en" : "august",
-    "fr" : "août"
+    "fr" : "août",
+    "es" : "agosto",
+    "de" : "august",
+    "it" : "agosto"
   }, 
   "september" : {
     "en" : "september",
-    "fr" : "septembre"
+    "fr" : "septembre",
+    "es" : "septiembre",
+    "de" : "september",
+    "it" : "settembre"
   }, 
   "october" : {
     "en" : "october",
-    "fr" : "octobre"
+    "fr" : "octobre",
+    "es" : "octubre",
+    "de" : "oktober",
+    "it" : "ottobre"
   }, 
   "november" : {
     "en" : "november",
-    "fr" : "novembre"
+    "fr" : "novembre",
+    "es" : "noviembre",
+    "de" : "november",
+    "it" : "novembre"
   }, 
   "december" : {
     "en" : "december",
-    "fr" : "décembre"
+    "fr" : "décembre",
+    "es" : "diciembre",
+    "de" : "dezember",
+    "it" : "dicembre"
   }, 
   "monday" : {
     "en" : "monday",
-    "fr" : "lundi"
+    "fr" : "lundi",
+    "es" : "lunes",
+    "de" : "montag",
+    "it" : "lunedì"
   }, 
   "tuesday" : {
     "en" : "tuesday",
-    "fr" : "mardi"
+    "fr" : "mardi",
+    "es" : "martes",
+    "de" : "dienstag",
+    "it" : "martedì"
   }, 
   "wednesday" : {
     "en" : "wednesday",
-    "fr" : "mercredi"
+    "fr" : "mercredi",
+    "es" : "miercoles",
+    "de" : "mittwoch",
+    "it" : "mercoledì"
   }, 
   "thursday" : {
     "en" : "thursday",
-    "fr" : "jeudi"
+    "fr" : "jeudi",
+    "es" : "jueves",
+    "de" : "donnerstag",
+    "it" : "giovedì"
   }, 
   "friday" : {
     "en" : "friday",
-    "fr" : "vendredi"
+    "fr" : "vendredi",
+    "es" : "viernes",
+    "de" : "freitag",
+    "it" : "venerdì"
   }, 
   "saturday" : {
     "en" : "saturday",
-    "fr" : "samedi"
+    "fr" : "samedi",
+    "es" : "sabado",
+    "de" : "samstag",
+    "it" : "sabato"
   }, 
   "sunday" : {
     "en" : "sunday",
-    "fr" : "dimanche"
+    "fr" : "dimanche",
+    "es" : "domingo",
+    "de" : "sonntag",
+    "it" : "domenica"
   }
 };
