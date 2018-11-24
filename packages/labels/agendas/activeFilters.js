53b5710808ae363252e6900b028d183783b776fa
Kari Olafsson <kaoreolafsson@gmail.com>
2018-11-24 19:00:44 +0100

    tweak(labels): added active filter labels

---
+++ packages/labels/agendas/activeFilters.js
@@ -0,0 +1,84 @@
+"use strict";
+
+module.exports = {
+  "fromTo": {
+    "en" : "from %start% to %end%",
+    "fr" : "du %start% au %end%"
+  },
+  "january": {
+    "en" : "january",
+    "fr" : "janvier"
+  },
+  "february" : {
+    "en" : "february",
+    "fr" : "février"
+  },
+  "march" : {
+    "en" : "march",
+    "fr" : "mars"
+  }, 
+  "april" : {
+    "en" : "april",
+    "fr" : "avril"
+  }, 
+  "may" : {
+    "en" : "may",
+    "fr" : "mai"
+  }, 
+  "june" : {
+    "en" : "june",
+    "fr" : "juin"
+  }, 
+  "july" : {
+    "en" : "july",
+    "fr" : "juillet"
+  }, 
+  "august" : {
+    "en" : "august",
+    "fr" : "août"
+  }, 
+  "september" : {
+    "en" : "september",
+    "fr" : "septembre"
+  }, 
+  "october" : {
+    "en" : "october",
+    "fr" : "octobre"
+  }, 
+  "november" : {
+    "en" : "november",
+    "fr" : "novembre"
+  }, 
+  "december" : {
+    "en" : "december",
+    "fr" : "décembre"
+  }, 
+  "monday" : {
+    "en" : "monday",
+    "fr" : "lundi"
+  }, 
+  "tuesday" : {
+    "en" : "tuesday",
+    "fr" : "mardi"
+  }, 
+  "wednesday" : {
+    "en" : "wednesday",
+    "fr" : "mercredi"
+  }, 
+  "thursday" : {
+    "en" : "thursday",
+    "fr" : "jeudi"
+  }, 
+  "friday" : {
+    "en" : "friday",
+    "fr" : "vendredi"
+  }, 
+  "saturday" : {
+    "en" : "saturday",
+    "fr" : "samedi"
+  }, 
+  "sunday" : {
+    "en" : "sunday",
+    "fr" : "dimanche"
+  }
+};
