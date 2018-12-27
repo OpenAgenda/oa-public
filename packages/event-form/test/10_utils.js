b63604392709663c70c7cbd70dfd984a9b48117f
Kari Olafsson <kaoreolafsson@gmail.com>
2018-12-27 14:26:55 +0100

    feature(event-form): timings component does not display recurrence link if enabled ranges spans less than seven days

--- packages/event-form/test/10_utils.js
+++ packages/event-form/test/10_utils.js
@@ -1,6 +1,8 @@
 import getMultilingualFieldNames from '../src/utils/getMultilingualFieldNames';
 import transferMultilingualValues from '../src/utils/transferMultilingualValues';
 import identifyLanguageChanges from '../src/utils/identifyLanguageChanges';
+import getTimingsSpan from '../src/utils/getTimingsSpan';
+
 
 describe( 'event-form utils unit tests', () => {
 
@@ -46,6 +48,26 @@ describe( 'event-form utils unit tests', () => {
       removedLanguages: [ 'fr' ],
       changedLanguages: [ 'es' ]
     } );
+
+  } );
+
+  test( 'given a list of timings, returns in ms the time between the first begin time and the last end time', () => {
+
+    const ms = getTimingsSpan( [ {
+      begin: '2018-12-24T10:00:00.0002',
+      end: '2018-12-24T11:00:00.0002'
+    }, {
+      begin: '2018-12-31T09:00:00.0002',
+      end: '2018-12-31T22:00:00.0002'
+    }, {
+      begin: '2018-12-24T15:00:00.0002',
+      end: '2018-12-24T19:00:00.0002'
+    }, {
+      begin: '2018-12-25T09:00:00.0002',
+      end: '2018-12-25T10:00:00.0002'
+    } ] );
+
+    expect( ms / 1000 / 60 / 60 / 24 ).toEqual( 7.5 );
 
   } );
 
