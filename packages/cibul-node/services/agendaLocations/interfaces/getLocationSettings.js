7ff48bcd67aa1f4b278938af2be8fc04291df487
Kari Olafsson <kaoreolafsson@gmail.com>
2018-12-28 08:10:18 +0100

    tweak(cibulNode): agenda location interface to get settings from agenda schema

---
+++ packages/cibul-node/services/agendaLocations/interfaces/getLocationSettings.js
@@ -0,0 +1,24 @@
+"use strict";
+
+const _ = require( 'lodash' );
+
+const agendas = require( '@openagenda/agendas' );
+const log = require( '@openagenda/logs' )( 'services/agendaLocations/interfaces/getAgendaLocations' );
+
+const core = require( '../../../core' );
+
+module.exports = async ( agendaId, cb ) => {
+
+  const agenda = await agendas.get( { id: agendaId }, { private: null } );
+
+  if ( !agenda ) return cb( 'agenda not found: ' + agendaId );
+
+  const schema = await core.agendas( agenda.uid ).settings.get();
+
+  if ( !schema ) return cb();
+
+  const locationField = _.first( schema.fields.filter( f => f.field === 'location' ) );
+
+  cb( null, _.get( locationField, 'legacy', null ) );
+
+}
