5e927956411738e5b20fde680a1d76746314bd2c
Kari Olafsson <kaoreolafsson@gmail.com>
2019-01-09 11:38:39 +0400

    feature(labels): added german labels

--- packages/labels/home/index.js
+++ packages/labels/home/index.js
@@ -8,102 +8,127 @@ module.exports = {
   },
   "createAgenda": {
     "fr": "Créer un agenda",
-    "en": "Create an agenda"
+    "en": "Create an agenda",
+    "de": "Kalender erstellen"
   },
   "searchAgenda": {
     "fr": "Rechercher un agenda",
-    "en": "Search an agenda"
+    "en": "Search an agenda",
+    "de": "Kalender suchen"
   },
   "myEvents": {
     "fr": "Mes événements",
-    "en": "My events"
+    "en": "My events",
+    "de": "Meine Veranstaltungen"
   },
   "messages": {
     "fr": "Messages",
-    "en": "Messages"
+    "en": "Messages",
+    "de": "Nachrichten"
   },
   "notifications": {
     "fr": "Notifications",
-    "en": "Notifications"
+    "en": "Notifications",
+    "de": "Benachrichtigungen"
   },
   "addAnEvent": {
     "fr": "Ajouter un événement",
-    "en": "Add an event"
+    "en": "Add an event",
+    "de": "Veranstaltung erstellen"
   },
   "manage": {
     "fr": "Gérer",
-    "en": "Manage"
+    "en": "Manage",
+    "de": "Verwalten"
   },
   "moderate": {
     "fr": "Modérer",
-    "en": "Moderate"
+    "en": "Moderate",
+    "de": "Moderieren"
   },
   "officialAgenda": {
     "fr": "Agenda officiel",
-    "en": "Official agenda"
+    "en": "Official agenda",
+    "de": "Offizieller Kalender"
   },
   "welcome": {
     "fr": "Bienvenue sur votre compte OpenAgenda!",
-    "en": "Welcome to your OpenAgenda account!"
+    "en": "Welcome to your OpenAgenda account!",
+    "de": "Willkommen in Ihrem OpenAgenda-Konto!"
   },
   "orContributeToExisting": {
     "fr": "Ou contribuez à un agenda existant",
-    "en": "Or contribute to an existing agenda"
+    "en": "Or contribute to an existing agenda",
+    "de": "Oder fügen Sie Ihre Veranstaltung einem bestehenden Kalender hinzu."
   },
   "noResult": {
     "fr": "Aucun résultat trouvé",
-    "en": "No result found"
+    "en": "No result found",
+    "de": "Keine Ergebnisse gefunden"
   },
   "createEvent": {
     "fr": "Créer un événement",
-    "en": "Create an event"
+    "en": "Create an event",
+    "de": "Eine Veranstaltung erstellen"
   },
   "searchEvent": {
     "fr": "Rechercher un événement",
-    "en": "Search an event"
+    "en": "Search an event",
+    "de": "Eine Veranstaltungen finden"
   },
   "selectAgenda": {
     "fr": "Sélectionner l'agenda dans lequel créer l'événement",
-    "en": "Select an agenda in which to create the event"
+    "en": "Select an agenda in which to create the event",
+    "de": "Wählen Sie die Agenda, in der Sie Ihre Veranstaltung eintragen möchten."
   },
   "privateAgenda": {
     "fr": "Agenda privé",
-    "en": "Private agenda"
+    "en": "Private agenda",
+    "de": "Privater Kalender"
   },
   "privateEvent": {
     "fr": "Événement privé",
-    "en": "Private event"
+    "en": "Private event",
+    "de": "Private Veranstalltung"
   },
   "draft": {
     "fr": "Brouillon",
-    "en": "Draft"
+    "en": "Draft",
+    "de": "Entwurf"
   },
   "contact": {
     "fr": "Contacter",
-    "en": "Contact"
+    "en": "Contact",
+    "de": "Nachricht"
   },
   "see": {
     "fr": "Voir",
-    "en": "See"
+    "en": "See",
+    "de": "Sehen"
   },
   "support": {
     "fr": "Support",
-    "en": "Support"
+    "en": "Support",
+    "de": "Unterstützung"
   },
   "modify": {
     "fr": "Modifier",
-    "en": "Modify"
+    "en": "Modify",
+    "de": "Bearbeiten"
   },
   "noTitle": {
     "fr": "Titre non défini",
-    "en": "Title not defined"
+    "en": "Title not defined",
+    "de": "Bitte fügen Sie Ihrer Veranstaltung einen Titel hinzu."
   },
   "noLocation": {
     "fr": "Lieu non défini",
-    "en": "Location not defined"
+    "en": "Location not defined",
+    "de": "Bitte geben Sie den Veranstaltungsort an."
   },
   "noEventsCreated": {
     "fr": "Vous n'avez encore créé aucun événement",
-    "en": "You haven't created any event yet"
+    "en": "You haven't created any event yet",
+    "de": "Sie haben noch keine Veranstaltung erstellt"
   }
 }
