4237e80e75ceba6a65f0d93494d9837f3b772542
Kari Olafsson <kaoreolafsson@gmail.com>
2019-01-14 11:50:41 +0400

    feature(legacy): controlData operations are taskable

--- packages/legacy/controlData/lib/utils/promisifyRedis.js
+++ packages/legacy/controlData/lib/utils/promisifyRedis.js
@@ -4,5 +4,8 @@ const { promisify } = require( 'util' );
 
 module.exports = redis => ( {
   set: promisify( redis.set ).bind( redis ),
-  get: promisify( redis.get ).bind( redis )
+  get: promisify( redis.get ).bind( redis ),
+  blpop: promisify( redis.blpop ).bind( redis ),
+  rpush: promisify( redis.rpush ).bind( redis ),
+  duplicate: redis.duplicate.bind( redis )
 } );
