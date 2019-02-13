79fff0c2f0faf990b3789eb367bdd4543eb6bbf8
Kari Olafsson <kaoreolafsson@gmail.com>
2019-02-13 12:38:31 +0100

    tweak(events): logging, longer pool for unique slug generation

--- packages/events/service/create.js
+++ packages/events/service/create.js
@@ -5,7 +5,6 @@ const _ = require( 'lodash' );
 const uuidV4 = require( 'uuid/v4' );
 const w = require( 'when' );
 
-const logger = require( '@openagenda/logs' );
 const slugs = require( '@openagenda/slugs' );
 
 const cleanCreateArgs = require( './lib/cleanCreateArgs' );
@@ -19,7 +18,9 @@ const processImage = require( './lib/processImage' );
 
 const dbParse = require( '@openagenda/mysql-utils/mapper' )( map );
 
-let schemas, service, knex, config, log, legacySchemas;
+const log = require( '@openagenda/logs' )( 'events service/create' );
+
+let schemas, service, knex, config, legacySchemas;
 
 module.exports = _.extend( ( d, o, c ) => {
 
@@ -54,13 +55,13 @@ module.exports = _.extend( ( d, o, c ) => {
 
     config = c;
 
-    log = logger( 'events service/create' );
-
   }
 } );
 
 
 async function createPromise( data, options ) {
+
+  log( 'create called' );
 
   const cleanOptions = cleanCreateOptions( options );
 
@@ -89,11 +90,15 @@ async function createPromise( data, options ) {
 
     if ( cleanOptions.draft ) {
 
+      log( 'draft create' );
+
       preCleanEvent = validate.draft( data );
 
       preCleanEvent.draft = true;
 
     } else {
+
+      log( 'non-draft create' );
 
       preCleanEvent = _.assign( validate.front( data, {
         optionalSlug: true,
@@ -104,11 +109,15 @@ async function createPromise( data, options ) {
 
   } catch ( frontValidationErrors ) {
 
+    log( 'pre-validation errors', frontValidationErrors );
+
     frontValidationErrors.forEach( e => errors.push( _.assign( e, { step: 'pre' } ) ) );
 
   }
 
   if ( !errors.length && preCleanEvent.uid && await _exists( 'uid', preCleanEvent.uid, cleanOptions.evaluateLegacyIdentifiers ) ) {
+
+    log( 'warn', 'uid was specified in data but is not available' );
 
     errors.push( {
       code: 'unavailable.uid',
@@ -119,6 +128,8 @@ async function createPromise( data, options ) {
 
   if ( !errors.length && preCleanEvent.slug && await _exists( 'slug', preCleanEvent.slug, cleanOptions.evaluateLegacyIdentifiers ) ) {
 
+    log( 'warn', 'slug was specified in data but is not available' );
+
     errors.push( {
       code: 'unavailable.slug',
       message: 'slug is taken by another event'
@@ -128,17 +139,21 @@ async function createPromise( data, options ) {
 
   if ( !errors.length && !preCleanEvent.uid ) {
 
+    log( 'assigning event uid' );
+
     preCleanEvent.uid = await _defineUnique( 'uid', () => Math.ceil( Math.random() * 99999999 ) );
 
   }
 
   if ( !errors.length && !preCleanEvent.slug ) {
 
+    log( 'assigning event slug' );
+
     const title = preCleanEvent.title[ _.keys( preCleanEvent.title )[ 0 ] ];
 
     preCleanEvent.slug = await _defineUnique( 'slug', prev => [
       title ? slugs.generate( title ) : null,
-      prev || !title ? Math.ceil( Math.random()*1000 ) : null
+      prev || !title ? Math.ceil( Math.random()*1000000 ) : null
     ].filter( v => !!v ).join( '_' ) );
 
   }
@@ -147,11 +162,15 @@ async function createPromise( data, options ) {
 
     if ( cleanOptions.protected || !data.createdAt instanceof Date ) {
 
+      log( 'specifying createdAt' );
+
       preCleanEvent.createdAt = new Date();
 
     }
 
     if ( cleanOptions.protected || !data.updatedAt instanceof Date ) {
+
+      log( 'specifying updatedAt' );
 
       preCleanEvent.updatedAt = new Date();
 
@@ -162,6 +181,8 @@ async function createPromise( data, options ) {
       cleanEvent = ( cleanOptions.draft ? validate.draft : validate )( _.omit( _.assign( {}, data, preCleanEvent ), [ 'id' ]  ) );
 
     } catch ( validationErrors ) {
+
+      log( 'loaded event data did not validate', validationErrors );
 
       validationErrors.forEach( e => errors.push( _.assign( e, { step: 'validation' } ) ) );
 
@@ -195,6 +216,8 @@ async function createPromise( data, options ) {
 
     } catch( e ) {
 
+      log( 'error', 'db insert error found for event %j', _.pick( cleanEvent, [ 'slug', 'uid' ] ), e );
+
       errors.push( { step: 'db', caught: e } );
 
     }
@@ -210,6 +233,8 @@ async function createPromise( data, options ) {
   if ( !errors.length && cleanOptions.transferToLegacy ) {
 
     try {
+
+      log( 'transfering to legacy' );
 
       const result = await service.legacy.update( { id: createdId } );
 
@@ -228,6 +253,8 @@ async function createPromise( data, options ) {
   if ( createdId ) {
 
     try {
+
+      log( 'fetching created event' );
 
       created = await service.get( { id: createdId }, {
         internal: true,
@@ -250,6 +277,8 @@ async function createPromise( data, options ) {
   }
 
   if ( created && _.get( interfaces, 'onCreate' ) ) {
+
+    log( 'calling onCreate interface' );
 
     try {
 
@@ -277,9 +306,13 @@ async function _defineUnique( field, generator  ) {
 
   let value = null;
 
+  log( 'defining unique %s', field );
+
   value = generator( value );
 
   while ( await _exists( field, value ) ) {
+
+    log( 'field %s value %s is taken, generating new value', field, value );
 
     value = generator( value );
 
