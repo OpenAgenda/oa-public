748bd1c3f2f14ddcd6677df90e28dec6960d7054
Kari Olafsson <kaoreolafsson@gmail.com>
2019-02-25 14:43:44 +0100

    tweak(labels): reformatting label files

--- packages/labels/agendas/activeFilters.js
+++ packages/labels/agendas/activeFilters.js
@@ -1,165 +1,165 @@
 "use strict";
 
 module.exports = {
-  "map" : {
-    "en" : "map",
-    "fr" : "carte",
-    "es" : "mapa",
-    "it" : "carta",
-    "de" : "karte"
+  "map": {
+    "en": "map",
+    "fr": "carte",
+    "es": "mapa",
+    "it": "carta",
+    "de": "karte"
   },
-  "passed" : {
-    "en" : "past events",
-    "fr" : "événements passés",
-    "es" : "pasado",
-    "it" : "passato",
-    "de" : "vergangenheit"
+  "passed": {
+    "en": "past events",
+    "fr": "événements passés",
+    "es": "pasado",
+    "it": "passato",
+    "de": "vergangenheit"
   },
-  "location" : {
-    "en" : "place",
-    "fr" : "lieu",
-    "es" : "lugar",
-    "it" : "luogo",
-    "de" : "platz"
+  "location": {
+    "en": "place",
+    "fr": "lieu",
+    "es": "lugar",
+    "it": "luogo",
+    "de": "platz"
   },
   "fromTo": {
-    "en" : "from %start% to %end%",
-    "fr" : "du %start% au %end%",
-    "es" : "desde el %start% hasta %end%",
-    "it" : "dal %start% al %end%",
-    "de" : "von %start% bis %end%"
+    "en": "from %start% to %end%",
+    "fr": "du %start% au %end%",
+    "es": "desde el %start% hasta %end%",
+    "it": "dal %start% al %end%",
+    "de": "von %start% bis %end%"
   },
   "january": {
-    "en" : "january",
-    "fr" : "janvier",
-    "es" : "enero",
-    "de" : "januar",
-    "it" : "gennaio"
+    "en": "january",
+    "fr": "janvier",
+    "es": "enero",
+    "de": "januar",
+    "it": "gennaio"
   },
-  "february" : {
-    "en" : "february",
-    "fr" : "février",
-    "es" : "febrero",
-    "de" : "februar",
-    "it" : "febbraio"
+  "february": {
+    "en": "february",
+    "fr": "février",
+    "es": "febrero",
+    "de": "februar",
+    "it": "febbraio"
   },
-  "march" : {
-    "en" : "march",
-    "fr" : "mars",
-    "es" : "marzo",
-    "de" : "märz",
-    "it" : "marzo"
+  "march": {
+    "en": "march",
+    "fr": "mars",
+    "es": "marzo",
+    "de": "märz",
+    "it": "marzo"
   },
-  "april" : {
-    "en" : "april",
-    "fr" : "avril",
-    "es" : "abril",
-    "de" : "april",
-    "it" : "aprile"
+  "april": {
+    "en": "april",
+    "fr": "avril",
+    "es": "abril",
+    "de": "april",
+    "it": "aprile"
   },
-  "may" : {
-    "en" : "may",
-    "fr" : "mai",
-    "es" : "mayo",
-    "de" : "mai",
-    "it" : "maggio"
+  "may": {
+    "en": "may",
+    "fr": "mai",
+    "es": "mayo",
+    "de": "mai",
+    "it": "maggio"
   },
-  "june" : {
-    "en" : "june",
-    "fr" : "juin",
-    "es" : "junio",
-    "de" : "juni",
-    "it" : "giugno"
+  "june": {
+    "en": "june",
+    "fr": "juin",
+    "es": "junio",
+    "de": "juni",
+    "it": "giugno"
   },
-  "july" : {
-    "en" : "july",
-    "fr" : "juillet",
-    "es" : "julio",
-    "de" : "juli",
-    "it" : "luglio"
+  "july": {
+    "en": "july",
+    "fr": "juillet",
+    "es": "julio",
+    "de": "juli",
+    "it": "luglio"
   },
-  "august" : {
-    "en" : "august",
-    "fr" : "août",
-    "es" : "agosto",
-    "de" : "august",
-    "it" : "agosto"
+  "august": {
+    "en": "august",
+    "fr": "août",
+    "es": "agosto",
+    "de": "august",
+    "it": "agosto"
   },
-  "september" : {
-    "en" : "september",
-    "fr" : "septembre",
-    "es" : "septiembre",
-    "de" : "september",
-    "it" : "settembre"
+  "september": {
+    "en": "september",
+    "fr": "septembre",
+    "es": "septiembre",
+    "de": "september",
+    "it": "settembre"
   },
-  "october" : {
-    "en" : "october",
-    "fr" : "octobre",
-    "es" : "octubre",
-    "de" : "oktober",
-    "it" : "ottobre"
+  "october": {
+    "en": "october",
+    "fr": "octobre",
+    "es": "octubre",
+    "de": "oktober",
+    "it": "ottobre"
   },
-  "november" : {
-    "en" : "november",
-    "fr" : "novembre",
-    "es" : "noviembre",
-    "de" : "november",
-    "it" : "novembre"
+  "november": {
+    "en": "november",
+    "fr": "novembre",
+    "es": "noviembre",
+    "de": "november",
+    "it": "novembre"
   },
-  "december" : {
-    "en" : "december",
-    "fr" : "décembre",
-    "es" : "diciembre",
-    "de" : "dezember",
-    "it" : "dicembre"
+  "december": {
+    "en": "december",
+    "fr": "décembre",
+    "es": "diciembre",
+    "de": "dezember",
+    "it": "dicembre"
   },
-  "monday" : {
-    "en" : "monday",
-    "fr" : "lundi",
-    "es" : "lunes",
-    "de" : "montag",
-    "it" : "lunedì"
+  "monday": {
+    "en": "monday",
+    "fr": "lundi",
+    "es": "lunes",
+    "de": "montag",
+    "it": "lunedì"
   },
-  "tuesday" : {
-    "en" : "tuesday",
-    "fr" : "mardi",
-    "es" : "martes",
-    "de" : "dienstag",
-    "it" : "martedì"
+  "tuesday": {
+    "en": "tuesday",
+    "fr": "mardi",
+    "es": "martes",
+    "de": "dienstag",
+    "it": "martedì"
   },
-  "wednesday" : {
-    "en" : "wednesday",
-    "fr" : "mercredi",
-    "es" : "miercoles",
-    "de" : "mittwoch",
-    "it" : "mercoledì"
+  "wednesday": {
+    "en": "wednesday",
+    "fr": "mercredi",
+    "es": "miercoles",
+    "de": "mittwoch",
+    "it": "mercoledì"
   },
-  "thursday" : {
-    "en" : "thursday",
-    "fr" : "jeudi",
-    "es" : "jueves",
-    "de" : "donnerstag",
-    "it" : "giovedì"
+  "thursday": {
+    "en": "thursday",
+    "fr": "jeudi",
+    "es": "jueves",
+    "de": "donnerstag",
+    "it": "giovedì"
   },
-  "friday" : {
-    "en" : "friday",
-    "fr" : "vendredi",
-    "es" : "viernes",
-    "de" : "freitag",
-    "it" : "venerdì"
+  "friday": {
+    "en": "friday",
+    "fr": "vendredi",
+    "es": "viernes",
+    "de": "freitag",
+    "it": "venerdì"
   },
-  "saturday" : {
-    "en" : "saturday",
-    "fr" : "samedi",
-    "es" : "sabado",
-    "de" : "samstag",
-    "it" : "sabato"
+  "saturday": {
+    "en": "saturday",
+    "fr": "samedi",
+    "es": "sabado",
+    "de": "samstag",
+    "it": "sabato"
   },
-  "sunday" : {
-    "en" : "sunday",
-    "fr" : "dimanche",
-    "es" : "domingo",
-    "de" : "sonntag",
-    "it" : "domenica"
+  "sunday": {
+    "en": "sunday",
+    "fr": "dimanche",
+    "es": "domingo",
+    "de": "sonntag",
+    "it": "domenica"
   }
-};
+}
