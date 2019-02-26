b1f85522e54913da0b362e1fba85d72da3002e52
Kari Olafsson <kaoreolafsson@gmail.com>
2019-02-26 12:59:25 +0100

    tweak(labels): agenda contribute form translation in italian and german

--- packages/labels/event/form.js
+++ packages/labels/event/form.js
@@ -3,198 +3,296 @@
 module.exports = {
   "descriptionSection": {
     "fr": "Descriptifs",
-    "en": "Description fields"
+    "en": "Description fields",
+    "it": "Descrizioni",
+    "de": "Beschreibungsfelder"
   },
   "location": {
     "fr": "Lieu",
-    "en": "Location"
+    "en": "Location",
+    "it": "Posizione",
+    "de": "Ort"
   },
   "locationInfo": {
     "fr": "Saisissez le nom du lieu où se déroule l'événement",
-    "en": "Type in the name of the location where the event takes place"
+    "en": "Type in the name of the location where the event takes place",
+    "it": "Inserire il nome del luogo in cui si svolge l'evento",
+    "de": "Geben Sie den Namen des Ortes ein, an dem die Veranstaltung stattfindet."
   },
   "locationSub": {
     "fr": "Si aucun lieu ne correspond à votre saisie, ajoutez-le en cliquant sur 'Créer un lieu'",
-    "en": "If no location matches the name, add a new location by clicking on 'Create a new location'"
+    "en": "If no location matches the name, add a new location by clicking on 'Create a new location'",
+    "it": "Se nessun luogo corrisponde alla tua voce, aggiungilo cliccando su 'Crea un luogo'.",
+    "de": "Wenn kein Standort mit dem Namen übereinstimmt, fügen Sie einen neuen Standort hinzu, indem Sie auf \"Neuen Standort erstellen\" klicken."
   },
   "locationSection": {
     "fr": "Lieu",
-    "en": "Location"
+    "en": "Location",
+    "it": "Posizione",
+    "de": "Ort"
   },
   "locationCreate": {
     "fr": "Nouveau lieu",
-    "en": "New location"
+    "en": "New location",
+    "it": "Nuova posizione",
+    "de": "Neuer Standort"
   },
   "title": {
     "fr": "Titre",
-    "en": "Title"
+    "en": "Title",
+    "it": "Titolo",
+    "de": "Titel"
   },
   "titleSub": {
     "fr": "",
-    "en": ""
+    "en": "",
+    "it": "",
+    "de": ""
   },
   "titlePlaceholder": {
     "fr": "Le titre de votre événement",
-    "en": "Title of your event"
+    "en": "Title of your event",
+    "it": "Il titolo del tuo evento",
+    "de": "Titel Ihrer Veranstaltung"
   },
   "description": {
     "fr": "Description courte",
-    "en": "Short description"
+    "en": "Short description",
+    "it": "Breve descrizione",
+    "de": "Kurzbeschreibung"
   },
   "descriptionPlaceholder": {
     "fr": "Une courte description de votre événement",
-    "en": "A short description of your event"
+    "en": "A short description of your event",
+    "it": "Una breve descrizione del vostro evento",
+    "de": "Eine kurze Beschreibung Ihrer Veranstaltung"
   },
   "descriptionSub": {
     "fr": "",
-    "en": ""
+    "en": "",
+    "it": "",
+    "de": ""
   },
   "image": {
     "fr": "Image de l'événement",
-    "en": "Image of the event"
+    "en": "Image of the event",
+    "it": "Immagine dell'evento",
+    "de": "Bild der Veranstaltung"
   },
   "imageInfo": {
     "fr": "Chargez une image d'au moins 300 pixels de large",
-    "en": "Load an image of at least 300 pixels width"
+    "en": "Load an image of at least 300 pixels width",
+    "it": "Carica un'immagine di almeno 300 pixel di larghezza",
+    "de": "Laden Sie ein Bild mit einer Breite von mindestens 300 Pixeln."
   },
   "imageCredits": {
     "fr": "Crédits de l'image",
-    "en": "Image credits"
+    "en": "Image credits",
+    "it": "Crediti d'immagine",
+    "de": "Bildnachweise"
   },
   "longDescription": {
     "fr": "Description longue",
-    "en": "Long description"
+    "en": "Long description",
+    "it": "Descrizione lunga",
+    "de": "Lange Beschreibung"
   },
   "longDescriptionPlaceholder": {
     "fr": "Saisissez une description détaillée de votre événement.",
-    "en": "Type in a detailed description of your event."
+    "en": "Type in a detailed description of your event.",
+    "it": "Inserisci una descrizione dettagliata del tuo evento.",
+    "de": "Geben Sie eine detaillierte Beschreibung Ihrer Veranstaltung ein."
   },
   "longDescriptionSub": {
     "fr": "",
-    "en": ""
+    "en": "",
+    "it": "",
+    "de": ""
   },
   "keywords": {
     "fr": "Mots clés",
-    "en": "Keywords"
+    "en": "Keywords",
+    "it": "Parole Chiave",
+    "de": "Schlüsselwörter"
   },
   "keywordsPlaceholder": {
     "fr": "Séparez les mots clés par des tabulation ou des virgules",
-    "en": "Separate each keyword with tabs or commas"
+    "en": "Separate each keyword with tabs or commas",
+    "it": "Parole chiave separate con schede o virgole",
+    "de": "Trennen Sie jedes Keyword durch Tabulatoren oder Kommas."
   },
   "keywordsSub": {
     "fr": "Les mots clés sont utiles pour les fonctions de recherche",
-    "en": "Keywords are useful for search features"
+    "en": "Keywords are useful for search features",
+    "it": "Le parole chiave sono utili per le funzioni di ricerca",
+    "de": "Keywords sind nützlich für Suchfunktionen."
   },
   "conditions": {
     "fr": "Conditions de participation, tarifs",
-    "en": "Attendence conditions, pricing"
+    "en": "Attendence conditions, pricing",
+    "it": "Condizioni di partecipazione, prezzi",
+    "de": "Teilnahmebedingungen, Preisgestaltung"
   },
   "conditionsPlaceholder": {
     "fr": "Entrée libre, sur inscription, autre...",
-    "en": "Free access, registration required, other..."
+    "en": "Free access, registration required, other...",
+    "it": "Ingresso libero, su registrazione, altro.....",
+    "de": "Freier Zugang, Registrierung erforderlich, andere....."
   },
   "conditionsSub": {
     "fr": "",
-    "en": ""
+    "en": "",
+    "it": "",
+    "de": ""
   },
   "registration": {
     "fr": "Outils d'inscription",
-    "en": "Registration"
+    "en": "Registration",
+    "it": "Strumenti di registrazione",
+    "de": "Registrierung"
   },
   "registrationSub": {
     "fr": "Séparez les items par des tabulation ou des virgules",
-    "en": "Separate each item with tabs or commas"
+    "en": "Separate each item with tabs or commas",
+    "it": "Separare le voci per tabulazione o virgole",
+    "de": "Trennen Sie jedes Element mit Tabs oder Kommas."
   },
   "registrationInfo": {
     "fr": "Liens, emails ou numéros de téléphone",
-    "en": "Links, emails or phone numbers"
+    "en": "Links, emails or phone numbers",
+    "it": "Link, e-mail o numeri di telefono",
+    "de": "Links, E-Mails oder Telefonnummern"
   },
   "accessibility": {
     "fr": "Accessibilité particulière",
-    "en": "Accessibility conditions"
+    "en": "Accessibility conditions",
+    "it": "Accessibilità speciale",
+    "de": "Bedingungen für die Zugänglichkeit"
   },
   "ticketLink": {
     "fr": "Lien de réservation",
-    "en": "Reservation link"
+    "en": "Reservation link",
+    "it": "Link per la prenotazione",
+    "de": "Reservierungslink"
   },
   "age": {
     "fr": "Age du public ciblé",
-    "en": "Targeted public age"
+    "en": "Targeted public age",
+    "it": "Età del pubblico di riferimento",
+    "de": "Gezieltes öffentliches Alter"
   },
   "timings": {
     "fr": "Horaires",
-    "en": "Timings"
+    "en": "Timings",
+    "it": "Orari",
+    "de": "Timings"
   },
   "timingsInfo": {
     "fr": "Définissez les horaires de votre événement",
-    "en": "Specify timings for your event"
+    "en": "Specify timings for your event",
+    "it": "Definire gli orari del vostro evento",
+    "de": "Festlegen von Zeitplänen für Ihre Veranstaltung"
   },
   "timingsHelp": {
     "fr": "Comment ça marche?",
-    "en": "How does this work?"
+    "en": "How does this work?",
+    "it": "Come funziona?",
+    "de": "Wie funktioniert das?"
   },
   "uploadButton": {
     "fr": "Sélectionner",
-    "en": "Select"
+    "en": "Select",
+    "it": "Selezionare",
+    "de": "Auswählen"
   },
   "languages": {
     "fr": "Choisissez une langue",
-    "en": "Pick a language"
+    "en": "Pick a language",
+    "it": "Scegli una lingua",
+    "de": "Wählen Sie eine Sprache"
   },
   "addLanguage": {
     "en": "Add a language",
-    "fr": "Ajouter une langue"
+    "fr": "Ajouter une langue",
+    "it": "Aggiungere una lingua",
+    "de": "Eine Sprache hinzufügen"
   },
   "cancelLanguageChange": {
     "fr": "Annuler",
-    "en": "Cancel"
+    "en": "Cancel",
+    "it": "Annulla",
+    "de": "Abbrechen"
   },
   "noDates": {
     "fr": "Au moins un horaire doit être défini",
-    "en": "At least one timing must be defined"
+    "en": "At least one timing must be defined",
+    "it": "Deve essere definito almeno un programma previsionale",
+    "de": "Es muss mindestens ein Zeitpunkt definiert werden."
   },
   "categoriesTitle": {
     "en": "Categories",
-    "fr": "Catégories"
+    "fr": "Catégories",
+    "it": "Categorie",
+    "de": "Kategorien"
   },
   "tagsTitle": {
     "en": "Tags",
-    "fr": "Tags"
+    "fr": "Tags",
+    "it": "Etichette",
+    "de": "Tags"
   },
   "categoriesInfo": {
     "en": "These are used to index your event in the agenda. Pick one.",
-    "fr": "Celles-ci servent pour l'indexation dans l'agenda. Vous pouvez en choisir une."
+    "fr": "Celles-ci servent pour l'indexation dans l'agenda. Vous pouvez en choisir une.",
+    "it": "Questi sono utilizzati per l'indicizzazione nell'agenda. Puoi sceglierne uno.",
+    "de": "Diese werden verwendet, um Ihre Veranstaltung in der Agenda zu indexieren. Such dir einen aus."
   },
   "defaultTagGroupName": {
     "en": "Tags",
-    "fr": "Tags"
+    "fr": "Tags",
+    "it": "Etichette",
+    "de": "Tags"
   },
   "defaultTagGroupInfo": {
     "en": "These are used to index your event in the agenda. You can select several.",
-    "fr": "Ceux-ci servent pour l'indexation dans l'agenda. Vous pouvez en choisir plusieurs."
+    "fr": "Ceux-ci servent pour l'indexation dans l'agenda. Vous pouvez en choisir plusieurs.",
+    "it": "Questi sono utilizzati per l'indicizzazione nell'agenda. È possibile sceglierne diversi.",
+    "de": "Diese werden verwendet, um Ihre Veranstaltung in der Agenda zu indexieren. Sie können mehrere auswählen."
   },
   "required": {
     "en": "Required.",
-    "fr": "Requis."
+    "fr": "Requis.",
+    "it": "Obbligatorio.",
+    "de": "Erforderlich."
   },
   "categoriesRequired": {
     "en": "Required.",
-    "fr": "Requis."
+    "fr": "Requis.",
+    "it": "Obbligatorio.",
+    "de": "Erforderlich."
   },
   "requiredTagError": {
     "en": "You must select at least one item",
-    "fr": "Vous devez faire au minimum une sélection"
+    "fr": "Vous devez faire au minimum une sélection",
+    "it": "È necessario effettuare almeno una selezione",
+    "de": "Sie müssen mindestens eine Position auswählen."
   },
   "requiredCategoryError": {
     "en": "You must select one item",
-    "fr": "Vous devez faire une sélection"
+    "fr": "Vous devez faire une sélection",
+    "it": "È necessario effettuare una selezione",
+    "de": "Sie müssen eine Position auswählen."
   },
   "noSelection": {
     "en": "None of the above",
-    "fr": "Aucun choix"
+    "fr": "Aucun choix",
+    "it": "Nessuna scelta",
+    "de": "Keiner der oben genannten Punkte"
   },
   "compulsoryNote": {
     "en": "Fields marked with a * must be filled",
-    "fr": "Les champs marqués avec des * sont obligatoires"
+    "fr": "Les champs marqués avec des * sont obligatoires",
+    "it": "Le vostre attività saranno visualizzate qui.",
+    "de": "Die mit * markierten Felder müssen ausgefüllt werden."
   }
 }
