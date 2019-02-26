65ada4d1d470e2a2be1f3d4af6e70ad47365cfe5
Kari Olafsson <kaoreolafsson@gmail.com>
2019-02-26 12:55:49 +0100

    tweak(labels): file reformatting

--- packages/labels/event/form.js
+++ packages/labels/event/form.js
@@ -1,200 +1,200 @@
 "use strict";
 
 module.exports = {
-  descriptionSection: {
-    fr: 'Descriptifs',
-    en: 'Description fields'
+  "descriptionSection": {
+    "fr": "Descriptifs",
+    "en": "Description fields"
   },
-  location: {
-    fr: 'Lieu',
-    en: 'Location'
+  "location": {
+    "fr": "Lieu",
+    "en": "Location"
   },
-  locationInfo: {
-    fr: 'Saisissez le nom du lieu où se déroule l\'événement',
-    en: 'Type in the name of the location where the event takes place'
+  "locationInfo": {
+    "fr": "Saisissez le nom du lieu où se déroule l'événement",
+    "en": "Type in the name of the location where the event takes place"
   },
-  locationSub: {
-    fr: 'Si aucun lieu ne correspond à votre saisie, ajoutez-le en cliquant sur \'Créer un lieu\'',
-    en: 'If no location matches the name, add a new location by clicking on \'Create a new location\''
+  "locationSub": {
+    "fr": "Si aucun lieu ne correspond à votre saisie, ajoutez-le en cliquant sur 'Créer un lieu'",
+    "en": "If no location matches the name, add a new location by clicking on 'Create a new location'"
   },
-  locationSection: {
-    fr: 'Lieu',
-    en: 'Location'
+  "locationSection": {
+    "fr": "Lieu",
+    "en": "Location"
   },
-  locationCreate: {
-    fr: 'Nouveau lieu',
-    en: 'New location'
+  "locationCreate": {
+    "fr": "Nouveau lieu",
+    "en": "New location"
   },
-  title: {
-    fr: 'Titre',
-    en: 'Title'
+  "title": {
+    "fr": "Titre",
+    "en": "Title"
   },
-  titleSub: {
-    fr: '',
-    en: ''
+  "titleSub": {
+    "fr": "",
+    "en": ""
   },
-  titlePlaceholder: {
-    fr : 'Le titre de votre événement',
-    en : 'Title of your event'
+  "titlePlaceholder": {
+    "fr": "Le titre de votre événement",
+    "en": "Title of your event"
   },
-  description: {
-    fr: 'Description courte',
-    en: 'Short description'
+  "description": {
+    "fr": "Description courte",
+    "en": "Short description"
   },
-  descriptionPlaceholder: {
-    fr : 'Une courte description de votre événement',
-    en : 'A short description of your event'
+  "descriptionPlaceholder": {
+    "fr": "Une courte description de votre événement",
+    "en": "A short description of your event"
   },
-  descriptionSub: {
-    fr: '',
-    en: ''
+  "descriptionSub": {
+    "fr": "",
+    "en": ""
   },
-  image: {
-    fr : 'Image de l\'événement',
-    en : 'Image of the event'
+  "image": {
+    "fr": "Image de l'événement",
+    "en": "Image of the event"
   },
-  imageInfo: {
-    fr : 'Chargez une image d\'au moins 300 pixels de large',
-    en : 'Load an image of at least 300 pixels width'
+  "imageInfo": {
+    "fr": "Chargez une image d'au moins 300 pixels de large",
+    "en": "Load an image of at least 300 pixels width"
   },
-  imageCredits: {
-    fr : 'Crédits de l\'image',
-    en: 'Image credits'
+  "imageCredits": {
+    "fr": "Crédits de l'image",
+    "en": "Image credits"
   },
-  longDescription: {
-    fr: 'Description longue',
-    en: 'Long description'
+  "longDescription": {
+    "fr": "Description longue",
+    "en": "Long description"
   },
-  longDescriptionPlaceholder: {
-    fr: 'Saisissez une description détaillée de votre événement.',
-    en: 'Type in a detailed description of your event.'
+  "longDescriptionPlaceholder": {
+    "fr": "Saisissez une description détaillée de votre événement.",
+    "en": "Type in a detailed description of your event."
   },
-  longDescriptionSub: {
-    fr: '',
-    en: ''
+  "longDescriptionSub": {
+    "fr": "",
+    "en": ""
   },
-  keywords: {
-    fr: 'Mots clés',
-    en: 'Keywords'
+  "keywords": {
+    "fr": "Mots clés",
+    "en": "Keywords"
   },
-  keywordsPlaceholder: {
-    fr: 'Séparez les mots clés par des tabulation ou des virgules',
-    en: 'Separate each keyword with tabs or commas'
+  "keywordsPlaceholder": {
+    "fr": "Séparez les mots clés par des tabulation ou des virgules",
+    "en": "Separate each keyword with tabs or commas"
   },
-  keywordsSub: {
-    fr : 'Les mots clés sont utiles pour les fonctions de recherche',
-    en : 'Keywords are useful for search features'
+  "keywordsSub": {
+    "fr": "Les mots clés sont utiles pour les fonctions de recherche",
+    "en": "Keywords are useful for search features"
   },
-  conditions: {
-    fr: 'Conditions de participation, tarifs',
-    en: 'Attendence conditions, pricing'
+  "conditions": {
+    "fr": "Conditions de participation, tarifs",
+    "en": "Attendence conditions, pricing"
   },
-  conditionsPlaceholder: {
-    fr: 'Entrée libre, sur inscription, autre...',
-    en: 'Free access, registration required, other...'
+  "conditionsPlaceholder": {
+    "fr": "Entrée libre, sur inscription, autre...",
+    "en": "Free access, registration required, other..."
   },
-  conditionsSub: {
-    fr: '',
-    en: ''
+  "conditionsSub": {
+    "fr": "",
+    "en": ""
   },
-  registration: {
-    fr: 'Outils d\'inscription',
-    en: 'Registration'
+  "registration": {
+    "fr": "Outils d'inscription",
+    "en": "Registration"
   },
-  registrationSub: {
-    fr: 'Séparez les items par des tabulation ou des virgules',
-    en: 'Separate each item with tabs or commas'
+  "registrationSub": {
+    "fr": "Séparez les items par des tabulation ou des virgules",
+    "en": "Separate each item with tabs or commas"
   },
-  registrationInfo: {
-    fr: 'Liens, emails ou numéros de téléphone',
-    en: 'Links, emails or phone numbers'
+  "registrationInfo": {
+    "fr": "Liens, emails ou numéros de téléphone",
+    "en": "Links, emails or phone numbers"
   },
-  accessibility: {
-    fr: 'Accessibilité particulière',
-    en: 'Accessibility conditions'
+  "accessibility": {
+    "fr": "Accessibilité particulière",
+    "en": "Accessibility conditions"
   },
-  ticketLink: {
-    fr: 'Lien de réservation',
-    en: 'Reservation link'
+  "ticketLink": {
+    "fr": "Lien de réservation",
+    "en": "Reservation link"
   },
-  age: {
-    fr: 'Age du public ciblé',
-    en: 'Targeted public age'
+  "age": {
+    "fr": "Age du public ciblé",
+    "en": "Targeted public age"
   },
-  timings: {
-    fr: 'Horaires',
-    en: 'Timings'
+  "timings": {
+    "fr": "Horaires",
+    "en": "Timings"
   },
-  timingsInfo: {
-    fr: 'Définissez les horaires de votre événement',
-    en: 'Specify timings for your event'
+  "timingsInfo": {
+    "fr": "Définissez les horaires de votre événement",
+    "en": "Specify timings for your event"
   },
-  timingsHelp: {
-    fr: 'Comment ça marche?',
-    en: 'How does this work?'
+  "timingsHelp": {
+    "fr": "Comment ça marche?",
+    "en": "How does this work?"
   },
-  uploadButton: {
-    fr: 'Sélectionner',
-    en: 'Select'
+  "uploadButton": {
+    "fr": "Sélectionner",
+    "en": "Select"
   },
-  languages: {
-    fr : 'Choisissez une langue',
-    en : 'Pick a language'
+  "languages": {
+    "fr": "Choisissez une langue",
+    "en": "Pick a language"
   },
-  addLanguage: {
-    'en' : 'Add a language',
-    'fr' : 'Ajouter une langue'
+  "addLanguage": {
+    "en": "Add a language",
+    "fr": "Ajouter une langue"
   },
-  cancelLanguageChange: {
-    fr: 'Annuler',
-    en: 'Cancel'
+  "cancelLanguageChange": {
+    "fr": "Annuler",
+    "en": "Cancel"
   },
-  noDates: {
-    fr: 'Au moins un horaire doit être défini',
-    en: 'At least one timing must be defined'
+  "noDates": {
+    "fr": "Au moins un horaire doit être défini",
+    "en": "At least one timing must be defined"
   },
-  categoriesTitle: {
-    en: 'Categories',
-    fr: 'Catégories'
+  "categoriesTitle": {
+    "en": "Categories",
+    "fr": "Catégories"
   },
-  tagsTitle: {
-    en: 'Tags',
-    fr: 'Tags'
+  "tagsTitle": {
+    "en": "Tags",
+    "fr": "Tags"
   },
-  categoriesInfo: {
-    en: 'These are used to index your event in the agenda. Pick one.',
-    fr: 'Celles-ci servent pour l\'indexation dans l\'agenda. Vous pouvez en choisir une.'
+  "categoriesInfo": {
+    "en": "These are used to index your event in the agenda. Pick one.",
+    "fr": "Celles-ci servent pour l'indexation dans l'agenda. Vous pouvez en choisir une."
   },
-  defaultTagGroupName: {
-    en: 'Tags',
-    fr: 'Tags'
+  "defaultTagGroupName": {
+    "en": "Tags",
+    "fr": "Tags"
   },
-  defaultTagGroupInfo: {
-    en: 'These are used to index your event in the agenda. You can select several.',
-    fr: 'Ceux-ci servent pour l\'indexation dans l\'agenda. Vous pouvez en choisir plusieurs.'
+  "defaultTagGroupInfo": {
+    "en": "These are used to index your event in the agenda. You can select several.",
+    "fr": "Ceux-ci servent pour l'indexation dans l'agenda. Vous pouvez en choisir plusieurs."
   },
-  required: {
-    en: 'Required.',
-    fr: 'Requis.'
+  "required": {
+    "en": "Required.",
+    "fr": "Requis."
   },
-  categoriesRequired: {
-    en: 'Required.',
-    fr: 'Requis.'
+  "categoriesRequired": {
+    "en": "Required.",
+    "fr": "Requis."
   },
-  requiredTagError: {
-    en: 'You must select at least one item',
-    fr: 'Vous devez faire au minimum une sélection'
+  "requiredTagError": {
+    "en": "You must select at least one item",
+    "fr": "Vous devez faire au minimum une sélection"
   },
-  requiredCategoryError: {
-    en: 'You must select one item',
-    fr: 'Vous devez faire une sélection'
+  "requiredCategoryError": {
+    "en": "You must select one item",
+    "fr": "Vous devez faire une sélection"
   },
-  noSelection: {
-    en: 'None of the above',
-    fr: 'Aucun choix'
+  "noSelection": {
+    "en": "None of the above",
+    "fr": "Aucun choix"
   },
-  compulsoryNote: {
-    en: 'Fields marked with a * must be filled',
-    fr: 'Les champs marqués avec des * sont obligatoires'
+  "compulsoryNote": {
+    "en": "Fields marked with a * must be filled",
+    "fr": "Les champs marqués avec des * sont obligatoires"
   }
 }
