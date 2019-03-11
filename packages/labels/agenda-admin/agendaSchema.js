d27e1f213467be991591690c190b661511f18d31
Kari Olafsson <kaoreolafsson@gmail.com>
2019-03-11 08:25:54 +0100

    tweak(labels): agenda schema & agenda admin layout labels

---
+++ packages/labels/agenda-admin/agendaSchema.js
@@ -0,0 +1,20 @@
+"use strict";
+
+module.exports = {
+  "network": {
+    "en": "Network",
+    "fr": "Réseau"
+  },
+  "networkDetail": {
+    "en": "Field required by the agenda network",
+    "fr": "Champ requis par le réseau d'agendas"
+  },
+  "event" : {
+    "en" : "Standard",
+    "fr" : "Standard"
+  },
+  "eventDetail" : {
+    "en" : "Standard event field",
+    "fr" : "Champ événement standard"
+  }
+}
---
+++ packages/labels/layout/agendaAdmin.js
@@ -0,0 +1,24 @@
+"use strict";
+
+module.exports = {
+  administration: {
+    fr: 'Administration',
+    en: 'Administration'
+  },
+  back: {
+    fr: 'Retour',
+    en: 'Back'
+  },
+  manage: {
+    fr: 'Gérer',
+    en: 'Manage'
+  },
+  export: {
+    fr: 'Exporter',
+    en: 'Export'
+  },
+  settings: {
+    fr: 'Paramétrer',
+    en: 'Settings'
+  }
+}
