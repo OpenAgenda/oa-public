9281fc253174b92d0c43732fda407a1d71e9b5c2
Bertho <kevin.bertho@gmail.com>
2019-03-11 10:26:02 +0100

    cibul-templates: inject store when app changes

--- packages/cibul-templates/webapp/utils/historyActionMaker.js
+++ packages/cibul-templates/webapp/utils/historyActionMaker.js
@@ -1,13 +1,11 @@
 import _ from 'lodash';
-import asyncMatchRoutes, { matchRoutes } from '@openagenda/react-utils/dist/asyncMatchRoutes';
+import { matchRoutes } from '@openagenda/react-utils/dist/asyncMatchRoutes';
 import { createLocation } from 'history';
 import getNotFoundState from './getNotFoundState';
 
 export default function historyActionMaker( {
   history,
-  apps,
-  onLocationChangeStart,
-  onLocationChangeFinish
+  apps
 } ) {
   return action => ( path, state ) => {
     const newLocation = createLocation(
@@ -25,7 +23,7 @@ export default function historyActionMaker( {
     const componentsPerApps = Object.values( apps )
       .map( app => app.routes && matchRoutes( app.routes, newLocation.pathname ).map( v => v.route.component ) )
 
-    const { visibleApps, notFoundApps } = Object.values( apps )
+    const { visibleApps/* , notFoundApps */ } = Object.values( apps )
       .reduce( ( result, app, key ) => {
         if ( componentsPerApps[ key ].some( v => (v && v.isNotFound) ) ) {
           result.notFoundApps.push( app );
@@ -36,31 +34,12 @@ export default function historyActionMaker( {
         return result;
       }, { visibleApps: [], notFoundApps: [] } );
 
-    const someNotReady = Object.values( apps ).some( ( app, key ) => {
-      if ( notFoundApps.includes( app ) ) {
-        return false;
-      }
-
-      return componentsPerApps[ key ].some( v => (!v || (typeof v.isReady === 'function' && !v.isReady())) );
-    } );
-
-    const needToPreload = someNotReady && !_.isEqual( newLocation.state, history.location.state );
-
-    if ( needToPreload ) {
-      if ( typeof onLocationChangeStart === 'function' ) {
-        onLocationChangeStart();
-      }
-
-      Promise.all( visibleApps.map( app => asyncMatchRoutes( app.routes, newLocation.pathname ) ) )
-        .then( matchResults => {
-          action( newLocation );
-
-          if ( matchResults.every( ( { components } ) => !components.some( v => v[ '@@redial-hooks' ] ) ) ) {
-            if ( typeof onLocationChangeFinish === 'function' ) {
-              onLocationChangeFinish();
-            }
-          }
-        } );
+    if ( !_.isEqual( newLocation.state, history.location.state ) ) {
+      Promise.all(
+        visibleApps.map( app => app.triggerHooks( { pathname: newLocation.pathname, hooks: [ 'inject' ] } ) )
+      ).then( () => {
+        action( newLocation );
+      } )
     } else {
       action( newLocation );
     }
