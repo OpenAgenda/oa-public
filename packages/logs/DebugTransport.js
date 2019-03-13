b0149537ffefed80b32d3c7d3706a2e07befcc95
Bertho <kevin.bertho@gmail.com>
2019-03-13 14:58:01 +0100

    logs: fix DebugTransport with forced namespace

--- packages/logs/DebugTransport.js
+++ packages/logs/DebugTransport.js
@@ -8,7 +8,6 @@ const debug = require( 'debug' );
 const isEmptyObject = obj => obj && Object.keys( obj ).length === 0 && obj.constructor === Object;
 
 class DebugTransport extends winston.Transport {
-
   constructor( options ) {
     super( options );
 
@@ -19,7 +18,7 @@ class DebugTransport extends winston.Transport {
     this.prefix = params.prefix;
     this.namespace = params.namespace;
 
-    const debugName = (params.prefix || '') + (params.namespace || '');
+    const debugName = this.getDebugName();
 
     if ( !process.env.DEBUG && params.enable && !debug.enabled( debugName ) ) {
       debug.names.push( new RegExp( '^' + debugName.replace( /\*/g, '.*?' ) + '$' ) );
@@ -28,19 +27,30 @@ class DebugTransport extends winston.Transport {
     this.debug = debug( debugName );
   }
 
+  getDebugName( namespace ) {
+    return (this.prefix || '') + (namespace || this.namespace || '');
+  }
+
   log( level, msg, meta, cb ) {
     const displayedMeta = meta instanceof Error ? meta : _.omit( meta, 'namespace' );
     const args = [ msg ].concat(
       typeof displayedMeta !== 'undefined' && !isEmptyObject( displayedMeta )
-        ? util.inspect( displayedMeta, { colors: debug.useColors() } )
+        ? util.inspect( displayedMeta, { colors: this.debug.useColors } )
         : []
     );
 
-    this.debug.apply( null, args );
+    // Overwrite namespace
+    const originalNamespace = this.debug.namespace;
+    this.debug.namespace = this.getDebugName( meta.namespace );
+
+    // Log
+    this.debug.apply( this.debug, args );
+
+    // Restore namespace
+    this.debug.namespace = originalNamespace;
 
     cb( null, true );
   }
-
 }
 
 module.exports = DebugTransport;
