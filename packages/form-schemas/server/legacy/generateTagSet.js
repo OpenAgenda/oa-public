bc04171c4f99b9aa911a19bdd6736cde028a80d6
Kari Olafsson <kaoreolafsson@gmail.com>
2019-03-14 15:57:25 +0100

    feature(form-schemas): non-breaking schema-to-tags function

--- packages/form-schemas/server/legacy/generateTagSet.js
+++ packages/form-schemas/server/legacy/generateTagSet.js
@@ -6,34 +6,95 @@ const includeTypes = [ 'radio', 'select', 'checkbox' ];
 
 const uniques = [ 'radio', 'select' ];
 
-module.exports = schema => {
+module.exports = ( schema, currentTagSet = null ) => {
+
+  const tagSetGroups = currentTagSet ? currentTagSet.groups : [];
 
   const tagSettableFields = schema.fields
     .filter( f => includeTypes.includes( f.fieldType ) );
 
   const messages = tagSettableFields
-    .filter( f => !!f.origin )
+    .filter( f => !f.origin )
     .map( f => `${f.field}: field origin is not set` );
 
   if ( !tagSettableFields.length ) return null;
 
-  const tagSet = {
-    groups: tagSettableFields.map( f => ( {
-      name: _monoLabel( f.label ),
-      required: !f.optional,
-      unique: uniques.includes( f.fieldType ),
-      tags: f.options.map( o => ( {
-        label: _monoLabel( o.label ),
-        slug: o.value,
-        schemaOptionId: [ f.schemaId || schema.id, o.id ].join( '.' )
-      } ) )
-    } ) )
-  }
+  tagSettableFields.forEach( f => {
+
+    const index = tagSetGroups.map( g => g.name ).indexOf( _monoLabel( f.label ) );
+
+    const tags = _defineTags( schema.id, index === -1 ? [] : tagSetGroups[ index ].tags, f.options );
+
+    if ( index === -1 ) {
+
+      tagSetGroups.push( {
+        name: _monoLabel( f.label ),
+        required: !f.optional,
+        unique: uniques.includes( f.fieldType ),
+        tags
+      } );
+
+    } else {
+
+      tagSetGroups[ index ].tags = tags ;
+
+    }
+
+  } );
 
   return {
-    tagSet,
+    tagSet: {
+      groups: tagSetGroups
+    },
     messages
   }
+
+}
+
+function _defineTags( schemaId, tags = [], options = [] ) {
+
+  options.forEach( o => {
+
+    let matchingTagIndex = -1;
+
+    const label = _monoLabel( o.label );
+    const slug = o.value;
+    const schemaOptionId = `${schemaId}.${o.id}`;
+
+    // if schemaOptionId is specified in tag, use it for match
+    if ( _hasSchemaOptionId( tags ) ) {
+
+      matchingTagIndex = _.findIndex( tags, { schemaOptionId } );
+
+    } else { // we match based on label
+
+      matchingTagIndex = _.findIndex( tags, { label: _monoLabel( o.label ) } );
+
+    }
+
+    if ( matchingTagIndex !== -1 ) {
+
+      _.assign( tags[ matchingTagIndex ], { slug, label, schemaOptionId } );
+
+    } else {
+
+      tags.push( {
+        slug,
+        label,
+        schemaOptionId
+      });
+
+    }
+
+  } );
+
+  return tags;
+
+}
+
+function _hasSchemaOptionId( tags ) {
+
+  return !!tags.filter( t => t.schemaOptionId ).length;
 
 }
 
