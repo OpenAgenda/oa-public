38e2a933a5c35b3d5077cefa22579709e2a9568a
Kari Olafsson <kaoreolafsson@gmail.com>
2019-04-10 22:52:04 +0200

    fix(agenda-schemas): network is not properly loaded

--- packages/cibul-node/services/agendaSchema/interfaces/getSchemaExtensions.js
+++ packages/cibul-node/services/agendaSchema/interfaces/getSchemaExtensions.js
@@ -3,6 +3,7 @@
 const _ = require( 'lodash' );
 const eventSchema = require( '@openagenda/event-form/build/schema' );
 const networks = require( '@openagenda/networks' );
+const formSchemas = require( '@openagenda/form-schemas' );
 
 const log = require( '@openagenda/logs' )( 'events/interfaces/getSchemaExtensions' );
 
@@ -12,9 +13,9 @@ module.exports = async agenda => {
 
   const networkUid = _.get( agenda, 'networkUid' );
 
-  const network = networkUid ? ( await networks.get( { uid: networkUid } ) ) : null;
+  const network = networkUid ? ( await networks.get( networkUid ) ) : null;
 
-  const networkFormSchemaId = _.get( network, 'form_schema_id' );
+  const networkFormSchemaId = _.get( network, 'formSchemaId' );
 
   return {
     event: _.assign( { id: -1 }, eventSchema( {} ) ),
