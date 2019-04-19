9da85e773a1154879e6fea0e9fdd855933a810d4
Kari Olafsson <kaoreolafsson@gmail.com>
2019-04-19 18:50:02 +0200

    tweak(user-apps): spanish

--- packages/user-apps/src/components/ProfileSettings.js
+++ packages/user-apps/src/components/ProfileSettings.js
@@ -42,6 +42,7 @@ export default class ProfileSettings extends Component {
         <option value="fr">Français</option>
         <option value="en">English</option>
         <option value="de">Deutsch</option>
+        <option value="es">Español</option>
       </select>
       {field.meta.touched && field.meta.error && (
         <div className="text-danger">{_.upperFirst( this.context.getLabel( field.meta.error ) )}</div>
