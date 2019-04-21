71c1da6c60f6f6b6e1b4cfa22b3cc097893be757
Kari Olafsson <kaoreolafsson@gmail.com>
2019-04-21 11:34:58 +0200

    tweak(networks): Service now is usable through instance

--- packages/networks/index.js
+++ packages/networks/index.js
@@ -1,10 +1,18 @@
 "use strict";
 
-
+const _ = require( 'lodash' );
 const config = require( './config' );
 const get = require( './get' );
 
-module.exports = {
-  init: config.init,
-  get
+module.exports = ( options = {} ) => {
+
+  const config = _.assign( {
+    knex: null,
+    schema: 'network'
+  }, options );
+
+  return {
+    get: get.bind( null, config )
+  }
+
 }
