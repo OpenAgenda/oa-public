d7ee04ccbba83e6ca08b9a6fd3881ba82d98d492
Kari Olafsson <kaoreolafsson@gmail.com>
2019-05-21 18:57:59 +0200

    fix(cibulNode): a save of a network schema clears all tag sets

--- packages/cibul-node/core/agendas/settings/legacy/update.js
+++ packages/cibul-node/core/agendas/settings/legacy/update.js
@@ -3,6 +3,7 @@
 const _ = require( 'lodash' );
 
 const custom = require( '@openagenda/custom' );
+const log = require( '@openagenda/logs' )( 'core/agendas/settings/legacy/update' );
 
 const getAgenda = require( '../../utils/getAgenda' );
 const updateTagSetFromSchema = require( './updateTagSetFromSchema' );
@@ -10,9 +11,12 @@ const updateCustomFromSchema = require( './updateCustomFromSchema' );
 const resyncLegacyIndex = require( './resyncLegacyIndex' );
 const controlData = require( '../../../../services/legacy' ).controlData;
 
+
 module.exports = async ( config, agendaOrUid, force = false ) => {
 
-  const agenda = _.isObject( agendaOrUid ) ? agendaOrUid : await getAgenda( agendaOrUid );
+  const agenda = _requiresAgendaLoad( agendaOrUid ) ? await getAgenda( agendaOrUid ) : agendaOrUid;
+
+  log( 'syncing legacy config and data of agenda %s (%s)%s', agenda.uid, agenda.slug, force ? ' forced' : '' );
 
   await updateTagSetFromSchema( config, agenda, force );
 
@@ -23,5 +27,15 @@ module.exports = async ( config, agendaOrUid, force = false ) => {
   await resyncLegacyIndex( agenda.id );
 
   await controlData.rebuild( agenda.uid );
+
+}
+
+function _requiresAgendaLoad( agendaOrUid ) {
+
+  if ( !_.isObject( agendaOrUid ) ) return true;
+
+  return _.keys( agendaOrUid )
+    .filter( f => [ 'id', 'networkUid', 'formSchemaId' ].includes( f ) )
+    .length !== 3;
 
 }
