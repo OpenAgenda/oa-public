0728ff3527c6215b506d611a071851e7f0614051
Kari Olafsson <kaoreolafsson@gmail.com>
2019-06-08 10:31:46 +0200

    fix(agenda-event-references): calls to webtools make legacy event form crash

--- packages/agenda-event-references/react/src/Editor/store/configure.prod.js
+++
@@ -1,7 +0,0 @@
-import { createStore, applyMiddleware, compose } from 'redux'
-import rootReducer from '../reducers'
-import thunkMiddleware from 'redux-thunk'
-
-export default compose(
-  applyMiddleware( thunkMiddleware )
-)
