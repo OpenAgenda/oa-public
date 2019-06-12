f1f4fb15235540fda3c5200ff2b3976b242de504
Kari Olafsson <kaoreolafsson@gmail.com>
2019-06-12 16:30:12 +0200

    tweak(cibulNode): function renaming for updating search & control data after location change

---
+++ packages/cibul-node/services/agendaLocations/tasks/syncImpactedEventsAndAgendas.js
@@ -0,0 +1,43 @@
+"use strict";
+
+const _ = require( 'lodash' );
+
+const agendaEvents = require( '@openagenda/agenda-events' );
+const events = require( '@openagenda/events' );
+const log = require( '@openagenda/logs' )( 'agendaLocations/tasks/reindexImpactedEvents' );
+
+const legacyEventSearch = require( '../../elasticsearch' );
+const controlData = require( '../../legacy' ).controlData;
+
+module.exports = async function( before, after ) {
+
+  const uids = await events
+    .list( { locationUid: before.uid }, 0, 1000, { fetched: [ 'uid' ] } )
+    .then( ( { events } ) => events.map( e => e.uid ) );
+
+  const impactedAgendaUids = [];
+
+  // reindex events
+  for ( const uid of uids ) {
+    try {
+      await legacyEventSearch.updateEvent( { uid } );
+    } catch ( e ) {
+      log( 'error', 'could not update event %s index', uid, e );
+    }
+  }
+
+  // update control data of impacted agendas
+  for ( const uid of uids ) {
+    await agendaEvents.list.byEventUid( uid )
+      .then( ( { items } ) => items
+        .forEach( ( { agendaUid } ) => {
+          impactedAgendaUids.push( agendaUid )
+        } )
+      );
+  }
+
+  for ( const agendaUid of _.uniq( impactedAgendaUids ) ) {
+    await controlData.locationSet( { agendaUid, location: after } );
+  }
+
+}
