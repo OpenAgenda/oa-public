f0ca7d66cbbcd6a1a62269c98a63fda877fee411
Bertho <kevin.bertho@gmail.com>
2019-07-15 16:15:54 +0200

    remove inexistant package from jest config

--- jest.config.js
+++ jest.config.js
@@ -6,7 +6,6 @@ module.exports = {
     '<rootDir>/packages/logs',
     '<rootDir>/packages/mails',
     '<rootDir>/packages/queues',
-    '<rootDir>/packages/react-timings-picker',
     '<rootDir>/packages/sdk-js',
   ],
   collectCoverage: true
