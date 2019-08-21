5f1d957eef3c732d9fd394cc834de64fa8ee7ad8
Kari Olafsson <kaoreolafsson@gmail.com>
2019-08-21 18:41:15 +0200

    tweak(cibul-node): avoid fail on agenda location onUpdate interface call

--- packages/cibul-node/services/agendaLocations/interfaces/onUpdate.js
+++ packages/cibul-node/services/agendaLocations/interfaces/onUpdate.js
@@ -1,13 +1,17 @@
 "use strict";
 
-const log = require( '@openagenda/logs' )( 'agendaLocations/onCreate' );
+const log = require( '@openagenda/logs' )( 'agendaLocations/onUpdate' );
 
 const { distance } = require( '@openagenda/agenda-locations' ).utils;
 
 module.exports = async ( { queue }, before, after ) => {
 
-  if ( distance( before, after ) > 10 ) {
-    queue( 'syncImpactedEventsAndAgendas', before, after );
+  try {
+    if ( distance( before, after ) > 10 ) {
+      queue( 'syncImpactedEventsAndAgendas', before, after );
+    }
+  } catch ( e ) {
+    log( 'error', 'failed to evaluate distance', e );
   }
 
 }
