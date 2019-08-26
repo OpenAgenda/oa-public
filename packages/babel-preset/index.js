5367d3f37166110be38c70c637cf09065c697eab
Bertho <kevin.bertho@gmail.com>
2019-08-26 17:09:17 +0200

    fix build of agenda-contribute

--- packages/babel-preset/index.js
+++ packages/babel-preset/index.js
@@ -1,16 +1,33 @@
 'use strict';
 
-const { declare } = require( '@babel/helper-plugin-utils' );
+const path = require('path');
+const { declare } = require('@babel/helper-plugin-utils');
 
-function isBabelLoader( caller ) {
+function isBabelLoader(caller) {
   return !!(caller && caller.name === 'babel-loader');
 }
 
+function getBabelRuntimeName(corejs) {
+  if (!corejs) {
+    return '@babel/runtime';
+  }
 
-module.exports = declare( ( api, options ) => {
-  api.assertVersion( 7 );
+  if (typeof corejs === 'number') {
+    return `@babel/runtime-corejs${corejs}`;
+  }
 
-  const isWebpack = api.caller( isBabelLoader );
+  if (typeof corejs.version === 'number') {
+    return `@babel/runtime-corejs${corejs.version}`;
+  }
+
+  throw new Error(`Preset @openagenda: 'corejs' option is invalid.`);
+}
+
+
+module.exports = declare((api, options) => {
+  api.assertVersion(7);
+
+  const isWebpack = api.caller(isBabelLoader);
 
   const debug = typeof options.debug === 'boolean' ? options.debug : false;
   const useBuiltIns = typeof options.useBuiltIns !== 'undefined' ? options.useBuiltIns : 'usage';
@@ -18,11 +35,16 @@ module.exports = declare( ( api, options ) => {
   const modules = typeof options.modules !== 'undefined' ? options.modules : 'auto';
   const development = typeof options.development === 'boolean'
     ? options.development
-    : api.cache( () => process.env.NODE_ENV !== 'production' );
+    : api.cache(() => process.env.NODE_ENV !== 'production');
+
+  const useAbsoluteRuntime = typeof options.absoluteRuntime === 'boolean' ? options.absoluteRuntime : true;
+  const absoluteRuntimePath = useAbsoluteRuntime
+    ? path.dirname(require.resolve(`${getBabelRuntimeName(corejs)}/package.json`))
+    : undefined;
 
   const presets = [
     [
-      require( '@babel/preset-env' ),
+      require('@babel/preset-env'),
       {
         debug,
         useBuiltIns,
@@ -43,7 +65,7 @@ module.exports = declare( ( api, options ) => {
       }
     ],
     [
-      require( '@babel/preset-react' ),
+      require('@babel/preset-react'),
       {
         development
       }
@@ -51,50 +73,51 @@ module.exports = declare( ( api, options ) => {
   ];
 
   const plugins = [
-    require( 'babel-plugin-lodash' ),
-    require( 'babel-plugin-add-module-exports' ),
+    require('babel-plugin-lodash'),
+    require('babel-plugin-add-module-exports'),
     [
-      require( '@babel/plugin-transform-runtime' ),
+      require('@babel/plugin-transform-runtime'),
       {
-        corejs
+        corejs,
+        absoluteRuntime: absoluteRuntimePath
       }
     ],
-    require( '@babel/plugin-syntax-dynamic-import' ),
+    require('@babel/plugin-syntax-dynamic-import'),
     isWebpack
       ? null
-      : require( 'babel-plugin-dynamic-import-node' ),
+      : require('babel-plugin-dynamic-import-node'),
 
-    require( '@babel/plugin-proposal-object-rest-spread' ),
+    require('@babel/plugin-proposal-object-rest-spread'),
 
     // Stage 0
-    require( '@babel/plugin-proposal-function-bind' ),
+    require('@babel/plugin-proposal-function-bind'),
 
     // Stage 1
-    require( '@babel/plugin-proposal-export-default-from' ),
-    require( '@babel/plugin-proposal-do-expressions' ),
+    require('@babel/plugin-proposal-export-default-from'),
+    require('@babel/plugin-proposal-do-expressions'),
 
     // Stage 2
     [
-      require( '@babel/plugin-proposal-decorators' ),
+      require('@babel/plugin-proposal-decorators'),
       {
         legacy: true
       }
     ],
-    require( '@babel/plugin-proposal-export-namespace-from' ),
-    require( '@babel/plugin-proposal-throw-expressions' ),
+    require('@babel/plugin-proposal-export-namespace-from'),
+    require('@babel/plugin-proposal-throw-expressions'),
 
     // Stage 3
     [
-      require( '@babel/plugin-proposal-class-properties' ),
+      require('@babel/plugin-proposal-class-properties'),
       {
         loose: true
       }
     ]
   ]
-    .filter( v => v !== null );
+    .filter(v => v !== null);
 
   return {
     presets,
     plugins
   };
-} );
+});
