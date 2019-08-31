5cd5a226857fe49f4e26399de6367a09c9767136
Kari Olafsson <kaoreolafsson@gmail.com>
2019-08-31 11:52:28 +0200

    refactor(cibul-node): removed stakeholders from agendas interfaces

--- packages/cibul-node/services/agendaStakeholders/getExistingCredentials.js
+++ packages/cibul-node/services/agendaStakeholders/getExistingCredentials.js
@@ -5,23 +5,7 @@ const agendas = require( '@openagenda/agendas' );
 let log = console.log;
 
 module.exports = ( agendaId, cb ) => {
-
-  agendas.get( { id: agendaId }, { instanciate: true, private: null }, ( err, agenda ) => {
-
-    if ( err ) return cb( err );
-
-    if ( !agenda ) return cb( null, [] );
-
-    agenda.getRoles( ( err, credentials ) => {
-
-      if ( err ) return cb( err );
-
-      cb( null, credentials.map( c => c.value ) );
-
-    } );
-
-  } );
-
+  cb(null, []);
 }
 
 module.exports.setLog = l => log = l;
