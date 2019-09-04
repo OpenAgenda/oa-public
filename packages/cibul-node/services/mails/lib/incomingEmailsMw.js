eef79b4f329f62be5cd7f5ddda20ccf091263942
Bertho <kevin.bertho@gmail.com>
2019-09-04 15:12:29 +0200

    cibul-node: fix incomingEmailMw when References does not exist

--- packages/cibul-node/services/mails/lib/incomingEmailsMw.js
+++ packages/cibul-node/services/mails/lib/incomingEmailsMw.js
@@ -18,7 +18,20 @@ module.exports = ({ services }) => async (req, res, next) => {
       return res.sendStatus(200);
     }
 
-    const { address: referenceEmail } = addressParser(req.body['References'])[0];
+    const { address: referenceEmail } = addressParser(req.body['References'])[0] || {};
+
+    if (!referenceEmail) {
+      return res.sendStatus(200);
+      // throw new Error('Invalid reference');
+    }
+
+    const referenceMatches = referenceEmail.match(REFERENCE_REG);
+    const conversationId = parseInt(referenceMatches && referenceMatches[1], 10);
+
+    if (!conversationId) {
+      return res.sendStatus(200);
+      // throw new Error('Invalid conversation id');
+    }
 
     const replyMatches = req.body.recipient.match(REPLY_REG);
     const replyToken = replyMatches && replyMatches[1];
@@ -38,14 +51,6 @@ module.exports = ({ services }) => async (req, res, next) => {
     if (!user) {
       return res.sendStatus(200);
       // throw new Error('User not found');
-    }
-
-    const referenceMatches = referenceEmail.match(REFERENCE_REG);
-    const conversationId = parseInt(referenceMatches && referenceMatches[1], 10);
-
-    if (!conversationId) {
-      return res.sendStatus(200);
-      // throw new Error('Invalid conversation id');
     }
 
     const conversation = await Inboxes.user(user.uid).conversations.get(conversationId);
