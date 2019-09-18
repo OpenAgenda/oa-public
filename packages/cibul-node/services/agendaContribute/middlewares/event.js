21d277293e3fab7a91581de8a356c1c7b7ae74b0
Kari Olafsson <kaoreolafsson@gmail.com>
2019-09-18 19:36:40 +0200

    fix(cibul-node): drafts are innaccessible

--- packages/cibul-node/services/agendaContribute/middlewares/event.js
+++ packages/cibul-node/services/agendaContribute/middlewares/event.js
@@ -1,26 +1,18 @@
 "use strict";
 
-const _ = require( 'lodash' );
-const ih = require( 'immutability-helper' );
-
-const log = require( '@openagenda/logs' )( 'services/agendaContribute/middlewares/event' );
-
-const core = require( '../../../core' );
+const _ = require('lodash');
+const ih = require('immutability-helper');
+const log = require('@openagenda/logs')('services/agendaContribute/middlewares/event');
+const core = require('../../../core');
 
 module.exports = ( req, res, next ) => {
+  core.agendas(req.agenda.uid)
+    .events.get(req.params.eventUid, { internal: true })
+    .then(event => {
+      if (!event) return next(404);
 
-  core.agendas( req.agenda.uid ).events.get( req.params.eventUid, { internal: true } ).then( event => {
+      req.event = event;
 
-    if ( !event ) return next( 404 );
-
-    req.event = event;
-
-    next();
-
-  } ).catch( err => {
-
-    next( err );
-
-  } );
-
+      next();
+    }).catch(next);
 }
