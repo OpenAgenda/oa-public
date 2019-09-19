796ea62604fba920b7ee52cffcca5fdbb742f008
Bertho <kevin.bertho@gmail.com>
2019-09-19 11:19:27 +0200

    users: fix html errors

--- packages/users/hooks/isValidToken.js
+++ packages/users/hooks/isValidToken.js
@@ -5,7 +5,10 @@ const errors = require('@feathersjs/errors');
 
 module.exports = function isValidToken(localKey, foreignKey) {
   return context => {
-    if (_.get(context, localKey) !== _.get(context, foreignKey)) {
+    const localValue = _.get(context, localKey);
+    const foreignValue = _.get(context, foreignKey);
+
+    if (typeof foreignValue === 'undefined' || localValue !== foreignValue) {
       throw new errors.BadRequest('Bad token');
     }
   };
