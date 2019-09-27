7ce02421c3a2acfeddbdc1a44230d44e6140f573
Bertho <kevin.bertho@gmail.com>
2019-09-27 17:46:59 +0200

    upgrade Image component

--- packages/react-components/components/Image.js
+++ packages/react-components/components/Image.js
@@ -1,70 +1,87 @@
-import React, { Component } from 'react';
+import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
-import { setPropTypes, withState, lifecycle, mapProps, compose, pure } from 'recompose';
 
 // checks for svg if last 4 characters === '.svg'
-function srcRepresentsSvgFile( src ) {
-  return src && /\.svg$/.test( src );
+function srcRepresentsSvgFile(src) {
+  return src.endsWith('.svg');
 }
 
-@compose(
-  pure,
-  // make sure we recieved original image src prop
-  setPropTypes( { src: PropTypes.string.isRequired, fallbackSrc: PropTypes.string } ),
-  // track original image src and current image src
-  withState( 'imgState', 'updateImgState', ( { src } ) => ({ origSrc: src, currentSrc: src }) ),
-  lifecycle( {
-    componentWillReceiveProps( nextProps ) {
-      // if this Image components src changes, make sure we update origSrc and currentSrc
-      if ( nextProps.src !== nextProps.imgState.origSrc ) {
-        nextProps.updateImgState( { origSrc: nextProps.src, currentSrc: nextProps.src } );
-      }
-    },
-  } ),
-  mapProps( ( { fallbackSrc, imgState, updateImgState, ...propsToPass } ) => {
-    return {
-      ...propsToPass,
-      src: imgState.currentSrc,
-      // on image load error:
-      // spread origSrc to remember it, and update new currentSrc to the fallback image
-      onError: () => {
-        if ( !fallbackSrc ) return;
-        updateImgState( s => ({ ...s, currentSrc: fallbackSrc }) );
-      }
+class Image extends PureComponent {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      origSrc: props.src,
+      currentSrc: props.src
     };
-  } )
-)
-export default class Image extends Component {
-  constructor( props ) {
-    super( props );
-    this.assignImageRef = this.assignImageRef.bind( this );
   }
 
   componentDidMount() {
     const { src } = this.props;
+
+    if (!this._image.complete) {
+      return;
+    }
+
     // if image hasn't completed loading, then let react handle error
-    if ( !this._image.complete ) return;
     /*
      * If an image has finished loading and has 'errored' (errored when naturalWidth === 0, or if svg check width)
      * Then run the onError callback
      * NOTE IF SVG: need to use width because firefox and IE assign naturalWidth from the svg elements width property,
      * but some svgs don't have that specified
      */
-    const isInvalidSvg = srcRepresentsSvgFile( src ) && !this._image.width;
+    const isInvalidSvg = srcRepresentsSvgFile(src) && !this._image.width;
     const imgFailedToLoad = this._image.naturalWidth === 0;
 
-    if ( isInvalidSvg || imgFailedToLoad ) {
-      this.props.onError();
+    if (isInvalidSvg || imgFailedToLoad) {
+      this.onError();
     }
   }
 
-  assignImageRef( r ) {
-    this._image = r;
-  }
+  assignImageRef = r => (this._image = r);
+
+  onError = () => {
+    const { fallbackSrc } = this.props;
+
+    if (!fallbackSrc) {
+      return;
+    }
+
+    this.setState(s => ({ ...s, currentSrc: fallbackSrc }));
+  };
 
   render() {
-    const { src, onError, ...propsToPass } = this.props;
+    const { alt, onError, fallbackSrc, ...propsToPass } = this.props;
+    const { currentSrc } = this.state;
 
-    return <img ref={this.assignImageRef} src={src} onError={onError} {...propsToPass} />;
+    return (
+      <img
+        {...propsToPass}
+        ref={this.assignImageRef}
+        src={currentSrc}
+        alt={alt}
+        onError={this.onError}
+      />
+    );
   }
+}
+
+Image.propTypes = {
+  src: PropTypes.string.isRequired,
+  fallbackSrc: PropTypes.string,
+  alt: PropTypes.string
 };
+
+Image.getDerivedStateFromProps = (props, state) => {
+  // if this Image components src changes, make sure we update origSrc and currentSrc
+  if (props.src !== state.origSrc) {
+    return {
+      origSrc: props.src,
+      currentSrc: props.src
+    };
+  }
+
+  return null;
+};
+
+export default Image;
