5f5d9c8dbf7e7f330d93bbd2b1d764f7e305dbb1
Kari Olafsson <kaoreolafsson@gmail.com>
2019-10-24 15:31:26 +0200

    feature(legacy): added generateCategorySet

---
+++ packages/legacy/tagsAndCustom/lib/utils/generateCategorySet.js
@@ -0,0 +1,65 @@
+'use strict';
+
+const _ = require('lodash');
+const ih = require('immutability-helper');
+const getMatchingIndex = require('./getMatchingIndex');
+const extractLabelString = require('./extractLabelString');
+
+const includeTypes = ['radio', 'select'];
+
+module.exports = (schema, currentCategorySet = null) => {
+
+  const field = _.first(schema.fields
+    .filter(f => includeTypes.includes(f.fieldType))
+    .filter(f => f.origin == 'categories'));
+
+  const messages = field && !field.origin ? [
+  `${field.field}: field origin is not set`
+  ] : [];
+
+  if (!field) {
+    return {
+      categorySet: null,
+      messages: ['no category set field was found']
+    }
+  }
+
+  return {
+    categorySet: {
+      name: extractLabelString(field.label),
+      required: !field.optional,
+      categories: _defineCategories(field.schemaId, _.get(currentCategorySet, 'categories', []), field.options)
+    },
+    messages
+  }
+}
+
+function _defineCategories(schemaId, currentCategories = [], options = []) {
+  return options.map(o => {
+    let matchingCategoryIndex = -1;
+
+    const label = extractLabelString(o.label);
+    const slug = o.value;
+    const schemaOptionId = `${schemaId}.${o.id}`;
+
+    matchingCategoryIndex = _.findIndex(currentCategories, { schemaOptionId });
+
+    if (matchingCategoryIndex === -1) {
+      matchingCategoryIndex = getMatchingIndex(currentCategories.map(c => c.label), o.label);
+    }
+
+    if (matchingCategoryIndex !== -1) {
+      return ih(currentCategories[matchingCategoryIndex], {
+        slug: { $set: slug },
+        label: { $set: label },
+        schemaOptionId: { $set: schemaOptionId }
+      });
+    }
+
+    return {
+      slug,
+      label,
+      schemaOptionId
+    };
+  });
+}
---
+++ packages/legacy/tagsAndCustom/test/04_utils_generateCategorySet.test.js
@@ -0,0 +1,239 @@
+"use strict";
+
+const should = require('should');
+
+const generateCategorySet = require('../lib/utils/generateCategorySet');
+
+describe('04 - utils - generateCategorySet', () => {
+
+  it('transforms a radio field into a category set', () => {
+    generateCategorySet({
+      fields: [{
+        field: 'nantes',
+        label: 'Nantes',
+        fieldType: 'radio',
+        schemaId: 1,
+        origin: 'categories',
+        options: [{
+          id: 1,
+          value: 'un',
+          label: 'Un'
+        }]
+      }]
+    }).categorySet.categories.length.should.equal(1);
+  });
+
+  it('does not consider field if origin is set to another value than "categories"', () => {
+    should(generateCategorySet({
+      fields: [{
+        field: 'nantes',
+        label: 'Nantes',
+        fieldType: 'radio',
+        schemaId: 1,
+        origin: 'custom',
+        options: [{
+          id: 1,
+          value: 'un',
+          lable: 'Un'
+        }]
+      }]
+    }).categorySet).equal(null);
+  });
+
+  it('a pre-existing category is completed with a schemaOptionId key', () => {
+    const schema = {
+      fields: [{
+        field: 'nantes',
+        label: { fr: 'Nantes', en: 'Nantes' },
+        fieldType: 'radio',
+        schemaId: 1,
+        origin: 'categories',
+        options: [{
+          id: 1,
+          value: 'un',
+          label: 'Un'
+        }]
+      }]
+    };
+
+    const categorySet = {
+      name: 'Nantes',
+      categories: [ {
+        id: 1,
+        value: 'un',
+        label: 'Un'
+      } ]
+    };
+
+    generateCategorySet(schema, categorySet).categorySet
+      .categories[0].schemaOptionId.should.equal('1.1');
+  });
+
+  it('a schemaOptionId is used for category match if set', () => {
+    const schema = {
+      fields: [{
+        field: 'nantes',
+        label: { fr: 'Nantes', en: 'Nantes' },
+        fieldType: 'radio',
+        schemaId: 1,
+        origin: 'categories',
+        options: [{
+          id: 1,
+          value: 'un',
+          label: 'Un'
+        }]
+      }]
+    };
+
+    const categorySet = {
+      name: 'Nantes',
+      categories: [{
+        id: 123,
+        schemaOptionId: '1.1',
+        value: 'douze',
+        label: 'Douze'
+      }]
+    };
+
+    generateCategorySet(schema, categorySet).categorySet
+      .categories[0].id.should.equal(123);
+  });
+
+  it('updates pre-existing category set', () => {
+    const schema = {
+      fields: [ {
+        origin: 'tags',
+        field: 'nantes',
+        label: 'Nantes',
+        fieldType: 'checkbox',
+        schemaId: 1,
+        options: [ {
+          id: 1,
+          value: 'un',
+          label: 'Un'
+        }, {
+          id: 2,
+          value: 'deux',
+          label: 'Deux'
+        } ]
+      }, {
+        origin: 'categories',
+        field: 'paris',
+        label: 'Paris',
+        fieldType: 'radio',
+        schemaId: 1,
+        options: [ {
+          id: 3,
+          value: 'trois',
+          label: 'Trois'
+        }, {
+          id: 4,
+          value: 'quatre',
+          label: 'Quatre'
+        } ]
+      } ]
+    };
+
+    const categorySet = {
+      name: 'Paris',
+      required: true,
+      unique: false,
+      categories: [ {
+        slug: 'trois',
+        label: 'Trois',
+        id: 192018
+      }, {
+        label: 'Cinq',
+        slug: 'cinq'
+      } ]
+    };
+
+    generateCategorySet(schema, categorySet).categorySet.should.eql({
+      "name": "Paris",
+      "required": true,
+      "categories": [{
+        "label": "Trois",
+        "slug": "trois",
+        "id": 192018, // maintain those, its important
+        "schemaOptionId": "1.3" // add those
+      }, {
+        "slug": "quatre",
+        "label": "Quatre",
+        "schemaOptionId": "1.4"
+      }]
+    });
+
+  });
+
+
+  it('takes a form schema and returns a matching category set', () => {
+
+    const schema = {
+      nextOptionId: 8,
+      "defaultLabelLanguage": null,
+      fields: [{
+        field: "style-musical",
+        label: {
+          "fr": "Style musical"
+        },
+        info: null,
+        placeholder : null,
+        sub : null,
+        help: null,
+        helpLink: null,
+        write: 'contributor',
+        read: null,
+        "optional": true,
+        display: true,
+        schemaId: 1,
+        "options": [{
+          "id": 5,
+          "value": "a-cappella",
+          "label": {
+            "fr": "Style musical : A cappella"
+          }
+        }, {
+          "id": 6,
+          "value": "afrique",
+          "label": {
+            "fr": "Style musical : Afrique"
+          }
+        }, {
+          "id": 7,
+          "value": "baroque",
+          "label": {
+            "fr": "Style musical : Baroque"
+          }
+        }],
+        "fieldType": "radio",
+        "origin" : "categories",
+        enableWith : null,
+        related: [],
+        default: null
+      }],
+      custom: null
+    };
+
+    generateCategorySet(schema).categorySet.should.eql({
+      "name": "Style musical",
+      "categories": [{
+        "label": "Style musical : A cappella",
+        "slug": "a-cappella",
+        "schemaOptionId" : "1.5"
+      }, {
+        "label": "Style musical : Afrique",
+        "slug": "afrique",
+        "schemaOptionId" : "1.6"
+      }, {
+        "label": "Style musical : Baroque",
+        "slug": "baroque",
+        "schemaOptionId" : "1.7"
+      }],
+      "required": false
+    });
+
+  } );
+
+} );
+
+
