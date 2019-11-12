5194c44ab87b9acd7f9bf508e30c4eb325adfd1d
Bertho <kevin.bertho@gmail.com>
2019-11-12 10:47:53 +0100

    babel-preset: add plugins from stages

--- packages/babel-preset/index.js
+++ packages/babel-preset/index.js
@@ -59,16 +59,31 @@ module.exports = declare((api, options) => {
         corejs
       }
     ],
-    require('@babel/plugin-syntax-dynamic-import'),
-    isWebpack ? null : require('babel-plugin-dynamic-import-node'),
-
-    require('@babel/plugin-proposal-object-rest-spread'),
 
     // Stage 0
     require('@babel/plugin-proposal-function-bind'),
 
     // Stage 1
     require('@babel/plugin-proposal-export-default-from'),
+    require('@babel/plugin-proposal-logical-assignment-operators'),
+    [
+      require('@babel/plugin-proposal-optional-chaining'),
+      {
+        loose: false
+      }
+    ],
+    [
+      require('@babel/plugin-proposal-pipeline-operator'),
+      {
+        proposal: 'minimal'
+      }
+    ],
+    [
+      require('@babel/plugin-proposal-nullish-coalescing-operator'),
+      {
+        loose: false
+      }
+    ],
     require('@babel/plugin-proposal-do-expressions'),
 
     // Stage 2
@@ -78,16 +93,22 @@ module.exports = declare((api, options) => {
         legacy: true
       }
     ],
+    require('@babel/plugin-proposal-function-sent'),
     require('@babel/plugin-proposal-export-namespace-from'),
+    require('@babel/plugin-proposal-numeric-separator'),
     require('@babel/plugin-proposal-throw-expressions'),
 
     // Stage 3
+    require('@babel/plugin-syntax-dynamic-import'),
+    isWebpack ? null : require('babel-plugin-dynamic-import-node'),
+    require('@babel/plugin-syntax-import-meta'),
     [
       require('@babel/plugin-proposal-class-properties'),
       {
         loose: true
       }
     ],
+    require('@babel/plugin-proposal-json-strings'),
 
     isWebpack && development ? require('react-hot-loader/babel') : null
   ]
