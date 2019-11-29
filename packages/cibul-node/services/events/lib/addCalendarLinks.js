c3892ca5477521cbc35ee3193d7e36ce92ffd3e2
Kari Olafsson <kaoreolafsson@gmail.com>
2019-11-29 16:12:08 +0100

    tweak(cibul-node): explicit error when absent timings at addCalendarLinks

--- packages/cibul-node/services/events/lib/addCalendarLinks.js
+++ packages/cibul-node/services/events/lib/addCalendarLinks.js
@@ -9,6 +9,10 @@ function _linkifyTime( time ) {
 }
 
 module.exports = function addCalendarLinks(event, eventUrl, agenda, lang) {
+  if (!event.timings) {
+    throw new Error('timings is not defined');
+  }
+
   event.timings.forEach( ( timing, i ) => {
     const eventTitle = getLocaleValue(event.title, lang)
     const eventDescription = getLocaleValue(event.description, lang)
