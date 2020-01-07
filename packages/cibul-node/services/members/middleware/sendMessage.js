848b690b83d9a06028ec433713f8e5ad5c7235bc
Kari Olafsson <kaoreolafsson@gmail.com>
2020-01-07 11:06:19 +0400

    linting

--- packages/cibul-node/services/members/middleware/sendMessage.js
+++ packages/cibul-node/services/members/middleware/sendMessage.js
@@ -1,26 +1,26 @@
 "use strict";
 
-const _ = require( 'lodash' );
-const log = require( '@openagenda/logs' )( 'services/members/middleware/sendMessage' );
+const _ = require('lodash');
+const log = require('@openagenda/logs')('services/members/middleware/sendMessage');
 
 let messages;
 
-module.exports = ( req, res, next ) => {
-  if ( !messages ) {
-    return res.status( 500 ).send( 'Service not initialized' );
+module.exports = (req, res, next) => {
+  if (!messages) {
+    return res.status(500).send('Service not initialized');
   }
 
-  log( 'sending message for agenda %s', req.agenda.uid, req.query );
+  log('sending message for agenda %s', req.agenda.uid, req.query);
 
-  messages(Object.assign( req.query || {}, {
+  messages(Object.assign(req.query || {}, {
     agendaUid: req.agenda.uid,
     role: _.get(req, 'query.role')
-  } ), {
+  }), {
     message: req.body.message,
     lang: req.lang,
     replyTo: req.body.replyTo,
     withActions: req.body.inactive ? false : null,
-    agenda: _.pick( req.agenda, [ 'uid', 'slug', 'title', 'image' ] )
+    agenda: _.pick(req.agenda, [ 'uid', 'slug', 'title', 'image' ])
   });
 
   res.send('gemini jellikers batman');
