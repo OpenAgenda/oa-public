467b678cd7b83716b5327b2214ba5d3744f2edcf
Kari Olafsson <kaoreolafsson@gmail.com>
2020-02-18 10:43:49 +0400

    linting

--- packages/cibul-node/services/formSchemas.js
+++ packages/cibul-node/services/formSchemas.js
@@ -1,18 +1,18 @@
 "use strict";
 
-const _ = require( 'lodash' );
-const formSchemas = require( '@openagenda/form-schemas' );
+const _ = require('lodash');
+const formSchemas = require('@openagenda/form-schemas');
 
 module.exports.init = config => {
-  formSchemas.init( {
+  formSchemas.init({
     knex: config.knex,
     tmpFolder: config.tmpFolderPath,
-    s3: _.pick( config.aws, [
+    s3: _.pick(config.aws, [
       'accessKeyId',
       'secretAccessKey',
       'region',
       'bucket'
-    ] ),
+    ]),
     schemas: {
       formSchema: 'form_schema',
       network: 'network'
@@ -21,7 +21,7 @@ module.exports.init = config => {
       knex: config.knex,
       schemas: config.schemas
     },
-    logger: config.getLogConfig( 'svc', 'form-schemas' )
+    logger: config.getLogConfig('svc', 'form-schemas')
   } );
 
   return formSchemas;
