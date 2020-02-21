0aced8d309e4200bc4981da19569265fb4c1dd04
Kari Olafsson <kaoreolafsson@gmail.com>
2020-02-21 11:39:03 +0100

    tweak(cibul-node): redo services/aggregators instantiation

--- packages/cibul-node/services/aggregators/index.js
+++ packages/cibul-node/services/aggregators/index.js
@@ -7,102 +7,94 @@ const log = require('@openagenda/logs')('services/aggregators');
 
 const plugApp = require('./plugApp');
 
-module.exports = {
-  notify: () => log('warn', 'aggregator instance is not initialized'),
-  addSource: () => log('warn', 'aggregator instance is not initialized'),
-  removeSource: () => log('warn', 'aggregator instance is not initialized'),
-  init: (config, services) => {
-    log('init');
-    const {
-      agendas,
-      tracker
-    } = services;
+module.exports.init = (config, services) => {
+  log('init');
+  const {
+    agendas,
+    tracker
+  } = services;
 
-    const aggregators = Aggregators({
-      knex: config.knex,
-      queues: services.queues,
-      logger: config.getLogConfig('svc', 'aggregators'),
-      interfaces: {
-        getMergedSchema: agendaUid => {
-          return services.core.agendas(agendaUid).settings.schema.getMerged()
-        },
-        updateSourcePaths: services.agendaEvents.utils.setSourcePaths,
-        referenceEvent: async (aggregatorAgendaUid, eventUid, data, { batched, paths, sourceAgenda }) => {
-          tracker('aggregators.referenceEvent');
-          try {
-            await services.core
-              .agendas(aggregatorAgendaUid)
-              .events.add(eventUid, data, {
-                aggregated: true,
-                paths,
-                sourceAgenda,
-                batched
-              });
-            tracker('aggregators.referenceEvent.done');
-            return {
-              success: true
-            }
-          } catch (e) {
-            log('error', 'could not add event %s from %s to aggregator %s',
-              eventUid,
-              sourceAgenda.uid,
-              aggregatorAgendaUid,
-              e.name === 'validationError' ? e.jse_info.errors : e
-            );
-            return {
-              success: false,
-              errors: e.name === 'validationError' ? e.jse_info.errors : e
-            };
+  const aggregators = Aggregators({
+    knex: config.knex,
+    queues: services.queues,
+    logger: config.getLogConfig('svc', 'aggregators'),
+    interfaces: {
+      getMergedSchema: agendaUid => {
+        return services.core.agendas(agendaUid).settings.schema.getMerged()
+      },
+      updateSourcePaths: services.agendaEvents.utils.setSourcePaths,
+      referenceEvent: async (aggregatorAgendaUid, eventUid, data, { batched, paths, sourceAgenda }) => {
+        tracker('aggregators.referenceEvent');
+        try {
+          await services.core
+            .agendas(aggregatorAgendaUid)
+            .events.add(eventUid, data, {
+              aggregated: true,
+              paths,
+              sourceAgenda,
+              batched
+            });
+          tracker('aggregators.referenceEvent.done');
+          return {
+            success: true
           }
-        },
-        unreferenceEvent: async (aggregatorAgendaUid, eventUid, { batched }) => {
-          try {
-            await services.core.agendas(aggregatorAgendaUid).events.remove(eventUid, { batched });
-            return {
-              success: true
-            }
-          } catch (e) {
-            log('error', 'could not remove event %s from aggregator %s',
-              eventUid,
-              aggregatorAgendaUid,
-              e.name === 'validationError' ? e.jse_info.errors : e
-            );
-            return {
-              success: false,
-              errors: e.name === 'validationError' ? e.jse_info.errors : e
-            };
+        } catch (e) {
+          log('error', 'could not add event %s from %s to aggregator %s',
+            eventUid,
+            sourceAgenda.uid,
+            aggregatorAgendaUid,
+            e.name === 'validationError' ? e.jse_info.errors : e
+          );
+          return {
+            success: false,
+            errors: e.name === 'validationError' ? e.jse_info.errors : e
+          };
+        }
+      },
+      unreferenceEvent: async (aggregatorAgendaUid, eventUid, { batched }) => {
+        try {
+          await services.core.agendas(aggregatorAgendaUid).events.remove(eventUid, { batched });
+          return {
+            success: true
           }
-        },
-        getEventReference: (agendaUid, eventUid) => services
-          .agendaEvents(agendaUid).get(eventUid)
-          .then(ae => ae ? {
-            sourcePaths: ae.sourceAgendaUid,
-            aggregated: ae.aggregated
-          } : null),
-        listEventReferences: (agendaUid, lastId, aggregated = null) => services.core.agendas(agendaUid)
-          .events.list({ state: 2, aggregated }, { lastId }, { load: { events: false, custom: false } }),
-        loadEvent: (agendaUid, eventUid) => services.core.agendas(agendaUid)
-          .events.get(eventUid, { detailed: true }),
-        getAgendasByUidsAndSearch: (agendaUids, search = null) => agendas.list({
-          uid: agendaUids,
-          ...(search ? { search } : {})
-        }, 0, 200, {
-          internal: true,
-          includeImagePath: true,
-          useDefaultImage: true
-        }).then(({ agendas }) => agendas.map(a =>
-          _.pick(a, ['id', 'uid', 'title', 'slug', 'image', 'official', 'createdAt', 'updatedAt'])
-        ))
-      }
+        } catch (e) {
+          log('error', 'could not remove event %s from aggregator %s',
+            eventUid,
+            aggregatorAgendaUid,
+            e.name === 'validationError' ? e.jse_info.errors : e
+          );
+          return {
+            success: false,
+            errors: e.name === 'validationError' ? e.jse_info.errors : e
+          };
+        }
+      },
+      getEventReference: (agendaUid, eventUid) => services
+        .agendaEvents(agendaUid).get(eventUid)
+        .then(ae => ae ? {
+          sourcePaths: ae.sourceAgendaUid,
+          aggregated: ae.aggregated
+        } : null),
+      listEventReferences: (agendaUid, lastId, aggregated = null) => services.core.agendas(agendaUid)
+        .events.list({ state: 2, aggregated }, { lastId }, { load: { events: false, custom: false } }),
+      loadEvent: (agendaUid, eventUid) => services.core.agendas(agendaUid)
+        .events.get(eventUid, { detailed: true }),
+      getAgendasByUidsAndSearch: (agendaUids, search = null) => agendas.list({
+        uid: agendaUids,
+        ...(search ? { search } : {})
+      }, 0, 200, {
+        internal: true,
+        includeImagePath: true,
+        useDefaultImage: true
+      }).then(({ agendas }) => agendas.map(a =>
+        _.pick(a, ['id', 'uid', 'title', 'slug', 'image', 'official', 'createdAt', 'updatedAt'])
+      ))
+    }
 
-    });
+  });
 
-    // for compatibility with legacy refs
-    Object.assign(module.exports, aggregators);
-
-    return Object.assign({
-      plugApp: plugApp.bind(null, config),
-      ...aggregators
-    });
-  }
+  return Object.assign({
+    plugApp: plugApp.bind(null, config),
+    ...aggregators
+  });
 }
