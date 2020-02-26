7ac4cb2c24f528a2347d90112e1137dac43df957
Kari Olafsson <kaoreolafsson@gmail.com>
2020-02-26 17:00:38 +0100

    tweak(labels): deactivated agenda

--- packages/labels/event/addEvent.js
+++ packages/labels/event/addEvent.js
@@ -4,7 +4,7 @@ module.exports = {
     "en": "Deactivated agenda"
   },
   "message": {
-    "fr": "Cet agenda est inactif depuis plus de 6 mois et son utilisation requiert une mise à jour de sa configuration.\n\nVous voulez y publier des événements ? Faites une demande au support et nous le réactiverons!",
+    "fr": "Bonjour,\ndans le cadre d'une évolution importante, nous avons temporairement désactivé votre agenda, qui figure parmi les \"anciens\" de la plateforme. Une opération manuelle est nécessaire si vous continuez d'utiliser cet agenda. Contactez-nous au support, cela sera rapide.",
     "en": "This agenda has not been active for more than 6 months and its continued use requires an update in its configuration. Contact us if you wish to continue using it, and we will reactivate it."
   },
   "support": {
@@ -12,3 +12,4 @@ module.exports = {
     "en": "Contact support"
   }
 }
+
