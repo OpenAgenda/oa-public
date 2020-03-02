5f4889b4dad54ee7036edb0f52230290e33d8484
Bertho <kevin.bertho@gmail.com>
2020-03-02 11:54:29 +0100

    sdk-js: lint

--- packages/sdk-js/src/index.js
+++ packages/sdk-js/src/index.js
@@ -6,13 +6,15 @@ import Locations from './Locations';
 import parseJsonResponse from './utils/parseJsonResponse';
 import reduceAccessToken from './utils/reduceAccessToken';
 
-
 export default class OaSdk {
-  constructor( options ) {
-    this.params = _.merge( {
-      publicKey: null,
-      secretKey: null
-    }, options );
+  constructor(options) {
+    this.params = _.merge(
+      {
+        publicKey: null,
+        secretKey: null
+      },
+      options
+    );
 
     this.accessToken = null;
     this.expiresIn = null;
@@ -20,24 +22,25 @@ export default class OaSdk {
     this.agent = superagent.agent();
   }
 
-  events = new Events( this );
-  locations = new Locations( this );
+  events = new Events(this);
 
-  async connect( secretKey ) {
+  locations = new Locations(this);
+
+  async connect(secretKey) {
     const time = new Date().getTime();
 
-    if ( !this.params.secretKey ) {
+    if (!this.params.secretKey) {
       this.params.secretKey = secretKey;
     }
 
     const response = await this.agent
-      .post( `${baseUrl.v2}/requestAccessToken` )
-      .accept( 'json' )
-      .send( {
+      .post(`${baseUrl.v2}/requestAccessToken`)
+      .accept('json')
+      .send({
         'grant-type': 'authorization_code',
         code: secretKey || this.params.secretKey
-      } )
-      .then( parseJsonResponse )
+      })
+      .then(parseJsonResponse);
 
     this.accessToken = response.body.access_token;
     this.expiresIn = response.body.expires_in;
@@ -49,22 +52,22 @@ export default class OaSdk {
   }
 
   async refreshToken() {
-    if ( this.tokenIsExpired() ) {
+    if (this.tokenIsExpired()) {
       await this.connect();
     }
   }
 
   tokenIsExpired() {
-    if ( !this.accessToken ) {
+    if (!this.accessToken) {
       return true;
     }
 
-    return new Date().getTime() > this.requestTokenTime + (this.expiresIn * 1000);
+    return new Date().getTime() > this.requestTokenTime + this.expiresIn * 1000;
   }
 
   getNonce() {
     const ms = new Date().getTime() - this.requestTokenTime;
 
-    return parseInt('' + _.random(Math.pow(10, 5)) + this.reducedAccessToken + ms, 10);
+    return parseInt(`${_.random(10 ** 5)}${this.reducedAccessToken}${ms}`, 10);
   }
 }
--- packages/sdk-js/test/OaSdk.test.js
+++ packages/sdk-js/test/OaSdk.test.js
@@ -4,98 +4,99 @@ import moment from 'moment';
 import OaSdk from '../src';
 import testconfig from '../testconfig';
 
-describe( 'connection', () => {
+describe('connection', () => {
   jest.setTimeout(10000);
 
-  it( 'simple connect', async () => {
-    const oa = new OaSdk( { secretKey: testconfig.secretKey } );
+  it('simple connect', async () => {
+    const oa = new OaSdk({ secretKey: testconfig.secretKey });
     await oa.connect();
 
-    expect( oa.accessToken ).toHaveLength( 32 );
-  } );
+    expect(oa.accessToken).toHaveLength(32);
+  });
 
-  it( 'simple connect - key provided on connect', async () => {
+  it('simple connect - key provided on connect', async () => {
     const oa = new OaSdk();
-    await oa.connect( testconfig.secretKey );
+    await oa.connect(testconfig.secretKey);
 
-    expect( oa.accessToken ).toHaveLength( 32 );
-  } );
+    expect(oa.accessToken).toHaveLength(32);
+  });
 
-  it( 'fail connection', async () => {
-    const oa = new OaSdk();
-
-    await expect(
-      oa.connect( 'inexistant' )
-    ).rejects.toThrow( 'Forbidden' );
-  } );
-} );
-
-describe( 'refresh expired token', () => {
-  it( 'refresh token if needed', async () => {
+  it('fail connection', async () => {
     const oa = new OaSdk();
 
-    const spy = sinon.spy( oa, 'connect' );
+    await expect(oa.connect('inexistant')).rejects.toThrow('Forbidden');
+  });
+});
 
-    await oa.connect( testconfig.secretKey );
+describe('refresh expired token', () => {
+  it('refresh token if needed', async () => {
+    const oa = new OaSdk();
 
-    const clock = sinon.useFakeTimers( Date.now() );
+    const spy = sinon.spy(oa, 'connect');
 
-    await oa.events.get( 12345678 );
+    await oa.connect(testconfig.secretKey);
 
-    expect( spy.callCount ).toBe( 1 );
+    const clock = sinon.useFakeTimers(Date.now());
 
-    clock.tick( oa.expiresIn * 1000 );
+    await oa.events.get(12345678);
 
-    await oa.events.get( 12345678 );
+    expect(spy.callCount).toBe(1);
 
-    expect( spy.callCount ).toBe( 2 );
+    clock.tick(oa.expiresIn * 1000);
+
+    await oa.events.get(12345678);
+
+    expect(spy.callCount).toBe(2);
 
     clock.restore();
-  } );
-} );
+  });
+});
 
-describe.skip( 'locations', () => {
-  it( 'create a location', async () => {
-    const oa = new OaSdk( { secretKey: testconfig.secretKey } );
+describe.skip('locations', () => {
+  it('create a location', async () => {
+    const oa = new OaSdk({ secretKey: testconfig.secretKey });
     await oa.connect();
 
-    const location = await oa.locations.create( testconfig.agendaUid, {
+    const location = await oa.locations.create(testconfig.agendaUid, {
       name: 'Gare Meuse TGV',
       address: 'Lieu dit Le Cugnet, 55220 Les Trois-Domaines',
       latitude: 48.9736458,
       longitude: 5.2723537
-    } );
+    });
 
-    expect( location ).toMatchObject({
+    expect(location).toMatchObject({
       uid: expect.any(Number)
-    })
-  } );
+    });
+  });
 
-  it( 'fails to create a location', async () => {
-    const oa = new OaSdk( { secretKey: testconfig.secretKey } );
+  it('fails to create a location', async () => {
+    const oa = new OaSdk({ secretKey: testconfig.secretKey });
     await oa.connect();
 
-    try {
-      await oa.locations.create( testconfig.agendaUid, {
+    await expect(
+      oa.locations.create(testconfig.agendaUid, {
         name: 'Gare Meuse TGV',
         address: 'Lieu dit Le Cugnet, 55220 Les Trois-Domaines'
-      } );
-    } catch ( e ) {
-      expect( e.response.body ).toEqual( {
-        error: 'invalid_request',
-        error_description: 'latitude: Latitude is required, longitude: Longitude is required'
-      } );
-    }
-  } );
-} );
+      })
+    ).rejects.toMatchObject({
+      response: {
+        body: {
+          error: 'invalid_request',
+          error_description:
+            'latitude: Latitude is required, longitude: Longitude is required'
+        }
+      }
+    });
+  });
+});
 
-describe( 'events', () => {
-  it( 'create & delete an event', async () => {
-    const oa = new OaSdk( { secretKey: testconfig.secretKey } );
+describe('events', () => {
+  it('create & delete an event', async () => {
+    const oa = new OaSdk({ secretKey: testconfig.secretKey });
     await oa.connect();
 
-    const { success, event } = await oa.events.create( testconfig.agendaUid, {
-      slug: 'a-title-' + _.random( Math.pow( 10, 6 ) ),
+    const { success, event } = await oa.events.create(testconfig.agendaUid, {
+      slug: `a-title-${_.random(10 ** 6)}`,
       title: {
         fr: 'Un titre',
         en: 'A title'
@@ -105,26 +106,31 @@ describe( 'events', () => {
         en: 'We make a truc'
       },
       locationUid: 78372099,
-      timings: [ {
-        begin: moment(),
-        end: moment().add( 1, 'hour' )
-      }, {
-        begin: moment().add( 1, 'day' ),
-        end: moment().add( 1, 'day' ).add( 1, 'hour' )
-      } ]
-    } );
+      timings: [
+        {
+          begin: moment(),
+          end: moment().add(1, 'hour')
+        },
+        {
+          begin: moment().add(1, 'day'),
+          end: moment()
+            .add(1, 'day')
+            .add(1, 'hour')
+        }
+      ]
+    });
 
     expect(success).toBeTruthy();
     expect(typeof event.uid).toBe('number');
     expect(event.title.fr).toBe('Un titre');
-  } );
+  });
 
-  it( 'create & delete an event - with keywords', async () => {
-    const oa = new OaSdk( { secretKey: testconfig.secretKey } );
+  it('create & delete an event - with keywords', async () => {
+    const oa = new OaSdk({ secretKey: testconfig.secretKey });
     await oa.connect();
 
-    const { success, event } = await oa.events.create( testconfig.agendaUid, {
-      slug: 'a-title-' + _.random( Math.pow( 10, 6 ) ),
+    const { success, event } = await oa.events.create(testconfig.agendaUid, {
+      slug: `a-title-${_.random(10 ** 6)}`,
       title: {
         fr: 'Un titre',
         en: 'A title'
@@ -134,79 +140,136 @@ describe( 'events', () => {
         en: 'We make a truc'
       },
       locationUid: 78372099,
-      timings: [ {
-        begin: moment(),
-        end: moment().add( 1, 'hour' )
-      }, {
-        begin: moment().add( 1, 'day' ),
-        end: moment().add( 1, 'day' ).add( 1, 'hour' )
-      } ],
+      timings: [
+        {
+          begin: moment(),
+          end: moment().add(1, 'hour')
+        },
+        {
+          begin: moment().add(1, 'day'),
+          end: moment()
+            .add(1, 'day')
+            .add(1, 'hour')
+        }
+      ],
       keywords: {
-        fr: [ 'Toulouse', 'Toulouse Centre', 'Culture', 'Exposition', 'Tout public' ]
+        fr: [
+          'Toulouse',
+          'Toulouse Centre',
+          'Culture',
+          'Exposition',
+          'Tout public'
+        ]
       }
-    } );
+    });
 
-    expect( success ).toBeTruthy();
-    expect( typeof event.uid ).toBe( 'number' );
-    expect( event.title.fr ).toBe( 'Un titre' );
+    expect(success).toBeTruthy();
+    expect(typeof event.uid).toBe('number');
+    expect(event.title.fr).toBe('Un titre');
 
-    await oa.events.delete( testconfig.agendaUid, event.uid );
-  } );
+    await oa.events.delete(testconfig.agendaUid, event.uid);
+  });
 
-  it( 'fails to create an event', async () => {
-    const oa = new OaSdk( { secretKey: testconfig.secretKey } );
+  it('fails to create an event', async () => {
+    const oa = new OaSdk({ secretKey: testconfig.secretKey });
     await oa.connect();
 
-    try {
-      await oa.events.create( testconfig.agendaUid, {
-        slug: 'a-title-' + _.random( Math.pow( 10, 6 ) ),
+    await expect(
+      oa.events.create(testconfig.agendaUid, {
+        slug: `a-title-${_.random(10 ** 6)}`,
         description: {
           fr: 'On va faire un truc',
           en: 'We make a truc'
         },
         locationUid: 78372099,
-        timings: [ {
-          begin: moment(),
-          end: moment().add( 1, 'hour' )
-        }, {
-          begin: moment().add( 1, 'day' ),
-          end: moment().add( 1, 'day' ).add( 1, 'hour' )
-        } ]
-      } );
-    } catch ( e ) {
-      expect( e.response.body ).toEqual( {
-        errors: [
+        timings: [
           {
-            code: 'required',
-            field: 'title',
-            lang: 'fr',
-            message: 'a string is required',
-            origin: '',
-            step: 'validation'
+            begin: moment(),
+            end: moment().add(1, 'hour')
           },
           {
-            code: 'required',
-            field: 'title',
-            lang: 'en',
-            message: 'a string is required',
-            origin: '',
-            step: 'validation'
+            begin: moment().add(1, 'day'),
+            end: moment()
+              .add(1, 'day')
+              .add(1, 'hour')
           }
         ]
-      } );
-    }
-  } );
+      })
+    ).rejects.toMatchObject({
+      response: {
+        body: {
+          errors: [
+            {
+              code: 'required',
+              field: 'title',
+              lang: 'fr',
+              message: 'a string is required',
+              origin: '',
+              step: 'validation'
+            },
+            {
+              code: 'required',
+              field: 'title',
+              lang: 'en',
+              message: 'a string is required',
+              origin: '',
+              step: 'validation'
+            }
+          ]
+        }
+      }
+    });
+  });
 
-  it( 'get an event', async () => {
-    const oa = new OaSdk( {
+  it('get an event', async () => {
+    const oa = new OaSdk({
       publicKey: testconfig.publicKey,
       secretKey: testconfig.secretKey
-    } );
+    });
 
     await oa.connect();
 
-    const { event: createdEvent } = await oa.events.create( testconfig.agendaUid, {
-      slug: 'a-title-' + _.random( Math.pow( 10, 6 ) ),
+    const { event: createdEvent } = await oa.events.create(
+      testconfig.agendaUid,
+      {
+        slug: `a-title-${_.random(10 ** 6)}`,
+        title: {
+          fr: 'Un titre',
+          en: 'A title'
+        },
+        description: {
+          fr: 'On va faire un truc',
+          en: 'We make a truc'
+        },
+        locationUid: 78372099,
+        timings: [
+          {
+            begin: moment(),
+            end: moment().add(1, 'hour')
+          },
+          {
+            begin: moment().add(1, 'day'),
+            end: moment()
+              .add(1, 'day')
+              .add(1, 'hour')
+          }
+        ]
+      }
+    );
+
+    const event = await oa.events.get(createdEvent.uid);
+
+    expect(parseInt(event.uid, 10)).toBe(createdEvent.uid);
+
+    await oa.events.delete(testconfig.agendaUid, createdEvent.uid);
+  });
+
+  it('update an event', async () => {
+    const oa = new OaSdk({ secretKey: testconfig.secretKey });
+    await oa.connect();
+
+    const { success, event } = await oa.events.create(testconfig.agendaUid, {
+      slug: `a-title-${_.random(10 ** 6)}`,
       title: {
         fr: 'Un titre',
         en: 'A title'
@@ -216,60 +279,38 @@ describe( 'events', () => {
         en: 'We make a truc'
       },
       locationUid: 78372099,
-      timings: [ {
-        begin: moment(),
-        end: moment().add( 1, 'hour' )
-      }, {
-        begin: moment().add( 1, 'day' ),
-        end: moment().add( 1, 'day' ).add( 1, 'hour' )
-      } ]
-    } );
+      timings: [
+        {
+          begin: moment(),
+          end: moment().add(1, 'hour')
+        },
+        {
+          begin: moment().add(1, 'day'),
+          end: moment()
+            .add(1, 'day')
+            .add(1, 'hour')
+        }
+      ]
+    });
 
-    const event = await oa.events.get( createdEvent.uid );
+    const { event: updatedEvent } = await oa.events.update(
+      testconfig.agendaUid,
+      event.uid,
+      {
+        slug: event.slug,
+        title: {
+          fr: 'Titre mise à jour',
+          en: 'Updated title'
+        },
+        timings: event.timings
+      }
+    );
 
-    expect( parseInt( event.uid, 10 ) ).toBe( createdEvent.uid );
+    expect(success).toBeTruthy();
+    expect(typeof event.uid).toBe('number');
+    expect(event.title.fr).toBe('Un titre');
+    expect(updatedEvent.title.fr).toBe('Titre mise à jour');
 
-    await oa.events.delete( testconfig.agendaUid, createdEvent.uid );
-  } );
-
-  it( 'update an event', async () => {
-    const oa = new OaSdk( { secretKey: testconfig.secretKey } );
-    await oa.connect();
-
-    const { success, event } = await oa.events.create( testconfig.agendaUid, {
-      slug: 'a-title-' + _.random( Math.pow( 10, 6 ) ),
-      title: {
-        fr: 'Un titre',
-        en: 'A title'
-      },
-      description: {
-        fr: 'On va faire un truc',
-        en: 'We make a truc'
-      },
-      locationUid: 78372099,
-      timings: [ {
-        begin: moment(),
-        end: moment().add( 1, 'hour' )
-      }, {
-        begin: moment().add( 1, 'day' ),
-        end: moment().add( 1, 'day' ).add( 1, 'hour' )
-      } ]
-    } );
-
-    const { event: updatedEvent } = await oa.events.update( testconfig.agendaUid, event.uid, {
-      slug: event.slug,
-      title: {
-        fr: 'Titre mise à jour',
-        en: 'Updated title'
-      },
-      timings: event.timings
-    } );
-
-    expect( success ).toBeTruthy();
-    expect( typeof event.uid ).toBe('number');
-    expect( event.title.fr ).toBe( 'Un titre' );
-    expect( updatedEvent.title.fr ).toBe( 'Titre mise à jour' );
-
-    await oa.events.delete( testconfig.agendaUid, event.uid );
-  } );
-} );
+    await oa.events.delete(testconfig.agendaUid, event.uid);
+  });
+});
