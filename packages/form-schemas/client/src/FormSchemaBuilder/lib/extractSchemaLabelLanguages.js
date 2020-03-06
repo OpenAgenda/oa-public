d331a49498712f36367eb2c630685a9d5ee2c383
Kari Olafsson <kaoreolafsson@gmail.com>
2020-03-06 10:44:10 +0100

    fix(form-schemas): extractSchemaLanguageLabels cannot handle nulls

--- packages/form-schemas/client/src/FormSchemaBuilder/lib/extractSchemaLabelLanguages.js
+++ packages/form-schemas/client/src/FormSchemaBuilder/lib/extractSchemaLabelLanguages.js
@@ -3,7 +3,7 @@ import _ from 'lodash';
 export default schema => _.uniq( _.flatten(
   _.get( schema, 'fields', [] ).map( f => [ 'label', 'info', 'placeholder' ].reduce(
     (fieldLanguages, label) => _.uniq(
-      fieldLanguages.concat(_.isString(f[label]) ? [] : _.keys(f[label]).filter(key => f[label][key].length))
+      fieldLanguages.concat(_.isString(f[label]) ? [] : _.keys(f[label]).filter(key => (f[label][key] || '').length))
     ), []
   ) )
 ) )
