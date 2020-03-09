b40aa2f3e2845aa48cf5007bb39be15ab452f85a
Bertho <kevin.bertho@gmail.com>
2020-03-09 18:17:08 +0100

    aggregators: check deactivated_until column in getSourceAndAggregatorPairs

--- packages/aggregators/utils/getSourceAndAggregatorPairs.js
+++ packages/aggregators/utils/getSourceAndAggregatorPairs.js
@@ -10,12 +10,18 @@ module.exports = (knex, { id }) => knex('aggregator as ag')
     'ags.store as sourceStore',
     'ag.id as aggregatorId',
     'ag.store as aggStore'
-  ]).leftJoin('aggregator_source as ags', 'ag.id', 'ags.aggregator_id')
-    .leftJoin('review as r', 'ag.review_id', 'r.id')
-    .where('ags.review_id', id)
-    // .where('version', 2) -> not required as v1 is discontinued
-    .then(pairs => pairs.map(p => ({
-      agendaUid: p.agendaUid,
-      aggregatorRules: extractRules('aggregator', p.aggregatorId, p.aggStore),
-      sourceRules: extractRules('source', p.sourceId, p.sourceStore)
-    })));
+  ])
+  .leftJoin('aggregator_source as ags', 'ag.id', 'ags.aggregator_id')
+  .leftJoin('review as r', 'ag.review_id', 'r.id')
+  .where('ags.review_id', id)
+  .andWhere(
+    builder => builder
+      .whereNull('ag.deactivated_until')
+      .orWhere('ag.deactivated_until', '<', new Date(new Date().setHours(0, 0, 0, 0)))
+  )
+  // .where('version', 2) -> not required as v1 is discontinued
+  .then(pairs => pairs.map(p => ({
+    agendaUid: p.agendaUid,
+    aggregatorRules: extractRules('aggregator', p.aggregatorId, p.aggStore),
+    sourceRules: extractRules('source', p.sourceId, p.sourceStore)
+  })));
