00c3209b1634ef778effa735827775c5355d1c03
Kari Olafsson <kaoreolafsson@gmail.com>
2020-04-03 18:01:56 +0200

    style(images): removed a bunch of spaces

--- packages/images/test/10_download.js
+++ packages/images/test/10_download.js
@@ -2,58 +2,58 @@
 
 process.env.NODE_ENV = 'test';
 
-const _ = require( 'lodash' );
-const async = require( 'async' );
-const fs = require( 'fs' );
-const should = require( 'should' );
+const _ = require('lodash');
+const async = require('async');
+const fs = require('fs');
+const should = require('should');
 
 const fakeImage = 'https://s3-eu-west-1.amazonaws.com/cibulstatic/notanimage.jpg';
-const gm = require( 'gm' ).subClass( { imageMagick: true } );
+const gm = require('gm').subClass({ imageMagick: true });
 const imageSrc = 'https://s3-eu-west-1.amazonaws.com/cibulstatic/Galaxie-ESO-137-001.jpg';
-const imageSvc = require( '../' );
-const testconfig = require( '../testconfig.js' );
+const imageSvc = require('../');
+const testconfig = require('../testconfig.js');
 const tmpTestFile = '/var/tmp/testfile';
-const unlinkTestFiles = require( './helpers/unlinkTestFiles' );
+const unlinkTestFiles = require('./helpers/unlinkTestFiles');
 
 
-describe( 'images - unit (server): download', function() {
+describe('images - unit (server): download', function() {
 
-  beforeEach( () => {
+  beforeEach(() => {
 
-    imageSvc.init( testconfig );
+    imageSvc.init(testconfig);
 
-  } );
+  });
 
-  afterEach( () => unlinkTestFiles( __dirname ) );
+  afterEach(() => unlinkTestFiles(__dirname));
 
-  this.timeout( 20000 );
+  this.timeout(20000);
 
-  it( 'should send timeout error', function( done ) {
+  it('should send timeout error', function(done) {
 
-    imageSvc.init( _.extend( {}, testconfig, { timeout: 1 } ) );
+    imageSvc.init(_.extend({}, testconfig, { timeout: 1 }));
 
-    imageSvc.test._download( { url: imageSrc } ).done( null, function( err ) {
+    imageSvc.test._download({ url: imageSrc }).done(null, function(err) {
 
-      err.should.equal( 'timeout' );
+      err.should.equal('timeout');
 
       done();
 
     });
 
-  } );
+  });
 
 
-  it( 'should send max size error', done => {
+  it('should send max size error', done => {
 
-    imageSvc.init( _.extend( {}, testconfig, { maxSize: 1 } ) );
+    imageSvc.init(_.extend({}, testconfig, { maxSize: 1 }));
 
-    imageSvc.test._download( { url: imageSrc } ).done( null, err => {
+    imageSvc.test._download({ url: imageSrc }).done(null, err => {
 
-      err.should.eql( {
+      err.should.eql({
         code: 'image.toobig',
         max: 1,
         message: 'maximum size exceeded'
-      } );
+      });
 
       done();
 
@@ -61,40 +61,19 @@ describe( 'images - unit (server): download', function() {
 
   });
 
-  it( 'should send error with wrong status code', done => {
+  it('should send error with wrong status code', done => {
 
-    imageSvc.init( testconfig );
+    imageSvc.init(testconfig);
 
-    imageSvc.test._download( {
+    imageSvc.test._download({
       url: 'https://s3.eu-central-1.amazonaws.com/openagendatest/myevents.jpg'
-    } ).done( null, err => {
+    }).done(null, err => {
 
-      err.should.eql( {
+      err.should.eql({
         code: 'invalid.status',
         message: 'invalid status code',
         statusCode: 403
-      } );
-
-      done();
-
-    } );
-
-  } );
-
-
-  it( 'should load image content', function( done ) {
-
-    this.timeout( 10000 );
-
-    imageSvc.test._download( { url: imageSrc } ).done( function( values ) {
-
-      var stats = fs.statSync( values.path );
-
-      stats.size.should.match(function(n) {
-        return [ 260760, 270573 ].indexOf( n ) !== -1
       });
-
-      fs.unlinkSync( values.path );
 
       done();
 
@@ -102,4 +81,25 @@ describe( 'images - unit (server): download', function() {
 
   });
 
-} );
+
+  it('should load image content', function(done) {
+
+    this.timeout(10000);
+
+    imageSvc.test._download({ url: imageSrc }).done(function(values) {
+
+      var stats = fs.statSync(values.path);
+
+      stats.size.should.match(function(n) {
+        return [ 260760, 270573 ].indexOf(n) !== -1
+      });
+
+      fs.unlinkSync(values.path);
+
+      done();
+
+    });
+
+  });
+
+});
