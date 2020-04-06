8491e2fa0b7a5f80ec30f5d85be17c2a0597760e
Bertho <kevin.bertho@gmail.com>
2020-04-06 14:04:44 +0200

    chore: disable check of commit message length

--- commitlint.config.js
+++ commitlint.config.js
@@ -4,5 +4,8 @@ module.exports = {
   extends: [
     '@commitlint/config-conventional',
     '@commitlint/config-lerna-scopes'
-  ]
+  ],
+  rules: {
+    'header-max-length': [0, 'always', 72]
+  }
 };
