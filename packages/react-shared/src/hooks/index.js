fbac36f093b5bd17b6baee443042d4a32e58b0b2
Bertho <kevin.bertho@gmail.com>
2020-04-28 16:40:14 +0200

    feat(react-shared): add ApiClientContext and useApiClient

--- packages/react-shared/src/hooks/index.js
+++ packages/react-shared/src/hooks/index.js
@@ -1,3 +1,4 @@
+export { default as useApiClient } from './useApiClient';
 export { default as useConstant } from './useConstant';
 export * from './useMemoOne';
 export { default as useModal } from './useModal';
