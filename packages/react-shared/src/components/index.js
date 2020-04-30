a3c710e8a6971b6260227f6453e23bfdd258af69
Bertho <kevin.bertho@gmail.com>
2020-04-30 19:02:13 +0200

    feat(react-shared): add Switch and Case

--- packages/react-shared/src/components/index.js
+++ packages/react-shared/src/components/index.js
@@ -1,2 +1,4 @@
+export { default as Case } from './Case';
 export { default as ReactSelectField } from './ReactSelectField';
 export { default as ReactSelectInput } from './ReactSelectInput';
+export { default as Switch } from './Switch';
