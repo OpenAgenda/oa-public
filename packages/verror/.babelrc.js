e1919664557c58ec9c545cbf5ada54bd3129d3d4
Bertho <kevin.bertho@gmail.com>
2020-05-06 18:40:19 +0200

    feat(verror): disable transform runtime

--- packages/verror/.babelrc.js
+++ packages/verror/.babelrc.js
@@ -1,6 +1,13 @@
 'use strict';
 
 module.exports = {
-  presets: [require.resolve('@openagenda/babel-preset')],
+  presets: [
+    [
+      require.resolve('@openagenda/babel-preset'),
+      {
+        transformRuntime: false
+      }
+    ]
+  ],
   sourceType: 'unambiguous'
 };
