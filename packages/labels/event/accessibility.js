eea33ff446c2fa1d20ae7cfde613f1ef566e8c43
Kari Olafsson <kaoreolafsson@gmail.com>
2020-05-14 19:00:24 +0200

    feat(labels): accessibility labels merge

--- packages/labels/event/accessibility.js
+++ packages/labels/event/accessibility.js
@@ -1,51 +1,11 @@
 "use strict";
 
 module.exports = {
-  "motorImpairment": {
-    "en": "motor impairment",
-    "fr": "handicap moteur",
-    "de": "motorische Beeinträchtigung",
-    "es": "discapacidad motora",
-    "br": "dalc'het en o c'heflusk",
-    "it": "handicap fisico"
-  },
-  "hearingImpairment": {
-    "en": "hearing impairment",
-    "fr": "handicap auditif",
-    "de": "schwerhörig",
-    "es": "discapacidad auditiva",
-    "br": "pounnerglev",
-    "it": "problemi di udito"
-  },
-  "mentalImpairment": {
-    "en": "mental impairment",
-    "fr": "handicap psychique",
-    "de": "geistige Behinderung",
-    "es": "discapacidad mental",
-    "br": "dalc'het en o spered",
-    "it": "disabilità psichica"
-  },
-  "visualImpairment": {
-    "en": "visual impairment",
-    "fr": "handicap visuel",
-    "de": "Sehbehinderung",
-    "es": "discapacitados visuales",
-    "br": "na wel ket mat",
-    "it": "deficit visivo"
-  },
-  "signLanguage": {
-    "en": "Sign language",
-    "fr": "Langue des signes",
-    "de": "Zeichensprache",
-    "es": "Lenguaje de Señas",
-    "br": "Yezh ar sinoù",
-    "it": "Lingua dei segni"
-  },
   "mi": {
     "en": "Motor impairment",
     "fr": "Handicap moteur",
-    "de": "motorische Beeinträchtigung",
-    "es": "Discapacidad física",
+    "de": "Motorische beeinträchtigung",
+    "es": "Movilidad reducida",
     "br": "Dalc'het en o c'heflusk",
     "it": "Incapacità motoria"
   },
@@ -53,23 +13,30 @@ module.exports = {
     "en": "Hearing impairment",
     "fr": "Handicap auditif",
     "de": "Schwerhörig",
-    "es": "la discapacidad auditiva",
+    "es": "Discapacidad auditiva",
     "br": "Pounnerglev",
     "it": "Disabilità uditiva"
   },
-  "pi": {
+  "mei": {
     "en": "Mental impairment",
-    "fr": "Handicap psychique",
+    "fr": "Handicap mental",
     "de": "Geistige Behinderung",
-    "es": "discapacidad mental",
+    "es": "Discapacidad mental",
     "br": "Dalc'het en o spered",
-    "it": "Invalidità psichica"
+    "it": "Disabilità mental"
+  },
+  "pi": {
+    "en": "Psychic impairment",
+    "fr": "Handicap psychique",
+    "de": "Psychische behinderung",
+    "es": "Discapacidad intelectual",
+    "it": "Disabilità psichica"
   },
   "vi": {
     "en": "Visual impairment",
     "fr": "Handicap visuel",
     "de": "Sehbehinderung",
-    "es": "discapacidad visual",
+    "es": "Discapacidad visual",
     "br": "Na wel ket mat",
     "it": "Invalidità visiva"
   },
