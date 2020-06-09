753ae7164ba70d324f8b06429c4f35b5b5fbc7f2
Kari Olafsson <kaoreolafsson@gmail.com>
2020-06-09 11:19:21 +0200

    style(agenda-search): linting

--- packages/agenda-search/test/app/listInterface.js
+++ packages/agenda-search/test/app/listInterface.js
@@ -1,29 +1,25 @@
 "use strict";
 
-const _ = require( 'lodash' );
+const _ = require('lodash');
 
-const randomAgenda = require( './randomAgenda' );
+const randomAgenda = require('./randomAgenda');
 
-module.exports = ( total, query, offset, limit, { detailed } ) => {
+module.exports = (total, query, offset, limit, { detailed }) => {
 
-  const updatedAtGreaterThan = _.get( query, 'updatedAtGreaterThan' );
+  const updatedAtGreaterThan = _.get(query, 'updatedAtGreaterThan');
 
-  return new Promise( rs => {
-
+  return new Promise(rs => {
     const randomAgendaUids = [];
 
-    for( let i = 0; i < limit; i++ ) {
+    for(let i = 0; i < limit; i++) {
+      if (offset + i > total) continue;
 
-      if ( offset + i > total ) continue;
-
-      randomAgendaUids.push( Math.floor( Math.random() * 1000000 ) );
-
+      randomAgendaUids.push(Math.floor(Math.random() * 1000000));
     }
 
-    const agendas = randomAgendaUids.map( uid => randomAgenda( uid, detailed ) );
+    const agendas = randomAgendaUids.map(uid => randomAgenda(uid, detailed));
 
-    rs( agendas.filter( a => !updatedAtGreaterThan || ( ( new Date( updatedAtGreaterThan ) ) < a.updatedAt ) ) );
-
-  } );
+    rs(agendas.filter(a => !updatedAtGreaterThan || ((new Date(updatedAtGreaterThan)) < a.updatedAt)));
+  });
 
 }
