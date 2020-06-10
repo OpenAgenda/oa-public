5fc8913d3b328cee4e517914a189edfd4b88a812
Kari Olafsson <kaoreolafsson@gmail.com>
2020-06-10 10:52:43 +0200

    refactor(agenda-search): tests

--- packages/agenda-search/test/01_searches.js
+++ packages/agenda-search/test/01_searches.js
@@ -56,6 +56,14 @@ describe('search', function() {
       items[0].title.should.equal('Au Théâtre ce soir');
     });
 
+    it('Singular can provide plural', async () => {
+      const { items } = await svc({
+        search: 'musée'
+      });
+
+      items.length.should.equal(3);
+    });
+
     it('With accents but unspecified in search', async () => {
       const { items } = await svc({
         search: 'Theatre'
@@ -74,6 +82,34 @@ describe('search', function() {
       } = await svc.list({ search: 'mcc' }, 0, 10);
 
       items[0].title.should.equal('Journées Européennes du Patrimoine');
+    });
+
+  });
+
+  describe('Sorting', () => {
+
+    it('An agenda with upcoming events is prioritized for a given search', async () => {
+      const {
+        items
+      } = await svc({ search: 'musées' }, 0, 10);
+
+      items.map(a => a.title).should.eql([
+        'Nuit européenne des musées 2020 : Île-de-France',
+        'Nuit européenne des musées 2018 : Île-de-France',
+        'Nuit européenne des musées 2019 : Île-de-France'
+      ]);
+    });
+
+    it('Official agendas are prioritized in a search', async () => {
+      const {
+        items
+      } = await svc({ search: 'Rendez-vous aux jardins' }, 0, 10);
+
+      items.map(i => i.title).should.eql([
+        'Rendez-vous aux jardins : Pays de la Loire qui va bien',
+        'Rendez-vous aux jardins : Pays de la Loire qui ne va pas',
+        'Rendez-vous aux jardins'
+      ]);
     });
 
   });
