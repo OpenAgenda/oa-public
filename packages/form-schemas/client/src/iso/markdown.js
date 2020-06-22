a67811170820ff2818ac9b23b6de69072ab26f31
Kari Olafsson <kaoreolafsson@gmail.com>
2020-06-22 12:52:13 +0200

    fix(form-schemas): markdown editor does not load direct text links with underscores properly

--- packages/form-schemas/client/src/iso/markdown.js
+++ packages/form-schemas/client/src/iso/markdown.js
@@ -8,6 +8,31 @@ const TurndownService = turndown.default || turndown;
 
 const ts = new TurndownService();
 
+marked.use({
+  tokenizer: {
+    link(src) {
+      if (src.length === 1) {
+        // marked bug: a \ screws output, adds last character of given input
+        return {
+          type: 'text',
+          raw: ' ',
+          text: ''
+        }
+      } else if (src.indexOf('\\_') !== -1) {
+        const clean = src.replace('\\_', '_');
+        return {
+          type: 'link',
+          raw: clean,
+          href: clean,
+          text: clean
+        }
+      }
+
+      return false;
+    }
+  }
+});
+
 module.exports = {
   to: html => ts.turndown(html || ''),
   from: md => sanitizeHtml(marked(md || ''), {
