11d89e6558b3064611187cd902a0382d1f0efe1a
Kari Olafsson <kaoreolafsson@gmail.com>
2020-06-23 10:19:30 +0200

    feat(event-search): provide validation error details on invalid aggregation params formatting

--- packages/event-search/aggregations/index.js
+++ packages/event-search/aggregations/index.js
@@ -21,23 +21,74 @@ const aggregationTypes = {
   updatedAt: timestamp('_exclusiveUpdatedAt')
 }
 
+class BadRequest extends Error {
+  constructor(message, detail) {
+    super(message);
+    this.name = 'BadRequest';
+    this.statusCode = 400;
+    this.detail = detail;
+  }
+}
+
 module.exports = {
-  formatDSL: (requested, query, options = {}) => []
-    .concat(requested)
-    .map(extractKeyAndType)
-    .reduce((aggregationDSL, { key, type, requested }) => ({
-      ...aggregationDSL,
-      [key]: aggregationTypes[type].formatDSL(
-        query,
-        getOptions(requested, options)
-      )
-    }), {}),
+  formatDSL: (requested, query, options = {}) => {
+    const errors = getValidationErrors(requested);
+
+    if (errors.length) {
+      throw new BadRequest('Invalid requested aggregations', errors);
+    }
+
+    return [].concat(requested)
+      .map(extractKeyAndType)
+      .reduce((aggregationDSL, { key, type, requested }) => ({
+        ...aggregationDSL,
+        [key]: aggregationTypes[type].formatDSL(
+          query,
+          getOptions(requested, options)
+        )
+      }), {})
+  },
   formatResult: (requested, query, result, options = {}) => requested
     .map(extractKeyAndType)
     .reduce((formatted, { key, type, requested }) => ({
       ...formatted,
       [key]: aggregationTypes[type].formatResult(result[key], { ...getOptions(requested, options), query })
     }), {})
+}
+
+function getValidationErrors(requested) {
+  const errors = [];
+
+  if (requested instanceof Array) {
+    requested.forEach((item, index) => {
+      getRequestedItemErrors(item, index).forEach(e => errors.push(e));
+    });
+  } else {
+    getRequestedItemErrors(requested).forEach(e => errors.push(e));
+  }
+
+  return errors;
+}
+
+function getRequestedItemErrors(requestedAgg, index = null) {
+  const errors = [];
+
+  const { type, key } = extractKeyAndType(requestedAgg);
+
+  if (type === undefined) {
+    errors.push({
+      index,
+      message: `requested aggregation type is not defined`
+    });
+  }
+  if (key === undefined) {
+    errors.push({
+      index,
+      message: `requested aggregation key is not defined`
+    });
+  }
+
+  return errors;
 }
 
 function extractKeyAndType(requested) {
