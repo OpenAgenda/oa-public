8f6a22470f3ce7c2a337c9cd1108ab8522c81879
Kévin Berthommier <kevin.bertho@gmail.com>
2020-06-23 11:38:05 +0200

    feat(cibul-node): use qs with arrayLimit to Infinity

--- packages/cibul-node/app.js
+++ packages/cibul-node/app.js
@@ -2,11 +2,13 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const qs = require('qs');
 
 const app = express();
 
 app
   .set('trust proxy', ['loopback', 'uniquelocal'])
+  .set('query parser', str => qs.parse(str, { allowPrototypes: true, arrayLimit: Infinity }))
   .use(bodyParser.json({ limit: '5mb' }))
   .use(bodyParser.urlencoded({ limit: '500kb', extended: true }))
   .use((req, res, next) => {
