7d452ae77e2eb7796b83cd8466c670f022f62e74
Kévin Berthommier <kevin.bertho@gmail.com>
2020-07-06 18:31:12 +0200

    fix(verror): conserve ESM exports

--- packages/verror/src/verror.js
+++ packages/verror/src/verror.js
@@ -272,10 +272,13 @@ class WError extends VError {
 
 WError.prototype.name = 'WError';
 
-module.exports = VError;
-module.exports.VError = VError;
-module.exports.WError = WError;
-module.exports.MultiError = MultiError;
+export default VError;
+
+export {
+  VError,
+  WError,
+  MultiError
+};
 
 /*
  * Usage:
