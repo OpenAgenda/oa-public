50787abb37f6f4702d35ee1476f152110999e707
Kévin Berthommier <kevin.bertho@gmail.com>
2020-07-10 11:20:42 +0200

    feat(cibul-node): change default stats

--- packages/cibul-node/services/stats/defaultStats.js
+++ packages/cibul-node/services/stats/defaultStats.js
@@ -3,40 +3,6 @@
 module.exports = [
   {
     aggregation: {
-      type: 'regions'
-    },
-    chart: {
-      type: 'vertical',
-      loadMore: true,
-      dataKey: 'eventCount',
-      labelKey: 'key'
-    }
-  },
-  {
-    aggregation: {
-      type: 'departments'
-    },
-    chart: {
-      type: 'vertical',
-      loadMore: true,
-      dataKey: 'eventCount',
-      labelKey: 'key'
-    }
-  },
-  {
-    aggregation: {
-      type: 'cities'
-    },
-    chart: {
-      type: 'vertical',
-      loadMore: true,
-      dataKey: 'eventCount',
-      labelKey: 'key'
-    }
-  },
-  { separator: true },
-  {
-    aggregation: {
       type: 'timings'
     },
     chart: {
@@ -48,62 +14,6 @@ module.exports = [
       categoryTick: 'date'
     }
   },
-  // {
-  //   aggregation: [{ type: 'createdAt' }, { type: 'updatedAt' }],
-  //   chart: {
-  //     type: 'horizontal',
-  //     intervalSelector: true,
-  //     fromDataKey: ['eventCount', 'eventCount'],
-  //     dataKey: ['createdCount', 'updatedCount'],
-  //     labelKey: 'key',
-  //     tooltip: 'date',
-  //     categoryTick: 'date'
-  //   }
-  // },
-  {
-    aggregation: { type: 'createdAt' },
-    chart: {
-      type: 'horizontal',
-      intervalSelector: true,
-      dataKey: 'eventCount',
-      labelKey: 'key',
-      tooltip: 'date',
-      categoryTick: 'date'
-    }
-  },
-  {
-    aggregation: { type: 'updatedAt' },
-    chart: {
-      type: 'horizontal',
-      intervalSelector: true,
-      dataKey: 'eventCount',
-      labelKey: 'key',
-      tooltip: 'date',
-      categoryTick: 'date'
-    }
-  },
-  { separator: true },
-  {
-    aggregation: {
-      type: 'members'
-    },
-    chart: {
-      type: 'vertical',
-      dataKey: 'eventCount',
-      labelKey: 'member.name'
-    }
-  },
-  {
-    aggregation: {
-      type: 'originAgendas'
-    },
-    chart: {
-      type: 'vertical',
-      dataKey: 'eventCount',
-      labelKey: 'agenda.title'
-    }
-  },
-  { separator: true },
   {
     aggregation: {
       type: 'keywords'
@@ -114,29 +24,5 @@ module.exports = [
       labelKey: 'key',
       loadMore: true
     }
-  },
-  {
-    aggregation: {
-      type: 'states'
-    },
-    chart: {
-      type: 'pie',
-      dataKey: 'eventCount',
-      labelKey: 'key',
-      tooltip: 'state'
-    }
-  },
-  // {
-  //   aggregation: {
-  //     type: 'pastAndUpcoming'
-  //   },
-  //   chart: {}
-  // },
-  // {
-  //   aggregation: {
-  //     type: 'sourceAgendas',
-  //     interval
-  //   },
-  //   chart: {}
-  // },
+  }
 ];
