88743fb77355ac29859e837776e6b5a4d667a301
Kari Olafsson <kaoreolafsson@gmail.com>
2020-07-13 11:17:19 +0200

    fix(polyfills): /dist does not exist in locale-data folder of module

--- packages/polyfills/intl-locales.js
+++ packages/polyfills/intl-locales.js
@@ -1,44 +1,44 @@
 'use strict';
 
 /* eslint-disable global-require */
-require('@formatjs/intl-pluralrules/dist/locale-data/br');
-require('@formatjs/intl-pluralrules/dist/locale-data/de');
-require('@formatjs/intl-pluralrules/dist/locale-data/en');
-require('@formatjs/intl-pluralrules/dist/locale-data/es');
-require('@formatjs/intl-pluralrules/dist/locale-data/fr');
-require('@formatjs/intl-pluralrules/dist/locale-data/it');
+require('@formatjs/intl-pluralrules/locale-data/br');
+require('@formatjs/intl-pluralrules/locale-data/de');
+require('@formatjs/intl-pluralrules/locale-data/en');
+require('@formatjs/intl-pluralrules/locale-data/es');
+require('@formatjs/intl-pluralrules/locale-data/fr');
+require('@formatjs/intl-pluralrules/locale-data/it');
 
-require('@formatjs/intl-displaynames/dist/locale-data/br');
-require('@formatjs/intl-displaynames/dist/locale-data/de');
-require('@formatjs/intl-displaynames/dist/locale-data/en');
-require('@formatjs/intl-displaynames/dist/locale-data/es');
-require('@formatjs/intl-displaynames/dist/locale-data/fr');
-require('@formatjs/intl-displaynames/dist/locale-data/it');
+require('@formatjs/intl-displaynames/locale-data/br');
+require('@formatjs/intl-displaynames/locale-data/de');
+require('@formatjs/intl-displaynames/locale-data/en');
+require('@formatjs/intl-displaynames/locale-data/es');
+require('@formatjs/intl-displaynames/locale-data/fr');
+require('@formatjs/intl-displaynames/locale-data/it');
 
-require('@formatjs/intl-listformat/dist/locale-data/br');
-require('@formatjs/intl-listformat/dist/locale-data/de');
-require('@formatjs/intl-listformat/dist/locale-data/en');
-require('@formatjs/intl-listformat/dist/locale-data/es');
-require('@formatjs/intl-listformat/dist/locale-data/fr');
-require('@formatjs/intl-listformat/dist/locale-data/it');
+require('@formatjs/intl-listformat/locale-data/br');
+require('@formatjs/intl-listformat/locale-data/de');
+require('@formatjs/intl-listformat/locale-data/en');
+require('@formatjs/intl-listformat/locale-data/es');
+require('@formatjs/intl-listformat/locale-data/fr');
+require('@formatjs/intl-listformat/locale-data/it');
 
-require('@formatjs/intl-numberformat/dist/locale-data/br');
-require('@formatjs/intl-numberformat/dist/locale-data/de');
-require('@formatjs/intl-numberformat/dist/locale-data/en');
-require('@formatjs/intl-numberformat/dist/locale-data/es');
-require('@formatjs/intl-numberformat/dist/locale-data/fr');
-require('@formatjs/intl-numberformat/dist/locale-data/it');
+require('@formatjs/intl-numberformat/locale-data/br');
+require('@formatjs/intl-numberformat/locale-data/de');
+require('@formatjs/intl-numberformat/locale-data/en');
+require('@formatjs/intl-numberformat/locale-data/es');
+require('@formatjs/intl-numberformat/locale-data/fr');
+require('@formatjs/intl-numberformat/locale-data/it');
 
-require('@formatjs/intl-relativetimeformat/dist/locale-data/br');
-require('@formatjs/intl-relativetimeformat/dist/locale-data/de');
-require('@formatjs/intl-relativetimeformat/dist/locale-data/en');
-require('@formatjs/intl-relativetimeformat/dist/locale-data/es');
-require('@formatjs/intl-relativetimeformat/dist/locale-data/fr');
-require('@formatjs/intl-relativetimeformat/dist/locale-data/it');
+require('@formatjs/intl-relativetimeformat/locale-data/br');
+require('@formatjs/intl-relativetimeformat/locale-data/de');
+require('@formatjs/intl-relativetimeformat/locale-data/en');
+require('@formatjs/intl-relativetimeformat/locale-data/es');
+require('@formatjs/intl-relativetimeformat/locale-data/fr');
+require('@formatjs/intl-relativetimeformat/locale-data/it');
 
-require('@formatjs/intl-datetimeformat/dist/locale-data/br');
-require('@formatjs/intl-datetimeformat/dist/locale-data/de');
-require('@formatjs/intl-datetimeformat/dist/locale-data/en');
-require('@formatjs/intl-datetimeformat/dist/locale-data/es');
-require('@formatjs/intl-datetimeformat/dist/locale-data/fr');
-require('@formatjs/intl-datetimeformat/dist/locale-data/it');
+require('@formatjs/intl-datetimeformat/locale-data/br');
+require('@formatjs/intl-datetimeformat/locale-data/de');
+require('@formatjs/intl-datetimeformat/locale-data/en');
+require('@formatjs/intl-datetimeformat/locale-data/es');
+require('@formatjs/intl-datetimeformat/locale-data/fr');
+require('@formatjs/intl-datetimeformat/locale-data/it');
