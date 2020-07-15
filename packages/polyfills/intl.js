ada6a0d627134bb86a9b059cd6fb6973b486eb08
Kévin Berthommier <kevin.bertho@gmail.com>
2020-07-15 15:53:48 +0200

    fix(polyfills): fix intl polyfill

--- packages/polyfills/intl.js
+++ packages/polyfills/intl.js
@@ -1,15 +1,34 @@
 'use strict';
 
-const globalThis = require('globalthis')();
-
-globalThis.Intl = require('intl');
+require('intl');
+require('intl/locale-data/jsonp/en.js');
 
 /* eslint-disable global-require */
 require('@formatjs/intl-getcanonicallocales/polyfill');
+
 require('@formatjs/intl-locale/polyfill');
+
 require('@formatjs/intl-pluralrules/polyfill');
+require('@formatjs/intl-pluralrules/locale-data/en');
+
 require('@formatjs/intl-displaynames/polyfill');
+require('@formatjs/intl-displaynames/locale-data/en');
+
 require('@formatjs/intl-listformat/polyfill');
+require('@formatjs/intl-listformat/locale-data/en');
+
 require('@formatjs/intl-numberformat/polyfill');
+require('@formatjs/intl-numberformat/locale-data/en');
+
 require('@formatjs/intl-relativetimeformat/polyfill');
+require('@formatjs/intl-relativetimeformat/locale-data/en');
+
 require('@formatjs/intl-datetimeformat/polyfill');
+require('@formatjs/intl-datetimeformat/locale-data/en');
+
+
+
+
+
+
+
