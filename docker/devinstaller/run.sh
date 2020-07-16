bda22bc00c006a1fd79c0b3607e78587a1e0d68c
Kévin Berthommier <kevin.bertho@gmail.com>
2020-07-16 22:13:29 +0200

    chore: docker: no more need custom sudo user

--- docker/devinstaller/run.sh
+++ docker/devinstaller/run.sh
@@ -2,22 +2,24 @@
 
 ctx=$(readlink -f "$(dirname "$0")/../..")
 env_file="$ctx/.env"
+image_name="oa_devinstaller"
+container_name="oa_devinstaller"
 
 set -a
-  #shellcheck source=../../.env
-  source "$env_file"
+#shellcheck source=../../.env
+source "$env_file"
 set +a
 
-docker build -t devinstaller docker/devinstaller || exit
+docker build -t $image_name docker/devinstaller || exit
 docker run \
---name devinstaller \
--it \
---rm \
--u "$USERID:$GROUPID" \
--v "$ctx:/root/oa" \
--v "$SF_PROJECT_PATH:/root/cibul-symfony" \
--v "$SF_API_PROJECT_PATH:/root/cibulapi-symfony" \
--v "$SF_PROJECT_PATH/php.ini:/usr/local/etc/php/php.ini" \
---env-file "$env_file" \
--w /root/oa \
-devinstaller /root/oa/docker/devinstaller/command.sh "$SITE_DOMAIN" "$API_DOMAIN" "$PMA_HOST" "$ELASTICHQ_DOMAIN"
+  --name $container_name \
+  -it \
+  --rm \
+  -u "${DOCKER_USER:-}" \
+  -v "$ctx:/root/oa" \
+  -v "${SF_PROJECT_PATH}:/root/cibul-symfony" \
+  -v "${SF_API_PROJECT_PATH}:/root/cibulapi-symfony" \
+  -v "${SF_PROJECT_PATH}/php.ini:/usr/local/etc/php/php.ini" \
+  --env-file "$env_file" \
+  -w /root/oa \
+  $image_name /root/oa/docker/devinstaller/command.sh "${SITE_DOMAIN}" "${API_DOMAIN}" "${PMA_HOST}" "${ELASTICHQ_DOMAIN}"
