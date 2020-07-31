0736f67574122c827d34d234236f8866fcb15b0a
Kari Olafsson <kaoreolafsson@gmail.com>
2020-07-31 15:47:03 +0200

    refactor(agenda-locations): cleanup of utils test suite

--- packages/agenda-locations/test/12_utils.js
+++ packages/agenda-locations/test/12_utils.js
@@ -1,100 +1,90 @@
 "use strict";
 
-const _ = require( 'lodash' );
-const { promisify } = require( 'util' );
-const should = require( 'should' );
+const _ = require('lodash');
+const assert = require('assert');
+const { promisify } = require('util');
 
-const insee = require( '../utils/insee' );
-const distance = require( '../utils/distance' );
+const insee = require('../utils/insee');
+const distance = require('../utils/distance');
 
 const redisConfig = { host: 'localhost', port: 6379 };
 
 const ns = 'insee';
 
-const redisCli = require( 'redis' ).createClient( redisConfig.port, redisConfig.host );
+const redisCli = require('redis').createClient(redisConfig.port, redisConfig.host);
 
-const rcHGet = promisify( redisCli.hget.bind( redisCli ) );
+const rcHGet = promisify(redisCli.hget.bind(redisCli));
 
-describe( 'utils', () => {
+describe('utils', () => {
 
-  describe( 'insee', () => {
+  describe('insee', () => {
 
-    before( () => {
+    before(() => {
+      insee.init({ redis: redisConfig });
+    });
 
-      insee.init( { redis: redisConfig } );
-
-    } );
-
-    beforeEach( done => {
-
+    beforeEach(done => {
       // clear redis key
-      redisCli.del( ns, done );
+      redisCli.del(ns, done);
+    });
 
-    } );
-
-    it( 'retrieves insee reference', async () => {
-
-      const ref = await insee( {
+    it('retrieves insee reference', async () => {
+      const ref = await insee({
         city: 'Lamastre', // for caching
-        department: 'Ardèche', // for caching ( name might be not enough )
+        department: 'Ardèche', // for caching (name might be not enough)
         latitude: 44.9870015,
         longitude: 4.5737007
-      } );
+      });
 
-      ref.should.equal( '07129' );
+      assert.equal(ref, '07129');
+    });
 
-    } );
+    it('caches reference in redis', async () => {
+      const before = await rcHGet(ns, 'ardeche|lamastre');
 
-    it( 'caches reference in redis', async () => {
+      assert.equal(before, null);
 
-      const before = await rcHGet( ns, 'ardeche|lamastre' );
-
-      should( before ).equal( null );
-
-      await insee( {
+      await insee({
         city: 'Lamastre',
         department: 'Ardèche',
         latitude: 44.9870015,
         longitude: 4.5737007
-      } );
+      });
 
-      const after = await rcHGet( ns, 'ardeche|lamastre' );
+      const after = await rcHGet(ns, 'ardeche|lamastre');
 
-      _.pick( _.first( JSON.parse( after ) ), [ 'nom', 'code' ] ).should.eql( {
-        nom: 'Lamastre', code: '07129'
-      } );
+      assert.deepEqual(
+        _.pick(_.first(JSON.parse(after)), [ 'nom', 'code' ]),
+        { nom: 'Lamastre', code: '07129' }
+      );
+    });
 
-    } );
-
-    it( 'uses cache at second call', async () => {
-
-      await insee( {
+    it('uses cache at second call', async () => {
+      await insee({
         city: 'Lamastre',
         department: 'Ardèche',
         latitude: 44.9870015,
         longitude: 4.5737007
-      } );
+      });
 
-      insee.fromCache.should.equal( false );
+      assert.equal(insee.fromCache, false);
 
-      await insee( {
+      await insee({
         city: 'Lamastre',
         department: 'Ardèche',
         latitude: 44.9870015,
         longitude: 4.5737007
-      } );
+      });
 
-      insee.fromCache.should.equal( true );
+      assert.equal(insee.fromCache, true);
+    });
 
-    } );
+  });
 
-  } );
+  describe('distance', () => {
 
-  describe( 'distance', () => {
-
-    it( 'gets the distance in meters', () => {
-
-      const d = distance( {
+    it('gets the distance in meters', () => {
+      const d = distance({
         name: 'La boutique',
         latitude: 48.867622,
         longitude: 2.352210
@@ -102,12 +92,11 @@ describe( 'utils', () => {
         name: 'La Gaité Lyrique',
         latitude: 48.866771,
         longitude: 2.353651
-      } );
+      });
 
-      d.should.equal( 142 );
+      assert.equal(d, 142);
+    });
 
-    } );
+  });
 
-  } );
-
-} );
+});
