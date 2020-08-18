6929a19004ce335c0befcc317f1cea0067ea5dda
Kari Olafsson <kaoreolafsson@gmail.com>
2020-08-18 19:05:47 +0200

    chore(validators): linting

--- packages/validators/src/link.js
+++ packages/validators/src/link.js
@@ -1,18 +1,13 @@
-"use strict";
+import isString from 'lodash/isString';
+import isURL from 'validator/lib/isURL';
+import listify from './listify';
+import rgx from './regex';
+import emailValidator from './email';
 
-const _ = {
-  assign: require( 'lodash/assign' ),
-  isString: require( 'lodash/isString' )
-};
-const isURL = require( 'validator/lib/isURL' );
+const validateEmail = emailValidator();
 
-const listify = require('./listify');
-const rgx = require( './regex' );
-const emailValidator = require( './email' )();
-
-module.exports = config => {
-
-  const params = _.assign( {
+export default config => {
+  const params = {
     field: undefined,
     error: {
       code: 'link.invalid',
@@ -20,114 +15,86 @@ module.exports = config => {
     },
     list: false,
     type: 'link',
-    optional: true
-  }, config || {} );
+    optional: true,
+    ...(config || {})
+  };
 
   const shouldntMatch = [
     /\s/,
     /\/:/,
     /;/
-  ];
+ ];
 
-  const validator = Object.assign( validate, {
-    type: 'link',
-    field: params.field
-  } );
-
-  return params.list ? listify( validator, params ) : validator;
-
-  function validate( value ) {
-
+  const validate = value => {
     const templateError = {
-      field: validator.field,
+      field: params.field,
       code: 'link.invalid',
       message: 'value is not a link'
     };
 
     let clean = value;
 
-    const error = [ _.assign( {
-      origin: value
-    }, templateError ) ];
+    const error = [{
+      origin: value,
+      ...templateError
+    }];
 
-    if ( _.isString( value ) ) {
-
+    if (isString(value)) {
       clean = value.trim();
-
     }
 
-    if ( ( !value || !value.length ) && params.optional ) {
-
+    if ((!value || !value.length) && params.optional) {
       return params.default !== undefined ? params.default : clean;
-
     }
 
-    if ( /^mailto\:/.test( clean ) && _isEmail( clean.replace( /^mailto\:/, '' ) ) ) {
-
+    if (/^mailto\:/.test(clean) && _isEmail(clean.replace(/^mailto\:/, ''))) {
       return clean;
-
     }
 
     const startsWithProtocol = /^((http(s|):|)\/\/|mailto\:)/.test(clean);
 
-    if ( !startsWithProtocol && _isEmail( clean ) ) throw error;
+    if (!startsWithProtocol && _isEmail(clean)) throw error;
 
-    // add http:// if link is like www.google.com ( protocol missing )
+    // add http:// if link is like www.google.com (protocol missing)
     if (!startsWithProtocol) {
-
       clean = 'http://' + clean;
-
     }
 
-    if ( clean.indexOf( '.' ) == -1 ) {
-
+    if (clean.indexOf('.') == -1) {
       throw error;
-
     }
 
-    if ( clean.substr( clean.length - 1, 1 ) === '.' ) {
-
+    if (clean.substr(clean.length - 1, 1) === '.') {
       throw error;
-
     }
 
-    shouldntMatch.forEach( rgx => {
-
-      if ( rgx.test( clean ) ) {
-
+    shouldntMatch.forEach(rgx => {
+      if (rgx.test(clean)) {
         throw error;
-
       }
+    });
 
-    } );
-
-    if ( !isURL( clean, {
+    if (!isURL(clean, {
       allow_protocol_relative_urls: true
-    } ) ) {
+    })) {
       throw error;
-
     }
 
     return clean;
-
   };
 
+  validate.type = 'link';
+  validate.field = params.field;
+
+  return params.list ? listify(validate, params) : validate;
 }
 
-
-
-function _isEmail( v ) {
-
+function _isEmail(v) {
   try {
-
-    emailValidator( v );
-
-  } catch( e ) {
-
+    validateEmail(v);
+  } catch(e) {
     return false;
-
   }
 
   return true;
-
 }
