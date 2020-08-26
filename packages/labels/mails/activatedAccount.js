bc76305e89a428a2ad8de13d8fa72a4b809cb3f2
Kévin Berthommier <kevin.bertho@gmail.com>
2020-08-26 16:25:18 +0200

    feat(labels): add labels for activatedAccount mail

---
+++ packages/labels/mails/activatedAccount.js
@@ -0,0 +1,28 @@
+"use strict";
+
+module.exports = {
+  "title": {
+    "fr": "Votre compte est activé",
+    "en": "Your account is activated",
+    "de": "Ihr Konto ist aktiviert",
+    "es": "Tu cuenta esta activada",
+    "br": "",
+    "it": "Il tuo account è attivato"
+  },
+  "description": {
+    "fr": "Vous pouvez maintenant profiter pleinement d'OpenAgenda:",
+    "en": "You can now take full advantage of OpenAgenda:",
+    "de": "Sie können jetzt OpenAgenda voll ausnutzen:",
+    "es": "Ahora puede aprovechar al máximo OpenAgenda:",
+    "br": "",
+    "it": "Ora puoi sfruttare appieno OpenAgenda:"
+  },
+  "actionDescription": {
+    "fr": "Allez sur OpenAgenda",
+    "en": "Go to OpenAgenda",
+    "de": "Gehen Sie zu OpenAgenda",
+    "es": "Ir a OpenAgenda",
+    "br": "",
+    "it": "Vai a OpenAgenda"
+  }
+}
