c3cdb39455ad6ec618a3455b9ca2c7ce3666982e
Kévin Berthommier <kevin.bertho@gmail.com>
2020-09-08 15:46:42 +0200

    fix(cibul-node): don't always call toString

--- packages/cibul-node/app.js
+++ packages/cibul-node/app.js
@@ -6,10 +6,8 @@ const qs = require('qs');
 
 const app = express();
 
-function rawBodySaver(req, res, buf, encoding) {
-  if (buf && buf.length) {
-    req.rawBody = buf.toString(encoding || 'utf8');
-  }
+function rawBodySaver(req, res, buf) {
+  req.rawBody = buf;
 }
 
 app
