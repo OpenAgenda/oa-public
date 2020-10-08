6420305bc311068cf9e624a37521d31b09ccd967
Kari Olafsson <kaoreolafsson@gmail.com>
2020-10-08 10:51:11 +0200

    feat(react-shared): utility to display unload page warning message

--- packages/react-shared/src/utils/index.js
+++ packages/react-shared/src/utils/index.js
@@ -1,2 +1,3 @@
 // eslint-disable-next-line import/prefer-default-export
 export { default as a11yButtonActionHandler } from './a11yButtonActionHandler';
+export * as unloadWarning from './unloadWarning';
