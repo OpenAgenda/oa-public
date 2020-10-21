8fffbfa1772d0fbc21b27cd3b87a507105fbcd18
Kévin Berthommier <kevin.bertho@gmail.com>
2020-10-21 15:48:37 +0200

    feat(labels): add forbidden labels

---
+++ packages/labels/agendas/forbidden.js
@@ -0,0 +1,32 @@
+"use strict";
+
+module.exports = {
+  "title": {
+    "fr": "Accès non autorisé",
+    "en": "Unauthorized access",
+    "de": "Unautorisierter Zugriff",
+    "es": "Acceso no autorizado",
+    "it": "Accesso non autorizzato"
+  },
+  "content": {
+    "fr": "L'accès à cet agenda est restreint, contactez l'administrateur de l'agenda pour plus d'informations.",
+    "en": "Access to this calendar is restricted, contact the administrator of the calendar for more information.",
+    "de": "Der Zugriff auf diesen Kalender ist eingeschränkt. Weitere Informationen erhalten Sie vom Administrator des Kalenders.",
+    "es": "El acceso a este calendario está restringido, póngase en contacto con el administrador del calendario para obtener más información.",
+    "it": "L'accesso a questo calendario è limitato, contatta l'amministratore del calendario per ulteriori informazioni."
+  },
+  "contact": {
+    "fr": "Contacter l'administrateur",
+    "en": "Contact the administrator",
+    "de": "die Administratoren kontaktieren",
+    "es": "Póngase en contacto con el administrador",
+    "it": "Contattare l'amministratore"
+  },
+  "back": {
+    "fr": "Retour à l'agenda",
+    "en": "Back to the agenda",
+    "de": "Zurück zur Kalender",
+    "es": "Volver a la agenda",
+    "it": "Torna all'ordine del giorno"
+  }
+}
