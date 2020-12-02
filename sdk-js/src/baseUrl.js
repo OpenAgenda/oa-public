d6b19b87d3ed6d7bbe1a3be4957899544c2dc420
Kévin Berthommier <kevin.bertho@gmail.com>
2020-12-02 14:41:06 +0100

    chore: move sdk-js to public

---
+++ sdk-js/src/baseUrl.js
@@ -0,0 +1,12 @@
+const baseUrl = {
+  v1:
+    process.env.NODE_ENV !== 'development'
+      ? 'https://api.openagenda.com/v1'
+      : 'https://dapi.openagenda.com/frontend_dev.php/v1',
+  v2:
+    process.env.NODE_ENV !== 'development'
+      ? 'https://api.openagenda.com/v2'
+      : 'https://dapi.openagenda.com/v2'
+};
+
+export default baseUrl;
