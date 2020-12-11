2ab822453e8da2970364c7d8b9fefd2fd57fd964
Kaoré <kaore@openagenda.com>
2020-12-11 14:26:04 +0100

    feat(agenda-portal): added preRender middleware hook

--- agenda-portal/middleware/event.js
+++ agenda-portal/middleware/event.js
@@ -2,7 +2,7 @@
 
 const setPageProp = require('../lib/utils/setPageProp');
 
-module.exports = async (req, res, next) => {
+module.exports.get = async (req, res, next) => {
   const proxy = req.app.get('proxy');
   const transform = req.app.get('transforms').event.show;
 
@@ -14,11 +14,19 @@ module.exports = async (req, res, next) => {
 
   req.data.event = transform(event, req, res);
 
+  setPageProp(req, 'pageType', 'event');
+
+  next();
+};
+
+module.exports.render = (req, res, next) => {
+  if (!req.data.event) {
+    return next();
+  }
+
   if (req.query.data !== undefined && process.env.NODE_ENV === 'development') {
     return res.json(Object.assign(req.data, req.app.locals));
   }
 
-  setPageProp(req, 'pageType', 'event');
-
   res.render('event', req.data);
-};
+}
