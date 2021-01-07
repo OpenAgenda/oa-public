116025b07037e215670fc7da808fe6de16f67cd0
Kévin Berthommier <kevin.bertho@gmail.com>
2021-01-07 15:05:19 +0100

    feat(react-shared): add mergeLocales

--- packages/react-shared/src/utils/index.js
+++ packages/react-shared/src/utils/index.js
@@ -1,6 +1,7 @@
 export { default as a11yButtonActionHandler } from './a11yButtonActionHandler';
 export { default as createApp } from './createApp';
 export { default as loadable } from './loadable';
+export { default as mergeLocales } from './mergeLocales';
 export { default as nl2br } from './nl2br';
 export * as unloadWarning from './unloadWarning';
 export { default as withContext } from './withContext';
