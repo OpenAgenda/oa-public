9efc0b09484cf5808526ff86d2412fb506dcfc01
Kaoré <kaore@openagenda.com>
2021-01-26 13:29:25 +0100

    chore(cibul-node): add clement to superadmins

--- packages/cibul-node/config/index.js
+++ packages/cibul-node/config/index.js
@@ -13,7 +13,7 @@ const config = {
   all: {
     env: 'production',
     corpoLastUpdate: '2017-10-31T12:07:29.000Z',
-    superAdminIds: [1, 2, 11258, 15453],
+    superAdminIds: [1, 2, 11258, 15453, 124500],
     jsVersion: 42,
     cssVersion: 2,
     interfaceLanguages: ['fr', 'en', 'de', 'es', 'it', 'br'],
