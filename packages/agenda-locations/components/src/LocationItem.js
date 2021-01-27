d1feef41fbcf39038353b0ae90893520892389b7
Clément Lecroart <clement.lecroart@openagenda.com>
2021-01-27 18:47:01 +0100

    fix(agenda-locations): stopPropagation from remove Button on clickable div

--- packages/agenda-locations/components/src/LocationItem.js
+++ packages/agenda-locations/components/src/LocationItem.js
@@ -22,6 +22,13 @@ class LocationItem extends Component {
     // Binding
     this.onRemove = this.onRemove.bind(this);
     this.seeEvents = this.seeEvents.bind(this);
+    this.displayCantRemoveModal = this.displayCantRemoveModal.bind(this);
+  }
+
+  displayCantRemoveModal(e) {
+    const { displayCantDoModal } = this.props;
+    e.stopPropagation();
+    displayCantDoModal('remove');
   }
 
   onRemove(e) {
