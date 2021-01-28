59121f2f8e524146ae9b875741fbc0648e7d5f5a
Kaoré <kaore@openagenda.com>
2021-01-28 16:50:31 +0100

    chore(validators): linting

--- packages/validators/test/boolean.test.js
+++ packages/validators/test/boolean.test.js
@@ -1,135 +1,106 @@
 "use strict";
 
-let validators = require( '../src' );
+let validators = require('../src');
 
-describe( 'boolean validator', () => {
+describe('boolean validator', () => {
 
-  it( 'cleans true to true', () => {
+  it('cleans true to true', () => {
+    expect(validators.boolean()(true)).toBe(true);
+  });
 
-    expect(validators.boolean()( true )).toBe(true);
-
-  } );
-
-  it( 'returns null if nothing is given on an optional validator', () => {
-
-    let optionalValidate = validators.boolean( {
+  it('returns null if nothing is given on an optional validator', () => {
+    const optionalValidate = validators.boolean({
       optional: true
-    } );
+    });
 
-    expect( optionalValidate() ).toBeNull();
+    expect(optionalValidate()).toBeNull();
+  });
 
-  } );
-
-  it( 'returns default value if default value is given', () => {
-
-    let optionalValidate = validators.boolean( {
+  it('returns default value if default value is given', () => {
+    const optionalValidate = validators.boolean({
       optional: true,
       default: null
-    } );
+    });
 
-    expect( optionalValidate( null ) ).toBeNull();
+    expect(optionalValidate(null)).toBeNull();
+  });
 
-  } );
-
-  it( 'puts a default value if nothing is specified', () => {
-
-    let validate = validators.number( {
+  it('puts a default value if nothing is specified', () => {
+    const validate = validators.number({
       default: true
-    } );
+    });
 
     expect(validate()).toBe(true);
+  });
 
-  } );
-
-  it( 'throws an error if is not optional and no default is specified', () => {
-
-    let validate = validators.boolean( {
+  it('throws an error if is not optional and no default is specified', () => {
+    const validate = validators.boolean({
       optional: false
-    }), 
+    });
 
-    errors = [];
+    const errors = [];
 
     try {
-
       validate();
-
-    } catch( e ) {
-
+    } catch(e) {
       errors = e;
-
     }
 
     expect(errors.length).toBe(1);
-
     expect(errors[ 0 ].code).toBe('required');
-
-  } );
-
-
-  it( 'cleans a valid entry', () => {
-
-    let validate = validators.boolean( {
-      field: '11or12',
-      optional: false,
-    } );
-
-    expect(validate( '11' )).toBe(true);
-
   });
 
-  it( 'cleans \'0\' to false', () => {
 
-    expect(validators.boolean()( '0' )).toBe(false);
+  it('cleans a valid entry', () => {
+    const validate = validators.boolean({
+      field: '11or12',
+      optional: false,
+    });
 
-  } );
+    expect(validate('11')).toBe(true);
+  });
 
-  it( 'cleans \'false\' to false', () => {
+  it('cleans \'0\' to false', () => {
+    expect(validators.boolean()('0')).toBe(false);
+  });
 
-    expect(validators.boolean()( 'false' )).toBe(false);
+  it('cleans \'false\' to false', () => {
+    expect(validators.boolean()('false')).toBe(false);
+  });
 
-  } );
-
-  it( 'if default is null and nothing is given, returns null', () => {
-
-    let validate = validators.boolean( {
+  it('if default is null and nothing is given, returns null', () => {
+    const validate = validators.boolean({
       field: 'whocares',
       default: null
-    } );
+    });
 
-    expect( validate() ).toBeNull();
+    expect(validate()).toBeNull();
+  });
 
-  } );
-
-  it( 'if default is null and null is given, returns null', () => {
-
-    let validate = validators.boolean( {
+  it('if default is null and null is given, returns null', () => {
+    const validate = validators.boolean({
       field: 'whocares',
       default: null
-    } );
+    });
 
-    expect( validate( null ) ).toBeNull();
+    expect(validate(null)).toBeNull();
+  });
 
-  } );
-
-  it( 'if given value is null and default is not set, cleans as false', () => {
-
-    let validate = validators.boolean( {
+  it('if given value is null and default is not set, cleans as false', () => {
+    const validate = validators.boolean({
       field: 'meh'
-    } );
+    });
 
-    expect(validate( null )).toBe(false);
+    expect(validate(null)).toBe(false);
+  });
 
-  } );
-
-  it( 'if given value is null and allowNull option is true, cleans as null', () => {
-
-    let validate = validators.boolean( {
+  it('if given value is null and allowNull option is true, cleans as null', () => {
+    const validate = validators.boolean({
       field: 'mnieeeh',
       allowNull: true
-    } );
+    });
 
-    expect( validate( null ) ).toBeNull();
-
-  } );
-
-} );
+    expect(validate(null)).toBeNull();
+  });
+  
+});
