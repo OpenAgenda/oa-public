5bc05837471901d65db272c6d92b42ddb13a151e
Kaoré <kaore@openagenda.com>
2021-01-29 11:44:11 +0100

    refactor(members): explicit default values in validation

--- packages/members/iso/validate.js
+++ packages/members/iso/validate.js
@@ -18,7 +18,7 @@ schema.register({
   boolean,
   email,
   phone,
-  text
+  text,
 });
 
 const roles = require('./roles');
@@ -26,79 +26,81 @@ const roles = require('./roles');
 const fields = {
   base: {
     agendaUid: {
-      type: 'integer'
+      type: 'integer',
+      default: null,
     },
     userUid: {
-      type: 'integer'
+      type: 'integer',
+      default: null,
     },
     createdAt: {
-      type: 'date'
+      type: 'date',
     },
     updatedAt: {
-      type: 'date'
+      type: 'date',
     },
     custom: {
-      type: 'pass'
+      type: 'pass',
     },
     deletedUser: {
       type: 'boolean',
-      default: false
+      default: false,
     },
     role: {
       type: 'choice',
       unique: true,
       optional: false,
-      options: Object.values(roles)
+      options: Object.values(roles),
     },
     actionsCounter: {
       type: 'integer',
-      default: 0
-    }
+      default: 0,
+    },
   },
   legacy: {
     userId: {
-      type: 'integer'
+      type: 'integer',
     },
     agendaId: {
-      type: 'integer'
+      type: 'integer',
     },
     credential: {
       type: 'choice',
       unique: true,
-      options: Object.values(roles)
-    }
-  }
+      options: Object.values(roles),
+    },
+  },
 };
 
 const custom = required => ({
   organization: {
     type: 'text',
     optional: !required,
-    max: 255
+    max: 255,
   },
   contactName: {
     type: 'text',
     optional: !required,
-    max: 255
+    max: 255,
   },
   contactNumber: {
     type: 'phone',
     optional: !required,
-    limit: 255
+    limit: 255,
   },
   contactPosition: {
     type: 'text',
     optional: !required,
-    limit: 255
+    limit: 255,
   },
   email: {
     type: 'email',
-    optional: !required
-  }
+    optional: !required,
+  },
 });
 
 module.exports = Object.assign(schema(fields.base), {
   withLegacy: schema({ ...fields.base, ...fields.legacy }),
   withCustom: required => schema({ ...fields.base, custom: custom(required) }),
-  custom: required => schema(custom(required))
+  custom: required => schema(custom(required)),
 });
