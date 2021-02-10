31195af6eac41e5e52757ea6fa98a419b886a5b7
Kévin Berthommier <kevin.bertho@gmail.com>
2021-02-10 16:28:47 +0100

    fix(labels): fix fallback with flatten

--- packages/labels/flatten.js
+++ packages/labels/flatten.js
@@ -3,19 +3,21 @@
 /**
  * flatten a labels set to a single language keyed list
  */
-module.exports = function( labels, lang, fallback ) {
+module.exports = function (labels, lang, fallback) {
 
   var flat = {}, possibleLangs;
 
-  for ( var f in labels ) {
+  for (var f in labels) {
 
-    flat[ f ] = labels[ f ][ lang ];
+    flat[f] = labels[f][lang];
 
-    if ( ( typeof flat[ f ] === 'undefined' ) && fallback ) {
+    if ((typeof flat[f] === 'undefined') && fallback) {
 
-      possibleLangs = Object.keys( labels[ f ] );
+      possibleLangs = Object.keys(labels[f]);
 
-      if ( possibleLangs.length ) flat[ f ] = labels[ f ][ possibleLangs[ 0 ] ];
+      if (possibleLangs.length) {
+        flat[f] = labels[f][fallback] || labels[f][possibleLangs[0]];
+      }
 
     }
 
