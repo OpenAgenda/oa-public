6cbeb74e348255ce71df1193267f8d3c79c94f40
Kévin Berthommier <kevin.bertho@gmail.com>
2021-02-15 11:17:39 +0100

    fix(cibul-node): fix param format

--- packages/cibul-node/web.js
+++ packages/cibul-node/web.js
@@ -5,8 +5,8 @@ module.exports = app => {
   require('./services/mails').plugApp(app);
   require('./services/slackApp').plugApp(app);
   app.use('/events/search', app.services.eventSearch.apps.events());
-  app.use('/agendas/:agendaUid/events.v2:format', app.services.eventSearch.apps.agendas.public());
-  app.use('/agendas/:agendaUid/admin/events.v2:format', app.services.eventSearch.apps.agendas.restricted());
+  app.use('/agendas/:agendaUid/events.v2.:format', app.services.eventSearch.apps.agendas.public());
+  app.use('/agendas/:agendaUid/admin/events.v2.:format', app.services.eventSearch.apps.agendas.restricted());
   app.services.agendaLocations.apps(app, '/locations');
   app.services.agendaLocations.apps.agenda(app, '/agendas/:agendaUid/locations');
   app.services.agendaLocations.apps.agendaAdmin(app, '/:agendaSlug/admin/locations');
