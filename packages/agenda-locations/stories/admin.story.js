0731f079b69d90008ab55e7123af336248d1206e
Clément Lecroart <clement.lecroart@openagenda.com>
2021-02-16 09:35:42 +0100

    refactor(agenda-locations): rebase branch with origin/ put back admin.story

--- packages/agenda-locations/stories/admin.story.js
+++ packages/agenda-locations/stories/admin.story.js
@@ -4,6 +4,7 @@ import AdminApp from '../components/src/AgendaAdminLocations';
 export default ({ settings, res, set }) => (
   <div className="top-margined col-sm-8 col-sm-offset-2 wsq content">
     <div className="js_locations_counter" data-res="http://localhost:3000/unverified"></div>
+
     <AdminApp
       agenda={{
         slug: 'theagendaslug',
