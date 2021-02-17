598f1eab9559aa0b48f83659d4162c114f4699d9
Kaoré <kaore@openagenda.com>
2021-02-17 15:31:02 +0100

    chore(event-form): linting

--- packages/event-form/src/index.js
+++ packages/event-form/src/index.js
@@ -14,106 +14,104 @@ import schemaLanguages from './utils/schemaLanguages';
 import errorLabels from '@openagenda/labels/event/errors';
 
 const eventFormComponents = {
-  age: require( './components/Age' ),
-  registration: require( './components/Registration' ),
-  keywords: require( './components/Keywords' ),
-  timings: require( './components/Timings' ),
-  location: require( './components/Location' ),
-  languages: require( './components/Languages' ),
-  accessibility: require( './components/Accessibility' ),
-  references: require( './components/References' )
+  age: require('./components/Age'),
+  registration: require('./components/Registration'),
+  keywords: require('./components/Keywords'),
+  timings: require('./components/Timings'),
+  location: require('./components/Location'),
+  languages: require('./components/Languages'),
+  accessibility: require('./components/Accessibility'),
+  references: require('./components/References')
 }
 
-const eventSchema = require( './schema' );
+const eventSchema = require('./schema');
 
 export default class EventForm extends Component {
+  constructor(props) {
+    super(props);
 
-  constructor( props ) {
+    const languages = extractLanguages(this.props.values);
 
-    super( props );
+    const {
+      schema,
+      hash
+    } = this.buildEventSchema(languages, props);
 
-    const languages = extractLanguages( this.props.values );
-
-    const { schema, hash } = this.buildEventSchema( languages, props );
-
-    const values = ih( props.values, {
+    const values = ih(props.values, {
       languages: {
-        $set: schemaLanguages.getFromSchemaAndValues( schema, props.lang, languages )
+        $set: schemaLanguages.getFromSchemaAndValues(schema, props.lang, languages)
       }
-    } );
+    });
 
-    this.state = { values, schema, hash, files: [], loading: false };
-
+    this.state = {
+      values,
+      schema,
+      hash,
+      files: [],
+      loading: false
+    };
   }
 
-  onChange( { values, errors, files, loading, globalError } ) {
-
+  onChange({ values, errors, files, loading, globalError }) {
     const { lang } = this.props;
 
     const languageChanges = identifyLanguageChanges(
-      _.get( this.state, 'values.languages' ), // before
-      _.get( values, 'languages' ) // now
-    );
+      _.get(this.state, 'values.languages'), // before
+      _.get(values, 'languages') // now
+   );
 
-    const update = _.omitBy( {
+    const update = _.omitBy({
       errors,
       globalError,
       files,
       loading
-    }, _.isUndefined );
+    }, _.isUndefined);
 
-    if ( values ) update.values = values;
+    if (values) update.values = values;
 
-    const multilingualFieldNames = getMultilingualFieldNames( eventSchema( { languages: true } ) );
+    const multilingualFieldNames = getMultilingualFieldNames(eventSchema({ languages: true }));
 
     // if a unique language has been switcheds, content should not be lost
-    if ( languageChanges.swapped.length ) {
-
-      update.values = ih( transferMultilingualValues(
+    if (languageChanges.swapped.length) {
+      update.values = ih(transferMultilingualValues(
         this.state.values,
         multilingualFieldNames,
-        _.get( this, 'state.values.languages.0' ),
-        _.first( languageChanges.swapped )
-      ), {
+        _.get(this, 'state.values.languages.0'),
+        _.first(languageChanges.swapped)
+     ), {
         languages: {
           $set: [ languageChanges.swapped[ 0 ] ]
         }
-      } );
-
-    } else if ( languageChanges.removed.length ) {
-
-      update.values = ih( removeMultilingualValues(
+      });
+    } else if (languageChanges.removed.length) {
+      update.values = ih(removeMultilingualValues(
         this.state.values,
         multilingualFieldNames,
         languageChanges.removed
-      ), {
+     ), {
         languages: {
-          $set: this.state.values.languages.filter( l => !languageChanges.removed.includes( l ) )
+          $set: this.state.values.languages.filter(l => !languageChanges.removed.includes(l))
         }
-      } );
-
+      });
     }
 
-    if ( languageChanges.has ) {
-
-      _.assign( update, this.buildEventSchema( _.get( values, 'languages' ) ) );
+    if (languageChanges.has) {
+      _.assign(update, this.buildEventSchema(_.get(values, 'languages')));
 
       update.values.languages = schemaLanguages.getFromSchemaAndValues(
         update.schema,
         lang,
         update.values.languages
-      );
+     );
 
     }
 
-    if ( this.props.devOnChange ) this.props.devOnChange( update );
+    if (this.props.devOnChange) this.props.devOnChange(update);
 
-    return this.setState( update );
-
+    return this.setState(update);
   }
 
-  buildEventSchema( languages, props = null ) {
-
+  buildEventSchema(languages, props = null) {
     const p = props || this.props;
 
     return {
@@ -130,13 +128,11 @@ export default class EventForm extends Component {
           write: p.role
         }
       }),
-      hash: JSON.stringify( languages ) // only language changes may trigger schema changes
+      hash: JSON.stringify(languages) // only language changes may trigger schema changes
     }
-
   }
 
   render() {
-
     const {
       lang,
       actionComponents,
@@ -164,12 +160,12 @@ export default class EventForm extends Component {
       globalError={this.state.globalError}
       loading={this.state.loading}
       files={this.state.files}
-      onChange={this.onChange.bind( this )}
+      onChange={this.onChange.bind(this)}
       schema={schema}
       hash={hash}
-      classNames={ih( classNames, {
+      classNames={ih(classNames, {
         field: { $set: 'padding-v-sm form-group' }
-      } )}
+      })}
       actionComponents={actionComponents}
       onSubmitSuccess={onSubmitSuccess}
       labels={{
