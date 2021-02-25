0e91cc594fb191678b377942132f9c5bb3fba7b1
Kaoré <kaore@openagenda.com>
2021-02-25 11:12:12 +0100

    chore(cibul-node): loading services through services obj instead of require

--- packages/cibul-node/services/agendaEvents/onUpdate.js
+++ packages/cibul-node/services/agendaEvents/onUpdate.js
@@ -1,37 +1,39 @@
 "use strict";
 
-const _ = require( 'lodash' );
-const VError = require( 'verror' );
+const _ = require('lodash');
+const VError = require('verror');
 
-const log = require( '@openagenda/logs' )( 'agendaEvents/onUpdate' );
+const log = require('@openagenda/logs')('agendaEvents/onUpdate');
 
-const coms = require( '../../lib/coms' );
-const config = require( '../../config' );
-const controlDataSvc = require( '../legacy' ).controlData;
-const legacyEventSearch = require( '../elasticsearch' );
-const fallbackContextGet = require( './lib/fallbackContextGet' );
-const sendEventUpdate = require( './lib/sendEventUpdate' );
-const sendEventChangeState = require( './lib/sendEventChangeState' );
-const transferCustomFromLegacy = require( './lib/transferCustomFromLegacy' );
-const createActivities = require( './lib/createActivities' );
+const fallbackContextGet = require('./lib/fallbackContextGet');
+const sendEventUpdate = require('./lib/sendEventUpdate');
+const sendEventChangeState = require('./lib/sendEventChangeState');
+const transferCustomFromLegacy = require('./lib/transferCustomFromLegacy');
+const createActivities = require('./lib/createActivities');
 
 module.exports = async ({ config, services }, before, after, context) => {
+  const {
+    legacy: legacySvc,
+    elasticsearch: legacyEventSearch
+  } = services;
+
+  const controlDataSvc = legacySvc.controlData;
+
   log('updated agenda-event from %j to %j, %j', before, after, _.pick(context, ['legacy', 'aggregated', 'batched']));
 
-  const { agenda, event, user } = await fallbackContextGet( { services }, 'onUpdate', after, context );
+  const { agenda, event, user } = await fallbackContextGet({ services }, 'onUpdate', after, context);
 
-
-  if ( after.state === 2 ) {
+  if (after.state === 2) {
     try {
-      await controlDataSvc.set( after, event );
-    } catch ( e ) {
-      log( 'error', 'control data set failed', e );
+      await controlDataSvc.set(after, event);
+    } catch (e) {
+      log('error', 'control data set failed', e);
     }
-  } else if ( ( before.state === 2 ) && ( after.state !== 2 ) ) {
+  } else if ((before.state === 2) && (after.state !== 2)) {
     try {
-      await controlDataSvc.remove( before );
-    } catch ( e ) {
-      log( 'error', 'control data remove failed', e );
+      await controlDataSvc.remove(before);
+    } catch (e) {
+      log('error', 'control data remove failed', e);
     }
   }
 
@@ -64,21 +66,21 @@ module.exports = async ({ config, services }, before, after, context) => {
     try {
       await sendEventChangeState({ config, services }, { agendaEvent: after, before, context, agenda, event });
     } catch (error) {
-      log.error( new VError( error, 'Cannot send event change state emails' ) );
+      log.error(new VError(error, 'Cannot send event change state emails'));
     }
   }
 
   if (user) {
     try {
-      await createActivities({ agenda, event, user }, before, after);
+      await createActivities(services, { agenda, event, user }, before, after);
     } catch (e) {
-      log.error( new VError(e, 'Cannot create state change activities' ) );
+      log.error(new VError(e, 'Cannot create state change activities'));
     }
   }
 }
 
-function haveRealDiff( before, after ) {
-  _.uniq( [ ...Object.keys( before ), ...Object.keys( after ) ] )
-    .filter( key => [ 'createdAt', 'updatedAt', 'state' ].includes( key ) && before[ key ] !== after[ key ] )
+function haveRealDiff(before, after) {
+  _.uniq([ ...Object.keys(before), ...Object.keys(after) ])
+    .filter(key => [ 'createdAt', 'updatedAt', 'state' ].includes(key) && before[ key ] !== after[ key ])
     .length > 0;
 }
