71e45fa67750c4c0c912579d4bf356be9d09e3e2
Kaoré <kaore@openagenda.com>
2021-03-09 10:35:12 +0100

    chore(cibul-templates): linting

--- packages/cibul-templates/event/js/show.js
+++ packages/cibul-templates/event/js/show.js
@@ -1,55 +1,54 @@
 "use strict";
 
-var eventMap = require( './map' ),
+const  eventMap = require('./map');
 
-utils = require( '@openagenda/utils' ),
+const utils = require('@openagenda/utils');
 
-du = require( '@openagenda/dom-utils' ),
+const du = require('@openagenda/dom-utils');
 
-get = require( '@openagenda/utils/get' ),
+const get = require('@openagenda/utils/get');
 
-session = require( '@openagenda/sessions/client' ),
+const session = require('@openagenda/sessions/client');
 
-adminControls = require( './adminControls' ),
+const adminControls = require('./adminControls');
 
-ownershipTransfer = require( './ownershipTransfer' ),
+const ownershipTransfer = require('./ownershipTransfer');
 
-displayReferences = require( './displayReferences' ),
+const displayReferences = require('./displayReferences');
 
-privateData = require( './privateData' ),
+const privateData = require('./privateData');
 
-hours = require( './hours' ),
+const hours = require('./hours');
 
-debug = require( 'debug' ), log,
+const debug = require('debug');
 
-ROLES = {
+let log;
+
+const ROLES = {
   EVENTEDITOR: 2,     // owner or admin/moderator of agenda having edition rights over event
   AGENDAMODERATOR: 3, // moderator of current agenda env
   AGENDAADMIN: 4      // admin of current agenda env
-},
+};
 
-defaults = {
+const defaults = {
   selectors: {},
   agendaUid: false,     // uid of the current agenda environment
   ownerUid: false,      // uid of the owner
   adminAgendaUids: []  // uids of agendas with admin rights on event
 };
 
-defaults.selectors[ ROLES.EVENTEDITOR ] = '.js_role_event_editor';
-defaults.selectors[ ROLES.AGENDAMODERATOR ] = '.js_role_agenda_moderator';
-defaults.selectors[ ROLES.AGENDAADMIN ] = '.js_role_agenda_admin';
+defaults.selectors[ROLES.EVENTEDITOR] = '.js_role_event_editor';
+defaults.selectors[ROLES.AGENDAMODERATOR] = '.js_role_agenda_moderator';
+defaults.selectors[ROLES.AGENDAADMIN] = '.js_role_agenda_admin';
 
-if ( [ 'tpl', 'development' ].indexOf( window.env ) !== -1 ) {
-
-  debug.enable( '*' );
-
+if (['tpl', 'development'].indexOf(window.env) !== -1) {
+  debug.enable('*');
 }
 
-window.hook( function( options ) {
-
+window.hook(options => {
   adminControls.init();
 
-  hours( utils.extend( {}, options, {
+  hours(utils.extend({}, options, {
     selectors: {
       origin: '.js_timings_canvas',
       destination: '.js_hours',
@@ -57,13 +56,12 @@ window.hook( function( options ) {
       left: '.js_left',
       months: '.js_months'
     }
-  } ) );
+  }));
+});
 
-} );
+window.asap(options => {
 
-window.asap( options => {
-
-  var params = utils.extend( {
+  const params = utils.extend({
     agendaUid: null,
     moderatorCanPublish: false,
     hasCustomFields: false,
@@ -71,22 +69,21 @@ window.asap( options => {
     res: {
       detailedSession: '/session?detailed=1'
     }
-  }, defaults, options );
+  }, defaults, options);
 
-  log = debug( 'event' );
+  log = debug('event');
 
-  if ( window.env === 'tpl' ) {
+  if (window.env === 'tpl') {
 
     params.res.detailedSession = 'detailedsession.json';
 
   }
 
 
-  _defineRoles( params, ( err, roles ) => {
+  _defineRoles(params, (err, roles) => {
+    log('roles: [%s]', roles.join(','));
 
-    log( 'roles: [%s]', roles.join( ',' ) );
-
-    let showControls = adminControls( session, {
+    let showControls = adminControls(session, {
       lang: params.lang,
       eventUid: params.uid,
       agendaUid: params.agendaUid,
@@ -94,33 +91,26 @@ window.asap( options => {
       agendaTitle: params.agendaTitle,
       agendaImage: params.agendaImage,
       testFunc: () => roles.length,
-      displaySelectors: roles.map( r => params.selectors[ r ] )
-    } );
+      displaySelectors: roles.map(r => params.selectors[r])
+    });
 
-    var prv = privateData();
+    const prv = privateData();
 
-
-    if ( params.moderatorCanPublish ) {
-
-      du.addClass( du.el( '.js_moderator_can_publish' ), 'js_role_agenda_moderator' );
-
+    if (params.moderatorCanPublish) {
+      du.addClass(du.el('.js_moderator_can_publish'), 'js_role_agenda_moderator');
     }
 
-    displayReferences( params.agendaUid, params.uid );
+    displayReferences(params.agendaUid, params.uid);
 
-    if ( params.hasOwnershipTransfer ) {
-
-      ownershipTransfer( {
+    if (params.hasOwnershipTransfer) {
+      ownershipTransfer({
         lang: params.lang
-      } );
-
+      });
     }
 
     // ugly hack to display state if state control is not presented
-    if ( !roles.some( r => r == ROLES.AGENDAMODERATOR || r == ROLES.AGENDAADMIN ) ) {
-
-      du.removeClass( du.el( '.js_current_state' ), 'display-none' );
-
+    if (!roles.some(r => r == ROLES.AGENDAMODERATOR || r == ROLES.AGENDAADMIN)) {
+      du.removeClass(du.el('.js_current_state'), 'display-none');
     }
 
     if (roles.includes(ROLES.EVENTEDITOR)) {
@@ -130,86 +120,63 @@ window.asap( options => {
       du.removeClass(du.el('.js_disabled_cancel'), 'display-none');
     }
 
-    if ( roles.length && params.hasCustomFields ) {
-
-      prv.load( params.agendaUid, params.uid, params.lang );
-
+    if (roles.length && params.hasCustomFields) {
+      prv.load(params.agendaUid, params.uid, params.lang);
     }
 
-    if ( roles.includes( ROLES.EVENTEDITOR ) ) {
-
-      prv.activities( params.agendaUid, params.uid, params.lang );
-
+    if (roles.includes(ROLES.EVENTEDITOR)) {
+      prv.activities(params.agendaUid, params.uid, params.lang);
     }
 
     const user = session.getUser();
 
-    if ( user ) {
-
-      prv.inbox( params, { roles, ROLES } );
-
+    if (user) {
+      prv.inbox(params, { roles, ROLES });
     }
 
-  } );
+  });
 
   eventMap();
-
 });
 
 
-function _defineRoles( params, cb ) {
+function _defineRoles(params, cb) {
+  if (!session.isLogged()) return cb(null, []);
 
-  let roles = [], user;
-
-  if ( !session.isLogged() ) return cb( null, roles );
-
-  user = session.getUser();
+  const user = session.getUser();
+  const roles = [];
 
   // user is owner
-  if ( user.uid == params.ownerUid ) {
-
-    roles.push( ROLES.EVENTEDITOR );
-
+  if (user.uid == params.ownerUid) {
+    roles.push(ROLES.EVENTEDITOR);
   }
 
-  get( params.res.detailedSession, ( err, res ) => {
+  get(params.res.detailedSession, (err, res) => {
+    if (err) return cb(err);
 
-    if ( err ) return cb( err );
-
-    if ( params.adminAgendaUids
-
-      .filter( uid => res.agendas.map( a => a.uid ).indexOf( uid ) !== -1 )
-
-      .map( uid => res.agendas.filter( a => a.uid === uid )[ 0 ].role )
-
-      .filter( r => [ 'administrator', 'moderator' ].indexOf( r ) !== -1 )
-
-      .length ) {
-
-      roles.push( ROLES.EVENTEDITOR );
-
+    if (params.adminAgendaUids
+      .filter(uid => res.agendas.map(a => a.uid).indexOf(uid) !== -1)
+      .map(uid => res.agendas.filter(a => a.uid === uid)[0].role)
+      .filter(r => ['administrator', 'moderator'].indexOf(r) !== -1)
+      .length) {
+      roles.push(ROLES.EVENTEDITOR);
     }
 
-    if ( !params.agendaUid ) return cb( null, roles );
+    if (!params.agendaUid) return cb(null, roles);
 
-    let matches = res.agendas.filter( a => a.uid === params.agendaUid );
+    const matches = res.agendas.filter(a => a.uid === params.agendaUid);
 
-    if ( !matches.length ) return cb( null, roles );
+    if (!matches.length) return cb(null, roles);
 
-    if ( matches.filter( a => a.role === 'administrator' ).length ) {
-
-      roles.push( ROLES.AGENDAADMIN );
-
+    if (matches.filter(a => a.role === 'administrator').length) {
+      roles.push(ROLES.AGENDAADMIN);
     }
 
-    if ( matches.filter( a => a.role === 'moderator' ).length ) {
-
-      roles.push( ROLES.AGENDAMODERATOR );
-
+    if (matches.filter(a => a.role === 'moderator').length) {
+      roles.push(ROLES.AGENDAMODERATOR);
     }
 
-    cb( null, roles );
-
-  } )
+    cb(null, roles);
+  });
 
 }
