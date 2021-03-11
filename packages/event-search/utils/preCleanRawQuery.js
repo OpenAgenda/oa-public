eb440834b22518167067d5783b1fab29561c47db
Kévin Berthommier <kevin.bertho@gmail.com>
2021-03-11 16:35:12 +0100

    fix(event-search): validate attendanceMode

--- packages/event-search/utils/preCleanRawQuery.js
+++ packages/event-search/utils/preCleanRawQuery.js
@@ -17,6 +17,16 @@ module.exports = (query = {}) => {
     log('error', 'provided state is invalid %j', query);
   }
 
+  try {
+    if (query.attendanceMode) {
+      update.attendanceMode = {
+        $set: [].concat(query.attendanceMode).map(s => typeof s === 'string' ? parseInt(s) : s)
+      }
+    }
+  } catch (e) {
+    log('error', 'provided attendanceMode is invalid %j', query);
+  }
+
   if (query.date && (query.date.gte || query.date.lte)) {
     update['$unset'].push('date');
     update.timings = {
