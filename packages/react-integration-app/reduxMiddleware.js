6b39d169366ee05766bef16220d22b32aef199c4
Kévin Berthommier <kevin.bertho@gmail.com>
2021-03-12 17:35:41 +0100

    feat(react-integration-app): update agenda layout data when agenda is updated

--- packages/react-integration-app/reduxMiddleware.js
+++ packages/react-integration-app/reduxMiddleware.js
@@ -1,33 +1,47 @@
 'use strict';
 
-module.exports = function reduxMiddleware(layoutStore) {
-  return (/* store */) => next => action => {
-    switch (action.type) {
-      case 'user-apps/userSettings/UPDATE_USER_SUCCESS': {
-        layoutStore.dispatch({
-          type: 'react-layouts/main/UPDATE_USER',
-          payload: {
-            user: {
-              thumbnail: action.result.image
-                ? `${action.result.image}?d=${new Date().getTime()}`
-                : null,
-              ...action.result,
+const { produce } = require('immer');
+
+module.exports = function reduxMiddleware(layoutStore, queryClient) {
+  return (/* store */) => next => async action => {
+    try {
+      switch (action.type) {
+        case 'user-apps/userSettings/UPDATE_USER_SUCCESS': {
+          layoutStore.dispatch({
+            type: 'react-layouts/main/UPDATE_USER',
+            payload: {
+              user: {
+                thumbnail: action.result.image
+                  ? `${action.result.image}?d=${new Date().getTime()}`
+                  : null,
+                ...action.result,
+              },
             },
-          },
-        });
-        break;
+          });
+          break;
+        }
+        case 'agenda-settings/agenda/EDIT_SUCCESS': {
+          const queryKeyPrefix = ['react-layouts', 'agendaAdminData'];
+
+          // Cancel any outgoing refetches (so they don't overwrite our optimistic update)
+          await queryClient.cancelQueries(queryKeyPrefix);
+
+          const queryCache = queryClient.getQueryCache();
+          const query = queryCache.find(queryKeyPrefix);
+
+          queryClient.setQueryData(
+            query.queryKey,
+            produce(draft => {
+              draft.agenda = action.result.agenda;
+            })
+          );
+          break;
+        }
+        default:
+          break;
       }
-      case 'agenda-settings/agenda/EDIT_SUCCESS': {
-        layoutStore.dispatch({
-          type: 'react-layouts/agendaAdmin/UPDATE_AGENDA',
-          payload: {
-            agenda: action.result.agenda,
-          },
-        });
-        break;
-      }
-      default:
-        break;
+    } catch (e) {
+      console.log('Error in reduxMiddleware:', e);
     }
 
     return next(action);
