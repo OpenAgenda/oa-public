f74cb56a992639224f725c107d10d2a0a7ddd48a
Kaoré <kaore@openagenda.com>
2021-03-29 12:09:31 +0200

    chore(event-form): linting

--- packages/event-form/src/components/References/index.js
+++ packages/event-form/src/components/References/index.js
@@ -16,12 +16,11 @@ const isArrayLike = (obj = {}) => Object.keys(obj).filter(key => parseInt(key) !
 
 export default class References extends Component {
 
-  constructor( props ) {
-
-    super( props );
+  constructor(props) {
+    super(props);
 
     this.state = {
-      labels: flatten( labels, props.lang ),
+      labels: flatten(labels, props.lang),
       loading: false,
       events: [],
       modal: null,
@@ -35,96 +34,75 @@ export default class References extends Component {
       },
       suggestions: []
     }
-
   }
 
   componentDidMount() {
+    if (!this.props.value || !this.props.value.length) return;
 
-    if ( !this.props.value || !this.props.value.length ) return;
+    this.setState({ loading: true });
 
-    this.setState( { loading: true } );
-
-    this.get( { uids: this.props.value } ).then( events => {
-
-      this.setState( {
+    this.get({ uids: this.props.value }).then(events => {
+      this.setState({
         loading: false,
         events
-      } );
-
-    } );
-
+      });
+    });
   }
 
-  search( query, suggested = false ) {
-
+  search(query, suggested = false) {
     const update = {
       loading: { $set: true },
       query: { $set: query.search }
     };
 
-    this.setState( {
-      search: ih( this.state.search, update )
-    } );
+    this.setState({
+      search: ih(this.state.search, update)
+    });
 
-    this.get( query, suggested ).then( events => {
-
-      this.setState( {
-        search: ih( this.state.search, {
+    this.get(query, suggested).then(events => {
+      this.setState({
+        search: ih(this.state.search, {
           loading: { $set: false },
           events: { $set: events },
           error: { $set: false },
           dropdown: { $set: true },
           suggested: { $set: suggested }
-        } )
-      } );
-
+        })
+      });
     }, err => {
-
-      this.setState( { search: ih( this.state.search, {
+      this.setState({ search: ih(this.state.search, {
         loading: { $set: false },
         error: { $set: true }
-      } ) } );
-
-    } );
-
+      }) });
+    });
   }
 
-  get( query, suggestion = false ) {
-
-    return sa.get( this.props.field.res[ suggestion ? 'suggestions' : 'references' ], ih( query, {
-      exclude: { $set: this.state.events.map( e => e.uid ) }
-    } ) ).then( res => _.get( res, 'body' ).events )
-
+  get(query, suggestion = false) {
+    return sa.get(this.props.field.res[suggestion ? 'suggestions' : 'references'], ih(query, {
+      exclude: { $set: this.state.events.map(e => e.uid) }
+    })).then(res => _.get(res, 'body').events)
   }
 
-  onSearchFocus( input ) {
+  onSearchFocus(input) {
+    if ((input || '').length > 2) return;
 
-    if ( ( input || '' ).length > 2 ) return;
+    if (!this.hasRelatedValues()) return;
 
-    if ( !this.hasRelatedValues() ) return;
-
-    this.search( this.getSuggestQuery(), true );
-
+    this.search(this.getSuggestQuery(), true);
   }
 
-  onSearchChange( name, input ) {
-
-    this.search( { search: input } );
-
+  onSearchChange(name, input) {
+    this.search({ search: input });
   }
 
   onAddEvent() {
-
-    this.setState( {
-      search: ih( this.state.search, { show: { $set: true } } )
-    } );
-
+    this.setState({
+      search: ih(this.state.search, { show: { $set: true } })
+    });
   }
 
   hasRelatedValues() {
-
-    return !!_.keys( this.props.relatedValues ).filter( field => !!this.props.relatedValues[ field ] ).length;
-
+    return !!_.keys(this.props.relatedValues).filter(field => !!this.props.relatedValues[field]).length;
   }
 
   getSuggestQuery() {
@@ -135,10 +113,10 @@ export default class References extends Component {
           ...carry,
           ...v
         }), {}) : this.props.relatedValues,
-      limit: _.get( this.props, 'field.limit', 3 )
+      limit: _.get(this.props, 'field.limit', 3)
     };
 
-    if ( this.props.field.boost ) {
+    if (this.props.field.boost) {
       q.boost = this.props.field.boost;
     }
 
@@ -146,120 +124,99 @@ export default class References extends Component {
   }
 
   onSuggestEvents() {
+    if (!this.hasRelatedValues()) {
 
-    if ( !this.hasRelatedValues() ) {
-
-      return this.setState( {
+      return this.setState({
         modal: 'nothingToSuggest'
-      } );
+      });
 
     }
 
-    this.setState( { loading: true } );
+    this.setState({ loading: true });
 
-    this.get( this.getSuggestQuery(), true ).then( additionalEvents => {
-
-      if ( !additionalEvents.length && this.state.events.length ) {
-
-        this.setState( {
+    this.get(this.getSuggestQuery(), true).then(additionalEvents => {
+      if (!additionalEvents.length && this.state.events.length) {
+        this.setState({
           loading: false,
           modal: 'noOtherSuggestions'
-        } );
-
-      } else if ( !additionalEvents.length ) {
-
-        this.setState( {
+        });
+      } else if (!additionalEvents.length) {
+        this.setState({
           loading: false,
           modal: 'noSuggestions'
-        } );
-
+        });
       } else {
+        const events = this.state.events.concat(additionalEvents);
 
-        const events = this.state.events.concat( additionalEvents );
+        this.setState({ loading: false, events });
 
-        this.setState( { loading: false, events } );
-
-        this.props.onChange( events.map( e => e.uid ) );
-
+        this.props.onChange(events.map(e => e.uid));
       }
-
-    } );
-
+    });
   }
 
   onCloseModal() {
-
-    this.setState( {
+    this.setState({
       modal: null
-    } );
-
+    });
   }
 
-  onSelectEvent( event ) {
+  onSelectEvent(event) {
+    const events = this.state.events.concat(event);
 
-    const events = this.state.events.concat( event );
-
-    this.setState( {
-      search: ih( this.state.search, {
+    this.setState({
+      search: ih(this.state.search, {
         query: { $set: null },
         show: { $set: false },
         events: { $set: []},
         dropdown: { $set: false }
-      } ),
+      }),
       events
-    } );
+    });
 
-    this.props.onChange( events.map( e => e.uid ) );
-
+    this.props.onChange(events.map(e => e.uid));
   }
 
-  onRemoveEvent( event ) {
+  onRemoveEvent(event) {
+    const events = ih(this.state.events, { $splice: [[this.state.events.map(e => e.uid).indexOf(event.uid), 1]] });
 
-    const events = ih( this.state.events, { $splice: [ [ this.state.events.map( e => e.uid ).indexOf( event.uid ), 1 ] ] } );
+    this.setState({ events });
 
-    this.setState( { events } );
-
-    this.props.onChange( events.map( e => e.uid ) );
-
+    this.props.onChange(events.map(e => e.uid));
   }
 
   onCloseDropdown() {
-
-    this.setState( {
-      search: ih( this.state.search, {
+    this.setState({
+      search: ih(this.state.search, {
         query: { $set: null },
         show: { $set: false },
         events: { $set: [] },
         loading: { $set: false },
         dropdown: { $set: false }
-      } )
-    } );
-
+      })
+    });
   }
 
   renderActions() {
-
     const { labels } = this.state;
 
     const { suggest } = this.props.field;
 
     return <div>
-      <a className="btn btn-primary margin-right-sm" onClick={this.onAddEvent.bind( this )}>{labels.addEvent}</a>
+      <a className="btn btn-primary margin-right-sm" onClick={this.onAddEvent.bind(this)}>{labels.addEvent}</a>
       { suggest ? <span>
         <span className="margin-h-sm">{labels.addEventOr}</span>
         <a
           disabled={false}
-          className="btn margin-right-sm" onClick={this.onSuggestEvents.bind( this )}>{labels.addEventSuggest}</a>
+          className="btn margin-right-sm" onClick={this.onSuggestEvents.bind(this)}>{labels.addEventSuggest}</a>
       </span> : null }
     </div>
-
   }
 
   renderSearch() {
-
     const { labels, search } = this.state;
 
-    return <OutsideClickHandler onOutsideClick={this.onCloseDropdown.bind( this )}>
+    return <OutsideClickHandler onOutsideClick={this.onCloseDropdown.bind(this)}>
       <div className="search dropdown open">
         <SearchField
           loading={ search.loading }
@@ -268,8 +225,8 @@ export default class References extends Component {
           name="search"
           label={ labels.search }
           placeholder={ labels.search }
-          onFocus={this.onSearchFocus.bind( this )}
-          onChange={ this.onSearchChange.bind( this ) }
+          onFocus={this.onSearchFocus.bind(this)}
+          onChange={ this.onSearchChange.bind(this) }
         />
         { search.dropdown ? <div className="dropdown-menu">
           { search.loading ? <ul className="list-unstyled">
@@ -283,53 +240,46 @@ export default class References extends Component {
           { search.dropdown && !search.loading && search.events.length ? <ul className="list-unstyled">
             <li key="suggestion-section-item">
               <div className="media section-item">
-                <strong className="text-muted">{labels[ search.suggested ? 'suggestionResultTitle' : 'searchResultTitle' ] }</strong>
+                <strong className="text-muted">{labels[search.suggested ? 'suggestionResultTitle' : 'searchResultTitle'] }</strong>
               </div>
             </li>
-            { search.events.map( event => <li key={'event-item-' + event.uid}>
-              <EventItem event={event} onClick={this.onSelectEvent.bind( this )} lang={this.props.lang}/>
-            </li> ) }
+            { search.events.map(event => <li key={'event-item-' + event.uid}>
+              <EventItem event={event} onClick={this.onSelectEvent.bind(this)} lang={this.props.lang}/>
+            </li>) }
           </ul> : null }
         </div> : null }
       </div>
     </OutsideClickHandler>
-
   }
 
   renderModal() {
-
     return <Modal
-      title={_.get( this.state.labels, this.state.modal + 'Title' )}
-      onClose={this.onCloseModal.bind( this )}
+      title={_.get(this.state.labels, this.state.modal + 'Title')}
+      onClose={this.onCloseModal.bind(this)}
       disableBodyScroll >
-      <p>{_.get( this.state.labels, this.state.modal )}</p>
+      <p>{_.get(this.state.labels, this.state.modal)}</p>
     </Modal>
-
   }
 
   render() {
-
     const { labels, search, events, loading } = this.state;
 
-    const { suggest } = this.props.field;
-
     return <div className="event-references">
-      {this.state.modal ? this.renderModal() : null }
+      {this.state.modal ? this.renderModal() : null}
       <div className="configure">
-        { loading ? <Spinner/> : null }
+        {loading ? <Spinner/> : null}
         <div className="references">
-          { events.length ? <ul className="list-unstyled">
-            { events.map( e => <li key={'event-reference-' + e.uid}>
+          {events.length ? <ul className="list-unstyled">
+            {events.map(e => <li key={'event-reference-' + e.uid}>
               <EventItem event={e} onRemove={this.onRemoveEvent.bind(this)} />
-            </li> ) }
+            </li>)}
           </ul> : <ul className="list-unstyled">
             <li><span className="empty">{labels.emptyReferences}</span></li>
           </ul> }
         </div>
-        { search.show ? this.renderSearch() : this.renderActions() }
+        {search.show ? this.renderSearch() : this.renderActions()}
       </div>
     </div>
-
   }
 
 }
