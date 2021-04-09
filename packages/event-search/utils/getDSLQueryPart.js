f5499cd51f1e643496b8f006d65c1394cfc655ab
Kaoré <kaore@openagenda.com>
2021-04-09 15:45:25 +0200

    feat(event-search): addMethod filter, addMethods aggregation and associated test file

--- packages/event-search/utils/getDSLQueryPart.js
+++ packages/event-search/utils/getDSLQueryPart.js
@@ -1,5 +1,6 @@
 'use strict';
 
+const clean = require('@openagenda/validators/schema/clean');
 const _ = require('lodash');
 const getFormSchemaAdditionalFields = require('./getFormSchemaAdditionalFields');
 
@@ -65,7 +66,8 @@ function _getQueryFilterParts(cleanQuery, additionalFields) {
 
   const parts = [];
   const {
-    relative
+    relative,
+    addMethod
   } = cleanQuery;
 
   if (_.get(cleanQuery, 'set')) {
@@ -111,6 +113,10 @@ function _getQueryFilterParts(cleanQuery, additionalFields) {
 
   if (_.get(cleanQuery, 'sourceAgendaUid', []).length) {
     parts.push(_filterBySourceAgendaUid(cleanQuery.sourceAgendaUid));
+  }
+
+  if (addMethod?.length) {
+    parts.push(_terms('addMethod', addMethod));
   }
 
   Object.keys(termsFiltersMap)
