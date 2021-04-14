708a7576bf6af75a47736b63f30945896f58b4f7
Kévin Berthommier <kevin.bertho@gmail.com>
2021-04-14 10:19:54 +0200

    fix(agenda-docx): fix tests

---
+++ agenda-docx/config.dev.js
@@ -0,0 +1,19 @@
+'use strict';
+
+module.exports = {
+  s3: {
+    region: 'eu-west-1',
+    accessKeyId: process.env.AWS_DEV_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_DEV_SECRET_ACCESS_KEY,
+    bucket: process.env.AWS_TEST_BUCKET,
+  },
+  queue: {
+    namespace: 'docx',
+    separator: ':',
+    redis: {
+      port: 6379,
+      host: 'localhost'
+    }
+  },
+  localTmpPath: '/var/tmp'
+};
