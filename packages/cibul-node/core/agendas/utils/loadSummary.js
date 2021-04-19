859fecb553f3b4cc2bea72ba7090ee4ec0c7f341
Kaoré <kaore@openagenda.com>
2021-04-19 15:29:36 +0200

    feat(cibul-node): summary on detailed agenda get

---
+++ packages/cibul-node/core/agendas/utils/loadSummary.js
@@ -0,0 +1,54 @@
+'use strict';
+
+const getRecentlyAddedEvents = (core, agenda) => {
+  const recentThreshold = new Date();
+  recentThreshold.setDate(recentThreshold.getDate() - core.getConfig().agendaSearchRecentThreshold);
+
+  return core.services.eventSearch.agendas(agenda).search({
+    originAgendaUid: agenda.uid, // own contributions
+    state: null, // ... of any state
+    createdAt: { gte: recentThreshold },
+    date: { gte: new Date() }
+  }, { size: 0 }, { aggregations: ['addMethods'] })
+    .then(({ aggregations }) => aggregations.addMethods.reduce((carry, { key, eventCount }) => ({
+      ...carry,
+      [key]: eventCount
+    }), { contribution: 0, shared: 0, aggregation: 0 }));
+}
+
+module.exports = async (core, agenda, options = {}) => {
+  const {
+    access = 'public'
+  } = options;
+
+  const { search } = core.services.eventSearch.agendas(agenda);
+  
+  const publishedResult = await search({}, { size: 0 }, {
+    aggregations: ['cities', 'departments', 'regions', 'relative', 'keywords']
+  }).then(({ aggregations }) => aggregations);
+
+  const summary = {
+    keywords: ['cities', 'departments', 'regions', 'keywords'].reduce(
+      (keywords, agg) => keywords.concat(publishedResult[agg].map(a => a.key)),
+      []
+    ),
+    publishedEvents: publishedResult.relative.reduce((carry, { key, eventCount }) => ({
+      ...carry,
+      [key]: eventCount
+    }), {}),
+    recentlyAddedEvents: await getRecentlyAddedEvents(core, agenda)
+  };
+
+  if (['administrator', 'moderator', 'internal'].includes(access)) {
+    summary.eventCountsByState = await search({
+      state: null
+    }, { size: 0 }, {
+      aggregations: ['states']
+    }).then(({ aggregations }) => aggregations.states.reduce((carry, { key, eventCount }) => ({
+      ...carry,
+      [key]: eventCount
+    }), { '2': 0, '1': 0, '0': 0, '-1': 0 }));
+  }
+
+  return summary;
+}
