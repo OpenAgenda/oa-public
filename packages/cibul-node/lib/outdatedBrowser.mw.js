0efc85f6a29a88b6552de1870297eb836e0b8685
Kévin Berthommier <bertho-zero@users.noreply.github.com>
2021-05-03 16:56:57 +0200

    feat(cibul-node): use outdated script in contribute app

---
+++ packages/cibul-node/lib/outdatedBrowser.mw.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const { matchesUA } = require('browserslist-useragent');
+
+module.exports = function outdatedBrowserMw(req, res, next) {
+  const userAgent = req.headers['user-agent'];
+  const outdatedBrowser = !matchesUA(userAgent, {
+    ignoreMinor: true,
+    ignorePatch: true,
+    allowHigherVersions: true
+  });
+
+  if (outdatedBrowser) {
+    req.outdatedBrowser = true;
+  }
+
+  next();
+}
