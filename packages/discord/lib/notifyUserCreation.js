5fd346223e7bfa046d85fec3311b56cb9c776e9f
ohick <oliviahick1@gmail.com>
2021-05-25 12:26:14 +0200

    fix(discord): fixed undefiend userUid

--- packages/discord/lib/notifyUserCreation.js
+++ packages/discord/lib/notifyUserCreation.js
@@ -3,12 +3,12 @@
 const Discord = require('discord.js');
 
 module.exports = channel => user => {
-  const { fullName, email, userUid } = user;
+  const { fullName, email, uid } = user;
   const embed = new Discord.MessageEmbed()
     .setColor('#0d944a')
     .setTitle(fullName)
     .setDescription('Nouvel utilisateur')
-    .setURL(`https://openagenda.com/admin/users?userUid=${userUid}`)
+    .setURL(`https://openagenda.com/admin/users?userUid=${uid}`)
     .addField('Email', email);
   return channel.send(embed);
 };
