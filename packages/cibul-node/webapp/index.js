92862669858381580adc7ae292a904f1fb27a0ef
Kaoré <kaore@openagenda.com>
2021-06-08 11:04:16 +0200

    feat(cibul-node): updated legacy embeds app integration

--- packages/cibul-node/webapp/index.js
+++ packages/cibul-node/webapp/index.js
@@ -306,9 +306,12 @@ const initialState = async req => {
       lang,
       apiRoot: `http://localhost:${config.port}`,
       res: {
+        legacy: '/agendas/:agendaUid/admin/webembed',
+        events: '/api/agendas/:agendaUid/events?state[]=2&state[]=1&state[]=0',
         embeds: '/api/agendas/:agendaUid/embeds',
         preview: '/agendas/:agendaUid/previewEmbeds/:embedUid/events',
-        previewScript: '/js/embed/cibulBodyWidget.js'
+        previewScript: '/js/embed/cibulBodyWidget.js',
+        agendaSettings: '/api/agendas/:agendaUid'
       }
     },
     agendaActivities: {
