0a5e7eea9d3afea2609be66ba59dcfbf6da937c1
olivia <oliviahick1@gmail.com>
2021-06-22 18:38:18 +0200

    fix(react-share-menus): export modal integration

--- public/react-share-menus/stories/exportModal.stories.js
+++ public/react-share-menus/stories/exportModal.stories.js
@@ -1,30 +1,74 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '@openagenda/bs-templates/compiled/main.css';
 
 import ExportModal from '../src/components/ExportModal';
+import Canvas from './decorators/Canvas';
 
 export default {
   title: 'Export',
   component: 'ExportModal',
+  decorators: [Canvas],
 };
 
-export const Export = () => (
-  <ExportModal
-    res={[
-      { format: 'json', url: 'testurl.com' },
-      { format: 'pdf', url: 'testurl.com' },
-      { format: 'xl', url: 'testurl.com' },
-      { format: 'gcal', url: 'testurl.com' },
-      { format: 'ical', url: 'testurl.com' },
-      { format: 'csv', url: 'testurl.com' },
-      { format: 'ics', url: 'testurl.com' },
-      { format: 'rss', url: 'testurl.com' },
-    ]}
-    languages={[
-      { label: 'français', value: 'fr' },
-      { label: 'anglais', value: 'en' },
-      { label: 'espagnol', value: 'es' },
-      { label: 'allemand', value: 'd' },
-    ]}
-  />
-);
+export const Export = () => {
+  const [display, setDisplay] = useState(false);
+  const [languageQuery, setLanguageQuery] = useState('');
+
+  const urls = {
+    json: '/',
+    pdf: '/',
+    xl: '/',
+    gcal: '/',
+    ical: '/',
+    csv: '/',
+    ics: '/',
+    rss: '/',
+  };
+
+  const closeModal = () => {
+    setDisplay(false);
+  };
+
+  const handleExportLanguage = format => {
+    return format === 'csv' || format === 'xl'
+      ? `&cols.lang=${languageQuery}`
+      : '';
+  };
+
+  const formatExportLinks = res => {
+    const newUrls = Object.keys(res).reduce(
+      (url, key) => ({
+        ...url,
+        [key]: urls[key] + handleExportLanguage(key),
+      }),
+      {}
+    );
+    return newUrls;
+  };
+
+  const handleQuery = lang => {
+    setLanguageQuery(lang);
+    return languageQuery;
+  };
+
+  return (
+    <>
+      <button
+        className="js_export_button btn btn-link"
+        type="button"
+        onClick={() => setDisplay(true)}
+      >
+        <i className="fa fa-external-link" />
+        <span>&nbsp; Exporter la sélection</span>
+      </button>
+      {display ? (
+        <ExportModal
+          exportLanguage={(lang, format) => handleQuery(lang, format)}
+          onClose={() => setDisplay(false)}
+          res={formatExportLinks(urls)}
+          languages={['fr', 'de', 'en', 'es', 'it', 'nl']}
+        />
+      ) : null}
+    </>
+  );
+};
