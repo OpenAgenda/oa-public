cd1dfa3a73b077413c3963e4bf29ebfdf898b17b
Kaoré <kaore@openagenda.com>
2021-09-08 19:15:45 +0200

    chore(labels): share messages

---
+++ packages/labels/agenda-contribute/share.js
@@ -0,0 +1,12 @@
+"use strict";
+
+module.exports = {
+  "alreadyReferencedAndPublished": {
+    "fr": "L'événement est déjà référencé sur l'agenda '%agenda%'",
+    "en": "The event is already referenced on the agenda '%agenda%'"
+  },
+  "alreadyReferencedAndUnpublished": {
+    "fr": "L'événement est déjà référencé sur l'agenda '%agenda%' mais n'y a pas encore été publié",
+    "en": "The event is already referenced on the agenda '%agenda%' but it hasn't yet been published"
+  }
+}
