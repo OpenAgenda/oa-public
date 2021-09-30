81c4a9aafb3927d0b1a08807f8622cf06e5ed85f
Kaoré <kaore@openagenda.com>
2021-09-30 17:50:44 +0200

    fix(legacy): avoid hook infinite loop in ListMenu component

--- packages/legacy/embeds/app/src/containers/DashboardWrapper.js
+++ packages/legacy/embeds/app/src/containers/DashboardWrapper.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
+import { useLayoutData } from '@openagenda/react-shared';
 import { hot } from 'react-hot-loader/root';
 
 import Dashboard from './Dashboard';
 
-function WrapperComponent({ agendaUid, selectionMenuContainerRef }) {
+function WrapperComponent() {
   const res = useSelector(state => state.res ?? null);
+
+  const {
+    agenda,
+    filtersContainerRef: selectionMenuContainerRef
+  } = useLayoutData();
 
   return (
     <Dashboard
-      agendaUid={agendaUid}
+      agendaUid={agenda.uid}
       selectionMenuContainerRef={selectionMenuContainerRef}
       res={res}
     />
