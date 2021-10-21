6ed7921645bb933122d59255d098150e485bc2b4
Kaoré <kaore@openagenda.com>
2021-10-21 09:48:31 +0200

    chore(cibul-node): linting

--- packages/cibul-node/services/agendaContribute/middlewares/verifyMemberAuthorization.js
+++ packages/cibul-node/services/agendaContribute/middlewares/verifyMemberAuthorization.js
@@ -1,15 +1,8 @@
-"use strict";
+'use strict';
 
-const _ = require('lodash');
 const getLabel = require('@openagenda/labels/makeLabelGetter')(
   require('@openagenda/labels/agenda-contribute/authorization')
 );
-const {
-  isSuperiorTo
-} = require('@openagenda/members').utils.compareRoles;
-const types = require('@openagenda/agendas/service/validate/contributionTypes');
-
-const log = require('@openagenda/logs')('services/agendaContribute/middlewares/verifyMemberAuthorization');
 
 module.exports = async (req, res, next) => {
   const {
@@ -21,12 +14,16 @@ module.exports = async (req, res, next) => {
       code: 403,
       message: getLabel('noAccessToClosedAgenda', req.lang)
     });
-  } else if (await core.agendas(req.agenda).settings.isMembersOnly() && !req.member) {
+  }
+
+  if (await core.agendas(req.agenda).settings.isMembersOnly() && !req.member) {
     return res.redirect(302, `/${req.agenda.slug}/request-contribute/conversation/create`);
-  } else if (!req.member) {
+  }
+
+  if (!req.member) {
     req.member = await core.agendas(req.agenda).members.create(req.user.uid, 'contributor');
   }
-    
+
   req.authorizations = await core.users(req.user.uid).agendas(req.agenda.uid).getAuthorizations();
 
   if (!req.authorizations.canCreateEvent) {
@@ -37,15 +34,14 @@ module.exports = async (req, res, next) => {
   }
 
   next();
-}
-
+};
 
 module.exports.edit = async (req, res, next) => {
   const {
     core
   } = req.app.services;
 
-  core 
+  core
     .users(req.user.uid)
     .agendas(req.agenda.uid)
     .getAuthorizations(req.event)
@@ -61,4 +57,4 @@ module.exports.edit = async (req, res, next) => {
 
       next();
     });
-}
+};
