05bcb065bd14fe850eec1f8529e9e966e25bfcb4
Kévin Berthommier <bertho-zero@users.noreply.github.com>
2021-11-02 17:40:00 +0100

    feat(polyfills): add ca and eu locales

--- public/polyfills/intl-locales.js
+++ public/polyfills/intl-locales.js
@@ -7,6 +7,8 @@ require('@formatjs/intl-pluralrules/locale-data/en');
 require('@formatjs/intl-pluralrules/locale-data/es');
 require('@formatjs/intl-pluralrules/locale-data/fr');
 require('@formatjs/intl-pluralrules/locale-data/it');
+require('@formatjs/intl-pluralrules/locale-data/ca');
+require('@formatjs/intl-pluralrules/locale-data/eu');
 
 require('@formatjs/intl-displaynames/locale-data/br');
 require('@formatjs/intl-displaynames/locale-data/de');
@@ -14,6 +16,8 @@ require('@formatjs/intl-displaynames/locale-data/en');
 require('@formatjs/intl-displaynames/locale-data/es');
 require('@formatjs/intl-displaynames/locale-data/fr');
 require('@formatjs/intl-displaynames/locale-data/it');
+require('@formatjs/intl-displaynames/locale-data/ca');
+require('@formatjs/intl-displaynames/locale-data/eu');
 
 require('@formatjs/intl-listformat/locale-data/br');
 require('@formatjs/intl-listformat/locale-data/de');
@@ -21,6 +25,8 @@ require('@formatjs/intl-listformat/locale-data/en');
 require('@formatjs/intl-listformat/locale-data/es');
 require('@formatjs/intl-listformat/locale-data/fr');
 require('@formatjs/intl-listformat/locale-data/it');
+require('@formatjs/intl-listformat/locale-data/ca');
+require('@formatjs/intl-listformat/locale-data/eu');
 
 require('@formatjs/intl-numberformat/locale-data/br');
 require('@formatjs/intl-numberformat/locale-data/de');
@@ -28,6 +34,8 @@ require('@formatjs/intl-numberformat/locale-data/en');
 require('@formatjs/intl-numberformat/locale-data/es');
 require('@formatjs/intl-numberformat/locale-data/fr');
 require('@formatjs/intl-numberformat/locale-data/it');
+require('@formatjs/intl-numberformat/locale-data/ca');
+require('@formatjs/intl-numberformat/locale-data/eu');
 
 require('@formatjs/intl-relativetimeformat/locale-data/br');
 require('@formatjs/intl-relativetimeformat/locale-data/de');
@@ -35,6 +43,8 @@ require('@formatjs/intl-relativetimeformat/locale-data/en');
 require('@formatjs/intl-relativetimeformat/locale-data/es');
 require('@formatjs/intl-relativetimeformat/locale-data/fr');
 require('@formatjs/intl-relativetimeformat/locale-data/it');
+require('@formatjs/intl-relativetimeformat/locale-data/ca');
+require('@formatjs/intl-relativetimeformat/locale-data/eu');
 
 require('@formatjs/intl-datetimeformat/locale-data/br');
 require('@formatjs/intl-datetimeformat/locale-data/de');
@@ -42,3 +52,5 @@ require('@formatjs/intl-datetimeformat/locale-data/en');
 require('@formatjs/intl-datetimeformat/locale-data/es');
 require('@formatjs/intl-datetimeformat/locale-data/fr');
 require('@formatjs/intl-datetimeformat/locale-data/it');
+require('@formatjs/intl-datetimeformat/locale-data/ca');
+require('@formatjs/intl-datetimeformat/locale-data/eu');
