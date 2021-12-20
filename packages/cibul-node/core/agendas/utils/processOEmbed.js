435605498c0cf69800b87c238006325b7bed325a
ohick <oliviahick1@gmail.com>
2021-12-20 11:16:20 +0100

    feat(cibul-node): option to include non oembedded links

--- packages/cibul-node/core/agendas/utils/processOEmbed.js
+++ packages/cibul-node/core/agendas/utils/processOEmbed.js
@@ -3,15 +3,16 @@
 const _ = require('lodash');
 const log = require('@openagenda/logs')('core/utils/processOEmbed');
 
-module.exports = (oembed, text, current = []) => {
+module.exports = (oembed, text, { current = [], includeEmbedlessLinks = false }) => {
   if (!oembed) {
     log('oembed service is not initialized');
     return [];
   }
+
   log('processing oembed');
   return oembed.fromMarkdown(_.keys(text).reduce((concatenated, lang) => [
     concatenated, _.get(text, lang)
   ].join('\n'), ''), {
-    current
+    current, includeEmbedlessLinks
   }).then(links => links.map(link => _.set(link, 'type', 'oembed')));
 };
