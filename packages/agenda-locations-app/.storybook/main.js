fcb684e86473e5da0997b97f05e5e4ac7b3a0a51
Kévin Berthommier <bertho-zero@users.noreply.github.com>
2021-12-24 10:40:54 +0100

    feat(agenda-locations-app): update storybook

--- packages/agenda-locations-app/.storybook/main.js
+++ packages/agenda-locations-app/.storybook/main.js
@@ -3,4 +3,19 @@ module.exports = {
     '../stories/**/*.stories.mdx',
     '../stories/**/*.stories.@(js|jsx|ts|tsx)',
   ],
+  core: {
+    builder: 'webpack5',
+  },
+  webpackFinal(config) {
+    config.module.rules.push({
+      test: /\.(js|mjs|jsx)$/,
+      enforce: 'pre',
+      loader: require.resolve('source-map-loader'),
+      resolve: {
+        fullySpecified: false
+      }
+    });
+
+    return config;
+  }
 };
