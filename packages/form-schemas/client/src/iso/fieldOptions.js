80db290bc60587c816f44b3c2275aadece644082
Kaoré <kaore@openagenda.com>
2021-12-27 16:53:18 +0100

    chore(form-schemas): linting and considering that an id=0 is an id

--- packages/form-schemas/client/src/iso/fieldOptions.js
+++ packages/form-schemas/client/src/iso/fieldOptions.js
@@ -1,9 +1,35 @@
-"use strict";
+'use strict';
 
-const _ = {
-  get: require( 'lodash/get' ),
-  flatten: require( 'lodash/flatten' ),
-  uniq: require( 'lodash/uniq' )
+const _ = require('lodash');
+
+const fieldHasUnnassignedOptions = field => !!(field?.options ?? []).filter(o => o.id === undefined).length;
+const fieldHasSuperiorOptions = (field, nextOptionId) => !!(field?.options ?? []).filter(o => o.id > nextOptionId).length;
+
+function fieldAssignOptionIds(field, nextOptionId) {
+  let nextId = nextOptionId;
+  field.options.forEach(o => {
+    if (o.id !== undefined) {
+      return;
+    }
+    nextId += 1;
+    o.id = nextId;
+  });
+
+  return nextId;
+}
+
+function extractNextOptionId(formSchemaData) {
+  const definedNextOptionId = formSchemaData?.nextOptionId ?? 0;
+
+  const optionIds = _.uniq(_.flatten(
+    _.get(formSchemaData, 'fields', [])
+      .filter(f => f.options)
+      .map(f => f.options)
+  ));
+
+  const biggestId = optionIds.reduce((bId, optionId) => (bId < optionId ? optionId : bId), 0);
+
+  return definedNextOptionId > biggestId ? definedNextOptionId : biggestId + 1;
 }
 
 module.exports = {
@@ -11,44 +37,4 @@ module.exports = {
   fieldHasUnnassignedOptions,
   fieldAssignOptionIds,
   fieldHasSuperiorOptions
-}
-
-function fieldHasUnnassignedOptions( field ) {
-
-  return !!_.get( field, 'options', [] ).filter( o => !o.id ).length;
-
-}
-
-function fieldHasSuperiorOptions( field, nextOptionId ) {
-
-  return !!_.get( field, 'options', [] ).filter( o => o.id > nextOptionId ).length;
-
-}
-
-
-function fieldAssignOptionIds( field, nextOptionId ) {
-
-  field.options.forEach( o => {
-
-    if ( !o.id ) o.id = nextOptionId++;
-
-  } );
-
-  return nextOptionId;
-
-}
-
-function extractNextOptionId( formSchemaData ) {
-
-  const definedNextOptionId = _.get( formSchemaData, 'nextOptionId', 0 );
-
-  const optionIds = _.uniq( _.flatten( _.get( formSchemaData, 'fields', [] )
-    .filter( f => f.options )
-    .map( f => f.options )
-  ) );
-
-  const biggestId = optionIds.reduce( ( biggestId, optionId ) => biggestId < optionId ? optionId : biggestId, 0 );
-
-  return definedNextOptionId > biggestId ? definedNextOptionId : biggestId + 1;
-
-}
+};
