c1d64b157c48c0098e8728f1160b6493329663a5
Kévin Berthommier <bertho-zero@users.noreply.github.com>
2022-01-06 11:53:48 +0100

    fix(agenda-portal): reload partials

--- agenda-portal/dev/watchViews.js
+++ agenda-portal/dev/watchViews.js
@@ -1,9 +1,29 @@
 'use strict';
 
+const fs = require('fs');
+const path = require('path');
 const clientRefresher = require('browser-refresh-client');
 
-module.exports = function watchViews(hbs) {
+function registerPartial(hbs, partialsDir, filePath) {
+  const data = fs.readFileSync(filePath, 'utf-8');
+  const ext = path.extname(filePath);
+  const templateName = path.relative(partialsDir, filePath)
+    .slice(0, -(ext.length)).replace(/[ -]/g, '_')
+    .replace(/\\/g, '/');
+
+  hbs.registerPartial(templateName, data);
+}
+
+module.exports = function watchViews(hbs, partialsDir) {
   clientRefresher
     .enableSpecialReload('*.hbs')
-    .onFileModified(path => delete hbs.cache[path]);
+    .onFileModified(filePath => {
+      const isPartial = filePath.startsWith(partialsDir);
+
+      if (isPartial) {
+        registerPartial(hbs, partialsDir, filePath);
+      } else {
+        delete hbs.cache[filePath];
+      }
+    });
 };
