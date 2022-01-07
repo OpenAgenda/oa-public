13baceeb4befff7a6e9ddf11d63644422890e60c
Kaoré <kaore@openagenda.com>
2022-01-07 11:54:25 +0100

    feat(cibul-templates): display dates properly in event additional fields

--- packages/cibul-templates/event/js/additionalFields.utils.js
+++ packages/cibul-templates/event/js/additionalFields.utils.js
@@ -1,3 +1,5 @@
+import moment from 'moment-timezone';
+
 function hasAdditionalFields(schema) {
   return !!schema.fields.filter(f => f.schemaType !== 'event').length
 }
@@ -10,7 +12,7 @@ function flatLabel(label, lang) {
   return label[lang] ?? label[Object.keys(label).shift()];
 }
 
-function formatValue(field, value, lang) {
+function formatValue(field, value, { lang, timezone }) {
   if (Array.isArray(value) && value.length === 1 && value[0] === null) {
     return null;
   }
@@ -37,6 +39,10 @@ function formatValue(field, value, lang) {
     return labels.length ? labels : null;
   }
 
+  if (field.fieldType === 'date') {
+    return moment.tz(value, timezone).locale(lang).format('dddd D MMMM YYYY');
+  }
+
   if (['image', 'file'].includes(field.fieldType) && value) {
     return {
       link: `https://${(field.store ?? { type: 's3', bucket: 'cibul' }).bucket}.s3.amazonaws.com/${value.filename}`,
@@ -52,10 +58,12 @@ function formatAdditionalFieldData(schema, event, lang) {
     .filter(f => f.schemaType !== 'event')
     .filter(f => f.fieldType !== 'abstract');
 
+  const timezone = event.timezone ?? (event.location?.timezone ?? 'Europe/Paris');
+
   return additionalFields.map(field => {
     const value = event[field.field];
 
-    const formattedValue = formatValue(field, value, lang);
+    const formattedValue = formatValue(field, value, { lang, timezone });
     const label = flatLabel(field.label, lang);
 
     return {
