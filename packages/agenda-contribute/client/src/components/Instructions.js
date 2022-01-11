1cf2106552b4cd6d69e5d36fcd1fd02af85814da
Kaoré <kaore@openagenda.com>
2022-01-11 15:36:55 +0100

    fix(agenda-contribute): target blank links in instructions

--- packages/agenda-contribute/client/src/components/Instructions.js
+++ packages/agenda-contribute/client/src/components/Instructions.js
@@ -3,6 +3,6 @@ import ReactMarkdown from 'react-markdown';
 
 export default ( { message, className, boxed } ) => message ? <div className={ className || 'wsq' }>
   <div className="event-instruction padding-all-md padding-bottom-sm boxed">
-    <ReactMarkdown>{message}</ReactMarkdown>
+    <ReactMarkdown linkTarget="_blank">{message}</ReactMarkdown>
   </div>
 </div> : null;
