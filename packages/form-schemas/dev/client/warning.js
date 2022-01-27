7c14552b05f90580dcb5fe1f3192505063e7cad3
Kaoré <kaore@openagenda.com>
2022-01-27 12:03:32 +0100

    chore(form-schemas): linting

--- packages/form-schemas/dev/client/warning.js
+++ packages/form-schemas/dev/client/warning.js
@@ -1,29 +1,13 @@
-import React, { Component } from 'react';
-import { render } from 'react-dom';
+import renderSchemaFromProps from './renderSchemaFromProps';
 
-import FormSchemaComponent from '../../client/src/index';
-
-if (module.hot) module.hot.accept();
-
-class Main extends Component {
-  render() {
-    const props = {
-      lang: 'fr',
-      unloadWarning: true,
-      schema: {
-        fields : [{
-          field : 'bewarned',
-          fieldType : 'text',
-          label : 'Soyez avertis'
-        }]
-      }
-    }
-    return <div className="container wsq top-margined col-lg-offset-4 col-lg-4 col-md-offset-3 col-md-6 col-sm-offset-2 col-sm-8">
-      <div className="row margin-v-md margin-h-sm">
-        <FormSchemaComponent { ...props } />
-      </div>
-    </div>
+renderSchemaFromProps({
+  lang: 'fr',
+  unloadWarning: true,
+  schema: {
+    fields: [{
+      field: 'bewarned',
+      fieldType: 'text',
+      label: 'Soyez avertis'
+    }]
   }
-}
-
-render(<Main />, document.getElementById('app'));
+});
