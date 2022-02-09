0bf19e1b544f24aa4b5d9797d06895acced45b65
Kaoré <kaore@openagenda.com>
2022-02-09 17:26:15 +0100

    chore(form-schemas): linting

--- packages/form-schemas/dev/client/markdown.js
+++ packages/form-schemas/dev/client/markdown.js
@@ -1,17 +1,14 @@
 import _ from 'lodash';
 
-import PropTypes from 'prop-types';
 import React, { Component } from 'react';
 import { render } from 'react-dom';
 
 import FormSchemaComponent from '../../client/src/index';
 
-if ( module.hot ) module.hot.accept();
+if (module.hot) module.hot.accept();
 
 class Main extends Component {
-
   render() {
-
     const props = {
       res: {
         post: '',
@@ -19,73 +16,79 @@ class Main extends Component {
       },
       lang: 'fr',
       values: {
-        singlelangfield: "Avant\n\nhttp://le\\_monde.com\n\net après",
-        multilangfield: { "fr" : '*Et boum*' }
+        singlelangfield: 'Avant\n\nhttp://le\\_monde.com\n\net après',
+        multilangfield: { fr: '*Et boum*' }
       },
       schema: {
-        "fields" : [ {
-          "field" : "simpletextfield",
-          "fieldType" : "text",
-          "label" : {
-            "fr" : "C'est un champ basique"
+        fields: [{
+          field: 'simpletextfield',
+          fieldType: 'text',
+          label: {
+            fr: 'C\'est un champ basique'
           }
         }, {
-          "field" : "singlelangfield",
-          "fieldType" : "markdown",
-          "optional" : false,
-          "label" : {
-            "fr" : "C'est un champ qui pond du markdown"
+          field: 'singlelangfield',
+          fieldType: 'markdown',
+          optional: false,
+          label: {
+            fr: 'C\'est un champ qui pond du markdown'
           },
-          "info" : {
-            "fr" : "Le texte info"
+          info: {
+            fr: 'Le texte info'
           },
-          "sub" : {
-            "fr" : "Le texte dessous"
+          sub: {
+            fr: 'Le texte dessous'
           },
-          "max" : 10000
+          max: 10000
         }, {
-          "field" : "multilangfield",
-          "fieldType" : "markdown",
-          "optional" : false,
-          "languages" : [ "en", "fr" ],
-          "label" : {
-            "fr" : "C'est pareil, mais multilingue"
+          field: 'multilangfield',
+          fieldType: 'markdown',
+          optional: false,
+          languages: ['en', 'fr'],
+          label: {
+            fr: 'C\'est pareil, mais multilingue'
           },
-          "info" : {
-            "fr" : "Le texte info"
+          info: {
+            fr: 'Le texte info'
           },
-          "placeholder" : {
-            "fr" : "S'affiche dans le champ"
+          placeholder: {
+            fr: "S'affiche dans le champ"
           },
-          "sub" : {
-            "fr" : "Le texte dessous"
+          sub: {
+            fr: 'Le texte dessous'
           },
-          "max" : 4000
-        } ]
+          max: 4000
+        }, {
+          field: 'monolingualmarkdown',
+          optional: false,
+          fieldType: 'markdown',
+          label: 'Monolingual markdown field'
+        }]
       }
     };
 
-    return <div className="container top-margined">
-      <div className="row">
-        <div className="wsq col-lg-5 col-lg-offset-1">
-          <div className="margin-v-md margin-h-sm">
-            <p>An HTML field.</p>
-            <FormSchemaComponent { ...props } onChange={this.setState.bind( this )} />
+    return (
+      <div className="container top-margined">
+        <div className="row">
+          <div className="wsq col-lg-5 col-lg-offset-1">
+            <div className="margin-v-md margin-h-sm">
+              <p>An HTML field.</p>
+              <FormSchemaComponent {...props} onChange={state => this.setState(state)} />
+            </div>
           </div>
-        </div>
-        <div className="wsq col-lg-5 col-lg-offset-1">
-          <div className="margin-v-md margin-h-sm">
-            { _.get( this.state, 'values.singlelangfield' ) ?
-            <pre style={{minHeight: 400}}>
-              {_.get( this.state, 'values.singlelangfield' )}
-            </pre> : null }
+          <div className="wsq col-lg-5 col-lg-offset-1">
+            <div className="margin-v-md margin-h-sm">
+              {_.get(this.state, 'values.singlelangfield') ? (
+                <pre style={{ minHeight: 400 }}>
+                  {_.get(this.state, 'values.singlelangfield')}
+                </pre>
+              ) : null}
+            </div>
           </div>
         </div>
       </div>
-    </div>
-
+    );
   }
-
 }
 
-render( <Main />, document.getElementById( 'app' ) );
+render(<Main />, document.getElementById('app'));
