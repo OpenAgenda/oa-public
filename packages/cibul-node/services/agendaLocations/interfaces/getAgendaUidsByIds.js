f27c86b7505b185918e938b054e71bf334984e7f
Kaoré <kaore@openagenda.com>
2022-02-22 15:47:22 +0100

    fix(cibul-node): agendaLocations interface adapts to provided input

--- packages/cibul-node/services/agendaLocations/interfaces/getAgendaUidsByIds.js
+++ packages/cibul-node/services/agendaLocations/interfaces/getAgendaUidsByIds.js
@@ -1,5 +1,17 @@
 'use strict';
 
 module.exports = function getAgendaUidsByIds(services) {
-  return ids => services.agendas.list({ ids }, { private: null }).then(({ agendas }) => agendas.map(a => a.uid));
+  return ids => services.agendas.list({ ids }, {
+    private: null,
+    internal: true
+  }).then(
+    ({ agendas }) => {
+      const result = agendas.map(a => ({
+        uid: a.uid,
+        id: a.id
+      }));
+
+      return Array.isArray(ids) ? result : result.pop();
+    }
+  );
 };
