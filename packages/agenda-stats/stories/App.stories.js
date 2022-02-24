47650857f96c5b9d3b6f857a153926b122bdbd23
Kévin Berthommier <bertho-zero@users.noreply.github.com>
2022-02-24 10:41:14 +0100

    fix(agenda-stats): fix layout data in storybook

--- packages/agenda-stats/stories/App.stories.js
+++ packages/agenda-stats/stories/App.stories.js
@@ -1,7 +1,6 @@
-import React from 'react';
 import { createMemoryHistory } from 'history';
 import { storiesOf } from '@storybook/react';
-import { wrapApp, LayoutDataContext } from '@openagenda/react-shared';
+import { wrapApp } from '@openagenda/react-shared';
 import createApp from '../src/app';
 import PageDecorator from './decorators/PageDecorator';
 
@@ -19,25 +18,22 @@ const getDefaultState = () => ({
 
 export default storiesOf('App', module)
   .addDecorator(PageDecorator)
-  .add('all', () => (
-    <LayoutDataContext.Provider value={{ lang: 'fr' }}>
-      {wrapApp(
-        createApp({
-          history: createMemoryHistory(),
-          initialState: getDefaultState(),
-        }),
-        {
-          extraProps: {
-            agenda: {
-              uid: 48959239,
-              slug: 'la-gargouille',
-              title: 'La gargouille',
-            },
-            agendaSchema: {
-              fields: [],
-            },
-          },
-        }
-      )}
-    </LayoutDataContext.Provider>
+  .add('all', () => wrapApp(
+    createApp({
+      history: createMemoryHistory(),
+      initialState: getDefaultState(),
+    }),
+    {
+      extraProps: {
+        lang: 'fr',
+        agenda: {
+          uid: 48959239,
+          slug: 'la-gargouille',
+          title: 'La gargouille',
+        },
+        agendaSchema: {
+          fields: [],
+        },
+      },
+    }
   ));
