55aebf716ba468e7b22c862e54f941e2fae138a8
Kévin Berthommier <bertho-zero@users.noreply.github.com>
2022-02-25 09:14:34 +0100

    fix(labels): fix missing labels

---
+++ packages/labels/agenda-contribute/authorization.js
@@ -0,0 +1,35 @@
+"use strict";
+
+module.exports = {
+  "noAccessToClosedAgenda": {
+    "fr": "Cet agenda est fermé à la contribution",
+    "en": "This agenda is not open to contributions",
+    "de": "Diese Agenda ist nicht offen für Beiträge",
+    "es": "Este agenda está cerrado a la contribución",
+    "it": "Questo agenda è chiuso ai contributi",
+    "io": "crwdns11478:0crwdne11478:0"
+  },
+  "noAccessToEdit": {
+    "fr": "Vous n'avez pas accès à l'édition de cet événement",
+    "en": "You do not have edit rights to this event",
+    "it": "Non avete accesso all'edizione di questo evento",
+    "io": "crwdns11480:0crwdne11480:0"
+  },
+  "noAccessToCreate": {
+    "fr": "Vous n'avez pas accès à la création d'événements sur cet agenda",
+    "en": "You do not have the right to create this event",
+    "io": "crwdns11482:0crwdne11482:0"
+  },
+  "noAccessToDraft": {
+    "fr": "Vous n'avez pas accès à l'édition de ce brouillon",
+    "en": "You do not have edit rights to this draft",
+    "it": "Non avete accesso all'edizione di questa bozza.",
+    "io": "crwdns11484:0crwdne11484:0"
+  },
+  "eventNotLinkedToAgenda": {
+    "fr": "L'événement n'est pas réferencé sur l'agenda",
+    "en": "This event is not listed in agenda",
+    "it": "L'evento non è menzionato nell'ordine del giorno.",
+    "io": "crwdns11486:0crwdne11486:0"
+  }
+}
