4b3c4faeaddfa0efc9e1c6d129c58798fdf84941
Kévin Berthommier <bertho-zero@users.noreply.github.com>
2022-03-03 15:59:53 +0100

    refactor(event-admin-apps): deduplicate code

--- packages/event-admin-apps/src/components/FiltersPreview.js
+++ packages/event-admin-apps/src/components/FiltersPreview.js
@@ -1,115 +1,16 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
-import { useQuery } from 'react-query';
-import { useApiClient } from '@openagenda/react-shared';
-import {
-  getEvents,
-  ActiveFilters,
-  DateRangeFilter,
-  ChoiceFilter,
-  MapFilter,
-  ValueBadge,
-  useFilterTitle,
-} from '@openagenda/react-filters';
-import useFilterOptions from '../hooks/useFilterOptions';
+import React, { useMemo } from 'react';
+import { ActiveFilters } from '@openagenda/react-filters';
 
-function BadgePreview({
-  name, filter, label, onRemove, disabled, className
-}) {
-  const title = useFilterTitle(name, filter.fieldSchema);
-
-  return (
-    <span className={className} title={title}>
-      <ValueBadge label={label} onRemove={onRemove} disabled={disabled} />
-    </span>
+export default function FiltersPreview({ filters, getOptions, disabled }) {
+  const filtersWithSearch = useMemo(
+    () => [{ type: 'search', name: 'search' }, ...filters],
+    [filters]
   );
-}
-
-function ChoicePreview({
-  name,
-  filter,
-  valueOptions,
-  onRemove,
-  disabled,
-  className,
-}) {
-  const title = useFilterTitle(name, filter.fieldSchema);
-
-  return (
-    <span className={className} title={title}>
-      {valueOptions.map(option => (
-        <ValueBadge
-          key={option.value}
-          label={option.label}
-          onRemove={onRemove(option)}
-          disabled={disabled}
-        />
-      ))}
-    </span>
-  );
-}
-
-function DateRangePreviewer(props) {
-  return <DateRangeFilter.Preview component={BadgePreview} {...props} />;
-}
-
-function ChoicePreviewer(props) {
-  return <ChoiceFilter.Preview component={ChoicePreview} {...props} />;
-}
-
-function MapPreviewer(props) {
-  return <MapFilter.Preview component={BadgePreview} {...props} />;
-}
-
-export default function FiltersPreview({
-  agenda, query, page, filters
-}) {
-  const apiClient = useApiClient();
-  const res = useSelector(state => state.res);
-
-  const filtersQuery = useQuery(
-    ['event-admin-apps', 'filtersBase', agenda.slug],
-    () => getEvents(apiClient, res.jsonExport, agenda, filters, { size: 0 }),
-    {
-      staleTime: 1000,
-      notifyOnChangeProps: ['data', 'isFetching'],
-      enabled: false,
-    }
-  );
-
-  const { isFetching } = useQuery(
-    ['event-admin-apps', 'events', agenda.slug, { query, page }],
-    () => getEvents(
-      apiClient,
-      res.jsonExport,
-      agenda,
-      filters,
-      {
-        sort: 'updatedAt.desc',
-        ...query,
-        detailed: true,
-      },
-      page
-    ),
-    {
-      staleTime: 1000,
-      notifyOnChangeProps: ['isFetching'],
-      keepPreviousData: true, // because query and page change
-    }
-  );
-
-  const { aggregations: filterAggs } = filtersQuery.data;
-
-  const getOptions = useFilterOptions(filterAggs);
 
   return (
     <ActiveFilters
-      filters={filters}
-      disabled={isFetching || filtersQuery.isFetching}
-      dateRangeComponent={DateRangePreviewer}
-      choiceComponent={ChoicePreviewer}
-      mapComponent={MapPreviewer}
-      // getTotal={getTotal}
+      filters={filtersWithSearch}
+      disabled={disabled}
       getOptions={getOptions}
     />
   );
