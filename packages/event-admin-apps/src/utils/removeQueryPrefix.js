1650adb1815f3de67140e387aeb25a99d9b5eec3
Kévin Berthommier <bertho-zero@users.noreply.github.com>
2022-03-15 10:31:07 +0100

    feat(event-admin-apps): Sort and Search from react-filters

--- packages/event-admin-apps/src/utils/removeQueryPrefix.js
+++ packages/event-admin-apps/src/utils/removeQueryPrefix.js
@@ -1,13 +1,16 @@
 export default function removeQueryPrefix(query, prefix = 'q.') {
   const result = {};
+  const rest = {};
 
   for (const key in query) {
     if (Object.prototype.hasOwnProperty.call(query, key)) {
       if (key.startsWith(prefix)) {
         result[key.slice(prefix.length)] = query[key];
+      } else {
+        rest[key] = query[key];
       }
     }
   }
 
-  return result;
+  return { query: result, rest };
 }
