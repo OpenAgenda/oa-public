996b4ca9f98a4b9d6d53c16ee2eaaa1804ec8be9
ohick <oliviahick1@gmail.com>
2022-03-22 17:18:02 +0100

    fix(flat-exports): fix timezones in spreadsheet exports

--- packages/flat-exports/lib/transform/firstLastDate.js
+++ packages/flat-exports/lib/transform/firstLastDate.js
@@ -12,9 +12,9 @@ module.exports = ({ languages, includeLanguages }, { target, source, field }) =>
     source,
     target: targetLanguages.map(l => (target || source) + (languages.length > 1 ? ` - ${l.toUpperCase()}` : '')),
     field: field === 'firstDate' ? 'firstDate' : 'lastDate',
-    transform: time => {
-      const firstDate = time[0];
-      const lastDate = time[time.length - 1];
+    transform: ({ timings }) => {
+      const firstDate = timings[0];
+      const lastDate = timings[timings.length - 1];
       const timing = field === 'firstDate' ? firstDate : lastDate;
 
       return targetLanguages.map(lang => moment(timing.begin).locale(lang).format('dddd D MMMM YYYY'));
