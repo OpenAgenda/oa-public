4660b318e73c3b1750e8062b5d93dd3f502399e2
ohick <oliviahick1@gmail.com>
2022-03-25 15:12:43 +0100

    fix(legacy): fixed custom, permalink and tags conversions

--- packages/legacy/convertEventToLegacyFormat/lib/getPermalink.js
+++ packages/legacy/convertEventToLegacyFormat/lib/getPermalink.js
@@ -1,13 +1,3 @@
 'use strict';
 
-module.exports = (agendaSettings, event) => {
-  const permalink = `https://openagenda.com/agendas/${agendaSettings.uid}/events/${event.uid}`;
-
-  if (!event?.sourceAgendas) {
-    return permalink;
-  }
-
-  const source = event.sourceAgendas.find(sourceAgenda => sourceAgenda.uid === event.originAgenda.uid);
-
-  return source ? `https://openagenda.com/agendas/${source.uid}/events/${event.uid}` : permalink;
-};
+module.exports = (agendaSettings, event) => `https://openagenda.com/agendas/${agendaSettings.uid}/events/${event.uid}`;
