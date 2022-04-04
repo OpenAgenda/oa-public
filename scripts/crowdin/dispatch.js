30d5063aa467c9a0bf991b0eb2893344f3666270
Kévin Berthommier <bertho-zero@users.noreply.github.com>
2022-04-04 12:55:35 +0200

    fix: remove ignored files in crowdin script

--- scripts/crowdin/dispatch.js
+++ scripts/crowdin/dispatch.js
@@ -1,6 +1,7 @@
 // 1. Build & download .zip on Crowdin (https://crowdin.com/project/openagenda)
 // 2. Unzip the archive content in `archive` folder
 // 3. Run `node dispatch.js`
+// 4. Need to run `node dispatch.js` in packages/labels/.crowdin
 
 const fs = require('fs');
 const path = require('path');
@@ -12,8 +13,6 @@ const rootsMap = new Map([
   ['[OpenAgenda.oa] main', '.'],
   ['[OpenAgenda.oa-public] main', 'public'],
 ]);
-
-const ignoredPaths = ['packages/labels'];
 
 function walkSync(dir, basePath = __dirname) {
   const files = fs.readdirSync(path.join(basePath, dir));
@@ -37,18 +36,24 @@ for (const [archive, root] of rootsMap) {
   const files = walkSync('.', path.join(ARCHIVE, archive));
 
   for (const file of files) {
-    if (ignoredPaths.some(v => file.startsWith(v))) {
-      continue;
+    const crowdinLabels = require(path.join(ARCHIVE, archive, file));
+    let rawProjectLabels = '';
+
+    try {
+      rawProjectLabels = fs.readFileSync(path.join(PROJECT, root, file), 'utf-8');
+    } catch (e) {
+      console.log(`File '${file}' does not exists in project`);
     }
 
-    const crowdinLabels = require(path.join(ARCHIVE, archive, file));
-    const projectLabels = require(path.join(PROJECT, root, file));
+    const start = rawProjectLabels.slice(0, rawProjectLabels.indexOf('{'));
+    const end = rawProjectLabels.slice(rawProjectLabels.lastIndexOf('}') + 1);
+    const projectLabels = JSON.parse(rawProjectLabels);
 
     const labels = {
       ...projectLabels,
       ...crowdinLabels,
     };
 
-    fs.writeFileSync(path.join(PROJECT, root, file), JSON.stringify(labels, null, 2) + '\n');
+    fs.writeFileSync(path.join(PROJECT, root, file), `${start}${JSON.stringify(labels, null, 2)}${end}`);
   }
 }
