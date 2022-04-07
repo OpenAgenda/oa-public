6c1b803b5b0276164ace0d491076867c5c4b271b
Kévin Berthommier <bertho-zero@users.noreply.github.com>
2022-04-07 20:46:57 +0200

    fix(eslint-config): fix resolver

--- public/eslint-config/resolver.js
+++ public/eslint-config/resolver.js
@@ -1,10 +1,15 @@
 'use strict';
 
+const fs = require('graceful-fs');
 const path = require('path');
 const { builtinModules } = require('module');
 const enhancedResolve = require('enhanced-resolve');
+const CachedInputFileSystem = require('enhanced-resolve/lib/CachedInputFileSystem');
 
 const builtins = new Set(builtinModules);
+
+const nodeFileSystem = new CachedInputFileSystem(fs, 4000);
+const defaultResolver = enhancedResolve.create.sync(opts());
 
 function resolve(source, file, config) {
   if (builtins.has(source)) {
@@ -12,16 +17,24 @@ function resolve(source, file, config) {
   }
 
   try {
-    const resolver = config ? enhancedResolve.create.sync(config) : enhancedResolve.sync;
+    const resolver = config ? enhancedResolve.create.sync(opts(config)) : defaultResolver;
     const result = resolver(path.dirname(file), source);
 
     return { found: true, path: result };
   } catch (e) {
     return { found: false };
   }
-};
+}
+
+function opts(config) {
+  return Object.assign({
+    fileSystem: nodeFileSystem,
+    conditionNames: ['node'],
+    extensions: ['.mjs', '.js', '.json', '.node'],
+  }, config);
+}
 
 module.exports = {
   interfaceVersion: 2,
-  resolve
+  resolve,
 };
