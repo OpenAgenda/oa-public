5a3dac4bd9ed33f1784accdf2cf1f9566f806b61
Kaoré <kaore@openagenda.com>
2022-04-25 15:48:43 +0200

    chore(common-labels): added Required error label

---
+++ common-labels/messages/errors.js
@@ -0,0 +1,12 @@
+const { defineMessages } = require('react-intl');
+
+module.exports = defineMessages({
+  endLessThanBegin: {
+    id: 'common.errors.endLessThanBegin',
+    defaultMessage: 'At least one timing is invalid with an begin time occuring after the end time'
+  },
+  required: {
+    id: 'common.errors.required',
+    defaultMessage: 'Required'
+  }
+});
