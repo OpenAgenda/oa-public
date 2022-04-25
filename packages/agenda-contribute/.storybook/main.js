f10fd883bdce4ab1a46432a2d9b117c7e1c9d5a9
Kaoré <kaore@openagenda.com>
2022-04-25 15:52:47 +0200

    chore(agenda-contribute): moved error labels to common and updated storybook

--- packages/agenda-contribute/.storybook/main.js
+++ packages/agenda-contribute/.storybook/main.js
@@ -3,5 +3,18 @@ module.exports = {
     '../stories/**/*.stories.mdx',
     '../stories/**/*.stories.@(js|jsx|ts|tsx)',
   ],
+  core: {
+    builder: 'webpack5'
+  },
+  webpackFinal(config) {
+    config.module.rules.push({
+      test: /\.(js|mjs|jsx)$/,
+      resolve: {
+        fullySpecified: false,
+      },
+    });
+
+    return config;
+  }
 };
 
