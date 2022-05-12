53594f30124be6b2bf8f222d5a932386217bb1de
Kévin Berthommier <bertho-zero@users.noreply.github.com>
2022-05-12 11:28:44 +0200

    refactor(labels): move mails labels to cibul-node

--- packages/labels/.crowdin/aggregate.js
+++ packages/labels/.crowdin/aggregate.js
@@ -3,13 +3,12 @@
 const fs = require('fs');
 const path = require('path');
 const mkdirp = require('mkdirp');
+const { DEFAULT_LANGS } = require('@openagenda/intl');
 const getLabelFiles = require('../getLabelFiles');
-
-const LANGS = ['en', 'fr', 'de', 'it', 'es', 'br', 'ca', 'oc'];
 
 const labelFiles = getLabelFiles();
 
-for (const lang of LANGS) {
+for (const lang of DEFAULT_LANGS) {
   for (const labelFile of labelFiles) {
     const labels = require(path.join(__dirname, '..', labelFile));
     const locales = getLocaleLabels(labels, lang);
@@ -28,7 +27,7 @@ for (const file of getLabelFiles.walkSync('locales', __dirname)) {
   const labelFile = file.split('/').slice(2).join('/').replace(/\.json$/, '.js');
   const removed = !labelFiles.find(v => v === labelFile);
   if (removed) {
-    fs.unlinkSync(file);
+    fs.unlinkSync(path.join(__dirname, file));
   }
 }
 
