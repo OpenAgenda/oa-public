f61a5321b8b5836723a84a1f2a7575da3a7c7274
Kaoré <kaore@openagenda.com>
2022-05-13 16:40:52 +0200

    chore(agenda-portal): linting

--- public/agenda-portal/test/25_decorateOptionedFieldsWithLabels.test.js
+++ public/agenda-portal/test/25_decorateOptionedFieldsWithLabels.test.js
@@ -1,20 +1,35 @@
 'use strict';
 
-const decorateOptionedFieldsWithLabels = require("../boot/utils/decorateOptionedFieldsWithLabels");
+const decorateOptionedFieldsWithLabels = require('../boot/utils/decorateOptionedFieldsWithLabels');
 
-const event = {'type-devenement': 11};
-const fields = [{field: 'type-devenement', options: [ {id: 11, value: 'atelier-stage', label: {en: 'Atelier - Stage'}, display: true} ]}];
+const event = {
+  'type-devenement': 11
+};
 
-describe('25 - decorateOptionedFieldsWithLabels',() => {
-    test('type-devenement is now equal to corresponding label', () => {
-        expect(
-            decorateOptionedFieldsWithLabels(event, { agenda:{schema: {fields}}, lang:'en' })
-        ).toEqual({'type-devenement': ["Atelier - Stage"]})
+const fields = [{
+  field: 'type-devenement',
+  options: [{
+    id: 11,
+    value: 'atelier-stage',
+    label: { en: 'Atelier - Stage' },
+    display: true
+  }]
+}];
+
+describe('25 - decorateOptionedFieldsWithLabels', () => {
+  test('type-devenement is now equal to corresponding label', () => {
+    expect(
+      decorateOptionedFieldsWithLabels(event, { agenda: { schema: { fields } }, lang: 'en' })
+    ).toEqual({
+      'type-devenement': ['Atelier - Stage']
     });
+  });
 
-    test('type-devenement is no longer equal to 11', () => {
-        expect(
-            decorateOptionedFieldsWithLabels(event, { agenda:{schema: {fields}}, lang:'en' })
-        ).not.toEqual({'type-devenement': 11})
+  test('type-devenement is no longer equal to 11', () => {
+    expect(
+      decorateOptionedFieldsWithLabels(event, { agenda: { schema: { fields } }, lang: 'en' })
+    ).not.toEqual({
+      'type-devenement': 11
     });
+  });
 });
