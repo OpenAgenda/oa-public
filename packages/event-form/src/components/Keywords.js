7dc061599757743d6e13933e5a2ec063d068b939
Clément Lecroart <clement.lecroart@openagenda.com>
2022-06-29 16:49:42 +0200

    chore(event-form): lint keywords

--- packages/event-form/src/components/Keywords.js
+++ packages/event-form/src/components/Keywords.js
@@ -5,9 +5,6 @@ import React, { Component } from 'react';
 import ih from 'immutability-helper';
 import TagsInput from 'react-tagsinput';
 
-import Sub from '@openagenda/form-schemas/client/build/Components/Sub';
-import Counter from '@openagenda/form-schemas/client/build/Components/FieldCounter';
-
 const separatorRegex = /;|,|\|/;
 
 module.exports = class KeywordsComponent extends Component {
@@ -39,7 +36,6 @@ module.exports = class KeywordsComponent extends Component {
   }
 
   appendValue(language, item) {
-
     this.resetInput(language);
 
     this.props.onChange(ih(this.props.value || {}, _.set({}, language, {
@@ -72,47 +68,33 @@ module.exports = class KeywordsComponent extends Component {
 
     const preCleaned = preClean(value, l);
 
-    return <div>
-      <TagsInput
-        value={preCleaned}
-        onChange={this.onChange.bind(this, l)}
-        inputProps={{
-          value: _.get(this, ['state', 'inputValues', l], ''),
-          onChange: this.onInputChange.bind(this, l),
-          placeholder: field.placeholder,
-          onBlur: this.onInputBlur.bind(this, l),
-          style: !_.get(value, l) ? { width: '630px' } : null
-        }}
-      />
-{/*       <Counter value={_.isArray(preCleaned) ? preCleaned.join(',') : preCleaned} max={field.max} />
-      <Sub label={field.sub} error={error} /> */}
-    </div>
+    return (
+      <div>
+        <TagsInput
+          value={preCleaned}
+          onChange={this.onChange.bind(this, l)}
+          inputProps={{
+            value: _.get(this, ['state', 'inputValues', l], ''),
+            onChange: this.onInputChange.bind(this, l),
+            placeholder: field.placeholder,
+            onBlur: this.onInputBlur.bind(this, l),
+            style: !_.get(value, l) ? { width: '630px' } : null
+          }}
+        />
+      </div>
+    );
   }
 
   singleLanguageRender() {
-    return <div className="keywords">
-      {this.renderInput(_.first(this.props.field.languages))}
-    </div>
+    return (
+      <div className="keywords">
+        {this.renderInput(_.first(this.props.field.languages))}
+      </div>
+    );
   }
 
   render() {
-   /*  if (this.props.field.languages.length === 1) { */
-      return this.singleLanguageRender();
-    // }
-
-    return (
-      <div className="keywords">
-        <ul className="list-unstyled">
-          {this.props.field.languages.map(l => (
-            <li key={this.props.field.field + '_' + l}>
-              <div className="lang-input">
-                <label>{l}</label>
-                {this.renderInput(l)}
-              </div>
-            </li>))}
-        </ul>
-      </div>
-    );
+    return this.singleLanguageRender();
   }
 };
 
