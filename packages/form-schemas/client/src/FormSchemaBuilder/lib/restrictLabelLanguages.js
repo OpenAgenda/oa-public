19c784bf2a495127680020a0384f847c8f218c68
Kaoré <kaore@openagenda.com>
2022-06-30 16:46:36 +0200

    chore(form-schemas): linting

--- packages/form-schemas/client/src/FormSchemaBuilder/lib/restrictLabelLanguages.js
+++ packages/form-schemas/client/src/FormSchemaBuilder/lib/restrictLabelLanguages.js
@@ -2,42 +2,32 @@ import _ from 'lodash';
 import ih from 'immutability-helper';
 import labelKeys from './labelKeys';
 
-// see tests 16
+function restrictLabelLanguages(field, languages = []) {
+  return ih(field, labelKeys
+    .filter(labelKey => field[labelKey])
+    .reduce((updates, labelKey) => {
+      const currentLabelLanguages = _.isString(field[labelKey]) ? [] : _.keys(field[labelKey]);
 
-export default _.assign( restrictLabelLanguages, {
-  applyToSchema
-} );
+      const fillerLabel = currentLabelLanguages.length ? field[labelKey][currentLabelLanguages[0]] : field[labelKey];
 
-function restrictLabelLanguages( field, languages = [] ) {
-
-  return ih( field, labelKeys
-    .filter( labelKey => field[ labelKey ] )
-    .reduce( ( updates, labelKey ) => {
-
-      const currentLabelLanguages = _.isString( field[ labelKey ] ) ? [] : _.keys( field[ labelKey ] );
-
-      const fillerLabel = currentLabelLanguages.length ? field[ labelKey ][ currentLabelLanguages[ 0 ] ] : field[ labelKey ];
-
-      return _.set( updates, labelKey, {
-        $set: languages.length ? languages.reduce( ( labelValue, language ) => {
-
-          return _.set( labelValue, language, currentLabelLanguages.includes( language ) ? field[ labelKey ][ language ] : fillerLabel );
-
-        }, {} ) : fillerLabel
-      } );
-
-    }, {} ) );
-
+      return _.set(updates, labelKey, {
+        $set: languages.length ? languages.reduce((labelValue, language) => _.set(
+          labelValue, language, currentLabelLanguages.includes(language) ? field[labelKey][language] : fillerLabel
+        ), {}) : fillerLabel
+      });
+    }, {}));
 }
 
-function applyToSchema( schema, languages = [] ) {
+function applyToSchema(schema, languages = []) {
+  if (!_.get(schema, 'fields', []).length) return schema;
 
-  if ( !_.get( schema, 'fields', [] ).length ) return schema;
-
-  return ih( schema, {
+  return ih(schema, {
     fields: {
-      $set: schema.fields.map( f => restrictLabelLanguages( f, languages ) )
+      $set: schema.fields.map(f => restrictLabelLanguages(f, languages))
     }
-  } );
-
+  });
 }
+
+export default Object.assign(restrictLabelLanguages, {
+  applyToSchema
+});
