475883e193653092b9ed1ed053af868ff53e40ad
Kaoré <kaore@openagenda.com>
2022-07-28 17:33:22 +0200

    fix(event-search): older events images do not always have variants

--- packages/event-search/utils/filterImageTimestamps.js
+++ packages/event-search/utils/filterImageTimestamps.js
@@ -11,6 +11,10 @@ module.exports = function filterImageTimestamps(event) {
     if (draft.image.filename.indexOf('?') !== -1) {
       draft.image.filename = draft.image.filename.split('?').shift();
     }
+
+    if (!draft.image.variants) {
+      return draft;
+    }
   
     for (const variant of draft.image.variants) {
       if (variant.filename.indexOf('?') !== -1) {
