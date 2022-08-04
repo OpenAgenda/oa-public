821db6d8f765e71e461508ea8ac587a927d061ff
Kaoré <kaore@openagenda.com>
2022-08-04 09:24:11 +0200

    feat(agenda-portal): expose utility functions

--- public/agenda-portal/lib/timings/detailed.js
+++ public/agenda-portal/lib/timings/detailed.js
@@ -5,7 +5,7 @@ const getSchemaJSONLD = require('./getSchemaJSONLD');
 const links = require('./links');
 
 module.exports = function detailedTiming({ event, req }, timing, locale = 'en') {
-  const { defaultTimezone } = req.app.locals;
+  const { defaultTimezone } = req?.app?.locals ?? {};
   const timezone = event.timezone || event.location.timezone || defaultTimezone;
 
   return ({
