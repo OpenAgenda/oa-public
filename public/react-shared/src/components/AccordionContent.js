54e10f4ac5bd8daa8642296183e2c1acffaf0525
DFarau <doriane.farau@hetic.net>
2022-09-15 11:04:40 +0200

    feat(react-shared): update accordion component

--- public/react-shared/src/components/AccordionContent.js
+++ public/react-shared/src/components/AccordionContent.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
 const AccordionContent = ({ content }) => (
-  <div className="accordion_content">
+  <div className="accordion-content">
     <div className="wrapper">
-      <div className="wrapper_item">{content}</div>
+      <div className="wrapper-item">{content}</div>
     </div>
   </div>
 );
--- public/react-shared/src/components/AccordionHead.js
+++ public/react-shared/src/components/AccordionHead.js
@@ -2,8 +2,10 @@ import React from 'react';
 
 const AccordionHead = ({ Trigger, active, head }) => (
   <Trigger>
-    <div>
-      {head}
+    <div className="accordion-head">
+      <div>
+        {head}
+      </div>
       <i
         className={`control ${
           active ? 'fa fa-chevron-up up' : 'fa fa-chevron-down down'
