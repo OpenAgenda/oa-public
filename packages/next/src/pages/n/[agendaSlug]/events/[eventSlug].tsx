a85dce35ded1b34492586b3ed780029486cdb8cf
Kaoré <kaore@openagenda.com>
2022-10-07 12:44:09 +0200

    refactor(next): renaming slug agendaSlug

--- packages/next/src/pages/n/[agendaSlug]/events/[eventSlug].tsx
+++ packages/next/src/pages/n/[agendaSlug]/events/[eventSlug].tsx
@@ -9,8 +9,8 @@ export async function getServerSideProps({ query, req }) {
   } = getConfig();
 
   const [{ data: agenda }, { data: { event } }] = await Promise.all([
-    api(req, 'get', `/api/agendas/slug/${query.slug}`),
-    api(req, 'get', `/api/agendas/slug/${query.slug}/events/slug/${query.eventSlug}`),
+    api(req, 'get', `/api/agendas/slug/${query.agendaSlug}`),
+    api(req, 'get', `/api/agendas/slug/${query.agendaSlug}/events/slug/${query.eventSlug}`),
   ]);
 
   return {
