8f48e8de64dae40fefd2e03b3bef7f4cea8fdda4
Kévin Berthommier <bertho-zero@users.noreply.github.com>
2022-10-10 17:01:40 +0200

    fix(next): merge conflict

--- packages/next/src/components/Navbar.tsx
+++ packages/next/src/components/Navbar.tsx
@@ -6,15 +6,9 @@ export default function Navbar() {
   return (
     <Box as="nav" bg="white" boxShadow="sm">
       <Container maxW="container.xl" py="4">
-<<<<<<< HEAD
         <HStack spacing="8" justify="space-between">
-          <Image width="125" height="22" src={logoPic} />
-          <Flex justify="right">
-=======
-        <HStack spacing="10" justify="space-between">
           <Image alt="logo" width="125" height="22" src={logoPic} />
-          <Flex justify="right" flex="1">
->>>>>>> f699732761b78568018a73e8b543db20ff5a691e
+          <Flex justify="right">
             <HStack spacing="6">
               <Button variant="link" colorScheme="primary">Sign in</Button>
               <Button variant="link" colorScheme="primary">Sign up</Button>
