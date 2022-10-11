7c014fccc3ca997d1ec3109b3d48cfbc5770f813
Kaoré <kaore@openagenda.com>
2022-10-11 18:04:06 +0200

    fix(legacy): convertEventToLegacyFormat - lastDate and firstDate need to be utc-offsetted

--- packages/legacy/convertEventToLegacyFormat/lib/getFirstLastTimings.js
+++ packages/legacy/convertEventToLegacyFormat/lib/getFirstLastTimings.js
@@ -5,13 +5,13 @@ const moment = require('moment-timezone');
 module.exports = function getFirstLastTimings(timings) {
   const firstDateUTCOffset = moment.parseZone(timings[0].begin).utcOffset();
 
-  const firstDate = moment(timings[0].begin).format('YYYY-MM-DD');
+  const firstDate = moment(timings[0].begin).utcOffset(firstDateUTCOffset).format('YYYY-MM-DD');
   const firstTimeStart = moment.utc(timings[0].begin).utcOffset(firstDateUTCOffset).format('HH:mm');
   const firstTimeEnd = moment.utc(timings[0].end).utcOffset(firstDateUTCOffset).format('HH:mm');
 
   const lastTiming = timings[timings.length - 1];
   const lastDateUTCOffset = moment.parseZone(lastTiming.begin).utcOffset();
-  const lastDate = moment(lastTiming.begin).format('YYYY-MM-DD');
+  const lastDate = moment(lastTiming.begin).utcOffset(lastDateUTCOffset).format('YYYY-MM-DD');
   const lastTimeStart = moment.utc(lastTiming.begin).utcOffset(lastDateUTCOffset).format('HH:mm');
   const lastTimeEnd = moment.utc(lastTiming.end).utcOffset(lastDateUTCOffset).format('HH:mm');
 
