cb271b81f05d5214757e2d87a2ca37d426df5c33
Kévin Berthommier <bertho-zero@users.noreply.github.com>
2022-10-12 17:16:06 +0200

    feat(next): remove cibul-node dep

--- packages/next/next.config.js
+++ packages/next/next.config.js
@@ -1,17 +1,16 @@
-const { config: nodeConfig } = require('cibul-node');
 const { apiClient } = require('@openagenda/react-shared');
 
 const withTM = require('next-transpile-modules')(['@openagenda/uikit']);
 
-// TODO:
-// NEXT_PUBLIC_ASSET_PREFIX
-// NEXT_API_BASE_URL=`http://localhost:${config.port}`
-
 /** @type {() => import('next').NextConfig} */
 const config = async () => {
+  const {
+    NEXT_API_INTERNAL_BASE_URL,
+    NEXT_PUBLIC_ASSET_PREFIX,
+  } = process.env;
+
   const serverRuntimeConfig = {
-    config: nodeConfig,
-    api: (req, method, ...args) => apiClient(`http://localhost:${nodeConfig.port}`, req)[method](...args),
+    api: (req, method, ...args) => apiClient(NEXT_API_INTERNAL_BASE_URL, req)[method](...args),
   };
 
   return withTM({
@@ -26,14 +25,14 @@ const config = async () => {
         // ],
       },
     },
-    assetPrefix: nodeConfig?.next?.CDN || undefined,
+    assetPrefix: NEXT_PUBLIC_ASSET_PREFIX || undefined,
     serverRuntimeConfig,
     async rewrites() {
       return {
         fallback: [
           {
             source: '/:path*',
-            destination: `http://localhost:${nodeConfig?.port}/:path*`,
+            destination: `${NEXT_API_INTERNAL_BASE_URL}/:path*`,
           },
         ],
       };
