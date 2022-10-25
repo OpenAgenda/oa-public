a6d631a3e27fe2b6ab1ede883254f7661cd54cfb
Kaoré <kaore@openagenda.com>
2022-10-25 14:11:27 +0200

    fix(network-apps): handle new extentions in webpack

--- packages/network-apps/webpack.dist.js
+++ packages/network-apps/webpack.dist.js
@@ -93,5 +93,6 @@ module.exports = {
       // required only for the timings component
       'react': require.resolve( 'react' )
     },
+    extensions: ['.js', '.mjs', '.json', '.wasm'],
   },
 };
