a9d28c634bd479a81f6bf012d56615510833741e
Kaoré <kaore@openagenda.com>
2022-11-18 09:37:53 +0100

    chore(event-admin-apps): linting

--- packages/event-admin-apps/src/components/FiltersPart.js
+++ packages/event-admin-apps/src/components/FiltersPart.js
@@ -1,4 +1,4 @@
-import React, { useMemo, useState } from 'react';
+import { useMemo, useState } from 'react';
 import { useSelector } from 'react-redux';
 import { useQuery } from 'react-query';
 import { useIntl } from 'react-intl';
@@ -15,16 +15,15 @@ import {
 } from '@openagenda/react-filters';
 import { useApiClient } from '@openagenda/react-shared';
 
-function FiltersPart({
-  agenda, filters, query, page
-}) {
+function FiltersPart({ agenda, filters, query, page }) {
   const apiClient = useApiClient();
   const intl = useIntl();
   const res = useSelector(state => state.res);
 
   const geoRes = useMemo(
-    () => res.jsonExport.replace(':slug', agenda.slug).replace(':uid', agenda.uid),
-    [agenda.slug, agenda.uid]
+    () =>
+      res.jsonExport.replace(':slug', agenda.slug).replace(':uid', agenda.uid),
+    [agenda.slug, agenda.uid, res.jsonExport],
   );
 
   const filtersQuery = useQuery(
@@ -33,28 +32,29 @@ function FiltersPart({
     {
       staleTime: 1000,
       notifyOnChangeProps: ['data', 'isFetching'],
-    }
+    },
   );
 
   const { data, isFetching } = useQuery(
     ['event-admin-apps', 'events', agenda.slug, { query, page }],
-    () => getEvents(
-      apiClient,
-      res.jsonExport,
-      agenda,
-      filters,
-      {
-        sort: 'updatedAt.desc',
-        ...query,
-        detailed: true,
-      },
-      page
-    ),
+    () =>
+      getEvents(
+        apiClient,
+        res.jsonExport,
+        agenda,
+        filters,
+        {
+          sort: 'updatedAt.desc',
+          ...query,
+          detailed: true,
+        },
+        page,
+      ),
     {
       staleTime: 1000,
       notifyOnChangeProps: ['data', 'isFetching'],
       keepPreviousData: true, // because query and page change
-    }
+    },
   );
 
   const { aggregations: filterAggs } = filtersQuery.data;
@@ -69,39 +69,37 @@ function FiltersPart({
   const mapProps = useMemo(() => ({ query }), [query]);
 
   return (
-    <>
-      <div
-        className="oa-collapse"
-        css={css`
-          .leaflet-container {
-            height: 300px;
-          }
+    <div
+      className="oa-collapse"
+      css={css`
+        .leaflet-container {
+          height: 300px;
+        }
 
-          .oa-choice-option-label {
-            min-height: 20px;
-            line-height: 20px;
-            padding-left: 20px;
-            margin-bottom: 0;
-            font-weight: normal;
-            cursor: pointer;
-          }
-        `}
-      >
-        <Filters
-          filters={filters}
-          disabled={isFetching || filtersQuery.isFetching}
-          dateRangeComponent={DateRangeFilter.Collapsable}
-          choiceComponent={ChoiceFilter.Collapsable}
-          mapComponent={MapFilter}
-          mapProps={mapProps}
-          getTotal={getTotal}
-          getOptions={getOptions}
-          initialViewport={initialViewport}
-          loadGeoData={loadGeoData}
-          withRef
-        />
-      </div>
-    </>
+        .oa-choice-option-label {
+          min-height: 20px;
+          line-height: 20px;
+          padding-left: 20px;
+          margin-bottom: 0;
+          font-weight: normal;
+          cursor: pointer;
+        }
+      `}
+    >
+      <Filters
+        filters={filters}
+        disabled={isFetching || filtersQuery.isFetching}
+        dateRangeComponent={DateRangeFilter.Collapsable}
+        choiceComponent={ChoiceFilter.Collapsable}
+        mapComponent={MapFilter}
+        mapProps={mapProps}
+        getTotal={getTotal}
+        getOptions={getOptions}
+        initialViewport={initialViewport}
+        loadGeoData={loadGeoData}
+        withRef
+      />
+    </div>
   );
 }
 
