590e27eadd5b95922c0888f5182f23b1e4f30215
Kaoré <kaore@openagenda.com>
2022-11-30 08:18:03 +0100

    chore(form-schemas): linting

--- packages/form-schemas/stories/builder.integrated.stories.js
+++ packages/form-schemas/stories/builder.integrated.stories.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import FormSchemaBuilder from '../client/src/FormSchemaBuilder';
 
 import SimpleRowDecorator from './decorators/SimpleRow';
