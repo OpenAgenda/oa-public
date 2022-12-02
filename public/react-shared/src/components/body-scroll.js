94425d25fa919627d9aa10b1e3b736576c4cb5c1
Kaoré <kaore@openagenda.com>
2022-12-02 11:23:33 +0100

    chore(react-shared): linting

--- public/react-shared/src/components/body-scroll.js
+++ public/react-shared/src/components/body-scroll.js
@@ -1,62 +1,52 @@
+function parseStyle(style) {
+  const parsed = {};
+
+  (style || '').split(';').forEach(part => {
+    if (!part.length) return;
+
+    const [name, value] = part.split(':');
+
+    parsed[name] = value;
+  });
+
+  return parsed;
+}
+
+function stringifyStyle(style) {
+  return Object.keys(style)
+    .filter(k => style[k]?.length)
+    .map(k => `${k}:${style[k]}`)
+    .join(';');
+}
+
 // remove overflow:hidden from body
 export function enable() {
-
-  let bodyElem = document.querySelector('body');
+  const bodyElem = document.querySelector('body');
 
   if (!bodyElem) return;
 
-  let style = _parseStyle(bodyElem.getAttribute('style'));
+  const style = parseStyle(bodyElem.getAttribute('style'));
 
   style.overflow = undefined;
 
-  const stringifiedStyle = _stringifyStyle(style);
+  const stringifiedStyle = stringifyStyle(style);
 
   if (stringifiedStyle.length) {
     bodyElem.setAttribute('style', stringifiedStyle);
   } else {
     bodyElem.removeAttribute('style');
   }
-
 }
 
 // add overflow:hidden to body
 export function disable() {
-
-  let bodyElem = document.querySelector('body');
+  const bodyElem = document.querySelector('body');
 
   if (!bodyElem) return;
 
-  let style = _parseStyle(bodyElem.getAttribute('style'));
+  const style = parseStyle(bodyElem.getAttribute('style'));
 
   style.overflow = 'hidden';
 
-  bodyElem.setAttribute('style', _stringifyStyle(style));
-
-}
-
-function _stringifyStyle(style) {
-
-  return Object.keys(style)
-    .filter(k => style[k]?.length)
-    .map(k => k + ':' + style[k])
-    .join(';');
-
-}
-
-function _parseStyle(style) {
-
-  let parsed = {};
-
-  (style || '').split(';').forEach(part => {
-
-    if (!part.length) return;
-
-    let bits = part.split(':');
-
-    parsed[bits[0]] = bits[1];
-
-  });
-
-  return parsed;
-
+  bodyElem.setAttribute('style', stringifyStyle(style));
 }
