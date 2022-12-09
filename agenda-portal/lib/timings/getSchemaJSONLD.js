216cb1955c6e2a1f354004510a8f50d791292c06
Kaoré <kaore@openagenda.com>
2022-12-09 17:27:04 +0100

    feat(agenda-portal): added eventAttendanceMode, status and availability to ld+json

--- agenda-portal/lib/timings/getSchemaJSONLD.js
+++ agenda-portal/lib/timings/getSchemaJSONLD.js
@@ -5,6 +5,29 @@ const imageToUrl = require('../../utils/imageToUrl');
 const getJSONDuration = require('./getJSONDuration');
 const { getValue: getBeginValue } = require('./begin');
 
+function getEventAttendanceMode(code) {
+  const attendanceMode = {
+    1: 'OfflineEventAttendanceMode',
+    2: 'OnlineEventAttendanceMode',
+    3: 'MixedEventAttendanceMode',
+  }[code] ?? 'OfflineEventAttendanceMode';
+
+  return `https://schema.org/${attendanceMode}`;
+}
+
+function getEventStatus(code) {
+  const status = {
+    1: 'EventScheduled',
+    2: 'EventRescheduled',
+    3: 'EventMovedOnline',
+    4: 'EventPostponed',
+    5: 'EventScheduled', // but full.
+    6: 'EventCacelled',
+  }[code] ?? 'EventScheduled';
+
+  return `https://schema.org/${status}`;
+}
+
 module.exports = (event, timing, defaultTimezone) => {
   const { end, permalink } = timing;
 
@@ -29,11 +52,14 @@ module.exports = (event, timing, defaultTimezone) => {
       startDate: tz(begin, timezone).format('YYYY-MM-DDTHH:mm'),
       endDate: tz(end, timezone).format('YYYY-MM-DDTHH:mm'),
       duration: getJSONDuration(begin, end),
+      eventAttendanceMode: getEventAttendanceMode(event.attendanceMode),
+      eventStatus: getEventStatus(event.status),
       ...event.registration.some(r => r.type === 'link')
         ? {
           offers: {
             '@type': 'Offer',
             url: event.registration.find(r => r.type === 'link').value,
+            availability: `https://schema.org/${event.status === 5 ? 'SoldOut' : 'InStock'}`,
           },
         }
         : {},
