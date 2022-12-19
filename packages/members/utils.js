b155eb24fa0d6cc7c117aa7b88d7140cd14c21a9
Clément Lecroart <clement.lecroart@openagenda.com>
2022-12-19 14:22:02 +0100

    chore(members): moved utils to seperated file

---
+++ packages/members/utils.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const roles = require('./iso/roles');
+const compareRoles = require('./iso/compareRoles');
+const getRoleCode = require('./iso/getRoleCode');
+const getRoleSlug = require('./iso/getRoleSlug');
+
+module.exports = {
+  roles,
+  compareRoles,
+  getRoleCode,
+  getRoleSlug,
+};
