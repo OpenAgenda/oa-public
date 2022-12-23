bac1b4ea0d5163b4fbadd3898c8f30ca1a37469c
Clément Lecroart <clement.lecroart@openagenda.com>
2022-12-23 11:28:59 +0100

    chore(agenda-schemas-app): removed dist folder

--- packages/agenda-schemas-app/dist/containers/MemberFormBuilder.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-
-var _interopRequireWildcard = require("@babel/runtime-corejs3/helpers/interopRequireWildcard").default;
-
-var _react = _interopRequireWildcard(require("react"));
-//# sourceMappingURL=MemberFormBuilder.js.map
