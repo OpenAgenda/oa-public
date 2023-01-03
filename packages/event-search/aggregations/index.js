3c835db415aa2a09095bb5ed2af505c9ef93d1a6
Kaoré <kaore@openagenda.com>
2023-01-03 11:19:07 +0100

    chore(event-search): linting

--- packages/event-search/aggregations/index.js
+++ packages/event-search/aggregations/index.js
@@ -1,89 +1,92 @@
 'use strict';
 
+const {
+  BadRequest,
+} = require('@openagenda/verror');
+
 const terms = require('./terms');
 const timestamp = require('./timestamp');
 const missingAdditionalFields = require('./missingAdditionalFields');
 
-const {
-  BadRequest
-} = require('@openagenda/verror');
+const additionalFields = require('./additionalFields');
+const additionalFieldMetrics = require('./additionalFieldMetrics');
+const geohash = require('./geohash');
+const viewport = require('./viewport');
+const eventsByDateRanges = require('./eventsByDateRanges');
+const keywords = require('./keywords');
+const languages = require('./languages');
+const locations = require('./locations');
+const members = require('./members');
+const timespan = require('./timespan');
+const originAgendas = require('./originAgendas');
+const relative = require('./relative');
+const timings = require('./timings');
+const accessibilities = require('./accessibilities');
+const sourceAgendas = require('./sourceAgendas');
 
 const aggregationTypes = {
-  additionalFields: require('./additionalFields'),
-  additionalFieldMetrics: require('./additionalFieldMetrics'),
+  additionalFields,
+  additionalFieldMetrics,
   cities: terms('location.city'),
   districts: terms('location.district'),
-  geohash: require('./geohash'),
-  viewport: require('./viewport'),
-  eventsByDateRanges: require('./eventsByDateRanges'),
+  geohash,
+  viewport,
+  eventsByDateRanges,
   departments: terms('location.department'),
-  keywords: require('./keywords'),
-  languages: require('./languages'),
-  locations: require('./locations'),
-  members: require('./members'),
-  timespan: require('./timespan'),
-  originAgendas: require('./originAgendas'),
-  relative: require('./relative'),
+  keywords,
+  languages,
+  locations,
+  members,
+  timespan,
+  originAgendas,
+  relative,
   regions: terms('location.region'),
   adminLevels3: terms('location.adminLevel3'),
   adminLevels5: terms('location.adminLevel5'),
-  sourceAgendas: require('./sourceAgendas'),
+  sourceAgendas,
   states: terms('state', { order: { _key: 'desc' } }),
   status: terms('status', { order: { _key: 'asc' } }),
   addMethods: terms('addMethod'),
   attendanceModes: terms('attendanceMode', { order: { _key: 'desc' } }),
-  timings: require('./timings'),
+  timings,
   createdAt: timestamp('createdAt'),
   updatedAt: timestamp('_exclusiveUpdatedAt'),
   createdOrUpdatedAt: timestamp('updatedAt'),
-  accessibilities: require('./accessibilities'),
-  missingAdditionalFields
+  accessibilities,
+  missingAdditionalFields,
+};
+
+function extractKeyAndType(requested) {
+  return typeof requested === 'string' ? {
+    key: requested,
+    type: requested,
+    requested,
+  } : {
+    key: requested.key || requested.type,
+    type: requested.type,
+    requested,
+  };
 }
 
-module.exports = {
-  formatDSL: (requested, query, options = {}) => {
-    const errors = getValidationErrors(requested);
+function getRequestedItemErrors(requestedAgg, index = null) {
+  const errors = [];
 
-    if (errors.length) {
-      throw new BadRequest({
-        info: { errors }
-      }, 'Invalid requested aggregations');
-    }
+  const { type, key } = extractKeyAndType(requestedAgg);
 
+  if (type === undefined) {
+    errors.push({
+      index,
+      message: 'requested aggregation type is not defined',
+    });
+  }
+  if (key === undefined) {
+    errors.push({
+      index,
+      message: 'requested aggregation key is not defined',
+    });
+  }
 
-    // here it is necessary to build an aggregation specifically for
-    // empty values, when they are required (missing config on )
-
-    return [].concat(requested)
-      .map(extractKeyAndType)
-      .reduce((aggregationDSL, { key, type, requested }) => {
-        const formatDSL = aggregationTypes[type] && aggregationTypes[type].formatDSL;
-
-        if (typeof formatDSL !== 'function') {
-          throw new BadRequest({
-            info: { type }
-          }, 'Invalid requested aggregations: Unkown aggregation type');
-        }
-
-        return {
-          ...aggregationDSL,
-          [key]: formatDSL(
-            query,
-            getOptions(requested, options)
-          )
-        };
-      }, missingAdditionalFields.formatDSL(requested, query, options));
-  },
-  formatResult: (requested, query, result, options = {}) => missingAdditionalFields.dispatchMissingCounts(
-    requested,
-    result,
-    requested
-      .map(extractKeyAndType)
-      .reduce((formatted, { key, type, requested }) => ({
-        ...formatted,
-        [key]: aggregationTypes[type].formatResult(result[key], { ...getOptions(requested, options), query })
-      }), {})
-  )
+  return errors;
 }
 
 function getValidationErrors(requested) {
@@ -100,42 +103,54 @@ function getValidationErrors(requested) {
   return errors;
 }
 
-function getRequestedItemErrors(requestedAgg, index = null) {
-  const errors = [];
-
-  const { type, key } = extractKeyAndType(requestedAgg);
-
-  if (type === undefined) {
-    errors.push({
-      index,
-      message: `requested aggregation type is not defined`
-    });
-  }
-  if (key === undefined) {
-    errors.push({
-      index,
-      message: `requested aggregation key is not defined`
-    });
-  }
-
-  return errors;
-}
-
-function extractKeyAndType(requested) {
-  return typeof requested === 'string' ? {
-    key: requested,
-    type: requested,
-    requested
-  } : {
-    key: requested.key || requested.type,
-    type: requested.type,
-    requested
-  }
-}
-
 function getOptions(requested, options = {}) {
   return {
     ...options,
-    ...(typeof requested === 'string' ? {} : requested)
+    ...typeof requested === 'string' ? {} : requested,
   };
 }
+
+module.exports = {
+  formatDSL: (requested, query, options = {}) => {
+    const errors = getValidationErrors(requested);
+
+    if (errors.length) {
+      throw new BadRequest({
+        info: { errors },
+      }, 'Invalid requested aggregations');
+    }
+
+    // here it is necessary to build an aggregation specifically for
+    // empty values, when they are required (missing config on )
+
+    return [].concat(requested)
+      .map(extractKeyAndType)
+      .reduce((aggregationDSL, { key, type, requested: r }) => {
+        const formatDSL = aggregationTypes[type] && aggregationTypes[type].formatDSL;
+
+        if (typeof formatDSL !== 'function') {
+          throw new BadRequest({
+            info: { type },
+          }, 'Invalid requested aggregations: Unkown aggregation type');
+        }
+
+        return {
+          ...aggregationDSL,
+          [key]: formatDSL(
+            query,
+            getOptions(r, options),
+          ),
+        };
+      }, missingAdditionalFields.formatDSL(requested, query, options));
+  },
+  formatResult: (requested, query, result, options = {}) => missingAdditionalFields.dispatchMissingCounts(
+    requested,
+    result,
+    requested
+      .map(extractKeyAndType)
+      .reduce((formatted, { key, type, requested: r }) => ({
+        ...formatted,
+        [key]: aggregationTypes[type].formatResult(result[key], { ...getOptions(r, options), query }),
+      }), {}),
+  ),
+};
