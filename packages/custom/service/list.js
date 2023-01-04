918a9871145c57e1caa58924a5a4c3cb20a3574d
Kaoré <kaore@openagenda.com>
2023-01-04 12:57:56 +0100

    fix(custom): list with undefined identifier throws unhandled exception

--- packages/custom/service/list.js
+++ packages/custom/service/list.js
@@ -11,6 +11,7 @@ function _base(formSchemaId, query = {}) {
   if (!config.knex) throw new Error('db connector needs to be specified at service init');
 
   const cleanQuery = listQuery(query);
+  log('cleaned query: %j', cleanQuery);
 
   const k = config.knex(config.schemas.custom).where('form_schema_id', formSchemaId);
 
@@ -22,6 +23,7 @@ function _base(formSchemaId, query = {}) {
 }
 
 module.exports = async (formSchemaId, query = {}, offset = 0, limit = 20) => {
+  log('listing data form schema %s with query %j', formSchemaId, query);
   return {
     items: (
       await _base(formSchemaId, query).select(['id', 'identifier', 'store']).limit(limit).offset(offset)
