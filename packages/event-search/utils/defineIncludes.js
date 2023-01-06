27a9a238d35de9af2e9efb33c38277b25854bfb2
Kaoré <kaore@openagenda.com>
2023-01-06 10:22:01 +0100

    chore(event-search): linting

--- packages/event-search/utils/defineIncludes.js
+++ packages/event-search/utils/defineIncludes.js
@@ -2,29 +2,40 @@
 
 const getFormSchemaAdditionalFields = require('./getFormSchemaAdditionalFields');
 
+function _keepHigherOrderIncludes(includes = []) {
+  const higherOrderIncludes = includes.filter(i => i.indexOf('.') === -1);
+
+  return includes.filter(include => {
+    if (higherOrderIncludes.includes(include)) {
+      return true;
+    }
+    return !higherOrderIncludes.includes(include.split('.').shift());
+  });
+}
+
 module.exports = ({
   baseSearchIncludes,
-  detailedSearchIncludes
+  detailedSearchIncludes,
 }, {
   detailed,
   formSchema,
   access,
-  requested
+  requested,
 }) => {
   const additionalFields = formSchema ? getFormSchemaAdditionalFields(formSchema).map(f => f.field) : [];
   const knownFields = baseSearchIncludes.concat(detailedSearchIncludes).concat(additionalFields);
 
   const includes = [].concat(
-    !requested ? baseSearchIncludes : []
+    !requested ? baseSearchIncludes : [],
   ).concat(
-    !requested && detailed ? detailedSearchIncludes : []
+    !requested && detailed ? detailedSearchIncludes : [],
   ).concat(
-    requested ? requested : []
+    requested || [],
   ).concat(requested ? [] : additionalFields)
     .map(field => ({
-      field: field,
+      field,
       higherOrderField: field.split('.').shift(),
-      keep: knownFields.includes(field) ? field : null
+      keep: knownFields.includes(field) ? field : null,
     }))
     .map(({ higherOrderField, keep }) => {
       if (!requested || keep) {
@@ -36,7 +47,8 @@ module.exports = ({
       }
 
       return null;
-    }).filter(field => !!field);
+    })
+    .filter(field => !!field);
 
   if (!access || !formSchema) {
     return _keepHigherOrderIncludes(includes);
@@ -46,19 +58,8 @@ module.exports = ({
     const formSchemaField = formSchema.fields.filter(f => f.field === fieldName).pop();
 
     if (!formSchemaField && (baseSearchIncludes.includes(fieldName) || detailedSearchIncludes.includes(fieldName))) {
-      return true
+      return true;
     }
     return !formSchemaField || !formSchemaField.read || formSchemaField.read.includes(access);
   }));
-}
-
-function _keepHigherOrderIncludes(includes = []) {
-  const higherOrderIncludes = includes.filter(i => i.indexOf('.') === -1);
-
-  return includes.filter(include => {
-    if (higherOrderIncludes.includes(include)) {
-      return true;
-    }
-    return !higherOrderIncludes.includes(include.split('.').shift());
-  });
-}
+};
