195b65ac946f1fc59f224f71a53900deac738f64
Kaoré <kaore@openagenda.com>
2023-01-06 10:11:49 +0100

    feat(react-filters): handle deep schemas

---
+++ react-filters/src/utils/getAdditionalFilters.js
@@ -0,0 +1,23 @@
+const isAdditionalField = field => field.schemaId && ['checkbox', 'radio', 'multiselect', 'boolean', 'select'].includes(field.fieldType);
+const getFieldPath = (field, path = '') => (path.length ? `${path}:${field.slug ?? field.field}` : field.slug ?? field.field);
+
+export default function getAdditionalFilters(fields, path = '') {
+  return fields.reduce((additionalFilters, field) => {
+    const fieldPath = getFieldPath(field, path);
+
+    if (field.schema) {
+      return additionalFilters.concat(getAdditionalFilters(field.schema.fields, fieldPath));
+    }
+    if (!isAdditionalField(field)) {
+      return additionalFilters;
+    }
+    additionalFilters.push({
+      name: fieldPath,
+      fieldSchema: {
+        ...field,
+        field: fieldPath,
+      },
+    });
+    return additionalFilters;
+  }, []);
+}
