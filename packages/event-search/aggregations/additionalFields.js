58a70a023ca72aefff8bb0be8724ed8f1a343857
Kaoré <kaore@openagenda.com>
2023-01-13 17:51:02 +0100

    refactor(event-search): use form-schemas schema flattener

--- packages/event-search/aggregations/additionalFields.js
+++ packages/event-search/aggregations/additionalFields.js
@@ -5,7 +5,11 @@ const {
   BadRequest,
 } = require('@openagenda/verror');
 
-const getFlattenedSchema = require('../utils/getFlattenedSchema');
+const {
+  utils: {
+    flattenSchema: getFlattenedSchema,
+  },
+} = require('@openagenda/form-schemas');
 
 const isNumberLike = value => !Number.isNaN(Number(value)) && Number.isFinite(parseInt(value, 10));
 const extractFromAggregationKey = key => {
