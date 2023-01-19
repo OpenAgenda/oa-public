a30ccc790995b4a6dde8181878ef75422b23dc3c
Kévin Berthommier <bertho-zero@users.noreply.github.com>
2023-01-19 14:43:53 +0100

    fix(react-shared): fix test

--- react-shared/test/setup.js
+++ react-shared/test/setup.js
@@ -1,8 +1,4 @@
 import { configure } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
-import { createSerializer as createEnzymeSerializer } from 'enzyme-to-json';
-import { createSerializer as createEmotionSerializer } from 'jest-emotion';
+import Adapter from '@cfaester/enzyme-adapter-react-18';
 
 configure({ adapter: new Adapter() });
-expect.addSnapshotSerializer(createEnzymeSerializer({ mode: 'deep' }));
-expect.addSnapshotSerializer(createEmotionSerializer());
