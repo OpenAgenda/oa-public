96e4ccd11b6f84e31d70cff845ef178748bf8e19
Kévin Berthommier <bertho-zero@users.noreply.github.com>
2023-02-15 13:59:54 +0100

    fix(react-shared): add default value to ApiClientProvider

--- public/react-shared/src/components/ApiClientProvider.js
+++ public/react-shared/src/components/ApiClientProvider.js
@@ -2,7 +2,7 @@ import { useMemo } from 'react';
 import { ApiClientContext } from '../contexts';
 import apiClient from '../utils/apiClient';
 
-export default function ApiClientProvider({ value, children }) {
+export default function ApiClientProvider({ value = null, children }) {
   const client = useMemo(() => value || apiClient(), [value]);
 
   return (
