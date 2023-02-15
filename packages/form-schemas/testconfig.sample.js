74351d0983af5f487def788ea0d439ce9f8a835b
Kaoré <kaore@openagenda.com>
2023-02-15 17:50:49 +0100

    refactor(form-schemas): removed no longer used config

--- packages/form-schemas/testconfig.sample.js
+++ packages/form-schemas/testconfig.sample.js
@@ -4,20 +4,10 @@ module.exports = {
   mysql: {
     database: 'oatest_aes',
     user: 'root',
-    password: 'grut'
+    password: 'grut',
   },
   schemas: {
     network: 'network',
-    formSchema: 'form_schema'
+    formSchema: 'form_schema',
   },
-  legacy: {
-    database: 'oatest_aes',
-    user: 'root',
-    password: 'grut',
-    schemas: {
-      agenda: 'legacy_agenda',
-      tagSet: 'legacy_tag_set',
-      categorySet: 'legacy_category_set'
-    }
-  }
 };
