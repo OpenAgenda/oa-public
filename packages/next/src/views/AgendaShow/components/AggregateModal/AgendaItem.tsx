adef7a90ebe51d526125a1914a8bac9c17b5e866
Clément Lecroart <clement.lecroart@openagenda.com>
2023-03-07 18:07:37 +0100

    fix(next): AggregateModal graylogo

--- packages/next/src/views/AgendaShow/components/AggregateModal/AgendaItem.tsx
+++ packages/next/src/views/AgendaShow/components/AggregateModal/AgendaItem.tsx
@@ -5,6 +5,7 @@ import Image from 'components/Image';
 import OfficialAgenda from 'components/OfficialAgenda';
 import PrivateAgenda from 'components/PrivateAgenda';
 import keyCDNLoader from 'utils/keyCDNLoader';
+import graylogo140 from '../../../../../public/images/graylogo140.png';
 
 export default function AgendaItem({ agenda, targetAgenda }) {
   const isDev = process.env.NODE_ENV === 'development';
@@ -13,6 +14,7 @@ export default function AgendaItem({ agenda, targetAgenda }) {
 
   const url = new URL(router.asPath, 'http://n');
   // const redirectUrlPart = Buffer.from(url.pathname).toString('base64');
+  const imageSrc = targetAgenda.image && `${process.env.NEXT_PUBLIC_IMAGE_PREFIX}${targetAgenda.image}`;
 
   return (
     <Link
@@ -26,14 +28,14 @@ export default function AgendaItem({ agenda, targetAgenda }) {
           rounded="full"
           width="40"
           height="40"
-          src={targetAgenda.image}
-          fallbackSrc={isDev
-            ? targetAgenda.image.replace('cibuldev', 'cibul').replace('images-', 'imagesdev-')
+          src={imageSrc || graylogo140}
+          fallbackSrc={isDev && typeof imageSrc === 'string'
+            ? imageSrc.replace('cibuldev', 'cibul').replace('images-', 'imagesdev-')
             : undefined}
           fallbackStrategy="onError"
           alt=""
           draggable={false}
-          loader={keyCDNLoader}
+          loader={imageSrc ? keyCDNLoader : null}
           border="3px solid white"
           h="40px"
           fit="cover"
