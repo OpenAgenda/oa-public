9ae816fa76340bc0b09cdd8444e24d1559f2e59e
Kévin Berthommier <bertho-zero@users.noreply.github.com>
2023-03-07 11:24:41 +0100

    perf(uikit): lighter ChakraProvider without ColorModeProvider

--- public/uikit/src/Provider.tsx
+++ public/uikit/src/Provider.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import { ChakraProvider } from '@chakra-ui/react';
 import { CacheProvider, EmotionCache } from '@emotion/react';
+import { ChakraProvider } from './components/ChakraProvider';
 import theme from './theme';
 import defaultCache from './cache';
 
---
+++ public/uikit/src/components/ChakraProvider.tsx
@@ -0,0 +1,90 @@
+/*
+ * ChakraProvider without ColorModeProvider
+ * from https://github.com/chakra-ui/chakra-ui/blob/main/packages/components/provider/src/chakra-provider.tsx
+ */
+
+import {
+  CSSReset,
+  CSSPolyfill,
+  PortalManager,
+  GlobalStyle,
+  ThemeProvider,
+  ThemeProviderProps,
+  EnvironmentProvider,
+  EnvironmentProviderProps,
+} from '@chakra-ui/react';
+
+type Dict = Record<string, any>;
+
+export interface ChakraProviderProps
+  extends Pick<ThemeProviderProps, 'cssVarsRoot'> {
+  /**
+   * a theme. if omitted, uses the default theme provided by chakra
+   */
+  theme?: Dict;
+  /**
+   * Common z-index to use for `Portal`
+   *
+   * @default undefined
+   */
+  portalZIndex?: number;
+  /**
+   * If `true`, `CSSReset` component will be mounted to help
+   * you reset browser styles
+   *
+   * @default true
+   */
+  resetCSS?: boolean;
+  /**
+   * Your application content
+   */
+  children?: React.ReactNode;
+  /**
+   * The environment (`window` and `document`) to be used by
+   * all components and hooks.
+   *
+   * By default, we smartly determine the ownerDocument and defaultView
+   * based on where `ChakraProvider` is rendered.
+   */
+  environment?: EnvironmentProviderProps['environment'];
+  /**
+   * Disabled the use of automatic window and document detection.
+   * This removed the injected `<span/>` element
+   */
+  disableEnvironment?: boolean;
+}
+
+/**
+ * The global provider that must be added to make all Chakra components
+ * work correctly
+ */
+export const ChakraProvider: React.FC<ChakraProviderProps> = props => {
+  const {
+    children,
+    portalZIndex,
+    resetCSS = true,
+    theme = {},
+    environment,
+    cssVarsRoot,
+    disableEnvironment,
+  } = props;
+
+  const _children = (
+    <EnvironmentProvider
+      environment={environment}
+      disabled={disableEnvironment}
+    >
+      {children}
+    </EnvironmentProvider>
+  );
+
+  return (
+    <ThemeProvider theme={theme as Dict} cssVarsRoot={cssVarsRoot}>
+      {resetCSS ? <CSSReset /> : <CSSPolyfill />}
+      <GlobalStyle />
+      {portalZIndex ? (
+        <PortalManager zIndex={portalZIndex}>{_children}</PortalManager>
+      ) : _children}
+    </ThemeProvider>
+  );
+};
