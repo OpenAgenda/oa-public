1407b61b15f79dd4718469e0e23a327772101f0c
Clément Lecroart <clement.lecroart@openagenda.com>
2023-03-27 12:33:30 +0200

    chore(cibul-node): lib/taskify lint

--- packages/cibul-node/lib/taskify.js
+++ packages/cibul-node/lib/taskify.js
@@ -1,85 +1,67 @@
-"use strict";
+'use strict';
 
-const moment = require( 'moment' );
+const moment = require('moment');
 
-const utils = require( '@openagenda/utils' );
-
+const utils = require('@openagenda/utils');
 
 /**
  * prepare task for periodic and offsetted runs
  */
 
-module.exports = function ( run, options ) {
+module.exports = function (run, options) {
+  const params = utils.extend({
+    period: false, // periodicity of the task
+    bootOffset: 0, // offset time at which task will do its first run
+  }, options || {});
 
-  var params = utils.extend( {
-    period: false,  // periodicity of the task
-    bootOffset: 0   // offset time at which task will do its first run
-  }, options ? options : {} );
-
-  if ( !params.bootOffset ) {
-
-    params.bootOffset = _setBootOffset( params );
-
+  if (!params.bootOffset) {
+    params.bootOffset = _setBootOffset(params);
   }
 
-  if ( params.period === 'hourly' ) {
-
+  if (params.period === 'hourly') {
     params.period = 60000 * 60;
-
   }
 
-  if ( params.period === 'daily' ) {
-
+  if (params.period === 'daily') {
     params.period = 60000 * 60 * 24;
-
   }
 
-  if ( params.period === 'weekly' ) {
-
+  if (params.period === 'weekly') {
     params.period = 60000 * 60 * 24 * 7;
-
   }
 
-
-  setTimeout( function () {
-
+  setTimeout(() => {
     run();
 
-    if ( params.period !== false ) {
-
-      setInterval( run, params.period );
-
+    if (params.period !== false) {
+      setInterval(run, params.period);
     }
+  }, params.bootOffset);
+};
 
-  }, params.bootOffset );
-
-}
-
-function _setBootOffset( params ) {
-
+function _setBootOffset(params) {
   const { period, day, time } = params;
 
-  if ( !time ) return 0;
+  if (!time) return 0;
 
-  const [ hour, minute ] = time.split( ':' );
+  const [hour, minute] = time.split(':');
 
-  const now = moment.utc().locale( 'en' );
+  const now = moment.utc().locale('en');
   const nextTime = now.clone();
 
-  if ( day ) {
-    nextTime.startOf( 'week' ).day( day );
+  if (day) {
+    nextTime.startOf('week').day(day);
   }
 
-  nextTime.hour( hour ).minute( minute );
+  nextTime.hour(hour).minute(minute);
 
-  if ( nextTime.isBefore( now ) ) {
-    if ( period === 'weekly' ) {
-      nextTime.add( 1, 'week' );
+  if (nextTime.isBefore(now)) {
+    if (period === 'weekly') {
+      nextTime.add(1, 'week');
     } else {
-      nextTime.add( 1, 'day' );
+      nextTime.add(1, 'day');
     }
   }
 
-  return nextTime.diff( now );
-
+  return nextTime.diff(now);
 }
