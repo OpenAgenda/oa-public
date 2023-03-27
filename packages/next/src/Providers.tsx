7e4190eb1edd28839c43a4c342ab0a33fe66015d
Kévin Berthommier <bertho-zero@users.noreply.github.com>
2023-03-27 15:31:37 +0200

    feat(next): remove useless swr-devtools

--- packages/next/src/Providers.tsx
+++ packages/next/src/Providers.tsx
@@ -1,5 +1,4 @@
 import { SWRConfig } from 'swr';
-// import { SWRDevTools } from 'swr-devtools';
 import { CookiesProvider, Cookies } from 'react-cookie';
 import { UIKitProvider } from '@openagenda/uikit';
 import swrStatusMiddleware from 'utils/swrStatusMiddleware';
@@ -16,7 +15,6 @@ const Providers = ({ locale, intlMessages, cookies, children }: ProvidersProps) 
   <CookiesProvider cookies={cookies}>
     <UIKitProvider>
       <LanguageProvider locale={locale} messages={intlMessages}>
-        {/* <SWRDevTools> */}
         <SWRConfig
           value={{
             use: [swrStatusMiddleware],
@@ -24,7 +22,6 @@ const Providers = ({ locale, intlMessages, cookies, children }: ProvidersProps) 
         >
           {children}
         </SWRConfig>
-        {/* </SWRDevTools> */}
       </LanguageProvider>
     </UIKitProvider>
   </CookiesProvider>
