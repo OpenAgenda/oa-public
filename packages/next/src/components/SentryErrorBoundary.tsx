038c07d3f21193976d9a122dca93fb0682ea2ec9
Kévin Berthommier <bertho-zero@users.noreply.github.com>
2023-03-31 15:18:25 +0200

    feat(next): remove fatal level

--- packages/next/src/components/SentryErrorBoundary.tsx
+++ packages/next/src/components/SentryErrorBoundary.tsx
@@ -4,9 +4,6 @@ import { ErrorDisplay } from './ErrorDisplay';
 export default function SentryErrorBoundary({ children }) {
   return (
     <SErrorBoundary
-      beforeCapture={scope => {
-        scope.setLevel('fatal');
-      }}
       fallback={({ error, eventId, resetError }) => (
         <ErrorDisplay error={error} eventId={eventId} resetError={resetError} />
       )}
