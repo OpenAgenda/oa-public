2840b8ebbbee9c6af25c1b08ffa97791620bbb0c
Kévin Berthommier <bertho-zero@users.noreply.github.com>
2023-04-05 09:40:56 +0200

    feat(cibul-node): sentry don't handle all object errors

--- packages/cibul-node/lib/sentryErrorHandler.js
+++ packages/cibul-node/lib/sentryErrorHandler.js
@@ -2,10 +2,22 @@
 
 const sentry = require('@sentry/node');
 
+function isObject(o) {
+  return Object.prototype.toString.call(o) === '[object Object]';
+}
+
 module.exports = function sentryErrorHandler(options) {
   const originalHandler = sentry.Handlers.errorHandler(options);
 
   return function sentryErrorMiddleware(err, req, res, next) {
+    if (isObject(err)) {
+      const statusCode = err.statusCode || err.code || 500;
+
+      if (Number.isInteger(statusCode) && statusCode < 500) {
+        return next(err);
+      }
+    }
+
     sentry.withScope(scope => {
       if (options?.tag) {
         scope.setTag(options.tag, true);
