c6e2c3e74a9e78fafd7ad72838f6cb59d34f6208
Kévin Berthommier <bertho-zero@users.noreply.github.com>
2023-04-18 14:09:52 +0200

    feat(utils): remove hsts

--- packages/utils/express/https.js
+++ packages/utils/express/https.js
@@ -1,7 +1,5 @@
 'use strict';
 
-const hsts = require('hsts');
-
 module.exports = (req, res, next) => {
   if (!req.secure) {
     const redirectTo = 'https://' + req.hostname + req.originalUrl;
@@ -13,9 +11,5 @@ module.exports = (req, res, next) => {
     return res.redirect(301, redirectTo);
   }
 
-  hsts({
-    maxAge: 63072000,
-    includeSubDomains: true,
-    preload: true,
-  })(req, res, next);
+  next();
 }
