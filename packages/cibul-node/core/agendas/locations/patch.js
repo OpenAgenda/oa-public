0a865a1d83e86e620eb48a85a30da4ae1fd02962
Kaoré <kaore@openagenda.com>
2023-05-25 15:22:59 +0200

    chore(cibul-node): linting

--- packages/cibul-node/core/agendas/locations/patch.js
+++ packages/cibul-node/core/agendas/locations/patch.js
@@ -4,27 +4,25 @@ const log = require('@openagenda/logs')('core/agendas/locations/patch');
 
 const getAgenda = require('../utils/getAgenda');
 
-module.exports = (core, agendaOrUid) => {
-  return async function patchLocation(uid, data) {
-    const {
-      agendaLocations,
-    } = core.services;
+module.exports = (core, agendaOrUid) => async function patchLocation(uid, data) {
+  const {
+    agendaLocations,
+  } = core.services;
 
-    const agenda = await getAgenda(core.services, agendaOrUid);
+  const agenda = await getAgenda(core.services, agendaOrUid);
 
-    const endpoints = agenda.locationSetUid ? agendaLocations.sets(agenda.locationSetUid).locations : agendaLocations(agenda.uid);
+  const endpoints = agenda.locationSetUid ? agendaLocations.sets(agenda.locationSetUid).locations : agendaLocations(agenda.uid);
 
-    try {
-      const result = await endpoints.patch(uid, data, {
-        geocodeIfUndefined: true,
-        includeImagePath: true,
-        agendaUid: agenda.uid,
-      });
+  try {
+    const result = await endpoints.patch(uid, data, {
+      geocodeIfUndefined: true,
+      includeImagePath: true,
+      agendaUid: agenda.uid,
+    });
 
-      return result;
-    } catch (e) {
-      log('error', 'failed to patch location: %j', e.info);
-      throw new Error(e);
-    }
-  };
+    return result;
+  } catch (e) {
+    log('error', 'failed to patch location: %j', e.info);
+    throw new Error(e);
+  }
 };
