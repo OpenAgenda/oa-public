6f5bf7aceb9463cce31a9f259a3ae99c34baaa03
Kévin Berthommier <bertho-zero@users.noreply.github.com>
2023-06-05 14:05:16 +0200

    refactor(cibul-node): refactor resetApiCounters

--- packages/cibul-node/general/resetApiCounters.task.js
+++ packages/cibul-node/general/resetApiCounters.task.js
@@ -1,97 +1,39 @@
-"use strict";
+'use strict';
 
-const log = require( '@openagenda/logs' )( 'daily task' );
-
-var config = require( '../config' ),
-
-model = require( '../services/model' ),
-
-redis = require( 'redis' ),
-
-running = false,
-
-_onStart,
-
-_onComplete;
-
+const log = require('@openagenda/logs')('resetApiCounters task');
 
 /**
  * exposed function list
  */
 
-module.exports = run;
+module.exports = (config, services) => {
+  let running = false;
 
-// for testing
-module.exports.setOnStart = setOnStart;
-module.exports.setOnComplete = setOnComplete;
-
-
-
-function run() {
-
-  var cli;
-
-  if ( running ) {
-
-    log( 'info', 'already running' );
-
-    return;
-
-  }
-
-  log( 'running' );
-
-  if ( _onStart ) _onStart();
-
-  running = true;
-
-  cli = redis.createClient( config.redis.port, config.redis.host );
-
-  model.lib.eachQuery( 'select id from api_key_set', function( row, qcb ) {
-
-    cli.hget( config.api.redis.prefix + row.id, config.api.redis.publishCount, function( err, count ) {
-
-      if ( err ) return qcb( err );
-
-      log( 'info', { message: 'api counter' , keySetId: row.id , count: count });
-
-      if ( !count ) {
-
-        return qcb();
-
-      }
-
-      cli.hset( config.api.redis.prefix + row.id, config.api.redis.publishCount, 0, qcb );
-
-    } );
-
-  }, function( err ) {
-
-    cli.quit();
-
-    running = false;
-
-    if ( err ) {
-
-      log( 'error', err );
-
+  return async () => {
+    if (running) {
+      log('info', 'already running');
+      return;
     }
 
-    if ( _onComplete ) _onComplete();
+    log('running');
+    running = true;
 
-  } );
+    const { knex, redis } = services;
 
-}
+    const stream = knex('api_key_set').select('id').stream();
 
-function setOnStart( cb ) {
+    for await (const row of stream) {
+      try {
+        const count = await redis.hGet(config.api.redis.prefix + row.id, config.api.redis.publishCount);
 
-  _onStart = cb;
+        log.info({ message: 'api counter', keySetId: row.id, count });
 
-}
-
-
-function setOnComplete( cb ) {
-
-  _onComplete = cb;
-
-}
+        if (count) {
+          await redis.hSet(config.api.redis.prefix + row.id, config.api.redis.publishCount, 0);
+        }
+      } catch (e) {
+        log.error(e);
+      }
+    }
+  };
+};
