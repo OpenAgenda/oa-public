abc22e273738db61baf3de63aa00c7ba3287b5ea
Clément Lecroart <clement.lecroart@openagenda.com>
2023-07-12 14:51:58 +0200

    feat(next): addMatomoTracker with Custom

--- packages/next/src/utils/addMatomoTracker.js
+++ packages/next/src/utils/addMatomoTracker.js
@@ -1,14 +1,20 @@
 /* eslint-disable */
 
-export default function addMatomoTracker({ matomoUrl, matomoSiteId }) {
+export default function addMatomoTracker({ matomoUrl, matomoSiteId, matomoCustom = null }) {
   console.log('addMatomoTracker');
+  if (matomoCustom) {
+    const custom = new Function(matomoCustom);
+    custom();
+    return; 
+  }
+  
   var _paq = window._paq = window._paq || [];
   /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
   _paq.push(['trackPageView']);
   console.log('after _paq.push([\'trackPageView\']);');
   _paq.push(['enableLinkTracking']);
   (function() {
-    var u=matomoUrl;
+    var u=`${matomoUrl}`;
     _paq.push(['setTrackerUrl', u+'matomo.php']);
     _paq.push(['setSiteId', matomoSiteId]);
     var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
