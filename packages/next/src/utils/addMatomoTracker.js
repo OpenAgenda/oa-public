7fbe013a9646ed1f1999dfeefe436e3d90103e08
Clément Lecroart <clement.lecroart@openagenda.com>
2023-07-12 15:18:00 +0200

    feat(next): addMatomoTracker with Custom

--- packages/next/src/utils/addMatomoTracker.js
+++ packages/next/src/utils/addMatomoTracker.js
@@ -1,23 +1,22 @@
 /* eslint-disable */
 
 export default function addMatomoTracker({ matomoUrl, matomoSiteId, matomoCustom = null }) {
-  console.log('addMatomoTracker');
   if (matomoCustom) {
+    console.log('addMatomoCustomTracker');
     const custom = new Function(matomoCustom);
     custom();
     return; 
   }
-  
+  console.log('addMatomoDefaultTracker');
   var _paq = window._paq = window._paq || [];
   /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
   _paq.push(['trackPageView']);
-  console.log('after _paq.push([\'trackPageView\']);');
   _paq.push(['enableLinkTracking']);
   (function() {
     var u=`${matomoUrl}`;
     _paq.push(['setTrackerUrl', u+'matomo.php']);
     _paq.push(['setSiteId', matomoSiteId]);
     var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-    g.async=true; g.src='//cdn.matomo.cloud/dopenagenda.matomo.cloud/matomo.js'; s.parentNode.insertBefore(g,s);
+    g.async=true; g.src=`//cdn.matomo.cloud/${matomoUrl}matomo.js`; s.parentNode.insertBefore(g,s);
   })();
 }
