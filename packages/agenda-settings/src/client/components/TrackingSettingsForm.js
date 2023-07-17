4d07bb9da9999594b55ecac133fdf51500d461de
Clément Lecroart <clement.lecroart@openagenda.com>
2023-07-17 16:15:57 +0200

    feat(agenda-settings): add matomo infos to tracking form

--- packages/agenda-settings/src/client/components/TrackingSettingsForm.js
+++ packages/agenda-settings/src/client/components/TrackingSettingsForm.js
@@ -1,11 +1,12 @@
 import _ from 'lodash';
-import React, { useCallback, useContext, useMemo } from 'react';
+import React, { useCallback, useContext, useMemo, useState } from 'react';
 import { Form, Field } from 'react-final-form';
+import Select from 'react-select';
 import cn from 'classnames';
 import { useDispatch } from 'react-redux';
 import { useLayoutData } from '@openagenda/react-shared';
 import I18nContext from '../contexts/I18nContext';
-import { BasicInput } from '../utils/inputs';
+import { BasicInput, InputGroup } from '../utils/inputs';
 import * as agendaActions from '../reducers/agenda';
 import catchFormErrors from '../utils/catchFormErrors';
 
@@ -24,23 +25,37 @@ function getFormValues(agenda) {
     googleAnalytics: _.get(agenda, 'settings.tracking.googleAnalytics', null),
     googleAnalyticsSecret: _.get(agenda, 'settings.tracking.googleAnalyticsSecret', null),
     secretCheck: !!_.get(agenda, 'settings.tracking.googleAnalyticsSecret', null),
+    matomoUrl: agenda?.settings?.tracking?.matomoUrl ? agenda.settings.tracking.matomoUrl.replace('.matomo.cloud/', '') : null,
+    matomoSiteId: _.get(agenda, 'settings.tracking.matomoSiteId', null),
+    matomoAskForConsent: agenda?.settings?.tracking?.matomoAskForConsent === false ? false : true,
   };
+}
+
+function getServiceInitialValue(agenda) {
+  if (agenda?.settings?.tracking?.googleAnalytics) return { label: 'Google Analytics', value: 'ga' };
+  if (agenda?.settings?.tracking?.matomoUrl) return { label: 'Matomo', value: 'matomo' };
+  return null;
 }
 
 export default function TrackingSettingsForm() {
   const { agenda } = useLayoutData();
   const { getLabel } = useContext(I18nContext);
-
+  const [service, setService] = useState(getServiceInitialValue(agenda));
   const dispatch = useDispatch();
 
   const initialValues = useMemo(() => getFormValues(agenda), [agenda]);
-
   const onSubmit = useCallback(
-    (data, form) => dispatch(agendaActions.edit({ settings: { tracking: data } }))
+    (data, form) => {
+      if (data.matomoUrl) {
+        data = {...data, matomoUrl: `${data.matomoUrl}.matomo.cloud/`}
+      }
+      return dispatch(agendaActions.edit({ settings: { tracking: data } }))
       .then(result => form.reset(result.data.agenda.settings.tracking))
-      .catch(error => catchFormErrors(error, 'settings.tracking')),
+      .catch(error => catchFormErrors(error, 'settings.tracking'))},
     [dispatch]
   );
+
+  const selectOptions = [{ label: 'Matomo', value: 'matomo' }, { label: 'Google Analytics', value: 'ga' }]
 
   return (
     <Form
@@ -57,54 +72,125 @@ export default function TrackingSettingsForm() {
         <form onSubmit={handleSubmit}>
           <p>{getLabel('statsDescription')}</p>
 
-          <Field
-            onChange={v => {
-              if (v?.substr(0, 1) !== 'G') {
-                form.mutators.setFormAttribute("secretCheck", false);
-                form.mutators.setFormAttribute("googleAnalyticsSecret", "");
-              }
-            }}
-            name="googleAnalytics"
-            component={BasicInput}
-            type="text"
-            label={getLabel('googleAnalyticsId')}
-            placeholder="GA_TRACKING_ID"
-            className="form-control"
-            parse={_.identity} // to keep empty value
-          />
-
-          <Field
-            name="secretCheck"
-            type="checkbox"
-            subscription={{ value: true }}
-            render={({ input }) => {
-              const googleAnalyticsValue = form.getState().values.googleAnalytics;
-              const disabled = googleAnalyticsValue ? !(googleAnalyticsValue?.substr(0, 1) === 'G') : true;
-              return (
-                <div className="checkbox">
-                  <label for='gaSecretCheckbox'>
-                    <input id='gaSecretCheckbox' {...input} disabled={disabled} checked={!!form.getState().values?.secretCheck && !disabled} title={getLabel('googleAnalyticsTitle')}/>
-                    <b>{getLabel('googleAnalyticsLabel')}</b>
-                    <br />
-                    <span className="text-muted">{getLabel('googleAnalyticsInfo')}</span>
-                  </label>
-                </div>
-              )
-            }}
-          />
-
-          {form.getState().values?.secretCheck ? (
-            <Field
-              name="googleAnalyticsSecret"
-              component={BasicInput}
-              type="text"
-              label={getLabel('googleAnalyticsSecret')}
-              placeholder="GA_TRACKING_SECRET"
-              className="form-control"
-              parse={_.identity} // to keep empty value
+          <div className="margin-bottom-sm">
+            <Select
+              disabled={false}
+              options={selectOptions}
+              styles={{
+                // Fixes the overlapping problem of the component
+                menu: provided => ({ ...provided, zIndex: 9999 })
+              }}
+              value={service}
+              onChange={val => {
+                setService(val)
+              }}
+              clearable={false}
+              placeholder={getLabel('trackSelectPlaceholder')}
             />
-          ) : null}
+          </div>
 
+          <div className="margin-left-sm">
+            {service?.value === 'ga' ? (
+              <>
+                <Field
+                  onChange={v => {
+                    if (v?.substr(0, 1) !== 'G') {
+                      form.mutators.setFormAttribute("secretCheck", false);
+                      form.mutators.setFormAttribute("googleAnalyticsSecret", "");
+                    }
+                  }}
+                  name="googleAnalytics"
+                  component={BasicInput}
+                  type="text"
+                  label={getLabel('googleAnalyticsId')}
+                  placeholder="GA_TRACKING_ID"
+                  className="form-control"
+                  parse={_.identity} // to keep empty value
+                />
+
+                <Field
+                  name="secretCheck"
+                  type="checkbox"
+                  subscription={{ value: true }}
+                  render={({ input }) => {
+                    const googleAnalyticsValue = form.getState().values.googleAnalytics;
+                    const disabled = googleAnalyticsValue ? !(googleAnalyticsValue?.substr(0, 1) === 'G') : true;
+                    return (
+                      <div className="checkbox">
+                        <label htmlFor='gaSecretCheckbox'>
+                          <input id='gaSecretCheckbox' {...input} disabled={disabled} checked={!!form.getState().values?.secretCheck && !disabled} title={getLabel('googleAnalyticsTitle')} />
+                          <b>{getLabel('googleAnalyticsLabel')}</b>
+                          <br />
+                          <span className="text-muted">{getLabel('googleAnalyticsInfo')}</span>
+                        </label>
+                      </div>
+                    )
+                  }}
+                />
+              </>
+            ) : null}
+
+            {form.getState().values?.secretCheck ? (
+              <Field
+                name="googleAnalyticsSecret"
+                component={BasicInput}
+                type="text"
+                label={getLabel('googleAnalyticsSecret')}
+                placeholder="GA_TRACKING_SECRET"
+                className="form-control"
+                parse={_.identity} // to keep empty value
+              />
+            ) : null}
+
+            {service?.value === 'matomo' ? (
+              <>
+                <Field
+                  name="matomoUrl"
+                  component={InputGroup}
+                  type="text"
+                  label={getLabel('matomoUrl')}
+                  placeholder="example"
+                  className="form-control"
+                  parse={_.identity} // to keep empty value
+                  before={<span className="input-group-addon" id="basic-addon1">https://</span>}
+                  after={<span className="input-group-addon" id="basic-addon2">.matomo.cloud/</span>}
+                />
+
+                <Field
+                  name="matomoSiteId"
+                  component={BasicInput}
+                  type="text"
+                  label={getLabel('matomoSiteId')}
+                  placeholder="1"
+                  className="form-control"
+                  parse={_.identity} // to keep empty value
+                />
+                <Field
+                  name="matomoAskForConsent"
+                  type="checkbox"
+                  subscription={{ value: true }}
+                  render={({ input }) => {
+                    return (
+                      <div className="checkbox">
+                        <label htmlFor='matomoAskForConsent'>
+                          <input id='matomoAskForConsent' {...input} checked={!!form.getState().values?.matomoAskForConsent} title='matomoAskForConsent' />
+                          <b>{getLabel('matomoAskForConsent')}</b>
+                          <br />
+                          <span className="text-muted">{getLabel('matomoAskForConsentInfo')}</span>
+                        </label>
+                      </div>
+                    )
+                  }}
+                />
+
+                <div className="info-block-sm margin-bottom-sm">
+                  {getLabel('matomoCustomConfig')}
+                  <a href={`/support?origin=${encodeURIComponent(window.location.pathname)}&subject=matomoSettings}`} className="link-primary">
+                  {getLabel('matomoSupport')}
+                  </a>
+                </div>
+              </>) : null}
+          </div>
           < button
             type="submit"
             className={cn('btn btn-primary', { disabled: pristine || hasValidationError })}
