85a74612b7ad3d12e63e75a6aa85254c310d56a2
Clément Lecroart <clement.lecroart@openagenda.com>
2023-07-19 13:01:05 +0200

    feat(cibul-templates): matomo tracking for event page

---
+++ packages/cibul-templates/agenda/js/addMatomoTracker.js
@@ -0,0 +1,28 @@
+/* eslint-disable */
+
+export default function addMatomoTracker({ matomoUrl, matomoSiteId, matomoCustom = null }) {
+  if (matomoCustom) {
+    console.log('addMatomoCustomTracker');
+    const custom = new Function(matomoCustom);
+    custom();
+    return;
+  }
+  console.log('addMatomoDefaultTracker', matomoUrl, matomoSiteId, matomoCustom);
+  var _paq = window._paq = window._paq || [];
+  /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+  _paq.push(['trackPageView']);
+  _paq.push(['enableLinkTracking']);
+  var u = `https://${matomoUrl}`;
+  _paq.push(['setTrackerUrl', u + 'matomo.php']);
+  _paq.push(['setSiteId', matomoSiteId]);
+  var d = document, g = d.createElement('script'), s = d.getElementsByTagName('script')[0];
+  g.async = true; g.src = `//cdn.matomo.cloud/${matomoUrl}matomo.js`; s.parentNode.insertBefore(g, s);
+}
+
+/* [[['trackPageView']], ['enableLinkTracking'], ['setTrackerUrl', u + 'matomo.php']] */
+
+/* {
+  matomoUrl:
+  matomoSiteId: 
+  matomoPaq: []
+} */
