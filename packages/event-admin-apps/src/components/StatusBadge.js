a41281a1526e7517365df689a61ee155b71b59fe
Kaoré <kaore@openagenda.com>
2023-08-10 11:41:23 +0200

    refactor(event-admin-apps): avoid repetition

--- packages/event-admin-apps/src/components/StatusBadge.js
+++ packages/event-admin-apps/src/components/StatusBadge.js
@@ -1,62 +1,48 @@
 import statusMessages from '@openagenda/common-labels/event/statuses';
 
-export default function StatusBadge({ status, intl }) {
-  let elem;
+const statuses = [
+  {
+    id: 2,
+    slug: 'rescheduled',
+    className: 'label label-warning',
+  },
+  {
+    id: 3,
+    slug: 'movedOnline',
+    className: 'label label-warning',
+  },
+  {
+    id: 4,
+    slug: 'postponed',
+    className: 'label label-warning',
+  },
+  {
+    id: 5,
+    slug: 'full',
+    className: 'label label-danger',
+  },
+  {
+    id: 6,
+    slug: 'cancelled',
+    className: 'label label-danger',
+  },
+];
 
-  switch (status) {
-    case 2:
-      elem = (
-        <span
-          title={intl.formatMessage(statusMessages.rescheduledInfo)}
-          className="label label-warning"
-        >
-          {intl.formatMessage(statusMessages.rescheduled)}
-        </span>
-      );
-      break;
-    case 3:
-      elem = (
-        <span
-          title={intl.formatMessage(statusMessages.movedOnlineInfo)}
-          className="label label-warning"
-        >
-          {intl.formatMessage(statusMessages.movedOnline)}
-        </span>
-      );
-      break;
-    case 4:
-      elem = (
-        <span
-          title={intl.formatMessage(statusMessages.postponedInfo)}
-          className="label label-warning"
-        >
-          {intl.formatMessage(statusMessages.postponed)}
-        </span>
-      );
-      break;
-    case 5:
-      elem = (
-        <span
-          title={intl.formatMessage(statusMessages.fullInfo)}
-          className="label label-danger"
-        >
-          {intl.formatMessage(statusMessages.full)}
-        </span>
-      );
-      break;
-    case 6:
-      elem = (
-        <span
-          title={intl.formatMessage(statusMessages.cancelledInfo)}
-          className="label label-danger"
-        >
-          {intl.formatMessage(statusMessages.cancelled)}
-        </span>
-      );
-      break;
-    default:
-      return null;
+export default function StatusBadge({ status, intl }) {
+  const { slug, className } = statuses.find(s => s.id === status) ?? {};
+
+  if (!slug) {
+    return null;
   }
 
-  return <span className="padding-right-sm status">{elem}</span>;
+  return (
+    <span className="padding-right-sm status">
+      <span
+        title={intl.formatMessage(statusMessages[`${slug}Info`])}
+        className={className}
+      >
+        {intl.formatMessage(statusMessages[slug])}
+      </span>
+    </span>
+  );
 }
