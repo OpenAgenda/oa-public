23ed85b8e6af1797c96e3ebe3decbfa0026324c1
Kaoré <kaore@openagenda.com>
2023-08-16 18:37:44 +0200

    chore(cibul-node): linting

--- packages/cibul-node/services/mails/lib/filterBouncingAndUnsubscribed.js
+++ packages/cibul-node/services/mails/lib/filterBouncingAndUnsubscribed.js
@@ -1,45 +1,47 @@
-"use strict";
+'use strict';
 
-const _ = require( 'lodash' );
+const _ = require('lodash');
+const axios = require('axios');
 
-const isUnsubscribed = require( './isUnsubscribed' );
+const isUnsubscribed = require('./isUnsubscribed');
 
-const log = require( '@openagenda/logs' )( 'services/mails/filterBouncingAndUnsubscribed' );
+module.exports = async (services, config, params) => {
+  const { logs } = services;
+  const log = logs('services/mails/filterBouncingAndUnsubscribed');
 
-module.exports = async ( services, config, params ) => {
-  const unsubscriptions = params.to.unsubscriptions;
+  const { unsubscriptions } = params.to;
   const abilityArgs = unsubscriptions && unsubscriptions.length
-    ? _.find( unsubscriptions, 'memberId' ) || unsubscriptions[ unsubscriptions.length - 1 ]
+    ? _.find(unsubscriptions, 'memberId') || unsubscriptions[unsubscriptions.length - 1]
     : null;
   const email = params.to.address;
 
   try {
-    const { data: bounce } = await axios( `https://api.mailgun.net/v3/${config.mailgun.domain}/bounces/${email}`, {
+    const { data: bounce } = await axios(`https://api.mailgun.net/v3/${config.mailgun.domain}/bounces/${email}`, {
       auth: {
         username: 'api',
-        password: config.mailgun.apiKey
-      }
-    } );
+        password: config.mailgun.apiKey,
+      },
+    });
 
-    if ( bounce && bounce.code ) {
+    if (bounce && bounce.code) {
       return false;
     }
-  } catch ( error ) {
-    if ( error.response && error.response.status !== 404 ) {
-      log.error( 'Cannot check bounced address on Mailgun', error );
+  } catch (error) {
+    if (error.response && error.response.status !== 404) {
+      log.error('Cannot check bounced address on Mailgun', error);
     }
   }
 
-  if ( !abilityArgs || !abilityArgs.rule ) {
+  if (!abilityArgs || !abilityArgs.rule) {
     return true;
   }
 
   const { memberId, rule } = abilityArgs;
 
   // member
-  if ( memberId ) {
-    return !( await isUnsubscribed( services, config, { entityName: 'member', identifier: memberId }, ...rule ) );
+  if (memberId) {
+    return !await isUnsubscribed(services, config, { entityName: 'member', identifier: memberId }, ...rule);
   }
 
-  return !( await isUnsubscribed( services, config, email, ...rule ) );
-}
+  return !await isUnsubscribed(services, config, email, ...rule);
+};
