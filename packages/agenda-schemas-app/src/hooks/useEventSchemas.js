fb847038006e652a6618dbcdecb435823b7bea3b
Kévin Berthommier <bertho-zero@users.noreply.github.com>
2023-08-23 15:03:23 +0200

    fix(agenda-schemas-app): fix missing refetch

--- packages/agenda-schemas-app/src/hooks/useEventSchemas.js
+++ packages/agenda-schemas-app/src/hooks/useEventSchemas.js
@@ -6,11 +6,12 @@ import useRes from './useRes';
 export default (agenda, memberMode) => {
   if (memberMode) return false;
   const res = useRes(agenda);
-  const { isLoading, error, data } = useQuery(['agenda-eventSchema', agenda.uid], () =>
+  const { isLoading, error, refetch, data } = useQuery(['agenda-eventSchema', agenda.uid], () =>
     axios.get(res.eventSchema, { params: {} }).then(response => response.data), { staleTime: Infinity });
   return {
     isLoading,
     error,
+    refetch,
     schema: data?.schema || null,
     parents: data?.parents,
   };
