15156f7ba9e2b4d3278b7004db141ceba7b06833
Kaoré <kaore@openagenda.com>
2023-08-30 11:46:47 +0200

    fix(files): config sample file refs aws keys through env vars

--- packages/files/testconfig.sample.js
+++ packages/files/testconfig.sample.js
@@ -4,13 +4,13 @@ module.exports = {
   bucket: 'cibultest',
   tmpBucketPath: 'https://cibultmp.s3.amazonaws.com/',
   tmpBucket: 'cibultmp',
-  accessKeyId: 'AKIAJCTNQBIZSAPX7HUQ',
-  secretAccessKey: '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',
+  accessKeyId: process.env.AWS_KEY,
+  secretAccessKey: process.env.AWS_SECRET,
 
   // v3
   s3: {
-    accessKeyId: 'AKIAJCTNQBIZSAPX7HUQ',
-    secretAccessKey: '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',
+    accessKeyId: process.env.AWS_KEY,
+    secretAccessKey: process.env.AWS_SECRET,
     region: 'eu-west-1', // optional
     defaultBucket: 'oadev',
   },
