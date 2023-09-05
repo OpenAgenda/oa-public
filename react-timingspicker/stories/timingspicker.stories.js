52a029e387bb3fc1ed6be459eacff450d915975d
Kévin Berthommier <bertho-zero@users.noreply.github.com>
2023-09-05 12:22:44 +0200

    feat(react-timingspicker): upgrade storybook

--- react-timingspicker/stories/timingspicker.stories.js
+++ react-timingspicker/stories/timingspicker.stories.js
@@ -1,80 +1,91 @@
-import React from 'react';
-import { storiesOf } from '@storybook/react';
 import TimingsPicker from '../src';
 
 import 'react-day-picker/lib/style.css';
 import '../src/App.css';
 
-storiesOf('App', module)
-  .add('default', () => <TimingsPicker />)
-  .add('with allowed timings', () => (
-    <TimingsPicker
-      allowedTimings={[
-        {
-          begin: '2019-05-15',
-          end: '2019-05-16',
-        },
-        {
-          begin: '2019-05-17',
-          end: '2019-05-24',
-        },
-      ]}
-    />
-  ))
-  .add('with defined value', () => (
-    <TimingsPicker
-      value={[
-        {
-          begin: '2019-10-31T10:30',
-          end: '2019-10-31T22:30',
-        },
-      ]}
-    />
-  ))
-  .add('with scroll focused on value', () => (
-    <TimingsPicker
-      value={[
-        {
-          begin: '2019-08-10T04:30',
-          end: '2019-08-10T22:30',
-        },
-      ]}
-      locale="fr"
-    />
-  ))
-  .add('in French language', () => (
-    <TimingsPicker
-      allowedTimings={[
-        {
-          begin: '2019-05-15',
-          end: '2019-05-16',
-        },
-        {
-          begin: '2019-05-17',
-          end: '2019-05-24',
-        },
-      ]}
-      locale="fr"
-    />
-  ))
-  .add('override locales', () => (
-    <TimingsPicker
-      locales={{
-        fr: {
-          'rtp.defineTiming': 'Clique et glisse !',
-        },
-      }}
-      locale="fr"
-    />
-  ))
-  .add('DST', () => (
-    <TimingsPicker
-      value={[
-        {
-          begin: '2019-03-31T00:30:00',
-          end: '2019-03-31T02:30:00',
-        },
-      ]}
-    />
-  ))
-  .add('disable editOnClick', () => <TimingsPicker editOnClick={false} />);
+// Titre de la story
+export default {
+  title: 'App',
+  component: TimingsPicker,
+};
+
+// Stories individuelles
+export const Default = () => <TimingsPicker />;
+
+export const WithAllowedTimings = () => (
+  <TimingsPicker
+    allowedTimings={[
+      {
+        begin: '2019-05-15',
+        end: '2019-05-16',
+      },
+      {
+        begin: '2019-05-17',
+        end: '2019-05-24',
+      },
+    ]}
+  />
+);
+
+export const WithDefinedValue = () => (
+  <TimingsPicker
+    value={[
+      {
+        begin: '2019-10-31T10:30',
+        end: '2019-10-31T22:30',
+      },
+    ]}
+  />
+);
+
+export const WithScrollFocusedOnValue = () => (
+  <TimingsPicker
+    value={[
+      {
+        begin: '2019-08-10T04:30',
+        end: '2019-08-10T22:30',
+      },
+    ]}
+    locale="fr"
+  />
+);
+
+export const InFrenchLanguage = () => (
+  <TimingsPicker
+    allowedTimings={[
+      {
+        begin: '2019-05-15',
+        end: '2019-05-16',
+      },
+      {
+        begin: '2019-05-17',
+        end: '2019-05-24',
+      },
+    ]}
+    locale="fr"
+  />
+);
+
+export const OverrideLocales = () => (
+  <TimingsPicker
+    locales={{
+      fr: {
+        'rtp.defineTiming': 'Clique et glisse !',
+      },
+    }}
+    locale="fr"
+  />
+);
+
+export const DST = () => (
+  <TimingsPicker
+    value={[
+      {
+        begin: '2019-03-31T00:30:00',
+        end: '2019-03-31T02:30:00',
+      },
+    ]}
+  />
+);
+
+export const DisableEditOnClick = () => <TimingsPicker editOnClick={false} />;
