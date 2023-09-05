4154621df4e858363242fe41a65c772365c7a74f
Kévin Berthommier <bertho-zero@users.noreply.github.com>
2023-09-05 12:22:44 +0200

    feat(react-timingspicker): upgrade storybook

--- public/react-timingspicker/stories/bootstrap.stories.js
+++ public/react-timingspicker/stories/bootstrap.stories.js
@@ -1,25 +1,31 @@
-import React from 'react';
-import { storiesOf } from '@storybook/react';
 import TimingsPicker, { classNames } from '../src';
 
 import 'bootstrap/dist/css/bootstrap.css';
 import 'react-day-picker/lib/style.css';
 import '../src/App.css';
 
-storiesOf('Boostrap', module)
-  .add('default', () => <TimingsPicker />)
-  .add('with allowed timings', () => (
-    <TimingsPicker
-      allowedTimings={[
-        {
-          begin: '2019-05-15',
-          end: '2019-05-16',
-        },
-        {
-          begin: '2019-05-17',
-          end: '2019-05-24',
-        },
-      ]}
-      classNames={classNames.bootstrap3}
-    />
-  ));
+// Titre de la story
+export default {
+  title: 'Boostrap',
+  component: TimingsPicker,
+};
+
+// Première story
+export const Default = () => <TimingsPicker />;
+
+// Deuxième story
+export const WithAllowedTimings = () => (
+  <TimingsPicker
+    allowedTimings={[
+      {
+        begin: '2019-05-15',
+        end: '2019-05-16',
+      },
+      {
+        begin: '2019-05-17',
+        end: '2019-05-24',
+      },
+    ]}
+    classNames={classNames.bootstrap3}
+  />
+);
