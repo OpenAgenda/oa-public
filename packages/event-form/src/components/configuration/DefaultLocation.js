57ce986dba6f7ba51f091af9213ac5181fd4d33f
Clément Lecroart <clement.lecroart@openagenda.com>
2023-09-08 17:28:07 +0200

    feat(event-form): defaultLocation locationSelector

--- packages/event-form/src/components/configuration/DefaultLocation.js
+++ packages/event-form/src/components/configuration/DefaultLocation.js
@@ -1,7 +1,11 @@
 import Select from 'react-select';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 import countries from '@openagenda/countries/labels';
 import defaultLocationLabels from '@openagenda/labels/event/defaultLocation';
 import flattenLabels from '@openagenda/labels/flatten';
+import LocationSelector from '@openagenda/agenda-locations-app/dist/components/LocationSelector';
+import Provider from '@openagenda/agenda-locations-app/dist/decorators/Providers';
 
 const DefaultLocation = ({
   lang = 'fr',
@@ -18,29 +22,79 @@ const DefaultLocation = ({
 
   const options = extractCountryNames();
   const selectValue = options.find(option => option.value === value?.countryCode);
+  const [mode, setMode] = useState(value.uid ? 'show' : 'search');
+  const [loc, setLoc] = useState(null);
+
+  useEffect(() => {
+    if (value.uid) {
+      axios.get(field.res.getLocationDetails.replace(':locationUid', value.uid)).then(r => setLoc(r.data));
+    }
+  }, []);
+
+  const locationSelectorOnChange = (m, l) => {
+    setMode(m);
+    setLoc(l);
+    if (l.uid) onChange({ ...value, uid: l.uid });
+  };
 
   return (
-    <div
-      className={
-        enabled
-          ? 'form-group country'
-          : 'form-group country disabled'
-      }
-    >
-      <label htmlFor="Country">
-        {labels.chooseDefaultCountry}
-      </label>
+    <>
+      <div className="margin-bottom-md">
+        <label htmlFor="DefaultLocation">
+          {labels.chooseDefaultLocation}
+        </label>
+        <Provider lang={lang}>
+          <LocationSelector
+            enableDetails={false}
+            allowCreate={false}
+            confirmRequired={false}
+            tiles={null}
+            mode={mode}
+            disableChange={false}
+            detailedInfo={null}
+            classNames={{
+              input: '',
+            }}
+            allowRemove
+            onRemove={() => {
+              setLoc(null);
+              setMode('search');
+              onChange((({ uid, ...o }) => o)(value));
+            }}
+            location={loc}
+            lang={lang}
+            settings={null}
+            res={{
+              get: field.res.getLocationDetails,
+              index: field.res.listLocations,
+              staticTiles: field.res.staticTiles,
+            }}
+            onChange={(m, l) => locationSelectorOnChange(m, l)}
+          />
+        </Provider>
+      </div>
+      <div
+        className={
+          enabled
+            ? 'form-group country'
+            : 'form-group country disabled'
+        }
+      >
+        <label htmlFor="Country">
+          {labels.chooseDefaultCountry}
+        </label>
 
-      <Select
-        disabled={!enabled}
-        options={options}
-        value={selectValue}
-        onChange={val => {
-          onChange({ ...value, countryCode: val.value });
-        }}
-        clearable={false}
-      />
-    </div>
+        <Select
+          disabled={!enabled}
+          options={options}
+          value={selectValue}
+          onChange={val => {
+            onChange({ ...value, countryCode: val.value });
+          }}
+          clearable={false}
+        />
+      </div>
+    </>
   );
 };
 export default DefaultLocation;
