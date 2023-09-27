9e60f033d0a4675dd9e8f89aa5d024960577d947
Kévin Berthommier <bertho-zero@users.noreply.github.com>
2023-09-27 16:46:21 +0200

    feat(uikit): add colorScheme support to link

--- public/uikit/src/theme/components/index.ts
+++ public/uikit/src/theme/components/index.ts
@@ -1,6 +1,7 @@
 import { buttonTheme as Button } from './button';
 import { checkboxTheme as Checkbox } from './checkbox';
 import { headingTheme as Heading } from './heading';
+import { linkTheme as Link } from './link';
 import { menuTheme as Menu } from './menu';
 import { modalTheme as Modal } from './modal';
 
@@ -10,6 +11,7 @@ export const components = {
   Button,
   Checkbox,
   Heading,
+  Link,
   Menu,
   Modal,
 };
