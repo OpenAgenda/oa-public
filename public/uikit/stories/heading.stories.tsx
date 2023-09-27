66825e466a9745dd26bf662d7f713576e7f04464
Kévin Berthommier <bertho-zero@users.noreply.github.com>
2023-09-27 16:45:18 +0200

    feat(uikit): add heading components

---
+++ public/uikit/stories/heading.stories.tsx
@@ -0,0 +1,36 @@
+import { H1, H2, H3, H4, H5, H6, VStack } from '../src';
+import Provider from './decorators/Provider';
+
+export default {
+  title: 'OpenAgenda/Components/Heading',
+  decorators: [Provider],
+};
+
+export function All() {
+  return (
+    <VStack spacing="4">
+      <H1>
+        This is a h1
+      </H1>
+      <H2>
+        This is a h2
+      </H2>
+      <H3>
+        This is a h3
+      </H3>
+      <H4>
+        This is a h4
+      </H4>
+      <H5>
+        This is a h5
+      </H5>
+      <H6>
+        This is a h6
+      </H6>
+
+      <H1 size="md">
+        This is a h1 with an overridden size
+      </H1>
+    </VStack>
+  );
+}
