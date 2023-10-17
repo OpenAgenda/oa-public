811168515705e9bf269653b9f37d48883e7795f4
Kévin Berthommier <bertho-zero@users.noreply.github.com>
2023-10-17 16:30:01 +0200

    docs: update thumbor guide

--- wiki/jelastic/thumbor_loaders/thumbor_loaders/s3_loader.py
+++ wiki/jelastic/thumbor_loaders/thumbor_loaders/s3_loader.py
@@ -1,11 +1,15 @@
 from thumbor.loaders import http_loader
 
-BUCKET = 'cibul'
 REGION = 'eu-west-1'
 
-# https://cibul.s3.eu-west-1.amazonaws.com/0000261cd25648a7815ac41d9cbb2767.thumb.image.jpg
-
 async def load(context, url, *args, **kwargs):
-  new_url = 'https://' + BUCKET + '.s3.' + REGION + '.amazonaws.com/' + url
+  bucket, path = _get_bucket_and_path(url)
+  new_url = 'https://' + bucket + '.s3.' + REGION + '.amazonaws.com/' + path
 
   return await http_loader.load(context, new_url, *args, **kwargs)
+
+def _get_bucket_and_path(url):
+    segments = url.split('/')
+    bucket = segments[0]
+    path = '/'.join(segments[1:])
+    return bucket, path
