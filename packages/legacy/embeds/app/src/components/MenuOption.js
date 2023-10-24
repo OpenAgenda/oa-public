ffe6aae58bc378d9240d36c9484cc090c564fab8
Kaoré <kaore@openagenda.com>
2023-10-24 18:37:36 +0200

    feat(legacy): present embed options other than legacy

---
+++ packages/legacy/embeds/app/src/components/MenuOption.js
@@ -0,0 +1,18 @@
+export default function TableRow({ value, head, summary, selection, onSelect }) {
+  return (
+    <div className="radio margin-v-md">
+      <label htmlFor={value}>
+        <input
+          id={value}
+          name="embed-option"
+          type="radio"
+          checked={selection === value}
+          className="padding-right-xs"
+          onChange={onSelect.bind(null, value)}
+        />
+        {head}
+        <div className="text-muted">{summary}</div>
+      </label>
+    </div>
+  );
+}
