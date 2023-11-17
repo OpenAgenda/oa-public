5af146a1255342b0a0cf6b34d05a29f475fdd095
DFarau <doriane.farau@hetic.net>
2023-11-17 16:39:31 +0100

    feat(pdf-exports): add agenda property

--- pdf-exports/test/integrated.test.js
+++ pdf-exports/test/integrated.test.js
@@ -3,16 +3,19 @@ import { fileURLToPath } from 'url';
 import { dirname } from 'path';
 import PDFExports from '../index.js';
 import FixturesStream from './lib/fixturesStream.js';
+import agenda from './fixtures/agenda.json' assert { type: 'json' };
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-const eventStream = new FixturesStream(`${__dirname}/test/fixtures/event.json`);
+const eventStream = new FixturesStream(`${__dirname}/fixtures/event.json`);
 const writeStream = fs.createWriteStream(
-  `${__dirname}/pdf-test/streamOutputTest.pdf`,
+  `${__dirname}/../pdf-test/streamOutputTest.pdf`,
 );
 
 const pdfExports = PDFExports({});
 
 await eventStream.load();
-await pdfExports.GenerateExportStream(eventStream, writeStream);
+await pdfExports.GenerateExportStream(eventStream, writeStream, {
+  agenda,
+});
