802bab9ea0daf8bf390e12a5699876e07c0d0feb
Kaoré <kaore@openagenda.com>
2023-11-29 15:35:38 +0100

    refactor(cibul-node): removed unused parameter from function

--- packages/cibul-node/core/agendas/settings/getMergedSchema.js
+++ packages/cibul-node/core/agendas/settings/getMergedSchema.js
@@ -10,7 +10,7 @@ const getNetwork = require('../utils/getNetwork');
 const merge = require('../utils/merge');
 const getMemberSchema = require('../utils/getMemberSchema');
 
-async function loadFormSchema(formSchemas, agendaId, formSchemaId) {
+async function loadFormSchema(formSchemas, formSchemaId) {
   if (formSchemaId) {
     return formSchemas.get(formSchemaId);
   }
@@ -63,7 +63,6 @@ module.exports = async (services, agendaOrUid, options = {}) => {
   const agenda = _.isObject(agendaOrUid) ? agendaOrUid : await getAgenda(services, agendaOrUid);
 
   const {
-    id: agendaId,
     networkUid,
     formSchemaId,
   } = agenda;
@@ -72,7 +71,6 @@ module.exports = async (services, agendaOrUid, options = {}) => {
 
   const formSchema = await loadFormSchema(
     formSchemas,
-    agendaId,
     formSchemaId,
     !!_.get(network, 'formSchemaId'),
   ).then(s => (s ? { ...s, type: 'agenda' } : s));
