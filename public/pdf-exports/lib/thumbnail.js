dabb75301fe0d1355416f2164aa0fd01077b5b69
DFarau <doriane.farau@hetic.net>
2023-12-05 14:53:36 +0100

    feat(pdf-exports): add a function to manage thumbnails

---
+++ public/pdf-exports/lib/thumbnail.js
@@ -0,0 +1,29 @@
+import urlToBuffer from './urlToBuffer.js';
+
+export default async function thumbnail(
+  event,
+  __dirname,
+  imageWidth,
+  imageHeight,
+) {
+  const oaLogoPath = `${__dirname}/../images/oaLogo.png`;
+
+  const thumbnailFilename = event.image?.variants.find(
+    el => el.type === 'thumbnail',
+  )?.filename;
+
+  const newVersionThumbnail = thumbnailFilename?.includes('.thumb.image.jpg');
+
+  let imageUrl;
+
+  if (!thumbnailFilename) {
+    imageUrl = oaLogoPath;
+  } else if (!newVersionThumbnail) {
+    const baseImageUrl = `https://img.openagenda.com/u/${imageWidth}x${imageHeight}/cibul/`;
+    imageUrl = await urlToBuffer(baseImageUrl + event.image.filename);
+  } else {
+    imageUrl = await urlToBuffer(event.image.base + thumbnailFilename);
+  }
+
+  return imageUrl;
+}
