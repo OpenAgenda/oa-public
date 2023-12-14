2ff4e5b4c0b3b718bf2c6047a05b1e9769e2b367
Kaoré <kaore@openagenda.com>
2023-12-14 18:54:13 +0100

    refactor(date-range): replaced mochashould with jest

--- packages/date-range/test/index.test.js
+++ packages/date-range/test/index.test.js
@@ -4,78 +4,46 @@
 
 require( 'should' );
 
-var range = require('../'),
+var range = require('..'),
 
 testData = require( './data' );
 
 describe( 'date-range', () => {
 
   describe( 'no dates', () => {
-
     it( 'empty array input should display "no dates available"', () => {
-
-      range( [], 'en' )
-
-      .should.equal( 'no dates available' );
-
+      expect(range( [], 'en' )).toBe('no dates available');
     } );
 
     it( 'undefined input should display "no dates available', () => {
-
-      range( undefined, 'en' )
-
-      .should.equal( 'no dates available' );
-
+      expect(range( undefined, 'en' )).toBe('no dates available');
     } );
-
   } );
 
   describe( 'one date', () => {
-
     it( 'should displays as "Tuesday 18 December 2018, 07:00, 10:00, 11:00"', function() {
-
-      range( testData.oneDate.winterDefault, 'en' )
-
-      .should.be.equal( 'Tuesday 18 December 2018, 07:00, 10:00, 11:00' );
-
+      expect(range( testData.oneDate.winterDefault, 'en' )).toBe('Tuesday 18 December 2018, 07:00, 10:00, 11:00');
     });
 
     it( 'should display as "Tuesday 18 December 2018, 08:00, 11:00, 12:00"', () => {
-
-      range( testData.oneDate.winterDefault, 'en', 'Europe/Paris' )
-
-      .should.be.equal( 'Tuesday 18 December 2018, 08:00, 11:00, 12:00' );
-
+      expect(range( testData.oneDate.winterDefault, 'en', 'Europe/Paris' )).toBe('Tuesday 18 December 2018, 08:00, 11:00, 12:00');
     } );
 
     it( 'should display as "Wednesday 18 April 2018, 09:00, 12:00, 13:00"', () => {
-
-      range( testData.oneDate.summerDefault, 'en', 'Europe/Paris' )
-
-      .should.be.equal( 'Wednesday 18 April 2018, 09:00, 12:00, 13:00' );
-
+      expect(range( testData.oneDate.summerDefault, 'en', 'Europe/Paris' )).toBe('Wednesday 18 April 2018, 09:00, 12:00, 13:00');
     } );
 
     var output2 = 'Thursday 18 December 2014, 07:00, 10:00, 11:00';
 
     it( 'differentYear should displays as "'+ output2 +'"', () => {
-
-      range( testData.oneDate.differentYear, 'en' )
-
-      .should.be.equal( output2 );
-
+      expect(range( testData.oneDate.differentYear, 'en' )).toBe(output2);
     });
 
     var output3 = 'Jeudi 18 décembre 2014, 07h00, 10h00, 11h00';
 
     it( 'differentYear, French should displays as "'+ output3 +'"', () => {
-
-      range( testData.oneDate.differentYear, 'fr' )
-
-      .should.be.equal( output3 );
-
+      expect(range( testData.oneDate.differentYear, 'fr' )).toBe(output3);
     } );
-
   } );
 
   describe( 'Case: two dates', () => {
@@ -84,9 +52,7 @@ describe( 'date-range', () => {
 
     it( 'if two dates, displays as "'+ output +'"', () => {
 
-      range( testData.twoDates.default, 'en' )
-
-      .should.be.equal( output );
+      expect(range( testData.twoDates.default, 'en' )).toBe( output );
 
     } );
 
@@ -94,9 +60,7 @@ describe( 'date-range', () => {
 
     it( 'if two dates differentYear, displays as "'+ output2 +'"', () => {
 
-      range( testData.twoDates.differentYear, 'en' )
-
-      .should.be.equal( output2 );
+      expect(range( testData.twoDates.differentYear, 'en' )).toBe( output2 );
 
     } );
 
@@ -104,9 +68,7 @@ describe( 'date-range', () => {
 
     it( 'French: if two dates differentYear, displays as "'+ output3 +'"', () => {
 
-      range( testData.twoDates.differentYear, 'fr' )
-
-      .should.be.equal( output3 );
+      expect(range( testData.twoDates.differentYear, 'fr' )).toBe( output3 );
 
     } );
 
@@ -115,18 +77,14 @@ describe( 'date-range', () => {
 
     it( 'French: Different initialization if two dates differentYear, displays as "'+ output4 +'"', function(){
 
-      range( testData.twoDates.differentYear, 'fr' )
-
-      .should.be.equal( output4 );
+      expect(range( testData.twoDates.differentYear, 'fr' )).toBe( output4 );
 
     } );
 
 
     it( 'if one date on different year only, display both years: 16 décembre 2014 et 18 décembre 2016', () => {
 
-      range( testData.twoDates.oneDifferentYear, 'fr' )
-
-      .should.be.equal( '16 décembre 2014 et 18 décembre 2018' );
+      expect(range( testData.twoDates.oneDifferentYear, 'fr' )).toBe( '16 décembre 2014 et 18 décembre 2018' );
 
     } );
 
@@ -140,17 +98,13 @@ describe( 'date-range', () => {
 
     it( 'case:default should display as"'+ output +'"', () => {
 
-      range( testData.moreDates.default, 'en' )
-
-      .should.be.equal( output );
+      expect(range( testData.moreDates.default, 'en' )).toBe( output );
 
     });
 
     it( 'should display months when more than one', () => {
 
-      range( testData.moreDates.multipleMonths, 'en' )
-
-      .should.equal( '16 November - 19 December 2018' );
+      expect(range( testData.moreDates.multipleMonths, 'en' )).toBe( '16 November - 19 December 2018' );
 
     } );
 
@@ -161,26 +115,16 @@ describe( 'date-range', () => {
   describe( 'wrong input', () => {
 
     it( 'renders in english if unknown or non existing language is input', () => {
-
-      range( testData.moreDates.default, 'ak' )
-
-      .should.be.equal( '16 - 19 December 2018' );
-
+      expect(range( testData.moreDates.default, 'ak' )).toBe( '16 - 19 December 2018' );
     } );
 
   } );
 
 
   describe( 'patterns', () => {
-
     it( 'appends an information relative to the day of the week: 1 - 22 december 2015, on tuesdays', () => {
-
-      range( testData.moreDates.tuesdays, 'en' )
-
-      .should.equal( '1 - 22 December 2015, on tuesdays' );
-
+      expect(range( testData.moreDates.tuesdays, 'en' )).toBe( '1 - 22 December 2015, on Tuesdays' );
     } );
-
   } );
 
 
