62567bb42739f28786a548d90e635224ad93f9d2
Kaoré <kaore@openagenda.com>
2023-12-18 14:47:23 +0100

    refactor(cibul-node): refresh session when user cache is cleared

--- packages/cibul-node/services/users/lib/resetCache.js
+++ packages/cibul-node/services/users/lib/resetCache.js
@@ -5,9 +5,14 @@ const log = require('@openagenda/logs')('services/users/resetCache');
 module.exports = async function resetCache(services, user) {
   const {
     simpleCache,
+    sessions,
   } = services;
 
   log('resetting');
-  simpleCache('users', `api_key:${user.apiKey}`).del();
-  simpleCache('users', `api_secret:${user.apiSecret}`).del();
+
+  return Promise.all([
+    sessions.refresh(user.uid),
+    simpleCache('users', `api_key:${user.apiKey}`).del(),
+    simpleCache('users', `api_secret:${user.apiSecret}`).del(),
+  ]);
 };
