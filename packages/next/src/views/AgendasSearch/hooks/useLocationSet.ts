8de2f0a581418aa8a651a5b865a22879d8ebf970
Kévin Berthommier <bertho-zero@users.noreply.github.com>
2024-01-03 17:48:31 +0100

    feat(next): use default fetcher

--- packages/next/src/views/AgendasSearch/hooks/useLocationSet.ts
+++ packages/next/src/views/AgendasSearch/hooks/useLocationSet.ts
@@ -1,26 +1,13 @@
 import useSWRImmutable from 'swr/immutable';
 import useLocationQuery from 'hooks/useLocationQuery';
 
-const fetcher = url => fetch(url)
-  .then(
-    r => {
-      if (r.ok) return r.json();
-      // TODO should recreate an error with data in `await r.json()`
-      // console.log('ERROR response', await r.json());
-      throw new Error('Error');
-    },
-  );
-
 export default function useLocationSet() {
   const query = useLocationQuery();
 
   const {
     data: locationSet,
     ...rest
-  } = useSWRImmutable(
-    query.locationSet ? `/api/locationSets/${query.locationSet}` : null,
-    fetcher,
-  );
+  } = useSWRImmutable(query.locationSet ? `/api/locationSets/${query.locationSet}` : null);
 
   return { locationSet, ...rest };
 }
