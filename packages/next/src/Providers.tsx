1bc80d47b23b1acc8a5f2dd9eb234d48c0b4a845
Kévin Berthommier <bertho-zero@users.noreply.github.com>
2024-01-03 17:45:33 +0100

    feat(next): default fetcher

--- packages/next/src/Providers.tsx
+++ packages/next/src/Providers.tsx
@@ -1,4 +1,5 @@
 import { SWRConfig } from 'swr';
+import ky from 'ky';
 import { CookiesProvider, Cookies } from 'react-cookie';
 import { UIKitProvider } from '@openagenda/uikit';
 import swrStatusMiddleware from 'utils/swrStatusMiddleware';
@@ -11,12 +12,15 @@ type ProvidersProps = {
   children: React.ReactNode;
 };
 
+const fetcher = (input: string | URL | Request) => ky(input).json();
+
 const Providers = ({ locale, intlMessages, cookies, children }: ProvidersProps) => (
   <CookiesProvider cookies={cookies}>
     <UIKitProvider>
       <LanguageProvider locale={locale} messages={intlMessages}>
         <SWRConfig
           value={{
+            fetcher,
             use: [swrStatusMiddleware],
           }}
         >
