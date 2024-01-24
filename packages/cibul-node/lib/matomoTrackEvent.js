e705e3fd86b20e3fd79086a68ea54a8271821110
Kaoré <kaore@openagenda.com>
2024-01-24 11:00:50 +0100

    refactor(cibul-node): passing config to matomo tracking code

--- packages/cibul-node/lib/matomoTrackEvent.js
+++ packages/cibul-node/lib/matomoTrackEvent.js
@@ -8,23 +8,27 @@ const completeUrl = url => {
   return `${url}/matomo.php`;
 };
 
-module.exports = function matomoTrackEvent(matomoUrl, matomoSiteId, category, action, label, rest) {
+module.exports = function matomoTrackEvent({
+  matomoUrl,
+  matomoSiteId,
+  category,
+  action,
+  label,
+  rest,
+  rootPath,
+}) {
   const completedUrl = completeUrl(matomoUrl);
   const payload = {
     idsite: matomoSiteId,
     rec: '1',
     url: rest?.dl,
     action_name: `${category}_${action}_${label}`,
-    // Additional optional parameters
-    /*     cvar: JSON.stringify({
-        '1': ['Custom Dimension Name', 'Custom Dimension Value'],
-      }), */
   };
 
   return axios.post(`https://${completedUrl}`, payload, {
     headers: {
       'Content-Type': 'application/x-www-form-urlencoded',
-      Origin: 'https://d.openagenda.com',
+      Origin: rootPath,
     },
   });
 };
