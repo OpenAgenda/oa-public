b94d45309dfb29cc7785c05ce621b282e2206a44
Kaoré <kaore@openagenda.com>
2024-01-24 09:57:40 +0100

    chore: tweaks to make matomo self hosted

--- packages/cibul-node/services/dynamicScripts.js
+++ packages/cibul-node/services/dynamicScripts.js
@@ -32,7 +32,7 @@ module.exports.init = config => {
         _paq.push(['setTrackerUrl', u+'matomo.php']);
         _paq.push(['setSiteId', '1']);
         var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-        g.async=true; g.src='https://cdn.matomo.cloud/${config.matomoCloudId}/matomo.js'; s.parentNode.insertBefore(g,s);
+        g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
       })();
     ` : '',
   };
