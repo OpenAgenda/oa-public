df70cafed0b892fae691fdff7d4e8e9d08a5aef1
DFarau <doriane.farau@hetic.net>
2024-01-24 15:29:54 +0100

    feat(pdf-exports): add a function to manage registration items

---
+++ pdf-exports/lib/getTruncatedLabel.js
@@ -0,0 +1,38 @@
+import addText from './addText.js';
+
+function isOverflowing(size, columnWidth) {
+  return size.width > columnWidth;
+}
+
+export default function getTruncatedLabel(
+  doc,
+  cursor,
+  columnWidth,
+  itemLabel,
+  options = {},
+) {
+  const { fontSize = 10 } = options;
+
+  let slicedSize = addText(doc, cursor, itemLabel, {
+    fontSize,
+    simulate: true,
+  });
+  let slicedText = itemLabel;
+
+  while (isOverflowing(slicedSize, columnWidth)) {
+    slicedText = `${slicedText.slice(0, -2)}…`;
+    slicedSize = addText(doc, cursor, slicedText, {
+      fontSize,
+      simulate: true,
+    });
+
+    if (slicedText === '…') {
+      break;
+    }
+  }
+  return {
+    label: slicedText,
+    width: slicedSize.width,
+    height: slicedSize.height,
+  };
+}
---
+++ pdf-exports/test/addRegistration.test.js
@@ -0,0 +1,54 @@
+import fs from 'node:fs';
+import PDFDocument from 'pdfkit';
+import addRegistration from '../lib/addRegistration.js';
+import loadEventData from './lib/loadEventData.js';
+
+const {
+  AGENDA_UID: agendaUid,
+  PUBLIC_KEY: publicKey,
+  PDF_TEST_FOLDER: pdfTestFolder,
+} = process.env;
+
+(async (params = {}, options = {}) => {
+  const cursor = { x: 130, y: 0 };
+
+  const {
+    base = {
+      margin: 20,
+      color: '#413a42',
+      fontSize: 10,
+    },
+  } = params;
+
+  const { simulate = false } = options;
+
+  const iconHeightAndWidth = 10;
+  const imageWidth = 90;
+
+  const doc = new PDFDocument({ size: 'A4', margin: 0 });
+
+  const writeStream = fs.createWriteStream(
+    `${pdfTestFolder}/pdfTestRegistration.pdf`,
+  );
+
+  doc.pipe(writeStream);
+
+  const eventData = (await loadEventData(agendaUid, publicKey)).events;
+
+  for (const event of eventData) {
+    await addRegistration(
+      event,
+      doc,
+      cursor,
+      {
+        base,
+        iconHeightAndWidth,
+        imageWidth,
+      },
+      { simulate },
+    );
+    cursor.y += base.margin * 2;
+  }
+
+  doc.end();
+})();
