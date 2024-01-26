a79b136262c76517f2126d82b71543b5b929815b
Kévin Berthommier <bertho-zero@users.noreply.github.com>
2024-01-26 12:00:28 +0100

    style(intl): lint

--- intl/dist/getFallbackedMessages.mjs
+++
@@ -1,9 +0,0 @@
-import {
-  getFallbackedMessages
-} from "./chunk-XF6UE266.mjs";
-import "./chunk-DUAUT2UP.mjs";
-import "./chunk-L7V4YAKV.mjs";
-import "./chunk-FLP4ZIRX.mjs";
-export {
-  getFallbackedMessages as default
-};
--- intl/dist/getLocaleValue.mjs
+++
@@ -1,7 +0,0 @@
-import {
-  getLocaleValue
-} from "./chunk-BYM6ZJLT.mjs";
-import "./chunk-FLP4ZIRX.mjs";
-export {
-  getLocaleValue as default
-};
