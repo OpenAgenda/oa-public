fc43813ef038e98db36769f1fbf64ee6c598f54d
Kaoré <kaore@openagenda.com>
2024-02-05 12:37:31 +0100

    chore(validators): linting

--- packages/validators/test/regex.test.js
+++ packages/validators/test/regex.test.js
@@ -1,51 +1,38 @@
-"use strict";
+const validators = require('../src');
 
-const validators = require( '../src' );
+describe('regex validator', () => {
+  const validate = validators.regex({ field: 'stars', regex: /\*/g });
 
-describe( 'regex validator', () => {
-
-  var validate = validators.regex( { field: 'stars', regex: /\*/g } );
-
-  it( 'matches', () => {
-
+  it('matches', () => {
     let errors = [];
 
     try {
-
-      validate( '***' );
-
-    } catch ( e ) {
-
+      validate('***');
+    } catch (e) {
       errors = e;
-
     }
 
     expect(errors.length).toBe(0);
+  });
 
-  } );
-
-  it( 'does not match', () => {
-
+  it('does not match', () => {
     let errors = [];
 
-    try { validate( '+++' ) } catch( e ) { errors = e; }
+    try { validate('+++'); } catch (e) { errors = e; }
 
     expect(errors.length).toBe(1);
+  });
 
-  } );
+  it('cleans using regex', () => {
+    const validateAndClean = validators.regex({ regex: /[^/]+$/, clean: true });
 
-  it( 'cleans using regex', () => {
+    let errors = []; let
+      clean = false;
 
-    let validate = validators.regex( { regex: /[^\/]+$/, clean: true } ),
-
-    errors = [], clean = false;
-
-    try { clean = validate( '/image/path.png' ); } catch( e ) { errors = e; }
+    try { clean = validateAndClean('/image/path.png'); } catch (e) { errors = e; }
 
     expect(errors.length).toBe(0);
 
     expect(clean).toBe('path.png');
-
-  } );
-
-} );
+  });
+});
