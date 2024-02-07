88d66989e1b7ae7f9eac4b7fea1c91bff2621554
Kaoré <kaore@openagenda.com>
2024-02-07 16:37:41 +0100

    refactor(cibul-node): usageCounters logger is part of config like the rest and doesn't get its little side seat

--- packages/cibul-node/services/usageCounters/index.mjs
+++ packages/cibul-node/services/usageCounters/index.mjs
@@ -5,13 +5,11 @@ export function init(config) {
   return {
     ...usageCounters({
       logger: config.getLogConfig('svc', 'usage-counters'),
-      config: {
-        redisClient: config.redisClient,
-        knexClient: config.knex,
-        lifespan: 1000 * 60 * 60,
-        redisPrefix: null,
-        setKey: null,
-      },
+      redisClient: config.redisClient,
+      knexClient: config.knex,
+      lifespan: 1000 * 60 * 60,
+      redisPrefix: null,
+      setKey: null,
     }),
     mw: {
       increment,
