1b2de1b9c84b3576a80ed13ac9c24e80196323ff
DFarau <doriane.farau@hetic.net>
2024-02-09 11:58:58 +0100

    feat(pdf-exports): load api

---
+++ pdf-exports/test/lib/APIEventsStream.js
@@ -0,0 +1,83 @@
+import https from 'node:https';
+import { Readable } from 'readable-stream';
+import qs from 'qs';
+
+const API = 'https://api.openagenda.com/v2/agendas/{agendaUID}/events?key={APIKey}&detailed=1';
+
+export default class APIEventsStream extends Readable {
+  constructor({ agendaUID, APIKey, max }) {
+    super({ objectMode: true });
+
+    this.agendaUID = agendaUID;
+    this.APIKey = APIKey;
+
+    this.after = undefined;
+    this.buffer = [];
+    this.requestInProgress = false;
+    this.count = 0;
+    this.max = max;
+  }
+
+  _pushEvent() {
+    if (this.max !== undefined && this.count > this.max) {
+      this.push(null);
+      return;
+    }
+
+    this.push(this.buffer.shift());
+    this.count += 1;
+  }
+
+  _read() {
+    if (this.requestInProgress) return;
+
+    if (this.buffer.length !== 0) {
+      this._pushEvent();
+      return;
+    }
+
+    this.requestInProgress = true;
+
+    const url = [
+      API.replace('{agendaUID}', this.agendaUID).replace(
+        '{APIKey}',
+        this.APIKey,
+      ),
+    ]
+      .concat(this.after ? `&${qs.stringify({ after: this.after })}` : [])
+      .join('');
+
+    https
+      .get(url, response => {
+        let rawData = '';
+        response.setEncoding('utf8');
+        response.on('data', chunk => {
+          rawData += chunk;
+        });
+        response.on('end', () => {
+          try {
+            const result = JSON.parse(rawData);
+
+            const { events, after: newAfter } = result;
+
+            if (!events.length) {
+              this.push(null);
+              return;
+            }
+
+            this.after = newAfter.map(a => `${a}`);
+            this.buffer = events;
+            this.requestInProgress = false;
+            this._pushEvent();
+          } catch (error) {
+            this.emit('error', error);
+            this.requestInProgress = false;
+          }
+        });
+      })
+      .on('error', error => {
+        this.emit('error', error);
+        this.requestInProgress = false;
+      });
+  }
+}
