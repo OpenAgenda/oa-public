c1fb4b9b746a42113cccad0b8a4a3a6a7000e5e0
Kévin Berthommier <bertho-zero@users.noreply.github.com>
2024-02-14 11:06:55 +0100

    refactor(newsletter): upgrade mailjet

--- packages/newsletter/index.js
+++ packages/newsletter/index.js
@@ -1,51 +1,36 @@
-"use strict";
+'use strict';
 
-const _ = require( 'lodash' );
-const Mailjet = require( 'node-mailjet' );
-const logger = require( '@openagenda/logs' );
+const _ = require('lodash');
+const Mailjet = require('node-mailjet');
+const logger = require('@openagenda/logs');
 
-const log = logger( 'newsletter' );
-
-let config, mailjet;
-
-module.exports = {
-  init,
-  addSubscriber
-}
-
-/**
- * load up configuration in lib
- */
-function init( c ) {
-
-  config = _.merge( {
+module.exports = c => {
+  const config = _.merge({
     logger: false,
     mailjet: {
       apiKey: 'CanardLaKey',
       apiSecret: 'FranceToner',
       contactsListId: 'JM-France',
-      performApiCall: true // used for tests
-    }
-  }, c );
+    },
+  }, c);
 
-  if ( c.logger ) logger.setModuleConfig( c.logger );
+  if (c.logger) logger.setModuleConfig(c.logger);
 
-  mailjet = Mailjet.connect( config.mailjet.apiKey, config.mailjet.apiSecret, {
-    perform_api_call: config.mailjet.performApiCall
-  } );
-}
+  const mailjet = new Mailjet({
+    apiKey: config.mailjet.apiKey,
+    apiSecret: config.mailjet.apiSecret,
+  });
 
-/*
- * Add subscriber to Sendinblue
- */
-
-function addSubscriber( email ) {
-  return mailjet
-    .post( 'contactslist' )
-    .id( config.mailjet.contactsListId )
-    .action( 'managecontact' )
-    .request( {
-      Action: 'addnoforce',
-      Email: email
-    } );
-}
+  return {
+    async addSubscriber(email) {
+      return mailjet
+        .post('contactslist', { version: 'v3' })
+        .id(config.mailjet.contactsListId)
+        .action('managecontact')
+        .request({
+          Action: 'addnoforce',
+          Email: email,
+        });
+    },
+  };
+};
