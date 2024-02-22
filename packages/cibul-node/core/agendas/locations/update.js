a83c83c95cc89e37e10d106396ee555385726909
Clément Lecroart <clement.lecroart@openagenda.com>
2024-02-22 17:31:34 +0100

    chore(cibul-node): autocomplete from options for location patch and update

--- packages/cibul-node/core/agendas/locations/update.js
+++ packages/cibul-node/core/agendas/locations/update.js
@@ -9,6 +9,7 @@ module.exports = (core, agendaOrUid) => async (uid, data, options = {}) => {
 
   const {
     context = {},
+    autocomplete = true,
   } = options;
 
   const agenda = await getAgenda(core.services, agendaOrUid);
@@ -16,7 +17,7 @@ module.exports = (core, agendaOrUid) => async (uid, data, options = {}) => {
   const endpoints = agenda.locationSetUid ? agendaLocations.sets(agenda.locationSetUid).locations : agendaLocations(agenda.uid);
 
   return endpoints.update(uid, data, {
-    autocomplete: options.autocomplete,
+    autocomplete,
     includeImagePath: true,
     agendaUid: agenda.uid,
     context: {
