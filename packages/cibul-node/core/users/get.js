0da72d4953be65021efafc1c3f9d678ae1565295
Kaoré <kaore@openagenda.com>
2024-03-19 16:29:00 +0100

    chore(cibul-node): linting

--- packages/cibul-node/core/users/get.js
+++ packages/cibul-node/core/users/get.js
@@ -2,17 +2,15 @@
 
 const log = require('@openagenda/logs')('core/users/get');
 
-module.exports = core => {
-  return (userUid, options = {}) => {
-    log('getting user %s', userUid);
-    const {
-      users: usersSvc,
-    } = core.services;
+module.exports = core => (userUid, options = {}) => {
+  log('getting user %s', userUid);
+  const {
+    users: usersSvc,
+  } = core.services;
 
-    const {
-      detailed = false,
-    } = options;
+  const {
+    detailed = false,
+  } = options;
 
-    return usersSvc.findOne({ query: { uid: userUid }, detailed });
-  };
+  return usersSvc.findOne({ query: { uid: userUid }, detailed });
 };
