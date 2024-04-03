aeee82825feba4fc2599f17be77039198184dbb2
Kaoré <kaore@openagenda.com>
2024-04-03 15:30:18 +0200

    fix(agenda-contribute): removed references processing

--- packages/agenda-contribute/src/lib/cleanupSchemaForForm.js
+++ packages/agenda-contribute/src/lib/cleanupSchemaForForm.js
@@ -1,13 +1,5 @@
-import injectAgendaUIDAndSlug from './injectAgendaUID';
-
-export default function cleanupSchemaForForm(schema, { agenda, apiRoot }) {
+export default function cleanupSchemaForForm(schema) {
   schema.fields = schema.fields.filter(field => ![].concat(field.write).includes('internal'));
-
-  const referencesField = schema.fields.find(f => f.field === 'references');
-
-  if (referencesField) {
-    referencesField.res = injectAgendaUIDAndSlug(referencesField.res, apiRoot, agenda);
-  }
 
   return schema;
 }
