f2f476434bd7cacdd2297df3946db2bf25a16c61
Kaoré <kaore@openagenda.com>
2024-04-24 15:17:26 +0200

    chore(react-shared): linting

--- react-shared/stories/SelectInput.stories.js
+++ react-shared/stories/SelectInput.stories.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import '@openagenda/bs-templates/compiled/main.css';
 
 import ReactSelectInput from '../src/components/ReactSelectInput';
@@ -22,7 +22,7 @@ export const SelectInput = () => {
         options={[
           { label: 'first option', value: 'first option' },
           { label: 'second option', value: 'second option' },
-          { label: 'third option', value: 'third option' }
+          { label: 'third option', value: 'third option' },
         ]}
         onChange={onChange}
       />
@@ -40,7 +40,11 @@ export const InModal = () => {
     <div style={{ flex: '0 0 60%' }}>
       <p>Input permettant de rechercher et sélectionner une option.</p>
 
-      <button className="js_export_button btn btn-default btn-primary" type="button" onClick={() => setDisplay(true)}>
+      <button
+        className="js_export_button btn btn-default btn-primary"
+        type="button"
+        onClick={() => setDisplay(true)}
+      >
         <i className="fa fa-external-link" />
         <span>&nbsp; Open</span>
       </button>
