57bcf82c74a80982ec658bcc35c030198e915961
Clément Lecroart <clement.lecroart@openagenda.com>
2024-05-29 16:30:25 +0200

    refactor(legacy): use msw instead of axios-mock-adapter

--- packages/legacy/embeds/app/stories/integrated.stories.js
+++ packages/legacy/embeds/app/stories/integrated.stories.js
@@ -1,8 +1,7 @@
 import { wrapApp } from '@openagenda/react-shared';
 import React, { useRef } from 'react';
 import '@openagenda/bs-templates/compiled/main.css';
-import axios from 'axios';
-import MockAdapter from 'axios-mock-adapter';
+import { rest } from 'msw';
 import { HelmetProvider } from 'react-helmet-async';
 
 import createApp from '../src';
@@ -12,108 +11,136 @@ import apiAgendasToulouse from './fixtures/api.agendas.toulouse.get.json';
 import toulouseEvents from './fixtures/toulouse.events.json';
 import toulouseDefaultEmbed from './fixtures/toulouse.default.json';
 
-import AdminCanvas from './decorators/AdminCanvas';
+import AdminCanvas from './decorators/AdminCanvas'
 
 export default {
   title: 'Integrated',
   decorators: [AdminCanvas]
 };
 
-export function IntegratedDefaultTemplates() {
-  const selectionMenuRef = useRef();
-  const mock = new MockAdapter(axios, {
-    delayResponse: 1000
-  });
 
-  mock.onGet('/agendas/50522407').reply(200, apiAgendasToulouse);
-  mock.onGet('/agendas/50522407/embeds').reply(200, [toulouseDefaultEmbed]);
-  mock.onPost('/agendas/50522407/embeds').reply(200);
-  mock.onGet('/agendas/50522407/events').reply(200, toulouseEvents);
-
-  return (
-    <HelmetProvider>
-      <div className="col-md-3 col-md-push-5 col-sm-12" ref={selectionMenuRef} />
-      <div className="col-md-5 col-md-pull-3 col-sm-12 wsq padding-bottom-sm">
-        {
-          wrapApp(
-            createApp({
-              initialState: {
-                apiRoot: `http://localhost:${process.env.STORYBOOK_PORT}`,
-                prefix: '',
-                res: {
-                  events: '/agendas/:agendaUid/events',
-                  embeds: '/agendas/:agendaUid/embeds',
-                  agendaSettings: '/agendas/:agendaUid',
-                  preview: 'https://d.openagenda.com/agendas/:agendaUid/previewEmbeds/:embedUid/events',
-                  previewScript: 'https://d.openagenda.com/js/embed/cibulBodyWidget.js'
+export const IntegratedDefaultTemplates = {
+  render: () => {
+    const selectionMenuRef = useRef();
+    return (
+      <HelmetProvider>
+        <div className="col-md-3 col-md-push-5 col-sm-12" ref={selectionMenuRef} />
+        <div className="col-md-5 col-md-pull-3 col-sm-12 wsq padding-bottom-sm">
+          {
+            wrapApp(
+              createApp({
+                initialState: {
+                  apiRoot: '',
+                  prefix: '',
+                  res: {
+                    events: '/agendas/:agendaUid/events',
+                    embeds: '/agendas/:agendaUid/embeds',
+                    agendaSettings: '/agendas/:agendaUid',
+                    preview: 'https://d.openagenda.com/agendas/:agendaUid/previewEmbeds/:embedUid/events',
+                    previewScript: 'https://d.openagenda.com/js/embed/cibulBodyWidget.js'
+                  }
+                }
+              }),
+              {
+                disableScrollToTop: true,
+                extraProps: {
+                  lang: 'fr',
+                  agenda: {
+                    uid: 50522407,
+                    slug: 'toulouse',
+                    title: 'Métropole de Toulouse',
+                  },
+                  filtersContainerRef: selectionMenuRef
                 }
               }
-            }),
-            {
-              disableScrollToTop: true,
-              extraProps: {
-                lang: 'fr',
-                agenda: {
-                  uid: 50522407,
-                  slug: 'toulouse',
-                  title: 'Métropole de Toulouse',
-                },
-                filtersContainerRef: selectionMenuRef
-              }
-            }
-          )
-        }
-      </div>
-    </HelmetProvider>
-  );
+            )
+          }
+        </div>
+      </HelmetProvider>
+    );
+  }, parameters: {
+    msw: {
+      handlers: [
+        rest.get('/agendas/50522407', async (_req, res, ctx) => {
+          await new Promise(rs => setTimeout(rs, 1000));
+          return res(ctx.status(200), ctx.json(apiAgendasToulouse))
+        }),
+        rest.get('/agendas/50522407/embeds', async (_req, res, ctx) => {
+          await new Promise(rs => setTimeout(rs, 1000));
+          return res(ctx.status(200), ctx.json([toulouseDefaultEmbed]))
+        }),
+        rest.get('/agendas/50522407/events', async (_req, res, ctx) => {
+          await new Promise(rs => setTimeout(rs, 1000));
+          return res(ctx.status(200), ctx.json(toulouseEvents))
+        }),
+        rest.post('/agendas/50522407/embeds', async (_req, res, ctx) => {
+          await new Promise(rs => setTimeout(rs, 1000));
+          return res(ctx.status(200))
+        })
+      ]
+    }
+  }
 }
 
-export function IntegratedForUpdate() {
-  const mock = new MockAdapter(axios, {
-    delayResponse: 1000
-  });
-
-  mock.onGet('/agendas/50522407').reply(200, apiAgendasToulouse);
-  mock.onGet('/agendas/50522407/embeds').reply(200, [toulouseEmbed]);
-  mock.onGet('/agendas/50522407/events').reply(200, toulouseEvents);
-  mock.onPost('/agendas/50522407/embeds/80717033').reply(200);
-
-  const selectionMenuRef = useRef();
-
-  return (
-    <HelmetProvider>
-      <div className="col-md-3 col-md-push-5 col-sm-12" ref={selectionMenuRef} />
-      <div className="col-md-5 col-md-pull-3 col-sm-12 wsq padding-bottom-sm">
-        {
-          wrapApp(
-            createApp({
-              initialState: {
-                apiRoot: `http://localhost:${process.env.STORYBOOK_PORT}`,
-                prefix: '',
-                res: {
-                  events: '/agendas/:agendaUid/events',
-                  embeds: '/agendas/:agendaUid/embeds',
-                  agendaSettings: '/agendas/:agendaUid',
-                  preview: 'https://d.openagenda.com/agendas/:agendaUid/previewEmbeds/:embedUid/events',
-                  previewScript: 'https://d.openagenda.com/js/embed/cibulBodyWidget.js'
+export const IntegratedForUpdate = {
+  render: () => {
+    const selectionMenuRef = useRef();
+    return (
+      <HelmetProvider>
+        <div className="col-md-3 col-md-push-5 col-sm-12" ref={selectionMenuRef} />
+        <div className="col-md-5 col-md-pull-3 col-sm-12 wsq padding-bottom-sm">
+          {
+            wrapApp(
+              createApp({
+                initialState: {
+                  apiRoot: '',
+                  prefix: '',
+                  res: {
+                    events: '/agendas/:agendaUid/events',
+                    embeds: '/agendas/:agendaUid/embeds',
+                    agendaSettings: '/agendas/:agendaUid',
+                    preview: 'https://d.openagenda.com/agendas/:agendaUid/previewEmbeds/:embedUid/events',
+                    previewScript: 'https://d.openagenda.com/js/embed/cibulBodyWidget.js'
+                  }
+                }
+              }),
+              {
+                disableScrollToTop: true,
+                extraProps: {
+                  lang: 'fr',
+                  agenda: {
+                    uid: 50522407,
+                    slug: 'toulouse',
+                    title: 'Métropole de Toulouse',
+                  },
+                  filtersContainerRef: selectionMenuRef
                 }
               }
-            }),
-            {
-              disableScrollToTop: true,
-              extraProps: {
-                lang: 'fr',
-                agenda: {
-                  uid: 50522407,
-                  slug: 'toulouse',
-                  title: 'Métropole de Toulouse',
-                },
-                filtersContainerRef: selectionMenuRef
-              }
-            }
-          )
-        }
-      </div>
-    </HelmetProvider>
-  );
+            )
+          }
+        </div>
+      </HelmetProvider>
+    );
+  }, parameters: {
+    msw: {
+      handlers: [
+        rest.get('/agendas/50522407', async (_req, res, ctx) => {
+          await new Promise(rs => setTimeout(rs, 1000));
+          return res(ctx.status(200), ctx.json(apiAgendasToulouse))
+        }),
+        rest.get('/agendas/50522407/embeds', async (_req, res, ctx) => {
+          await new Promise(rs => setTimeout(rs, 1000));
+          return res(ctx.status(200), ctx.json([toulouseEmbed]))
+        }),
+        rest.get('/agendas/50522407/events', async (_req, res, ctx) => {
+          await new Promise(rs => setTimeout(rs, 1000));
+          return res(ctx.status(200), ctx.json(toulouseEvents))
+        }),
+        rest.post('/agendas/50522407/embeds', async (_req, res, ctx) => {
+          await new Promise(rs => setTimeout(rs, 1000));
+          return res(ctx.status(200))
+        })
+      ]
+    }
+  }
 }
