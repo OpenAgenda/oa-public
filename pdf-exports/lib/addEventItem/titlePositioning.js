2c9da26ce31336fe97f23f9215a8af1e5a26346c
DFarau <doriane.farau@hetic.net>
2024-06-01 16:13:41 +0200

    feat(pdf-exports): add variants

---
+++ pdf-exports/lib/addEventItem/titlePositioning.js
@@ -0,0 +1,25 @@
+import { getLocaleValue } from '@openagenda/intl';
+import addText from '../addText.js';
+import cleanString from '../cleanString.js';
+
+export default function titlePositioning(doc, cursor, event, options = {}) {
+  const { columnMaxWidth, fontSize, base, simulate, lang } = options;
+
+  const { height, width } = addText(
+    doc,
+    cursor,
+    cleanString(getLocaleValue(event.title, lang)),
+    {
+      width: columnMaxWidth,
+      fontSize,
+      base,
+      bold: true,
+      simulate,
+    },
+  );
+
+  return {
+    width,
+    height,
+  };
+}
