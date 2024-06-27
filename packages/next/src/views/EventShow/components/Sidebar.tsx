9dae95a87346a2645229a2f01fa432f4ec3f69d8
Kaoré <kaore@openagenda.com>
2024-06-27 15:47:18 +0200

    chore(next): linting

--- packages/next/src/views/EventShow/components/Sidebar.tsx
+++ packages/next/src/views/EventShow/components/Sidebar.tsx
@@ -232,7 +232,7 @@ export function RegistrationSection(props) {
   if (event.registration?.length && event.registration.filter(r => r.service === 'passCulture').length) {
     event.passCulture = {
       img: 'https://oasvc.s3.eu-west-1.amazonaws.com/registration-apps/pass-culture-22.png',
-      label:  intl.formatMessage(messages.accessPassOffer),
+      label: intl.formatMessage(messages.accessPassOffer),
       ...event.registration.find(r => r.service === 'passCulture'),
     };
     event.registration = event.registration.filter(r => r.service !== 'passCulture').length ? event.registration.filter(r => r.service === 'passCulture') : [];
