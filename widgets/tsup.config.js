b813f225478b9a7156772d9870289923361b89db
Kévin Berthommier <bertho-zero@users.noreply.github.com>
2024-06-27 18:13:29 +0200

    feat(widgets): better minify

--- widgets/tsup.config.js
+++ widgets/tsup.config.js
@@ -4,7 +4,12 @@ export default defineConfig({
   entry: ['src/index.js'],
   treeshake: true,
   sourcemap: true,
-  minify: process.env.NODE_ENV === 'production',
+  minify: process.env.NODE_ENV === 'production' ? 'terser' : false,
+  terserOptions: {
+    format: {
+      comments: false,
+    },
+  },
   clean: true,
   splitting: false,
   external: [],
