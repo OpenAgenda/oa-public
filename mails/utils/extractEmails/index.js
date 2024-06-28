9405592dddd6b54da62e2aacd476c1f3165cfff6
Kévin Berthommier <bertho-zero@users.noreply.github.com>
2024-06-28 11:05:34 +0200

    feat(mails): add extractEmails utils

---
+++ mails/utils/extractEmails/index.js
@@ -0,0 +1,30 @@
+'use strict';
+
+const tlds = require('tlds');
+const normalizeInput = require('./normalizeInput');
+
+module.exports = input => {
+  const matches = normalizeInput(input).match(
+    /(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))/g,
+  );
+
+  if (!matches) {
+    return [];
+  }
+
+  return matches
+    .map(email => email)
+    .filter(email => {
+      for (const tld of tlds) {
+        if (email.endsWith(`.${tld}`)) {
+          return true;
+        }
+      }
+
+      return false;
+    })
+    .filter((email, index, self) => self.indexOf(email) === index)
+    .map(email => ({
+      email,
+    }));
+};
