3d270c0f383f939c77bf8b3a6ebdc3281d41c5e1
Kévin Berthommier <bertho-zero@users.noreply.github.com>
2024-06-28 11:05:34 +0200

    feat(mails): add extractEmails utils

---
+++ mails/test/extractEmails.test.js
@@ -0,0 +1,58 @@
+'use strict';
+
+const extractEmails = require('../utils/extractEmails');
+const normalizeInput = require('../utils/extractEmails/normalizeInput');
+
+describe('extractEmails', () => {
+  it('basic case', () => {
+    expect(extractEmails('here is an email: support@oa.com')).toEqual([
+      {
+        email: 'support@oa.com',
+      },
+    ]);
+  });
+
+  it('several emails', () => {
+    expect(
+      extractEmails(
+        'here is one: support@oa.com, here is another: wigglypoof@oa.com',
+      ),
+    ).toEqual([
+      {
+        email: 'support@oa.com',
+      },
+      {
+        email: 'wigglypoof@oa.com',
+      },
+    ]);
+  });
+
+  it('normalizes different email formats', () => {
+    expect(normalizeInput('GAJUS@GAJUS.COM')).toBe('gajus@gajus.com');
+    expect(normalizeInput('：gajus@gajus.com')).toBe('gajus@gajus.com');
+    expect(normalizeInput('📧gajus@gajus.com')).toBe('gajus@gajus.com');
+    expect(normalizeInput('g a j u s [at] g a j u s [dot] c o m')).toBe(
+      'gajus@gajus.com',
+    );
+    expect(normalizeInput('foo g a j u s [at] g a j u s [dot] c o m bar')).toBe(
+      'foo gajus@gajus.com bar',
+    );
+    expect(normalizeInput('gajus[at]gajus[dot]co[dot]uk')).toBe(
+      'gajus@gajus.co.uk',
+    );
+    expect(normalizeInput('gajus[at]gajus[dot]com')).toBe('gajus@gajus.com');
+    expect(normalizeInput('gajus(at)gajus(dot)com')).toBe('gajus@gajus.com');
+    expect(normalizeInput('gajus [at] gajus   [dot]   com')).toBe(
+      'gajus@gajus.com',
+    );
+    expect(normalizeInput('gajus (at) gajus   (dot)   com')).toBe(
+      'gajus@gajus.com',
+    );
+    expect(normalizeInput('gajus <at> gajus   <dot>   com')).toBe(
+      'gajus@gajus.com',
+    );
+    expect(normalizeInput('gajus at gajus   dot   com')).toBe(
+      'gajus@gajus.com',
+    );
+  });
+});
---
+++ mails/utils/extractEmails/normalizeInput.js
@@ -0,0 +1,41 @@
+'use strict';
+
+const createEmojiRegex = require('emoji-regex');
+
+const emojiRegex = createEmojiRegex();
+
+const isLookbehindSupported = (() => {
+  try {
+    return /(?<=a)b/.test('ab');
+  } catch (error) {
+    return false;
+  }
+})();
+
+module.exports = input => {
+  let result = decodeURIComponent(input).replace(emojiRegex, ' ');
+
+  if (isLookbehindSupported) {
+    result = result.replace(
+      /(?<=\s|^)([a-z0-9.-_@])\s?(?=[a-z0-9.-_@](?:\s|$))/g,
+      '$1',
+    );
+  }
+
+  result = result
+    .replace(/\s+at\s+/g, '@')
+    .replace(/\s+dot\s+/g, '.')
+    .replace(/\s*<at>\s*/g, '@')
+    .replace(/\s*<dot>\s*/g, '.')
+    .replace(/\s*\(at\)\s*/g, '@')
+    .replace(/\s*\(dot\)\s*/g, '.')
+    .replace(/\s*\[at\]\s*/g, '@')
+    .replace(/\s*\[dot\]\s*/g, '.')
+
+    // Matches all ASCII characters from the space to tilde.
+    .replace(/[^ -~]/g, ' ')
+    .trim()
+    .toLowerCase();
+
+  return result;
+};
