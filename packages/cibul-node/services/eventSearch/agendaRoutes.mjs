34a3df53323d8c70eb8f0e82e4b821e0ddb76bb5
Kaoré <kaore@openagenda.com>
2024-07-18 09:45:17 +0200

    chore(cibul-node): make rate limiter more constraining

--- packages/cibul-node/services/eventSearch/agendaRoutes.mjs
+++ packages/cibul-node/services/eventSearch/agendaRoutes.mjs
@@ -20,67 +20,63 @@ export default (config, services) => {
   const { redis } = services;
 
   const limiter = rateLimiter(redis.ioRedis, {
-    keyGenerator: req => `${req.ip}|export-${req.params.format}|${req.params.agendaUid}`,
+    keyGenerator: req =>
+      [req.ip, `export-${req.params.format}`].concat(req.params.format === 'pdf' ? [] : req.params.agendaUid).join('|'),
   });
 
   return {
-    getPublic: () => Router({
-      mergeParams: true,
-    }).get(
-      '',
-      ifFormat(
-        ['csv', 'xlsx', 'ics', 'txt', 'md', 'pdf'],
-        limiter,
+    getPublic: () =>
+      Router({
+        mergeParams: true,
+      }).get(
+        '',
+        ifFormat(['csv', 'xlsx', 'ics', 'txt', 'md', 'pdf'], limiter),
+        ifFormat(['csv', 'xlsx', 'ics', 'txt', 'md', 'pdf'], loadSearchEndpoint(services.core)),
+        ifFormat(['rss', 'json'], loadSearchEndpoint(services.core, { convertLegacy: true })),
+        loadAgendaLanguagesAndFormSchemas(services),
+        ifFormat(['csv', 'xlsx', 'ics', 'txt', 'md', 'pdf'], loadSearchStream()),
+        trackFormat,
+        ifFormat('csv', streamCSV),
+        ifFormat('xlsx', streamXLSX),
+        ifFormat('pdf', streamPDF.bind(null, config.pdf)),
+        ifFormat('ics', streamICS),
+        ifFormat(['md', 'txt'], streamMarkdown),
+        ifFormat('rss', RSSResponse(services.core)),
+        ifFormat('json', JSONResponse),
+        (req, res) => res.status(400).send('Unknown format'),
       ),
-      ifFormat(
-        ['csv', 'xlsx', 'ics', 'txt', 'md', 'pdf'],
-        loadSearchEndpoint(services.core),
+    getRestricted: () =>
+      Router({
+        mergeParams: true,
+      }).get(
+        '',
+        ifFormat(['csv', 'xlsx', 'ics', 'txt', 'md'], limiter),
+        services.members.mw.authorizeAdminModOrKey({
+          agendaUidPath: 'params.agendaUid',
+        }),
+        loadSearchEndpoint(services.core, { admin: true }),
+        loadAgendaLanguagesAndFormSchemas(services),
+        ifFormat(['csv', 'xlsx', 'ics', 'txt', 'md'], loadSearchStream()),
+        trackFormat,
+        ifJSONStreamRequested(loadSearchStream()),
+        ifFormat('csv', streamCSV),
+        ifFormat('xlsx', streamXLSX),
+        ifFormat('ics', streamICS),
+        ifFormat(['md', 'txt'], streamMarkdown),
+        ifJSONStreamRequested(JSONResponse.streamResponse),
+        ifFormat('rss', RSSResponse(services.core)),
+        ifFormat('json', JSONResponse),
+        handleError,
+        (req, res) => res.status(400).send('Unknown format'),
       ),
-      ifFormat(
-        ['rss', 'json'],
-        loadSearchEndpoint(services.core, { convertLegacy: true }),
-      ),
-      loadAgendaLanguagesAndFormSchemas(services),
-      ifFormat(['csv', 'xlsx', 'ics', 'txt', 'md', 'pdf'], loadSearchStream()),
-      trackFormat,
-      ifFormat('csv', streamCSV),
-      ifFormat('xlsx', streamXLSX),
-      ifFormat('pdf', streamPDF.bind(null, config.pdf)),
-      ifFormat('ics', streamICS),
-      ifFormat(['md', 'txt'], streamMarkdown),
-      ifFormat('rss', RSSResponse(services.core)),
-      ifFormat('json', JSONResponse),
-      (req, res) => res.status(400).send('Unknown format'),
-    ),
-    getRestricted: () => Router({
-      mergeParams: true,
-    }).get(
-      '',
-      ifFormat(['csv', 'xlsx', 'ics', 'txt', 'md'], limiter),
-      services.members.mw.authorizeAdminModOrKey({
-        agendaUidPath: 'params.agendaUid',
-      }),
-      loadSearchEndpoint(services.core, { admin: true }),
-      loadAgendaLanguagesAndFormSchemas(services),
-      ifFormat(['csv', 'xlsx', 'ics', 'txt', 'md'], loadSearchStream()),
-      trackFormat,
-      ifJSONStreamRequested(loadSearchStream()),
-      ifFormat('csv', streamCSV),
-      ifFormat('xlsx', streamXLSX),
-      ifFormat('ics', streamICS),
-      ifFormat(['md', 'txt'], streamMarkdown),
-      ifJSONStreamRequested(JSONResponse.streamResponse),
-      ifFormat('rss', RSSResponse(services.core)),
-      ifFormat('json', JSONResponse),
-      handleError,
-      (req, res) => res.status(400).send('Unknown format'),
-    ),
     getAgendaExportsSettings: ({ admin } = false) => [
-      admin ? services.members.mw.authorizeAdminModOrKey({
-        agendaUidPath: 'params.agendaUid',
-      }) : (req, res, next) => {
-        next();
-      },
+      admin
+        ? services.members.mw.authorizeAdminModOrKey({
+          agendaUidPath: 'params.agendaUid',
+        })
+        : (req, res, next) => {
+          next();
+        },
       loadSearchEndpoint(services.core),
       loadAgendaLanguagesAndFormSchemas(services),
       loadAgendaExportsSettings(services),
