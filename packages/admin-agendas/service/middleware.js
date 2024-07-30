ea49e09c7ce670050bbf1e8cd5e4554fb16a0426
Kaoré <kaore@openagenda.com>
2024-07-30 16:48:47 +0200

    refactor(admin-agendas): removed agenda search middleware

--- packages/admin-agendas/service/middleware.js
+++ packages/admin-agendas/service/middleware.js
@@ -1,4 +1,4 @@
-"use strict";
+'use strict';
 
 const _ = require('lodash');
 const validators = require('@openagenda/validators');
@@ -10,169 +10,119 @@ let agendas;
 module.exports = {
   init,
   agendas: {
-    list,
     get,
-    set
+    set,
   },
   members: {
-    list: membersList
-  }
+    list: membersList,
+  },
 };
 
 function init(s, c) {
-
   service = s;
 
   config = Object.assign(
     {
       limit: {
         default: 20,
-        max: 100
-      }
+        max: 100,
+      },
     },
     c.mw || {},
-    _.pick(c, 'interfaces')
+    _.pick(c, 'interfaces'),
   );
 
   agendas = c.services.agendas;
-
-}
-
-
-function list(req, res, next) {
-
-  let query = req.query.oas,
-
-    offset = 0,
-
-    limit = config.limit.default,
-
-    page = 1;
-
-  try {
-
-    page = validators.number({
-      min: 1,
-      default: 1
-    })(req.query.searchPage);
-
-    offset = (page - 1) * limit;
-
-  } catch (e) {
-  }
-
-  if (!req.xhr) return next();
-
-  // bad practice to call a service inside another service
-  agendas.list(query, offset, limit, {
-    total: true,
-    detailed: true,
-    private: null
-  }, (err, agendas, total) => {
-
-    if (err) return next(err);
-
-    return res.json({ agendas, total });
-
-  });
-
 }
 
 function get(req, res, next) {
-
   // bad practice to call a service inside another service
-  agendas.get(req.query, { detailed: true, internal: true, private: null }, (err, agenda) => {
+  agendas.get(
+    req.query,
+    { detailed: true, internal: true, private: null },
+    (err, agenda) => {
+      if (err) return next(err);
 
-    if (err) return next(err);
-
-    return res.json(_extendWithConfig(agenda));
-
-  });
-
+      return res.json(_extendWithConfig(agenda));
+    },
+  );
 }
 
 function _extendWithConfig(agenda) {
-
   const credsConfig = config.interfaces.getAgendaCredentialDetails();
 
-  const defaultCreds = Object.entries(credsConfig)
-    .reduce((accu, [key, value]) => ({
+  const defaultCreds = Object.entries(credsConfig).reduce(
+    (accu, [key, value]) => ({
       ...accu,
-      [key]: value.default
-    }), {});
+      [key]: value.default,
+    }),
+    {},
+  );
 
   return {
     ...agenda,
     credentials: {
       ...defaultCreds,
-      ...agenda.credentials
+      ...agenda.credentials,
     },
     config: {
-      credentials: credsConfig
-    }
+      credentials: credsConfig,
+    },
   };
 }
 
 function set(req, res, next) {
-
   // bad practice to call a service inside another service
-  agendas.set({ uid: req.params.uid }, req.body, {
-    internal: true,
-    protected: false,
-    private: null,
-    context: req.context || null
-  }, (err, result) => {
+  agendas.set(
+    { uid: req.params.uid },
+    req.body,
+    {
+      internal: true,
+      protected: false,
+      private: null,
+      context: req.context || null,
+    },
+    (err, result) => {
+      if (err) return next(err);
 
-    if (err) return next(err);
+      result.agenda = _extendWithConfig(result.agenda);
 
-    result.agenda = _extendWithConfig(result.agenda);
-
-    return res.json(result);
-
-  });
-
+      return res.json(result);
+    },
+  );
 }
 
-
 async function membersList(req, res, next) {
-
-  let agendaUid = parseInt( req.query.agendaUid, 10 ),
-
+  let agendaUid = parseInt(req.query.agendaUid, 10),
     offset = 0,
-
     limit = config.limit.default,
-
     page = 1;
 
   try {
-
     page = validators.number({
       min: 1,
-      default: 1
+      default: 1,
     })(req.query.membersPage);
 
     offset = (page - 1) * limit;
-
-  } catch (e) {
-  }
+  } catch (e) {}
 
   // bad practice to call a service inside another service
   try {
     const { total, members } = await service.members.list(
       {
         agendaUid,
-        deletedUser: null
+        deletedUser: null,
       },
       { order: 'role.desc', offset, limit },
-      { total: true, detailed: true, userOptions: { detailed: true } }
+      { total: true, detailed: true, userOptions: { detailed: true } },
     );
 
-    res.json( {
+    res.json({
       members,
-      total
-    } );
+      total,
+    });
   } catch (e) {
     return next(e);
   }
-
 }
