7a7d2d8815f8ba8608a731151adf970687375f7a
Clément Lecroart <clement.lecroart@openagenda.com>
2024-08-02 16:02:43 +0200

    feat(cibul-node): added lastProcessedAt to pass processApply return value

--- packages/cibul-node/services/registrations/utils/passCulture/processApply.js
+++ packages/cibul-node/services/registrations/utils/passCulture/processApply.js
@@ -2,28 +2,32 @@ import logs from '@openagenda/logs';
 
 const log = logs('services/registrations/processPassCultureApply');
 
-export default async function processPassCultureApply({ services }, agenda, clean) {
+export default async function processPassCultureApply(
+  { services },
+  agenda,
+  clean,
+) {
   log.info('called');
 
-  const {
-    registrations,
-    core,
-  } = services;
+  const { registrations, core } = services;
 
-  const passCultureService = registrations(agenda.settings.registration).passCulture;
+  const passCultureService = registrations(
+    agenda.settings.registration,
+  ).passCulture;
 
-  const {
-    passCulture,
-    event,
-  } = clean;
+  const { passCulture, event } = clean;
 
   const applied = await passCultureService.apply(event, passCulture, {
     imageBasePath: core.getConfig().aws.imageBucketPath,
   });
 
-  return event.registration.map(r => (r.service === 'passCulture' ? {
-    ...r,
-    data: applied,
-    value: passCultureService.getEventOfferLink(applied),
-  } : r));
+  return event.registration.map(r =>
+    (r.service === 'passCulture'
+      ? {
+        ...r,
+        data: applied,
+        value: passCultureService.getEventOfferLink(applied),
+        lastProcessedAt: new Date(),
+      }
+      : r));
 }
