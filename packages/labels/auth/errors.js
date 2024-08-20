856ef5554f53b859d5d84801797f02e77ddd1703
Kaoré <kaore@openagenda.com>
2024-08-20 15:22:40 +0200

    chore(labels): error should be bad request and not show on sentry

--- packages/labels/auth/errors.js
+++ packages/labels/auth/errors.js
@@ -1,100 +1,79 @@
-"use strict";
+'use strict';
 
 module.exports = {
-  "noAccountFound": {
-    "en": "No account was found",
-    "fr": "Compte introuvable",
-    "it": "Nessun account trovato",
-    "io": "crwdns13056:0crwdne13056:0",
-    "oc": "Pas cap compte trobat"
+  incorrectPassword: {
+    en: 'This password is incorrect',
+    fr: 'Le mot de passe est incorrect',
+    it: 'Questo password non è corretto',
+    io: 'crwdns13060:0crwdne13060:0',
+    oc: 'Lo senhal es incorrècte',
+    es: 'Esta contraseña es incorrecta',
   },
-  "accountAlreadyActivated": {
-    "en": "The account is already activated",
-    "fr": "Le compte est déjà activé",
-    "it": "Il account è già attivato",
-    "io": "crwdns13058:0crwdne13058:0",
-    "oc": "Lo compte es ja activat"
+  invalidToken: {
+    en: 'Invalid token',
+    fr: 'Token invalide',
+    it: 'Token non valido',
+    io: 'crwdns13062:0crwdne13062:0',
+    oc: 'Geton invalid',
   },
-  "incorrectPassword": {
-    "en": "This password is incorrect",
-    "fr": "Le mot de passe est incorrect",
-    "it": "Questo password non è corretto",
-    "io": "crwdns13060:0crwdne13060:0",
-    "oc": "Lo senhal es incorrècte",
-    "es": "Esta contraseña es incorrecta"
+  userNotFound: {
+    en: 'User not found',
+    fr: 'Utilisateur introuvable',
+    it: 'Nessun conto trovato',
+    io: 'crwdns13064:0crwdne13064:0',
+    oc: 'Utilizaire·a pas trobat·da',
   },
-  "invalidToken": {
-    "en": "Invalid token",
-    "fr": "Token invalide",
-    "it": "Token non valido",
-    "io": "crwdns13062:0crwdne13062:0",
-    "oc": "Geton invalid"
+  userNotActivated: {
+    en: 'User not activated',
+    fr: 'Utilisateur non activé',
+    it: 'Conto non attivato',
+    io: 'crwdns13066:0crwdne13066:0',
+    oc: 'Utilizaire·a pas activat·da',
   },
-  "userNotFound": {
-    "en": "User not found",
-    "fr": "Utilisateur introuvable",
-    "it": "Nessun conto trovato",
-    "io": "crwdns13064:0crwdne13064:0",
-    "oc": "Utilizaire·a pas trobat·da"
+  passwordsMustMatch: {
+    en: 'Password must match.',
+    fr: 'Le mot de passe ne correspond pas.',
+    it: 'La password deve corrispondere.',
+    io: 'crwdns13070:0crwdne13070:0',
+    oc: 'Lo senhal correspond pas.',
+    es: 'La contraseña debe coincidir.',
   },
-  "userNotActivated": {
-    "en": "User not activated",
-    "fr": "Utilisateur non activé",
-    "it": "Conto non attivato",
-    "io": "crwdns13066:0crwdne13066:0",
-    "oc": "Utilizaire·a pas activat·da"
+  fieldCannotBeEmpty: {
+    en: 'Field cannot be empty',
+    fr: 'Le champ ne peut être vide',
+    it: 'Questo campo non può essere vuoto',
+    io: 'crwdns13072:0crwdne13072:0',
+    oc: 'Lo camp pòt pas demorar voide',
   },
-  "userAlreadyActivated": {
-    "en": "User already activated",
-    "fr": "Utilisateur déjà activé",
-    "it": "Il conto è già attivato",
-    "io": "crwdns13068:0crwdne13068:0",
-    "oc": "Utilizaire·a ja activat·da"
+  passwordCouldNotBeModified: {
+    en: 'The password could not be modified',
+    fr: "Le mot de passe n'a pas pu être modifié",
+    it: 'La password non può essere modificata',
+    io: 'crwdns13074:0crwdne13074:0',
+    oc: 'Lo senhal se posquèt pas cambiar',
+    es: 'No se ha podido modificar la contraseña',
   },
-  "passwordsMustMatch": {
-    "en": "Password must match.",
-    "fr": "Le mot de passe ne correspond pas.",
-    "it": "La password deve corrispondere.",
-    "io": "crwdns13070:0crwdne13070:0",
-    "oc": "Lo senhal correspond pas.",
-    "es": "La contraseña debe coincidir."
+  passwordResetSent: {
+    en: 'A password reset is being sent to your email',
+    fr: 'Un lien de réinitialisation du mot de passe est envoyé à votre adresse email',
+    it: 'Una reimpostazione della password è stata inviata alla sua email',
+    io: 'crwdns13076:0crwdne13076:0',
+    oc: 'Una reïnicializacion de senhal es estada mandada a la vòstra adreça email',
+    es: 'A password reset is being sent to your email',
   },
-  "fieldCannotBeEmpty": {
-    "en": "Field cannot be empty",
-    "fr": "Le champ ne peut être vide",
-    "it": "Questo campo non può essere vuoto",
-    "io": "crwdns13072:0crwdne13072:0",
-    "oc": "Lo camp pòt pas demorar voide"
+  resetLinkOutdated: {
+    en: 'The link is outdated. Try again.',
+    fr: 'Le lien est obsolète. Réessayez.',
+    it: '',
+    io: 'crwdns13078:0crwdne13078:0',
+    oc: 'Lo ligam es passat. Tornatz ensajar.',
   },
-  "passwordCouldNotBeModified": {
-    "en": "The password could not be modified",
-    "fr": "Le mot de passe n'a pas pu être modifié",
-    "it": "La password non può essere modificata",
-    "io": "crwdns13074:0crwdne13074:0",
-    "oc": "Lo senhal se posquèt pas cambiar",
-    "es": "No se ha podido modificar la contraseña"
+  passwordUpdated: {
+    en: 'Your password has been updated.',
+    fr: 'Votre mot de passe a été mis à jour.',
+    it: 'La tua password è stata aggiornata.',
+    io: 'crwdns13080:0crwdne13080:0',
+    oc: 'Vòstre senhal es estat mes a jorn.',
+    es: 'Su contraseña ha sido actualizada.',
   },
-  "passwordResetSent": {
-    "en": "A password reset is being sent to your email",
-    "fr": "Un lien de réinitialisation du mot de passe est envoyé à votre adresse email",
-    "it": "Una reimpostazione della password è stata inviata alla sua email",
-    "io": "crwdns13076:0crwdne13076:0",
-    "oc": "Una reïnicializacion de senhal es estada mandada a la vòstra adreça email",
-    "es": "A password reset is being sent to your email"
-  },
-  "resetLinkOutdated": {
-    "en": "The link is outdated. Try again.",
-    "fr": "Le lien est obsolète. Réessayez.",
-    "it": "",
-    "io": "crwdns13078:0crwdne13078:0",
-    "oc": "Lo ligam es passat. Tornatz ensajar."
-  },
-  "passwordUpdated": {
-    "en": "Your password has been updated.",
-    "fr": "Votre mot de passe a été mis à jour.",
-    "it": "La tua password è stata aggiornata.",
-    "io": "crwdns13080:0crwdne13080:0",
-    "oc": "Vòstre senhal es estat mes a jorn.",
-    "es": "Su contraseña ha sido actualizada."
-  }
-}
+};
