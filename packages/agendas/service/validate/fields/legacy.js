2df5906c5d0f369701d44fd8f8ac728084234572
Kaoré <kaore@openagenda.com>
2024-08-20 17:33:09 +0200

    fix(agendas): tests need to be updated

--- packages/agendas/service/validate/fields/legacy.js
+++ packages/agendas/service/validate/fields/legacy.js
@@ -3,22 +3,27 @@
 const _ = require('lodash');
 const fieldsByAccess = require('./flattenedByFieldAccess');
 
-function objectify(fields, obj = {}) {
-  return fields.map(field => _.omit(field, ['read', 'write'])).reduce((tree, field) => {
-    const branches = field.field.split('.');
-    const name = branches.pop();
-    const path = branches.map(b => [b, 'fields'].join('.')).concat(name).join('.');
-    if (field.type === 'schema') {
-      _.set(tree, path, _.omit({ ...field, fields: {} }, ['type', 'field']));
-    } else {
-      _.set(tree, path, _.omit(field, ['field']));
-    }
-    return tree;
-  }, {});
+function objectify(fields) {
+  return fields
+    .map((field) => _.omit(field, ['read', 'write']))
+    .reduce((tree, field) => {
+      const branches = field.field.split('.');
+      const name = branches.pop();
+      const path = branches
+        .map((b) => [b, 'fields'].join('.'))
+        .concat(name)
+        .join('.');
+      if (field.type === 'schema') {
+        _.set(tree, path, _.omit({ ...field, fields: {} }, ['type', 'field']));
+      } else {
+        _.set(tree, path, _.omit(field, ['field']));
+      }
+      return tree;
+    }, {});
 }
 
 module.exports = {
   public: objectify(fieldsByAccess.read.legacyPublic),
   all: objectify(fieldsByAccess.read.legacy),
-  private: objectify(fieldsByAccess.read.legacyPrivate)
+  private: objectify(fieldsByAccess.read.legacyPrivate),
 };
