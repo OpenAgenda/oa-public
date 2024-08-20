a437162156262addb2e04e451c1123f3870eeabc
Kaoré <kaore@openagenda.com>
2024-08-20 11:53:21 +0200

    fix(cibul-node): change state is broken on private agendas

--- packages/cibul-node/api/middleware/eventUpdate.js
+++ packages/cibul-node/api/middleware/eventUpdate.js
@@ -8,17 +8,21 @@ export default function eventUpdate(req, res, next) {
     _.set(req.parsedData, 'image.path', _.get(req, 'file.path', undefined));
   }
 
-  req.app.core.agendas(req.agenda.uid).events.update(
-    req.event.uid,
-    _.omit(req.parsedData, ['ownerUid', 'creatorUid']),
-    {
-      partial: req.method === 'PATCH',
-      batched: parseBool(req.headers.batched || req.body.batched),
-      context: {
-        userUid: req.member.userUid,
+  req.app.core
+    .agendas(req.agenda.uid)
+    .events.update(
+      req.event.uid,
+      _.omit(req.parsedData, ['ownerUid', 'creatorUid']),
+      {
+        partial: req.method === 'PATCH',
+        batched: parseBool(req.headers.batched || req.body.batched),
+        context: {
+          userUid: req.member.userUid,
+        },
+        access: req.access,
+        defaultLang: req.headers.lang,
+        private: null,
       },
-      access: req.access,
-      defaultLang: req.headers.lang,
-    },
-  ).then(event => res.json({ success: true, event }), next);
+    )
+    .then(event => res.json({ success: true, event }), next);
 }
