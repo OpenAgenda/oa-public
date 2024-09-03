c146611827fa4851d74fd924e5d40c14b111a5a1
Kaoré <kaore@openagenda.com>
2024-09-03 12:56:51 +0200

    feat(event-admin-apps): maintain order of filtered filters when all filters are displayed

--- packages/event-admin-apps/stories/integrated.stories.js
+++ packages/event-admin-apps/stories/integrated.stories.js
@@ -123,7 +123,7 @@ export const WithSelectionOfFilters = {
                   settings: {
                     admin: {
                       filters: {
-                        displayed: ['search', 'keyword', 'map', 'state'],
+                        displayed: ['search', 'keyword', 'geo', 'state'],
                       },
                     },
                   },
