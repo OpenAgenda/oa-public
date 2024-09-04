673f01b567c003251a54e3942ae840a182da0ff9
Clément Lecroart <clement.lecroart@openagenda.com>
2024-09-04 15:21:50 +0200

    feat(cibul-node): added nullable option to boolQuery

--- packages/cibul-node/api/middleware/searchAgendaEvents.js
+++ packages/cibul-node/api/middleware/searchAgendaEvents.js
@@ -3,28 +3,40 @@ import boolQuery from '../../lib/boolQuery.js';
 
 const log = logs('api/middleware/searchAgendaEvents');
 
-export default function searchAgendaEvents(core, queryNamespace = 'convertedQuery') {
-  return (req, res, next) => core
-    .agendas(req.agenda.uid).events
-    .search(req[queryNamespace], req[queryNamespace], {
-      aggregations: req[queryNamespace].aggs,
-      ...req[queryNamespace],
-      useAfterKey: true,
-      userUid: req.user?.uid,
-      includeLocationImagePath: true,
-      includeEmbedScripts: boolQuery(req[queryNamespace].includeEmbedScripts, true),
-      agendaKey: req.agendaKey,
-    }).then(result => {
-      const response = JSON.stringify({ ...result, success: true });
-      req.result = result;
-      req.contentLength = Buffer.byteLength(response, 'utf8');
-      res.setHeader('Content-Type', 'application/json');
-      res.send(response);
-      next();
-    }, err => {
-      if (err.name !== 'BadRequest') {
-        log.error(err);
-      }
-      next(err);
-    });
+export default function searchAgendaEvents(
+  core,
+  queryNamespace = 'convertedQuery',
+) {
+  return (req, res, next) =>
+    core
+      .agendas(req.agenda.uid)
+      .events.search(req[queryNamespace], req[queryNamespace], {
+        aggregations: req[queryNamespace].aggs,
+        ...req[queryNamespace],
+        useAfterKey: true,
+        userUid: req.user?.uid,
+        includeLocationImagePath: true,
+        includeEmbedScripts: boolQuery(
+          req[queryNamespace].includeEmbedScripts,
+          { defaultValue: true },
+        ),
+        agendaKey: req.agendaKey,
+        removed: boolQuery(req[queryNamespace].removed, { nullable: true }),
+      })
+      .then(
+        result => {
+          const response = JSON.stringify({ ...result, success: true });
+          req.result = result;
+          req.contentLength = Buffer.byteLength(response, 'utf8');
+          res.setHeader('Content-Type', 'application/json');
+          res.send(response);
+          next();
+        },
+        err => {
+          if (err.name !== 'BadRequest') {
+            log.error(err);
+          }
+          next(err);
+        },
+      );
 }
