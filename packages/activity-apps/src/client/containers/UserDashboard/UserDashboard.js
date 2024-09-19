33dbd59a20e14fb023d29de3a9888c4b83832816
Kévin Berthommier <bertho-zero@users.noreply.github.com>
2024-09-19 14:24:52 +0200

    fix(activity-apps): fix throttle

--- packages/activity-apps/src/client/containers/UserDashboard/UserDashboard.js
+++ packages/activity-apps/src/client/containers/UserDashboard/UserDashboard.js
@@ -13,7 +13,13 @@ import { ActivityItem } from '../../components';
 import messages from '../../messages/activities';
 
 class UserDashboard extends Component {
-  throttledNextPage = _.throttle(this.nextPage, 400, { trailing: false });
+  constructor(props) {
+    super(props);
+    this.nextPage = this.nextPage.bind(this);
+    this.throttledNextPage = _.throttle(this.nextPage, 400, {
+      trailing: false,
+    });
+  }
 
   nextPage = () => {
     const { loading, nextLoading, activities, query, nextPage, lastPage } = this.props;
