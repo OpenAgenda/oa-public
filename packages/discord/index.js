26a9e05efcd319b87c337aec31ff0dd67bbfc7e4
Kévin Berthommier <bertho-zero@users.noreply.github.com>
2024-09-23 10:49:28 +0200

    style(discord): lint

--- packages/discord/index.js
+++ packages/discord/index.js
@@ -1,20 +1,19 @@
-'use strict';
-
 const DiscordClient = require('discord.js');
 const notifyUserCreation = require('./lib/notifyUserCreation');
 const notifyAgendaCreation = require('./lib/notifyAgendaCreation');
 
-const initializeClient = config => new Promise(rs => {
-  const client = new DiscordClient.Client();
-  client.login(config.token);
-  client.once('ready', () => {
-    rs(client);
+const initializeClient = (config) =>
+  new Promise((rs) => {
+    const client = new DiscordClient.Client();
+    client.login(config.token);
+    client.once('ready', () => {
+      rs(client);
+    });
   });
-});
 
 const noopSvc = async () => ({
   notifyUserCreation: async () => null,
-  notifyAgendaCreation: async () => null
+  notifyAgendaCreation: async () => null,
 });
 
 module.exports = async (config = {}) => {
--- packages/discord/test/emptyConfig.test.js
+++ packages/discord/test/emptyConfig.test.js
@@ -1,19 +1,16 @@
-'use strict';
-
 const Discord = require('..');
 
 let svc;
 
 beforeAll(async () => {
-  svc = await Discord({
-  });
+  svc = await Discord({});
 });
 
 test('no config given', async () => {
   const response = await svc.notifyUserCreation({
     fullName: 'Olivia',
     email: 'email@email.com',
-    userUid: 123456
+    userUid: 123456,
   });
   expect(response).toBe(null);
 });
