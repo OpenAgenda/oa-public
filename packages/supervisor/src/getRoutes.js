b8c5cf3cdf006b06e4c0d958f33469a6bd117d11
Kévin Berthommier <bertho-zero@users.noreply.github.com>
2024-09-24 09:51:13 +0200

    style(supervisor): lint

--- packages/supervisor/src/getRoutes.js
+++ packages/supervisor/src/getRoutes.js
@@ -1,12 +1,12 @@
 import { loadable } from '@openagenda/react-shared';
 
-const App = loadable(() => import(/* webpackChunkName: "supervisor-App" */ './containers/App'));
+const App = loadable(
+  () => import(/* webpackChunkName: "supervisor-App" */ './containers/App'),
+);
 
-export default function (prefix = '') {
-  return [
-    {
-      path: prefix,
-      component: App,
-    },
-  ];
-}
+export default (prefix = '') => [
+  {
+    path: prefix,
+    component: App,
+  },
+];
