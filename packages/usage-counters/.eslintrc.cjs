7adac5e8270856e4c6fc183f2e78c49efd02d0e8
Kévin Berthommier <bertho-zero@users.noreply.github.com>
2024-09-24 09:51:40 +0200

    style(usage-counters): lint

---
+++ packages/usage-counters/.eslintrc.cjs
@@ -0,0 +1,29 @@
+module.exports = {
+  extends: '@openagenda',
+
+  parserOptions: {
+    sourceType: 'module',
+  },
+
+  rules: {
+    'import/no-extraneous-dependencies': [
+      'error',
+      {
+        devDependencies: [
+          `${__dirname}/test/**/*.js`,
+          `${__dirname}/testconfig.sample.js`,
+          `${__dirname}/testconfig.js`,
+        ],
+      },
+    ],
+  },
+
+  overrides: [
+    {
+      files: ['**/*.js'],
+      rules: {
+        'import/extensions': ['error', 'ignorePackages'],
+      },
+    },
+  ],
+};
