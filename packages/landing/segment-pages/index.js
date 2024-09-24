38d2aba7dffd3eee08b87711cb6a699c4fa3b924
Kévin Berthommier <bertho-zero@users.noreply.github.com>
2024-09-24 09:24:39 +0200

    style(landing): lint

--- packages/landing/segment-pages/index.js
+++ packages/landing/segment-pages/index.js
@@ -1,306 +1,227 @@
-"use strict";
+'use strict';
 
-const _ = require( 'lodash' );
-const pug = require( 'pug' );
-const VError = require( '@openagenda/verror' );
-const mark = require( './mark' );
+const _ = require('lodash');
+const pug = require('pug');
+const VError = require('@openagenda/verror');
+const mark = require('./mark');
 
-module.exports = config => {
+function _cleanPageSegment(segment) {
+  if (typeof segment === 'string') {
+    return {
+      key: segment,
+    };
+  }
 
-  const params = _.defaults( config, {
+  return segment;
+}
+
+function _reduceLabels(obj, lang, labels = false) {
+  if (!_.isObject(obj)) return mark(obj);
+
+  function _reduceItem(item) {
+    if (!_.isObject(item) && labels && labels[item] !== undefined) {
+      return mark(labels[item][lang]);
+    }
+
+    if (!_.isObject(item)) {
+      return mark(item);
+    }
+
+    if (item[lang] === undefined) {
+      return _reduceLabels(item, lang, labels);
+    }
+
+    return mark(item[lang]);
+  }
+
+  return _.mapValues(obj, (o) => {
+    if (_.isArray(o)) {
+      return o.map((item) => _reduceItem(item));
+    }
+
+    return _reduceItem(o);
+  });
+}
+
+function _getBasePath(path, lang) {
+  if (typeof path === 'string') return path;
+
+  return path[lang];
+}
+
+function _buildLinks(render, params, lang = false) {
+  let withLinks = render;
+
+  params.pages.forEach((t) => {
+    let srcKeys = t.key ? [t.key] : [];
+
+    let destKey = t.key;
+
+    if (t.keys) {
+      srcKeys = srcKeys.concat(t.keys.map((t1) => t1.key));
+    }
+
+    if (lang && t.keys) {
+      destKey = t.keys.filter((k) => k.lang === lang)[0].key;
+    }
+
+    const rgx = new RegExp(`"#(${srcKeys.join('|')})"`, 'g');
+
+    withLinks = withLinks.replace(
+      rgx,
+      `"${_getBasePath(params.basePath, lang)}/${destKey}"`,
+    );
+  });
+
+  return withLinks;
+}
+
+function _matchPageKey(key) {
+  return (p) => {
+    if (p.keys) {
+      return !!p.keys.filter((k) => k.key === key).length;
+    }
+
+    return key === p.key;
+  };
+}
+
+function _pageLang(params, key) {
+  if (!params.keys) return false;
+
+  const match = params.keys.filter((k) => k.key === key);
+
+  if (!match.length) return false;
+
+  return match[0].lang;
+}
+
+module.exports = (config) => {
+  const params = _.defaults(config, {
     basePath: false, // optional. If urls are to be generated
     templates: {},
     segments: [],
     pages: [],
     labels: false, // optional. If set, will look into keys for match
-    throwOnUnknown: true
-  } );
+    throwOnUnknown: true,
+  });
 
   // create the feature base renderers
-  const renderers = params.segments.map( f => {
+  const renderers = params.segments.map((f) => ({
+    key: f.key,
+    defaults: f,
+    render: pug.render.bind(null, params.templates[f.template]),
+  }));
 
-    return {
-      key: f.key,
-      defaults: f,
-      render: pug.render.bind( null, params.templates[ f.template ] )
-    }
+  function getAlternateUrl(pageParams, lang) {
+    const item = pageParams.keys.filter((item1) => item1.lang === lang)[0];
 
-  } );
-
-  return page => {
-    if ( !page ) {
-
-      page = 'root';
-
-    }
-
-    let pageParams = params.pages.filter( _matchPageKey( page ) );
-
-    if ( !pageParams.length ) {
-
-      if ( params.throwOnUnknown ) throw new Error( 'unknown page ' + page );
-
-      return null;
-
-    }
-    let lang = _pageLang( pageParams[ 0 ], page );
-    return {
-      render: render.bind( null, pageParams[ 0 ], lang ),
-      getHeadPart: getHeadPart.bind( null, pageParams[ 0 ], lang ),
-      getLang: () => lang,
-      getAlternateUrl: getAlternateUrl.bind( null, pageParams[ 0 ] )
-    }
-
+    return `${_getBasePath(params.basePath, lang)}/${item.key}`;
   }
 
-  function render( pageParams, lang = false, options = {} ) {
+  function getHeadPart(pageParams, lang = false) {
+    const data = _reduceLabels(pageParams, lang, params.labels);
 
-    let renderParams = _.defaults( options || {}, {
-      lang
-    } );
+    const renderParts = [];
 
-    let r = segments( pageParams, renderParams );
+    if (!data.head) return '';
 
-    if ( pageParams.layout ) {
+    Object.keys(data.head).forEach((key) => {
+      if (key === 'title') {
+        renderParts.push(`<title>${data.head[key]}</title>`);
+      }
 
-      r = layout( pageParams, r, renderParams );
+      if (['title', 'description', 'keywords'].indexOf(key) !== -1) {
+        renderParts.push(`<meta name="${key}" content="${data.head[key]}" />`);
+      }
+    });
 
+    if (pageParams.keys) {
+      pageParams.keys.forEach((item) => {
+        if (item.lang === lang) return;
+
+        renderParts.push(
+          `<link rel="alternate" hreflang="${item.lang}" href="${_getBasePath(params.basePath, lang)}/${item.key}" />`,
+        );
+      });
     }
 
-    if ( params.basePath ) {
+    return renderParts.join('\n');
+  }
 
-      r = _buildLinks( r, params, lang );
+  function layout(pageParams, content, renderParams) {
+    const data = _reduceLabels(pageParams, renderParams.lang, params.labels);
 
+    const template = params.templates[pageParams.layout];
+
+    const merged = _.assign({ content }, data, renderParams);
+
+    try {
+      return pug.render(template, merged);
+    } catch (e) {
+      throw new VError(
+        'Failed to render template \n%s\nwith data\n%j',
+        template,
+        merged,
+        e,
+      );
+    }
+  }
+
+  function segments(pageParams, renderParams) {
+    let rendered = '';
+
+    pageParams.segments.forEach((d) => {
+      const s = _cleanPageSegment(d);
+
+      const renderer = renderers.filter((r) => r.key === s.key)[0];
+
+      let data = _.assign({}, renderer.defaults, s);
+
+      data = _reduceLabels(data, renderParams.lang, params.labels);
+
+      try {
+        rendered += renderer.render(data);
+      } catch (e) {
+        throw new Error(`Invalid data with segment ${s.key}: ${e}`);
+      }
+    });
+
+    return rendered;
+  }
+
+  function render(pageParams, lang = false, options = {}) {
+    const renderParams = _.defaults(options || {}, {
+      lang,
+    });
+
+    let r = segments(pageParams, renderParams);
+
+    if (pageParams.layout) {
+      r = layout(pageParams, r, renderParams);
+    }
+
+    if (params.basePath) {
+      r = _buildLinks(r, params, lang);
     }
 
     return r;
-
   }
 
-  function getAlternateUrl( pageParams, lang ) {
+  return (page = 'root') => {
+    const pageParams = params.pages.filter(_matchPageKey(page));
 
-    let item = pageParams.keys.filter( item => item.lang === lang )[ 0 ];
+    if (!pageParams.length) {
+      if (params.throwOnUnknown) throw new Error(`unknown page ${page}`);
 
-    return _getBasePath( params.basePath , lang ) + '/' + item.key;
-
-  }
-
-  function getHeadPart( pageParams, lang = false ) {
-
-    let data = _reduceLabels( pageParams, lang, params.labels ),
-
-    renderParts = [];
-
-    if ( !data.head ) return '';
-
-    Object.keys( data.head ).forEach( key => {
-
-      if ( key === 'title' ) {
-
-        renderParts.push( '<title>' + data.head[ key ] + '</title>' );
-
-      }
-
-      if ( [ 'title', 'description', 'keywords' ].indexOf( key ) !== -1 ) {
-
-        renderParts.push( '<meta name="' + key + '" content="' + data.head[ key ] + '" />' );
-
-      }
-
-    } );
-
-    if ( pageParams.keys ) {
-
-      pageParams.keys.forEach( item => {
-
-        if ( item.lang === lang ) return;
-
-        renderParts.push( '<link rel="alternate" hreflang="' + item.lang + '" href="' + _getBasePath( params.basePath, lang ) + '/' + item.key + '" />' );
-
-      } );
-
+      return null;
     }
-
-    return renderParts.join( '\n' );
-
-  }
-
-  function layout( pageParams, content, renderParams ) {
-
-    let data = _reduceLabels( pageParams, renderParams.lang, params.labels );
-
-    const template = params.templates[ pageParams.layout ];
-
-    const merged = _.assign( { content }, data, renderParams );
-
-    try {
-
-      return pug.render( template, merged );
-
-    } catch ( e ) {
-
-      throw new VError( 'Failed to render template \n%s\nwith data\n%j', template, merged, e );
-
-    }
-
-  }
-
-  function segments( pageParams, renderParams ) {
-
-    let rendered = '';
-
-    pageParams.segments.forEach( d => {
-
-      let s = _cleanPageSegment( d );
-
-      let renderer = renderers.filter( r => r.key === s.key )[ 0 ],
-
-      data = _.assign( {}, renderer.defaults, s );
-
-      data = _reduceLabels( data, renderParams.lang, params.labels );
-
-      try {
-
-        rendered += renderer.render( data );
-
-      } catch ( e ) {
-
-        throw new Error( 'Invalid data with segment ' + s.key + ': ' + e );
-
-      }
-
-    } );
-
-    return rendered;
-
-  }
-
-}
-
-
-function _cleanPageSegment( segment ) {
-
-  if ( typeof segment === 'string' ) {
-
+    const lang = _pageLang(pageParams[0], page);
     return {
-      key: segment
-    }
-
-  }
-
-  return segment;
-
-}
-
-
-function _reduceLabels( obj, lang, labels = false ) {
-
-  if ( !_.isObject( obj ) ) return mark( obj );
-
-  return _.mapValues( obj, o => {
-
-    if ( _.isArray( o ) ) {
-
-      return o.map( item => _reduceItem( item, lang, labels ) );
-
-    }
-
-    return _reduceItem( o, lang, labels );
-
-  } );
-
-}
-
-function _reduceItem( item, lang, labels ) {
-
-  if ( !_.isObject( item ) && labels && labels[ item ] !== undefined ) {
-
-    return mark( labels[ item ][ lang ] );
-
-  }
-
-  if ( !_.isObject( item ) ) {
-
-    return mark( item );
-
-  }
-
-  if ( item[ lang ] === undefined ) {
-
-    return _reduceLabels( item, lang, labels );
-
-  }
-
-  return mark( item[ lang ] );
-
-}
-
-function _buildLinks( render, params, lang = false ) {
-
-  let withLinks = render;
-
-  params.pages.forEach( t => {
-
-    let srcKeys = t.key ? [ t.key ] : [],
-
-    destKey = t.key;
-
-    if ( t.keys ) {
-
-      srcKeys = srcKeys.concat( t.keys.map( t => t.key ) );
-
-    }
-
-    if ( lang && t.keys ) {
-      destKey = t.keys.filter( k => k.lang === lang )[ 0 ].key;
-
-    }
-
-    let rgx = new RegExp( '\"#(' + srcKeys.join( '|' ) + ')\"', 'g' );
-
-    withLinks = withLinks.replace( rgx, '"' + _getBasePath( params.basePath, lang ) + '/' + destKey + '"' );
-
-  } );
-
-  return withLinks;
-
-}
-
-
-function _matchPageKey( key ) {
-
-  return p => {
-
-    if ( p.keys ) {
-
-      return !!p.keys.filter( k => k.key === key ).length;
-
-    }
-
-    return key === p.key;
-
-  }
-
-}
-
-
-function _pageLang( params, key ) {
-
-  if ( !params.keys ) return false;
-
-  let match = params.keys.filter( k => k.key === key );
-
-  if ( !match.length ) return false;
-
-  return match[ 0 ].lang;
-
-}
-
-
-function _getBasePath( path, lang ) {
-
-  if ( typeof path === 'string' ) return path;
-
-  return path[ lang ];
-
-}
+      render: render.bind(null, pageParams[0], lang),
+      getHeadPart: getHeadPart.bind(null, pageParams[0], lang),
+      getLang: () => lang,
+      getAlternateUrl: getAlternateUrl.bind(null, pageParams[0]),
+    };
+  };
+};
--- packages/landing/test/02_mark.test.js
+++ packages/landing/test/02_mark.test.js
@@ -1,25 +1,17 @@
-"use strict";
+'use strict';
 
-const mark = require( '../segment-pages/mark' );
+const mark = require('../segment-pages/mark');
 
-describe.only( '02 - markdown (unit)', () => {
+describe('02 - markdown (unit)', () => {
+  it('mark should not process links', () => {
+    expect(mark('https://openagenda.com')).toBe('https://openagenda.com');
+  });
 
-  it( 'mark should not process links', () => {
+  it('mark should not process emails', () => {
+    expect(mark('support@openagenda.com')).toBe('support@openagenda.com');
+  });
 
-    expect(mark( 'https://openagenda.com' )).toBe( 'https://openagenda.com' );
-
-  } );
-
-  it( 'mark should not process emails', () => {
-
-    expect(mark( 'support@openagenda.com' )).toBe( 'support@openagenda.com' );
-
-  } );
-
-  it( 'mark processes markdown', () => {
-
-    expect(mark( '**This is bold**' )).toBe( '<strong>This is bold</strong>' );
-
-  } );
-
-} );
+  it('mark processes markdown', () => {
+    expect(mark('**This is bold**')).toBe('<strong>This is bold</strong>');
+  });
+});
