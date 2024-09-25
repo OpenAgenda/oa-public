f102327da88e78823b3c74b09af843176bc55a70
Kévin Berthommier <bertho-zero@users.noreply.github.com>
2024-09-25 12:05:39 +0200

    style(md): lint

--- md/src/index.js
+++ md/src/index.js
@@ -2,8 +2,4 @@ import fromHTMLToMarkdown from './fromHTMLToMarkdown.js';
 import fromMarkdownToHTML from './fromMarkdownToHTML.js';
 import isHTML from './isHTML.js';
 
-export {
-  fromMarkdownToHTML,
-  fromHTMLToMarkdown,
-  isHTML,
-};
+export { fromMarkdownToHTML, fromHTMLToMarkdown, isHTML };
