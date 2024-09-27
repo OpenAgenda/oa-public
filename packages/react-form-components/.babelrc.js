11bbd9e5f07537e59db0ba1c0c9202248293313b
Kévin Berthommier <bertho-zero@users.noreply.github.com>
2024-09-27 10:04:55 +0200

    chore: remove dom-utils and react-form-components

--- packages/react-form-components/.babelrc.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-module.exports = {
-  presets: [require.resolve('@openagenda/babel-preset')],
-  sourceType: 'unambiguous',
-};
--- packages/react-form-components/validators/index.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-const validators = require('@openagenda/validators');
-
-validators.groupTags = require('./groupTags');
-
-module.exports = validators;
