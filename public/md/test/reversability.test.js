45357dd4abaa7afd5c2bc83d525c6ac8a24b5201
Kaoré <kaore@openagenda.com>
2024-10-11 17:04:56 +0200

    refactor(md): convert double new lines to new paragraphs, single to <br>

---
+++ public/md/test/reversability.test.js
@@ -0,0 +1,30 @@
+import fs from 'node:fs/promises';
+import * as url from 'node:url';
+
+import { fromMarkdownToHTML, fromHTMLToMarkdown } from '../src/index.js';
+
+const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
+
+const withSubtitleMarkdown = await fs.readFile(
+  `${__dirname}/fixtures/withSubtitle.md`,
+  'utf-8',
+);
+
+const withDoubleEndOfLines = await fs.readFile(
+  `${__dirname}/fixtures/withDoubleEndOfLines.md`,
+  'utf-8',
+);
+
+describe('reversability', () => {
+  test('with subtitle', () => {
+    const result = fromHTMLToMarkdown(fromMarkdownToHTML(withSubtitleMarkdown));
+
+    expect(result).toBe(withSubtitleMarkdown);
+  });
+
+  test('with double end of lines', () => {
+    const result = fromHTMLToMarkdown(fromMarkdownToHTML(withDoubleEndOfLines));
+
+    expect(result).toBe(withDoubleEndOfLines);
+  });
+});
