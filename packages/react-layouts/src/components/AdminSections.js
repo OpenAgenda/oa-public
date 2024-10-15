67a430ec6d732f05d0226df148e0e91903903e00
Kaoré <kaore@openagenda.com>
2024-10-15 10:14:15 +0200

    refactor(react-layouts): moved resync buttons to systems modal

--- packages/react-layouts/src/components/AdminSections.js
+++ packages/react-layouts/src/components/AdminSections.js
@@ -5,6 +5,8 @@ import { useIntl, defineMessages } from 'react-intl';
 import { useSelector } from 'react-redux';
 import compareRoles from '@openagenda/members/build/compareRoles';
 
+import AgendaSystemMenu from './AgendaSystemMenu';
+
 const TABS_IN_APP = [
   'events',
   'members',
@@ -39,67 +41,70 @@ export default function AdminSections({ sections, agenda, role }) {
   }
 
   return (
-    <ul className="list-unstyled">
-      {sections.map((section) =>
-        (section && section.tabs.length ? (
-          <React.Fragment key={section.label}>
-            <li>
-              <h2>{section.label}</h2>
-            </li>
+    <>
+      <ul className="list-unstyled">
+        {sections.map((section) =>
+          (section && section.tabs.length ? (
+            <React.Fragment key={section.label}>
+              <li>
+                <h2>{section.label}</h2>
+              </li>
 
-            {section.tabs.map((tab) => {
-              const { name, label, link, newFeature } = tab;
-              const selected = matchPath(location.pathname, {
-                path: link,
-                exact: name === 'events',
-              });
-              const tabInApp = TABS_IN_APP.includes(name);
+              {section.tabs.map((tab) => {
+                const { name, label, link, newFeature } = tab;
+                const selected = matchPath(location.pathname, {
+                  path: link,
+                  exact: name === 'events',
+                });
+                const tabInApp = TABS_IN_APP.includes(name);
 
-              const authorizedTab = compareRoles.isSuperiorToOrEqual(
-                role,
-                tab.roles,
-              );
+                const authorizedTab = compareRoles.isSuperiorToOrEqual(
+                  role,
+                  tab.roles,
+                );
 
-              if (!authorizedTab) {
-                if (selected) {
-                  window.location.href = `/${agenda.slug}/admin/events`;
+                if (!authorizedTab) {
+                  if (selected) {
+                    window.location.href = `/${agenda.slug}/admin/events`;
+                  }
+
+                  return null;
                 }
 
-                return null;
-              }
-
-              return (
-                <li
-                  key={name}
-                  className={`menu-item js_menu_item js_menu_item_${name}${
-                    selected ? ' selected' : ''
-                  }`}
-                >
-                  {tabInApp ? (
-                    <Link to={link}>{label}</Link>
-                  ) : (
-                    <a className={selected ? 'active' : ''} href={link}>
-                      {label}
-                    </a>
-                  )}
-                  {name === 'locations' && locationCount ? (
-                    <>
-                      {' '}
-                      <span className="badge badge-warning">
-                        {locationCount}
+                return (
+                  <li
+                    key={name}
+                    className={`menu-item js_menu_item js_menu_item_${name}${
+                      selected ? ' selected' : ''
+                    }`}
+                  >
+                    {tabInApp ? (
+                      <Link to={link}>{label}</Link>
+                    ) : (
+                      <a className={selected ? 'active' : ''} href={link}>
+                        {label}
+                      </a>
+                    )}
+                    {name === 'locations' && locationCount ? (
+                      <>
+                        {' '}
+                        <span className="badge badge-warning">
+                          {locationCount}
+                        </span>
+                      </>
+                    ) : null}
+                    {newFeature ? (
+                      <span className="badge badge-default badge-outline-primary badge-baseline margin-left-sm">
+                        {intl.formatMessage(messages.newTab)}
                       </span>
-                    </>
-                  ) : null}
-                  {newFeature ? (
-                    <span className="badge badge-default badge-outline-primary badge-baseline margin-left-sm">
-                      {intl.formatMessage(messages.newTab)}
-                    </span>
-                  ) : null}
-                </li>
-              );
-            })}
-          </React.Fragment>
-        ) : null))}
-    </ul>
+                    ) : null}
+                  </li>
+                );
+              })}
+            </React.Fragment>
+          ) : null))}
+      </ul>
+      <AgendaSystemMenu agenda={agenda} />
+    </>
   );
 }
