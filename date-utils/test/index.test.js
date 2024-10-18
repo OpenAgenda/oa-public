692bb8c6c93d683c4da9638d24c33aeb9aceeb3d
Kévin Berthommier <bertho-zero@users.noreply.github.com>
2024-10-18 11:03:12 +0200

    feat(date-utils): add locale and weekStartsOn options

--- date-utils/test/index.test.js
+++ date-utils/test/index.test.js
@@ -1,3 +1,4 @@
+import dateFnsLocale from 'date-fns/locale/fr';
 import spreadTimings from '../src/spreadTimings';
 
 describe('spreadTimings', () => {
@@ -75,6 +76,36 @@ describe('spreadTimings', () => {
         1: {
           '2024-09-01': [
             { begin: '2024-09-01T12:00:00Z', end: '2024-09-01T13:00:00Z' },
+          ],
+        },
+      },
+    });
+  });
+
+  it('handles correctly start of weeks', () => {
+    const timings = [
+      { begin: '2024-10-13T00:15:00+02:00', end: '2024-10-13T00:15:00+02:00' },
+      { begin: '2024-10-14T00:15:00+02:00', end: '2024-10-14T00:15:00+02:00' },
+    ];
+
+    const result = spreadTimings(timings, timezone, { locale: dateFnsLocale });
+
+    expect(result).toEqual({
+      '2024-10': {
+        2: {
+          '2024-10-13': [
+            {
+              begin: '2024-10-13T00:15:00+02:00',
+              end: '2024-10-13T00:15:00+02:00',
+            },
+          ],
+        },
+        3: {
+          '2024-10-14': [
+            {
+              begin: '2024-10-14T00:15:00+02:00',
+              end: '2024-10-14T00:15:00+02:00',
+            },
           ],
         },
       },
