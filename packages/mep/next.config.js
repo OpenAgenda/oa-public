682640510f4fb347e80d09336baca207208417bd
Kaoré <kaore@openagenda.com>
2024-10-22 16:16:03 +0200

    chore(mep): aligning keepalive times

--- packages/mep/next.config.js
+++ packages/mep/next.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps : [{
     name: 'next',
     script: '../../node_modules/.bin/next',
-    args: 'start -p ${port}',
+    args: 'start -p ${port} --keepAliveTimeout 56000',
     cwd: '/root/oa/packages/next',
     instances: 4,
     exec_mode: 'cluster'
