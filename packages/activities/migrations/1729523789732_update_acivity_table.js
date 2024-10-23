a67260bf725266a8d92f979d2d54230115f5ce64
Clément Lecroart <clement.lecroart@openagenda.com>
2024-10-23 10:31:45 +0200

    feat(activities): added migration for detail col

---
+++ packages/activities/migrations/1729523789732_update_acivity_table.js
@@ -0,0 +1,9 @@
+exports.up = (knex) => {
+  const { schemas } = knex.client.config;
+
+  return knex.raw(`ALTER TABLE ${schemas.activity} add detail longtext;`);
+};
+
+exports.down = (_knex) => {
+  //
+};
