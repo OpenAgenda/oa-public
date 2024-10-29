c91006c928d3c8fa1f73c4a40b43fa5ca43983bb
Kévin Berthommier <bertho-zero@users.noreply.github.com>
2024-10-29 17:00:28 +0100

    feat(pdf-exports): extract-messages

--- pdf-exports/locales/index.mjs
+++ pdf-exports/locales/index.mjs
@@ -2,13 +2,13 @@
 
 /* eslint-disable */
 
-export { default as br } from './br.json' assert { type: 'json' };
-export { default as ca } from './ca.json' assert { type: 'json' };
-export { default as de } from './de.json' assert { type: 'json' };
-export { default as en } from './en.json' assert { type: 'json' };
-export { default as es } from './es.json' assert { type: 'json' };
-export { default as eu } from './eu.json' assert { type: 'json' };
-export { default as fr } from './fr.json' assert { type: 'json' };
-export { default as io } from './io.json' assert { type: 'json' };
-export { default as it } from './it.json' assert { type: 'json' };
-export { default as oc } from './oc.json' assert { type: 'json' };
+export { default as br } from './br.json' with { type: 'json' };
+export { default as ca } from './ca.json' with { type: 'json' };
+export { default as de } from './de.json' with { type: 'json' };
+export { default as en } from './en.json' with { type: 'json' };
+export { default as es } from './es.json' with { type: 'json' };
+export { default as eu } from './eu.json' with { type: 'json' };
+export { default as fr } from './fr.json' with { type: 'json' };
+export { default as io } from './io.json' with { type: 'json' };
+export { default as it } from './it.json' with { type: 'json' };
+export { default as oc } from './oc.json' with { type: 'json' };
