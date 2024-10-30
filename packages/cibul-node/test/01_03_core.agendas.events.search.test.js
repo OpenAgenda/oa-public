2a73efe6a74d4ed37222a128d18e1cc814d8576a
Kaoré <kaore@openagenda.com>
2024-10-30 09:07:29 +0100

    fix(cibul-node): sourceAgendas are provided in event get

--- packages/cibul-node/test/01_03_core.agendas.events.search.test.js
+++ packages/cibul-node/test/01_03_core.agendas.events.search.test.js
@@ -616,6 +616,22 @@ describe('01 - core - functional (server): core.agendas().events.search()', () =
         });
       });
 
+      it('get can provide origin and source agenda data', async () => {
+        const response = await axios({
+          method: 'get',
+          url: 'http://localhost:3000/agendas/2/events/2?detailed=1',
+          headers: {
+            'content-type': 'application/json',
+          },
+          params: {
+            key: 'egP36aMb0toI8hAhFOm1if8auC1Vg1Nz',
+          },
+        }).then((r) => r.data);
+
+        expect(response.event.originAgenda).toBeDefined();
+        expect(response.event.sourceAgendas).toBeDefined();
+      });
+
       it('unpublished event is gettable by owning contributor', async () => {
         const { event } = await axios({
           method: 'get',
