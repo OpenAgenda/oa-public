4960ad55c727cbd06426cfe9f1c27f3f17b14f10
Kévin Berthommier <bertho-zero@users.noreply.github.com>
2024-11-07 16:41:01 +0100

    refactor(react-filters): cjs to esm

--- public/react-filters/src/hooks/useMapOnChange.js
+++ public/react-filters/src/hooks/useMapOnChange.js
@@ -1,5 +1,5 @@
 import { useCallback, useContext } from 'react';
-import FiltersAndWidgetsContext from '../contexts/FiltersAndWidgetsContext';
+import FiltersAndWidgetsContext from '../contexts/FiltersAndWidgetsContext.js';
 
 export default function useMapOnChange({
   input,
