e426dad994019525f25b4cf8b0ddc7a6445ca71c
Kévin Berthommier <bertho-zero@users.noreply.github.com>
2024-11-07 16:13:31 +0100

    fix(react-timingspicker): fix MaskedInput

--- react-timingspicker/src/NumberInput.js
+++ react-timingspicker/src/NumberInput.js
@@ -1,6 +1,6 @@
 import MaskedInputModule from 'react-text-mask';
 
-const MaskedInput = MaskedInputModule || MaskedInputModule;
+const MaskedInput = MaskedInputModule.default || MaskedInputModule;
 
 export default function NumberInput({ input, meta, mask, ...rest }) {
   return <MaskedInput {...input} {...rest} mask={mask} />;
