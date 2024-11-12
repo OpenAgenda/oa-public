20ce4cad8eadf7a2ec6f02fa1f0f095426fe9ad9
Kévin Berthommier <bertho-zero@users.noreply.github.com>
2024-11-12 11:49:35 +0100

    feat(event-admin-apps): update loadable

--- packages/event-admin-apps/src/getRoutes.js
+++ packages/event-admin-apps/src/getRoutes.js
@@ -1,14 +1,49 @@
-import { loadable } from '@openagenda/react-shared';
+import { loadableComponent } from '@openagenda/react-shared';
 
-const App = loadable(
-  () => import(/* webpackChunkName: "eventAdmin-App" */ './containers/App.js'),
-);
-const Dashboard = loadable(
-  () =>
+// eslint-disable-next-line camelcase
+const contextRequire = typeof __webpack_require__ !== 'undefined'
+  ? import.meta.webpackContext('.', {
+    recursive: true,
+    regExp: /\.js$/,
+    mode: 'weak',
+  })
+  : null;
+
+const App = loadableComponent({
+  chunkName: 'eventAdmin-App',
+  importAsync: () =>
     import(
-      /* webpackChunkName: "eventAdmin-Dashboard" */ './containers/Dashboard.js'
+      /* webpackChunkName: "eventAdmin-App" */
+      './containers/App.js'
     ),
-);
+  resolve: () => {
+    if (contextRequire) {
+      return contextRequire.resolve('./containers/App.js');
+    }
+    const { resolve } = import.meta;
+    if (typeof resolve === 'function') {
+      return resolve('./containers/App.js');
+    }
+  },
+});
+
+const Dashboard = loadableComponent({
+  chunkName: 'eventAdmin-Dashboard',
+  importAsync: () =>
+    import(
+      /* webpackChunkName: "eventAdmin-Dashboard" */
+      './containers/Dashboard.js'
+    ),
+  resolve: () => {
+    if (contextRequire) {
+      return contextRequire.resolve('./containers/Dashboard.js');
+    }
+    const { resolve } = import.meta;
+    if (typeof resolve === 'function') {
+      return resolve('./containers/Dashboard.js');
+    }
+  },
+});
 
 export default (prefix = '') => [
   {
