db8d1598c111cad97fa1ede58a3da0081e875a2b
Kévin Berthommier <bertho-zero@users.noreply.github.com>
2024-11-12 11:50:18 +0100

    fix(react-filters): fix build

--- react-filters/webpack.config.cjs
+++ react-filters/webpack.config.cjs
@@ -87,7 +87,11 @@ module.exports = (env = {}, argv = {}) => {
           resolve: {
             fullySpecified: false,
           },
-          exclude: [/\/node_modules\/nth-check\//],
+          exclude: [
+            /\/node_modules\/nth-check\//,
+            /\/node_modules\/@formatjs\//,
+            /\/node_modules\/intl-messageformat\//,
+          ],
         },
         {
           test: /\.(js|mjs|jsx)$/,
@@ -106,6 +110,9 @@ module.exports = (env = {}, argv = {}) => {
     resolve: {
       // symlinks: false,
       extensions: ['.wasm', '.mjs', '.js', '.jsx', '.json'],
+      alias: {
+        '@httptoolkit/esm': false,
+      },
       fallback: {
         buffer: require.resolve('buffer'),
       },
