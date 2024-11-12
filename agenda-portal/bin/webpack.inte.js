51c5748b58f08f958a844bcd135246ee5f079dec
Kévin Berthommier <bertho-zero@users.noreply.github.com>
2024-11-12 11:50:37 +0100

    fix(agenda-portal): fix build

--- agenda-portal/bin/webpack.inte.js
+++ agenda-portal/bin/webpack.inte.js
@@ -91,6 +91,11 @@ module.exports = {
       chunkFilename: '[id].css',
     }),
   ],
+  resolve: {
+    alias: {
+      '@httptoolkit/esm': false,
+    },
+  },
   optimization: {
     minimizer: ['...', new CssMinimizerPlugin()],
   },
