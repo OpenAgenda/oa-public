9d0ffdae135bec9d9e17d91ab706edaa3a62c062
Kaoré <kaore@openagenda.com>
2024-11-15 09:52:35 +0100

    feat(validators): added timezone validator

---
+++ packages/validators/src/timezone.js
@@ -0,0 +1,51 @@
+import omit from 'lodash/omit';
+import textValidator from './text';
+import cleanParams from './lib/params';
+import errors from './lib/errors';
+
+import listify from './listify';
+
+const timezoneRegex = /^[A-Z][a-zA-Z_]+\/[A-Z][A-Za-z_/]+$/;
+
+export default function timezone(config = {}) {
+  const params = cleanParams('timezone', config, {
+    error: {
+      code: 'timezone.invalid',
+      message: 'value is not a Continent/City timezone',
+    },
+  });
+
+  const validateText = textValidator(omit(params, ['list']));
+
+  const validate = (value) => {
+    let clean;
+
+    try {
+      clean = validateText(value);
+    } catch (textErrors) {
+      throw textErrors.map((e) => ({
+        ...e,
+        code: e.code.replace('text', 'timezone'),
+        message: e.message.replace('text', 'timezone'),
+      }));
+    }
+
+    if ([undefined, null].includes(clean)) {
+      return clean;
+    }
+
+    if (!timezoneRegex.test(value)) {
+      throw errors(
+        params,
+        value,
+        'timezone.invalid',
+        'must be in Continent/City format (e.g., Europe/Paris, America/New_York)',
+      );
+    }
+    return clean;
+  };
+  validate.type = 'integer';
+  validate.field = params.field;
+
+  return params.list ? listify(validate, params) : validate;
+}
---
+++ packages/validators/test/timezone.test.js
@@ -0,0 +1,27 @@
+import validators from '../src';
+
+describe('timezone validator', () => {
+  const defaultValidateTimezone = validators.timezone();
+  it('does not accept UTC+ format', () => {
+    let errors;
+    try {
+      defaultValidateTimezone('UTC+1');
+    } catch (e) {
+      errors = e;
+    }
+
+    expect(errors).toEqual([
+      {
+        origin: 'UTC+1',
+        code: 'timezone.invalid',
+        message: 'must be in Continent/City format (e.g., Europe/Paris, America/New_York)'
+      },
+    ]);
+  });
+
+  it('is happy with Continent/City format', () => {
+    const clean = defaultValidateTimezone('Europe/Paris');
+
+    expect(clean).toBe('Europe/Paris');
+  });
+});
