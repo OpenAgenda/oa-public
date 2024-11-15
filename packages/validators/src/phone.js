7d02b9c90e7f350b2b7f7cbc01934e7acae0fdf1
Kaoré <kaore@openagenda.com>
2024-11-15 11:11:54 +0100

    fix(validators): phone should be optional by default

--- packages/validators/src/phone.js
+++ packages/validators/src/phone.js
@@ -2,10 +2,10 @@ import rgx from './regex';
 
 const PHONE_REGEX = /^(\+|)([\d\s.-]|\([\d\s]\))+$/;
 
-export default (config) => rgx({
-  optional: config?.optional ?? false,
+export default (config = {}) => rgx({
+  optional: config?.optional ?? true,
   field: config?.field,
-  default: 'default' in config ? config.default : null,
+  default: config?.default ?? null,
   regex: PHONE_REGEX,
   error: {
     code: 'phone.invalid',
