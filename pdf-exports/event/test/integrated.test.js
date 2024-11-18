2a7038d662262b4e05d36f14d14f8a3b42d8a00b
Kaoré <kaore@openagenda.com>
2024-11-18 15:37:46 +0100

    refactor(pdf-exports): event export is not a stream

--- pdf-exports/event/test/integrated.test.js
+++ pdf-exports/event/test/integrated.test.js
@@ -4,16 +4,13 @@ import PDFExports from '../index.js';
 import agenda from './fixtures/loiretAgenda.json' assert { type: 'json' };
 import event from './fixtures/loiretEvent.json' assert { type: 'json' };
 
-const {
-  PDF_TEST_FOLDER: pdfTestFolder,
-  TEST_LANG: testLang = 'fr',
-} = process.env;
+const { PDF_TEST_FOLDER: pdfTestFolder, TEST_LANG: testLang = 'fr' } = process.env;
 
 const writeStream = fs.createWriteStream(`${pdfTestFolder}/eventPage.pdf`);
 
 const pdfExports = PDFExports({});
 
-await pdfExports.GenerateExportStream(writeStream, {
+await pdfExports.GenerateExport(writeStream, {
   agenda,
   event,
   lang: testLang,
