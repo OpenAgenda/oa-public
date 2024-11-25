b69e9af814d8de170d143a9bcbadb15b601d7271
Kaoré <kaore@openagenda.com>
2024-11-25 09:25:18 +0100

    fix(cibul-node): erroneous immer use on object sub-array

--- packages/cibul-node/services/eventSearch/lib/amendRestrictedFieldsWithInternalAccess.js
+++ packages/cibul-node/services/eventSearch/lib/amendRestrictedFieldsWithInternalAccess.js
@@ -1,9 +1,12 @@
 import { produce } from 'immer';
 
-export default produce((formSchema) => {
-  formSchema.fields.forEach((field) => {
-    if (Array.isArray(field.read) && !field.read.includes('internal')) {
-      field.read.push('internal');
-    }
-  });
-});
+export default produce((draft) => ({
+  ...draft,
+  fields: draft.fields.map((field) => ({
+    ...field,
+    read:
+      Array.isArray(field.read) && !field.read.includes('internal')
+        ? field.read.concat('internal')
+        : field.read,
+  })),
+}));
