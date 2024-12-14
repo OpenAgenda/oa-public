a1b139ad5c868ac69849e61934d29ce38619757a
Kévin Berthommier <bertho-zero@users.noreply.github.com>
2024-12-14 09:19:50 +0100

    fix(next): fix OAAttribution

--- packages/next/src/components/OAAttribution/index.tsx
+++ packages/next/src/components/OAAttribution/index.tsx
@@ -4,16 +4,15 @@ import logoPic from '../../../public/images/oa.svg';
 
 export default function OAAttribution() {
   return (
-    <Box
-      fontSize="sm"
-      display="flex"
-      justifyContent="flex-end"
-      px="2"
-      py="0.5"
-      bg="whiteAlpha.800"
-      zIndex="sticky"
-    >
-      <Link href="https://openagenda.com" target="_blank" display="flex">
+    <Box fontSize="sm" display="flex" justifyContent="flex-end" mt="2">
+      <Link
+        href="https://openagenda.com"
+        target="_blank"
+        display="flex"
+        bg="white"
+        p="2"
+        zIndex="sticky"
+      >
         <Image
           src={logoPic}
           alt="OpenAgenda"
