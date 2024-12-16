b7827887c28aad0f324bdeedd8bf4a276956481e
Kaoré <kaore@openagenda.com>
2024-12-16 13:21:28 +0100

    fix(agenda-portal): ConsentBanner path issue

--- agenda-portal/client/lib/trackConsent.js
+++ agenda-portal/client/lib/trackConsent.js
@@ -1,7 +1,7 @@
 import ReactDom from 'react-dom';
 import { getCookieConsentValue } from 'react-cookie-consent';
 import { IntlProvider } from 'react-intl';
-import { ConsentBanner } from '@openagenda/react-shared';
+import ConsentBanner from '@openagenda/react-shared/dist/components/ConsentBanner.js';
 
 export default (
   { lang, locales, cookieBannerLink },
