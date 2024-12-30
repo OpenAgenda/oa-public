52cc7a4ac52d0772b9fff5b2f12f5420690d8607
DFarau <doriane.farau@hetic.net>
2024-12-30 10:06:44 +0100

    refactor(react-shared): improve structure of SortableSelect and FilterSelect

--- public/react-shared/src/components/FilterSelect.js
+++ public/react-shared/src/components/FilterSelect.js
@@ -1,41 +1,4 @@
-import { useMemo } from 'react';
-import { DndContext } from '@dnd-kit/core';
-import Select, { components } from 'react-select';
-import { useIntl } from 'react-intl';
-import defaultSelectStyles from '../utils/defaultSelectStyles.js';
-import {
-  Droppable,
-  Draggable,
-  useDragAndDropSensors,
-  useHandleDragEnd,
-} from '../utils/dragAndDrop.js';
-
-function MultiValueContainer(props) {
-  const {
-    innerProps,
-    data: { value },
-  } = props;
-
-  return (
-    <Droppable id={value}>
-      <Draggable id={value}>
-        <components.MultiValueContainer
-          {...props}
-          innerProps={{ ...innerProps }}
-        />
-      </Draggable>
-    </Droppable>
-  );
-}
-
-function SelectContainer(props) {
-  const { innerProps } = props;
-  return (
-    <Droppable id="select">
-      <components.SelectContainer {...props} innerProps={{ ...innerProps }} />
-    </Droppable>
-  );
-}
+import SortableSelect from './SortableSelect.js';
 
 export default function FilterSelect({
   schema,
@@ -48,61 +11,18 @@ export default function FilterSelect({
   getFilterOptions,
   locationOptions = undefined,
 }) {
-  const intl = useIntl();
-
-  const sensors = useDragAndDropSensors();
-
-  const filterOptions = useMemo(() => {
-    if (locationOptions) {
-      return locationOptions.map((option) => ({
-        value: option.value,
-        label: intl.formatMessage(option.label),
-      }));
-    }
-    return getFilterOptions(intl, schema, exclude);
-  }, [intl, schema, exclude, locationOptions]);
-
-  const selectedOptions = useMemo(
-    () =>
-      value
-        .map((name) => filterOptions.find((o) => o.value === name))
-        .filter((v) => !!v),
-    [value, filterOptions],
-  );
-
-  const handleDragEnd = useHandleDragEnd(selectedOptions, onChange);
-
   return (
-    <DndContext onDragEnd={handleDragEnd} sensors={sensors}>
-      <Select
-        value={selectedOptions}
-        onChange={(update) => {
-          onChange(update.map((o) => o.value));
-        }}
-        options={filterOptions}
-        isMulti
-        closeMenuOnSelect
-        /* openMenuOnClick={false} */
-        openMenuOnFocus
-        placeholder={placeholder}
-        components={{
-          MultiValueContainer,
-          SelectContainer,
-        }}
-        isDisabled={disabled}
-        styles={{
-          ...defaultSelectStyles,
-          multiValue: (provided) => ({
-            ...provided,
-            margin: '1px',
-            padding: '0px',
-            borderRadius: '2px',
-            overflow: 'hidden',
-            cursor: 'grab',
-          }),
-        }}
-        menuPosition={menuPosition}
-      />
-    </DndContext>
+    <SortableSelect
+      schema={schema}
+      value={value}
+      onChange={onChange}
+      exclude={exclude}
+      placeholder={placeholder}
+      disabled={disabled}
+      menuPosition={menuPosition}
+      getFilterOptions={getFilterOptions}
+      locationOptions={locationOptions}
+      isFilterMode
+    />
   );
 }
