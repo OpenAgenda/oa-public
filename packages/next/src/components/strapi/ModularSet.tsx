cbfa57e1f09bee3e017929b7a404c2932adb4163
Kaoré <kaore@openagenda.com>
2025-01-17 10:04:57 +0100

    feat(next): modular set takes an optional description

--- packages/next/src/components/strapi/ModularSet.tsx
+++ packages/next/src/components/strapi/ModularSet.tsx
@@ -1,14 +1,19 @@
-import { Heading, Grid } from '@openagenda/uikit';
+import { Heading, Grid, Text } from '@openagenda/uikit';
 import Modular from './Modular';
 import SegmentContainer from './SegmentContainer';
 import CTAButton from './CTAButton';
 
-export default function ModularSet({ title, Components, CTA }) {
+export default function ModularSet({ title, description, Components, CTA }) {
   return (
     <SegmentContainer>
       <Heading as="h2" size="xl" textAlign="center">
         {title}
       </Heading>
+      {description && (
+        <Text textAlign="center" mt={4} mb={2}>
+          {description}
+        </Text>
+      )}
       <Grid
         templateColumns="repeat(auto-fit, minmax(250px, 1fr))"
         gap={8}
