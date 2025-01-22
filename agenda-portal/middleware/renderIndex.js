fb37903bfa0eada3a77f46f0ee85073a098234c9
DFarau <doriane.farau@hetic.net>
2025-01-22 16:33:36 +0100

    chore: publish agenda-portal 6.11.17

--- agenda-portal/middleware/renderIndex.js
+++ agenda-portal/middleware/renderIndex.js
@@ -44,7 +44,7 @@ export default async (req, res, next) => {
 
   if (req.app.locals.tracking?.useAgendaGoogleAnalytics) {
     const gaId = res.locals.agenda.settings.tracking?.googleAnalytics
-      && res.locals.agenda.settings.tracking?.enableTracking
+      && req.app.locals.tracking?.enableTracking
       ? res.locals.agenda.settings.tracking.googleAnalytics
       : null;
     if (!gaId) {
