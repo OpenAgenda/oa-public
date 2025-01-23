b4556296c71de277c68a09e1afb8126d42c897dd
Kévin Berthommier <bertho-zero@users.noreply.github.com>
2025-01-23 11:27:32 +0100

    fix(next): fix NavigateButton without navigation context

--- packages/next/src/views/EmbedEventShow/components/NavigateButton.tsx
+++ packages/next/src/views/EmbedEventShow/components/NavigateButton.tsx
@@ -34,7 +34,7 @@ export default function NavigateButton({
     direction,
     agenda,
     eventNc: omitParams(
-      getPrefilteredQuery({ query: eventNc, prefilter, filters }),
+      getPrefilteredQuery({ query: eventNc || {}, prefilter, filters }),
     ),
     setNc,
     query,
