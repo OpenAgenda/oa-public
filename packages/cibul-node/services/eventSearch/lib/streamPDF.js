025215e6971d4eb3418817deffd6e912c1c7a9fe
Kaoré <kaore@openagenda.com>
2025-01-24 19:42:49 +0100

    feat(cibul-node): link between pdf export menu and pdf lib

--- packages/cibul-node/services/eventSearch/lib/streamPDF.js
+++ packages/cibul-node/services/eventSearch/lib/streamPDF.js
@@ -5,15 +5,18 @@ export default function streamPDF(config, req, res) {
 
   const { withImageLimit = 200 } = config;
 
+  const { locationInHeader, sort = [] } = req.query;
+
   req
     .search(req.searchQuery, { size: 0 }, req.searchOptions)
     .then(({ total }) => {
       pdfExports.agenda.GenerateExportStream(req.stream, res, {
         agenda: req.agenda,
         includeEventImages: total < withImageLimit,
-        mode: ['locationName', 'city'].includes(req.query.mode)
-          ? req.query.mode
-          : undefined,
+        mode: locationInHeader === 'true' ? 'locationName' : undefined,
+        sections: sort.length
+          ? sort.map((s) => s.replace(/\.asc|\.desc/, ''))
+          : null,
       });
 
       res.writeHead(200, {
