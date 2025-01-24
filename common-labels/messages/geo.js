039cfa024fa8751be9457efd01fa981800149bbd
Kaoré <kaore@openagenda.com>
2025-01-24 19:38:36 +0100

    feat(common-labels): added geo labels

---
+++ common-labels/messages/geo.js
@@ -0,0 +1,26 @@
+'use strict';
+
+const { defineMessages } = require('react-intl');
+
+module.exports = defineMessages({
+  city: {
+    id: 'common.geo.city',
+    defaultMessage: 'City',
+  },
+  department: {
+    id: 'common.geo.department',
+    defaultMessage: 'Department',
+  },
+  region: {
+    id: 'common.geo.region',
+    defaultMessage: 'Region',
+  },
+  adminLevel3: {
+    id: 'common.geo.adminLevel3',
+    defaultMessage: 'Inter-city',
+  },
+  location: {
+    id: 'common.geo.location',
+    defaultMessage: 'Location',
+  },
+});
