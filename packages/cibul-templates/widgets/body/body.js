51573e54b83b92553a46c3e985c7e87a3e164c37
Kévin Berthommier <bertho-zero@users.noreply.github.com>
2025-01-29 13:17:53 +0100

    fix: replace old embeds

--- packages/cibul-templates/widgets/body/body.js
+++ packages/cibul-templates/widgets/body/body.js
@@ -1,3 +1,5 @@
+import '@openagenda/widgets/src/index';
+
 if (
   document.readyState === 'complete'
   || document.readyState === 'interactive'
@@ -14,24 +16,6 @@ function readyHandler() {
     elems = document.querySelectorAll('[data-oabdy]');
   }
 
-  window.oa = (function(d, s, id) {
-    var js, fjs = d.getElementsByTagName(s)[0],
-      t = window.oa || {};
-    if (d.getElementById(id)) return t;
-    js = d.createElement(s);
-    js.id = id;
-    js.src = 'https://openagenda.com/js/newEmbed.js';
-    js.async = true;
-    fjs.parentNode.insertBefore(js, fjs);
-
-    t._e = [];
-    t.ready = function(f) {
-      t._e.push(f);
-    };
-
-    return t;
-  }(document, 'script', 'oa-wjs'));
-
   for (const elem of elems) {
     try {
       const agendaUid = (elem.dataset.src || elem.src).match(/[0-9]+/g)[0];
@@ -39,16 +23,15 @@ function readyHandler() {
       elem.style.display = 'none';
       elem.src = 'about:blank';
 
-      window.oa.ready(() => {
-        const newElem = document.createElement('blockquote');
-        newElem.className = 'oa-agenda';
-        newElem.setAttribute('align', 'center');
-        newElem.innerHTML = `<a href="https://openagenda.com/agendas/${agendaUid}"></a>`;
-        elem.replaceWith(newElem);
-        window.oa.widgets.load();
-      });
+      const newElem = document.createElement('blockquote');
+      newElem.className = 'oa-agenda';
+      newElem.setAttribute('align', 'center');
+      newElem.innerHTML = `<a href="https://openagenda.com/agendas/${agendaUid}"></a>`;
+      elem.replaceWith(newElem);
     } catch (e) {
       console.log('Failed to replace old iframes', e);
     }
   }
+
+  window.oa.widgets.load();
 }
