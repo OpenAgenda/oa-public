6c67c3083476f654f3cf19828b62e49ef161dffd
Kévin Berthommier <bertho-zero@users.noreply.github.com>
2025-01-30 17:34:19 +0100

    refactor: remove @openagenda/registration

--- packages/cibul-node/services/event/middleware/components.js
+++ packages/cibul-node/services/event/middleware/components.js
@@ -1,15 +1,4 @@
-import React from 'react';
-import ReactDOMServer from 'react-dom/server';
 import templater from '@openagenda/cibul-templates';
-import Registration from '@openagenda/registration/lib/Display.js';
-
-function renderComponent(component, data) {
-  const rendered = ReactDOMServer.renderToStaticMarkup(
-    React.createElement(component, data),
-  );
-
-  return rendered === '<noscript></noscript>' ? false : rendered;
-}
 
 async function _timings(v) {
   return new Promise((resolve, reject) => {
@@ -31,16 +20,8 @@ async function _timings(v) {
   });
 }
 
-function _registration(v) {
-  v.req.formatted.registrationComponent = renderComponent(Registration, {
-    value: v.req.formatted.registration.map(({ value }) => value).join(', '),
-    lang: v.req.lang,
-  });
-}
-
 export default async function buildComponents(req, res, next) {
   try {
-    _registration({ req });
     await _timings({ req });
 
     next();
