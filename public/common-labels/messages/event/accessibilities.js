baff1db4ba9cbe389baa165252464999bc48bbf1
Kaoré <kaore@openagenda.com>
2025-02-24 15:57:38 +0100

    chore(common-labels): reworked accessibilities labels

--- public/common-labels/messages/event/accessibilities.js
+++ public/common-labels/messages/event/accessibilities.js
@@ -4,23 +4,32 @@ const { defineMessages } = require('react-intl');
 
 module.exports = defineMessages({
   hi: {
-    id: 'common.event.accessiblities.hi',
+    id: 'common.event.accessibilities.hi',
     defaultMessage: 'Hearing impairment',
   },
   vi: {
-    id: 'common.event.accessiblities.vi',
+    id: 'common.event.accessibilities.vi',
     defaultMessage: 'Visual impairment',
   },
   pi: {
-    id: 'common.event.accessiblities.pi',
+    id: 'common.event.accessibilities.pi',
     defaultMessage: 'Psychic impairment',
   },
   mi: {
-    id: 'common.event.accessiblities.mi',
+    id: 'common.event.accessibilities.mi',
     defaultMessage: 'Motor impairment',
   },
   ii: {
-    id: 'common.event.accessiblities.ii',
+    id: 'common.event.accessibilities.ii',
     defaultMessage: 'Intellectual impairment',
+  },
+  accessibleEvent: {
+    id: 'common.event.accessibilities.accessibleEvent',
+    defaultMessage: 'Accessibility installations',
+  },
+  accessibleDetail: {
+    id: 'common.event.accessibilities.detail',
+    defaultMessage:
+      'This event has installations setup to address specific types of disabilities',
   },
 });
