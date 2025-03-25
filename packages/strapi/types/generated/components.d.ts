337e238fa897b3d39e5630151be5050ca4293ff2
DFarau <doriane.farau@hetic.net>
2025-03-25 09:55:14 +0100

    chore(strapi): add accordionSet component

--- packages/strapi/types/generated/components.d.ts
+++ packages/strapi/types/generated/components.d.ts
@@ -192,7 +192,7 @@ export interface SegmentsAccordionSet extends Struct.ComponentSchema {
       ['none', 'sm', 'md', 'lg', 'xl', 'full']
     >;
     boxAlign: Schema.Attribute.Enumeration<['left', 'right', 'center']>;
-    Components: Schema.Attribute.Component<'components.accordion', false>;
+    Components: Schema.Attribute.Component<'components.accordion', true>;
     contentColor: Schema.Attribute.Relation<
       'oneToOne',
       'api::theme-color.theme-color'
