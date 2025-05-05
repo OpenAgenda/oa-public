d7cc26fce1aef966619a7f072473855f51c7dfa2
Kaoré <kaore@openagenda.com>
2025-05-05 12:29:24 +0200

    feat(pdf-exports): add noSelection label in italic when no values to display

--- pdf-exports/event/lib/addOptioned.js
+++ pdf-exports/event/lib/addOptioned.js
@@ -1,6 +1,8 @@
 import { getLocaleValue } from '@openagenda/intl';
+import getIntl from '../../utils/intl.js';
 import addText from './addText.js';
 import Cursor from './Cursor.js';
+import messages from './messages.js';
 
 export default async function addOptioned(doc, cursor, params = {}) {
   const {
@@ -12,6 +14,8 @@ export default async function addOptioned(doc, cursor, params = {}) {
     availableHeight,
     assessHeight = true,
   } = params;
+
+  const intl = getIntl(lang);
 
   if (assessHeight) {
     const { height } = await addOptioned(doc, cursor, {
@@ -27,7 +31,17 @@ export default async function addOptioned(doc, cursor, params = {}) {
   const localCursor = Cursor(cursor);
   const blockSize = { height: 0, width: 0 };
 
-  if (!value) {
+  if (!value?.length) {
+    const noSelectionBlock = await addText(doc, localCursor, {
+      content: intl.formatMessage(messages.noSelection),
+      width: availableWidth,
+      lang,
+      simulate,
+      italic: true,
+    });
+
+    blockSize.height += noSelectionBlock.height;
+    blockSize.width = Math.max(blockSize.width, noSelectionBlock.width);
     return blockSize;
   }
 
