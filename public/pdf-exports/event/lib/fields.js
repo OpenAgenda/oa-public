d9844e6b543934ce399fb8f3c2479161fa659abc
Kaoré <kaore@openagenda.com>
2025-05-09 13:03:15 +0200

    fix(pdf-exports): display location detail titles only when there is matching content

--- public/pdf-exports/event/lib/fields.js
+++ public/pdf-exports/event/lib/fields.js
@@ -53,51 +53,54 @@ export const conditionsAndRegistrationGroup = ({ agenda, event }) => [
   },
 ];
 
-export const locationGroup = [
-  {
-    fieldType: 'text',
-    value: 'À propos du lieu',
-    bold: true,
-    fontSize: '1.2em',
-  },
-  {
-    field: 'location.name',
-    fieldType: 'text',
-    bold: true,
-  },
-  {
-    field: 'location.address',
-    fieldType: 'text',
-  },
-  {
-    field: 'location.image',
-    fieldType: 'image',
-    relatedValues: [{ from: 'location.imageCredits', to: 'credits' }],
-  },
-  {
-    field: 'location.description',
-    fieldType: 'text',
-  },
-  {
-    field: 'location.access',
-    fieldType: 'text',
-    omitLabel: false,
-  },
-  {
-    fieldType: 'text',
-    value: 'Coordonnées',
-    bold: true,
-  },
-  {
-    field: 'location.website',
-    fieldType: 'link',
-  },
-  {
-    field: 'location.phone',
-    fieldType: 'phone',
-  },
-  {
-    field: 'location.email',
-    fieldType: 'email',
-  },
-].map((f) => ({ ...f, fontSize: f.fontSize ?? '0.9em' }));
+export const locationGroup = (location) =>
+  [
+    {
+      fieldType: 'text',
+      value: 'À propos du lieu',
+      bold: true,
+      fontSize: '1.2em',
+    },
+    {
+      field: 'location.name',
+      fieldType: 'text',
+      bold: true,
+    },
+    {
+      field: 'location.address',
+      fieldType: 'text',
+    },
+    {
+      field: 'location.image',
+      fieldType: 'image',
+      relatedValues: [{ from: 'location.imageCredits', to: 'credits' }],
+    },
+    {
+      field: 'location.description',
+      fieldType: 'text',
+    },
+    Object.keys(location.access ?? {}).length && {
+      field: 'location.access',
+      fieldType: 'text',
+      omitLabel: false,
+    },
+    ['website', 'phone', 'email'].filter((f) => !!location[f]).length && {
+      fieldType: 'text',
+      value: 'Coordonnées',
+      bold: true,
+    },
+    {
+      field: 'location.website',
+      fieldType: 'link',
+    },
+    {
+      field: 'location.phone',
+      fieldType: 'phone',
+    },
+    {
+      field: 'location.email',
+      fieldType: 'email',
+    },
+  ]
+    .filter((f) => !!f)
+    .map((f) => ({ ...f, fontSize: f.fontSize ?? '0.9em' }));
