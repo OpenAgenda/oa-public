9f9c6e7ba07a184457e11656b3b00a5ba3cfc320
Kaoré <kaore@openagenda.com>
2025-05-16 14:53:41 +0200

    chore(mep): allocate more heap size to tasks node process

--- packages/mep/ecosystem.config.js
+++ packages/mep/ecosystem.config.js
@@ -4,6 +4,7 @@ module.exports = {
     script: 'server.js',
     args: '${args}',
     cwd: '/root/oa/packages/cibul-node',
+    node_args: '${node_args}',
     instances: ${instances},
     exec_mode: 'cluster',
     env: ${env},
