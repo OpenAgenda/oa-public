d2d65ea3d95b87eb6f3e358bfdc15f513b16f3f6
Kévin Berthommier <kevin.bertho@gmail.com>
2025-05-23 14:48:34 +0200

    fix(uikit): fix fonts

--- uikit/src/theme/tokens/fonts.ts
+++ uikit/src/theme/tokens/fonts.ts
@@ -3,10 +3,10 @@ import { defineTokens } from '@chakra-ui/react';
 export const fonts = defineTokens.fonts({
   heading: {
     value:
-      'var(--font-noto-sans), Helvetica, -apple-system, BlinkMacSystemFont, "Segoe UI", Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"',
+      'var(--font-ubuntu-sans, ""), var(--font-noto-sans, ""), Helvetica, -apple-system, BlinkMacSystemFont, "Segoe UI", Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"',
   },
   body: {
     value:
-      'var(--font-noto-sans), Helvetica, -apple-system, BlinkMacSystemFont, "Segoe UI", Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"',
+      'var(--font-noto-sans, ""), Helvetica, -apple-system, BlinkMacSystemFont, "Segoe UI", Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"',
   },
 });
