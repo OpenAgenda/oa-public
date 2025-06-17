793028db378a7c839dc79ec1e10c2fd57819c115
Kaoré <kaore@openagenda.com>
2025-06-17 11:19:05 +0200

    feat(next): strapi SplitHero as segment

--- packages/next/src/components/strapi/SplitHeroSegment.tsx
+++ packages/next/src/components/strapi/SplitHeroSegment.tsx
@@ -1,15 +1,10 @@
 import SegmentContainer from './SegmentContainer';
 import SplitHero from './SplitHero';
 
-export default function SplitHeroSegment({ title, image, text, direction }) {
+export default function SplitHeroSegment(props) {
   return (
     <SegmentContainer>
-      <SplitHero
-        title={title}
-        image={image}
-        text={text}
-        direction={direction}
-      />
+      <SplitHero {...props} />
     </SegmentContainer>
   );
 }
