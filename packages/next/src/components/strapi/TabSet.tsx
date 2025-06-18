f524eb90fa6c5c0ccc3734d7ad34bb61f765dcf2
Kaoré <kaore@openagenda.com>
2025-06-18 19:10:31 +0200

    fix(next): strapi align tab size with content size

--- packages/next/src/components/strapi/TabSet.tsx
+++ packages/next/src/components/strapi/TabSet.tsx
@@ -22,14 +22,14 @@ export default function TabSet({
       <Tabs.Root defaultValue={TabsData[0].id}>
         <Tabs.List>
           {TabsData.map((tab) => (
-            <Tabs.Trigger key={tab.id} value={tab.id}>
+            <Tabs.Trigger key={tab.id} value={tab.id} fontSize="md">
               {tab.title}
             </Tabs.Trigger>
           ))}
         </Tabs.List>
 
         {TabsData.map((tab) => (
-          <Tabs.Content key={tab.id} value={tab.id}>
+          <Tabs.Content key={tab.id} value={tab.id} p={6}>
             <SplitHero {...tab.content} />
           </Tabs.Content>
         ))}
