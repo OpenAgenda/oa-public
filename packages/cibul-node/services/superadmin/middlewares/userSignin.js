ff7bc1b4187125c2b71cd17621f4b4dabb4ef0d6
Kévin Berthommier <kevin.bertho@gmail.com>
2025-07-22 16:06:35 +0200

    feat(cibul-node): fix session opened from /admin

--- packages/cibul-node/services/superadmin/middlewares/userSignin.js
+++ packages/cibul-node/services/superadmin/middlewares/userSignin.js
@@ -4,6 +4,10 @@ export default function userSignin(req, res) {
   const { sessions } = req.app.services;
 
   sessions.open(req, res, req.loadedUser, () => {
+    res.cookie('loggedAs', '1', {
+      expires: req.session.expires,
+    });
+
     if (req.xhr) return cmn.renderJson(req, res, { success: true });
 
     return res.redirect(302, '/home');
