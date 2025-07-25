8e5173ae970ce0387ec0b6a3f90ab8edf0e67314
Kévin Berthommier <kevin.bertho@gmail.com>
2025-07-25 15:45:41 +0200

    feat(discord): add link to supervisor users

--- packages/discord/lib/notifyUserCreation.js
+++ packages/discord/lib/notifyUserCreation.js
@@ -7,6 +7,11 @@ export default (channel) => (user) => {
     .setTitle(fullName)
     .setDescription('Nouvel utilisateur')
     .setURL(`https://openagenda.com/admin/users?userUid=${uid}`)
-    .addField('Email', email);
+    .addField('Email', email, true)
+    .addField(
+      'Logs',
+      `[Voir les logs](https://openagenda.com/supervisor/users?userUid=${uid})`,
+      true,
+    );
   return channel.send(embed);
 };
