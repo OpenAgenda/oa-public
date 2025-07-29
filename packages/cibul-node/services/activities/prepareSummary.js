23a4fe0ebcb3fe04539b0c7db080bc5b88221b8e
Kévin Berthommier <kevin.bertho@gmail.com>
2025-07-29 10:26:31 +0200

    fix(cibul-node): typo

--- packages/cibul-node/services/activities/prepareSummary.js
+++ packages/cibul-node/services/activities/prepareSummary.js
@@ -24,7 +24,7 @@ export default (services) => {
                 break;
               }
               default:
-                log.warn(`Unkown job ${job.name}`);
+                log.warn(`Unknown job ${job.name}`);
             }
           },
           {
