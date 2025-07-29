48b8ebb85214976fa865fc37a70385681907a31e
Kévin Berthommier <kevin.bertho@gmail.com>
2025-07-29 12:54:07 +0200

    fix(cibul-node): fix dubious merge

--- packages/cibul-node/services/agendaLocations/tasks/syncImpactedEventsAndAgendas.js
+++ packages/cibul-node/services/agendaLocations/tasks/syncImpactedEventsAndAgendas.js
@@ -7,80 +7,82 @@ const diffForPass = (before, after) =>
   || before.city !== after.city
   || before.postalCode !== after.postalCode;
 
-export default (services) =>
-  async function syncImpactedEventsAndAgendas(before, _after) {
-    const {
-      core,
-      events: eventsSvc,
-      agendaEvents,
-      tracker,
-      registrations,
-    } = services;
+export default async function syncImpactedEventsAndAgendas(
+  services,
+  { before, after: _after },
+) {
+  const {
+    core,
+    events: eventsSvc,
+    agendaEvents,
+    tracker,
+    registrations,
+  } = services;
 
-    tracker('agendaLocations.syncImpactedEventsAndAgendas');
+  tracker('agendaLocations.syncImpactedEventsAndAgendas');
 
-    const events = await eventsSvc.list(
-      { locationUid: before.uid },
-      { limit: 1000 },
-      {
-        includeFields: ['uid', 'registration', 'agenda'],
-        access: 'internal',
-        private: null,
-        draft: null,
-      },
-    );
+  const events = await eventsSvc.list(
+    { locationUid: before.uid },
+    { limit: 1000 },
+    {
+      includeFields: ['uid', 'registration', 'agenda'],
+      access: 'internal',
+      private: null,
+      draft: null,
+    },
+  );
+
+  log(
+    '%s impacted events by location %s update',
+    events.length,
+    before.uid,
+    events.length < 20 ? `(${events.map((e) => e.uid).join(', ')})` : null,
+  );
+
+  /* const impactedAgendaUids = []; */
+
+  for (const event of events) {
+    const relatedReferences = await agendaEvents.list
+      .byEventUid(event.uid)
+      .then(({ items }) => items);
 
     log(
-      '%s impacted events by location %s update',
-      events.length,
-      before.uid,
-      events.length < 20 ? `(${events.map((e) => e.uid).join(', ')})` : null,
+      'found %s references for event %s',
+      relatedReferences.length,
+      event.uid,
     );
 
-    /* const impactedAgendaUids = []; */
+    for (const ae of relatedReferences) {
+      const { agendaUid } = ae;
+      const passCulturePayload = event.registration?.find(
+        ({ service }) => service === 'passCulture',
+      )?.data;
 
-    for (const event of events) {
-      const relatedReferences = await agendaEvents.list
-        .byEventUid(event.uid)
-        .then(({ items }) => items);
+      if (diffForPass(before, _after) && passCulturePayload) {
+        await registrations.utils.passCulture.loadAndProcess(
+          agendaUid,
+          event.uid,
+        );
+      }
 
-      log(
-        'found %s references for event %s',
-        relatedReferences.length,
-        event.uid,
-      );
-
-      for (const ae of relatedReferences) {
-        const { agendaUid } = ae;
-        const passCulturePayload = event.registration?.find(
-          ({ service }) => service === 'passCulture',
-        )?.data;
-
-        if (diffForPass(before, _after) && passCulturePayload) {
-          await registrations.utils.passCulture.loadAndProcess(
-            agendaUid,
-            event.uid,
+      log('resyncing event %s in agenda %s', event.uid, agendaUid);
+      try {
+        await core.agendas(agendaUid).events.search.resyncEvent(event.uid);
+      } catch (error) {
+        if (error.name === 'NotFound') {
+          log.warn(
+            'failed to retrieve agenda matching evaluated agendaEvent ref',
+            { error, agendaUid, eventUid: event.uid },
           );
-        }
-
-        log('resyncing event %s in agenda %s', event.uid, agendaUid);
-        try {
-          await core.agendas(agendaUid).events.search.resyncEvent(event.uid);
-        } catch (error) {
-          if (error.name === 'NotFound') {
-            log.warn(
-              'failed to retrieve agenda matching evaluated agendaEvent ref',
-              { error, agendaUid, eventUid: event.uid },
-            );
-          } else {
-            log.error('failed to resync event', {
-              eventUid: event.uid,
-              agendaUid,
-              error,
-            });
-          }
+        } else {
+          log.error('failed to resync event', {
+            eventUid: event.uid,
+            agendaUid,
+            error,
+          });
         }
       }
     }
-    tracker('agendaLocations.syncImpactedEventsAndAgendas.done');
-  };
+  }
+  tracker('agendaLocations.syncImpactedEventsAndAgendas.done');
+}
