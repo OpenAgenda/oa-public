5216089c5d464faca031e1cbc5f4d4d67b7abbc1
Kévin Berthommier <kevin.bertho@gmail.com>
2025-08-04 11:25:59 +0200

    fix(member-apps): fix method

--- packages/member-apps/src/components/Form/index.js
+++ packages/member-apps/src/components/Form/index.js
@@ -224,7 +224,7 @@ export default ({
       ) : (
         <>
           <FormSchemaComponent
-            method={operation === 'update' ? 'patch' : 'post'}
+            method={operation === 'update' ? 'PATCH' : 'POST'}
             res={{
               patch: saveRes,
               post: saveRes,
