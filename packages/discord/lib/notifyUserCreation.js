8517744dcaa3b2a9ec6006dc715b27ae23d6e713
Kévin Berthommier <kevin.bertho@gmail.com>
2025-08-05 11:14:55 +0200

    feat(discord): add param from to logs link

--- packages/discord/lib/notifyUserCreation.js
+++ packages/discord/lib/notifyUserCreation.js
@@ -2,6 +2,15 @@ import Discord from 'discord.js';
 
 export default (channel) => (user) => {
   const { fullName, email, uid } = user;
+
+  const now = new Date();
+  const oneHourAgo = new Date(now.getTime() - 60 * 60 * 1000);
+
+  const parisTime = new Date(
+    oneHourAgo.toLocaleString('en-US', { timeZone: 'Europe/Paris' }),
+  );
+  const fromParam = parisTime.toISOString().slice(0, 16); // Format YYYY-MM-DDTHH:mm
+
   const embed = new Discord.MessageEmbed()
     .setColor('#0d944a')
     .setTitle(fullName)
@@ -10,7 +19,7 @@ export default (channel) => (user) => {
     .addField('Email', email, true)
     .addField(
       'Logs',
-      `[Voir les logs](https://openagenda.com/supervisor/users?userUid=${uid})`,
+      `[Voir les logs](https://openagenda.com/supervisor/users?userUid=${uid}&from=${fromParam})`,
       true,
     );
   return channel.send(embed);
