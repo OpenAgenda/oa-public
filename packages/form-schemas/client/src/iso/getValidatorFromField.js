84f9350a182089bea6dc6d56ca0c4928d0737f3a
clemLecro <clement.lecroart@openagenda.com>
2025-09-29 18:08:45 +0200

    feat(form-schemas): added allowFalse for boolean

--- packages/form-schemas/client/src/iso/getValidatorFromField.js
+++ packages/form-schemas/client/src/iso/getValidatorFromField.js
@@ -132,6 +132,7 @@ export default (field, options = {}) => {
       'optionalWith',
       'enableWith',
       'allowNull',
+      'allowFalse',
       'allowURL',
       'imageWithSizeAndVariants',
       'allowPath',
