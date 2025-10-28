d22f706f40b8860e60ebed16ef0648de4184927a
Kévin Berthommier <kevin.bertho@gmail.com>
2025-10-28 15:19:30 +0100

    chore: remove react-share-menus

--- react-share-menus/src/index.js
+++
@@ -1,5 +0,0 @@
-export { default as ExportModal } from './components/export-modal/ExportModal.js';
-export { default as SpreadsheetOptions } from './components/export-modal/SpreadsheetOptions.js';
-export { default as AggregatorModal } from './components/AggregatorModal.js';
-export { default as EventShareModal } from './components/EventShareModal/index.js';
-export * as modalLocales from './locales-compiled/index.js';
