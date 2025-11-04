106fd382fa0e509eee5499f66f8138ba641457c7
Kaoré <kaore@openagenda.com>
2025-11-04 16:39:49 +0100

    feat(form-schemas): related can be defined now

--- packages/form-schemas/client/src/iso/getValidatorFromField.js
+++ packages/form-schemas/client/src/iso/getValidatorFromField.js
@@ -131,6 +131,7 @@ export default (field, options = {}) => {
       'optional',
       'optionalWith',
       'enableWith',
+      'related',
       'allowNull',
       'allowFalse',
       'allowURL',
