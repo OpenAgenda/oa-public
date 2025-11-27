372f6e0bc44269562e8aa005eecc2ae03da66e0c
clemLecro <clement.lecroart@openagenda.com>
2025-11-27 14:24:37 +0100

    fix(next): change style for invalid event in contextBar

--- packages/next/src/views/EventShow/components/ContextBar/Edit.tsx
+++ packages/next/src/views/EventShow/components/ContextBar/Edit.tsx
@@ -1,13 +1,20 @@
 import { useId, useMemo } from 'react';
 import { useRouter } from 'next/router';
 import { useIntl } from 'react-intl';
-import { chakra, Link, useBreakpointValue } from '@openagenda/uikit';
+import {
+  chakra,
+  Link,
+  useBreakpointValue,
+  Text,
+  Flex,
+} from '@openagenda/uikit';
 import {
   MenuRoot,
   MenuTrigger,
   MenuContent,
   MenuItem,
   Tooltip,
+  Tag,
 } from '@openagenda/uikit/snippets';
 import { FaIcon } from 'icons';
 import { faPencil, faChevronDown } from 'icons/solid';
@@ -106,6 +113,8 @@ export default function Edit({ agenda, contextBarRef }) {
     return externalActions.filter((action) => action.action === 'edit');
   }, [externalActions]);
 
+  const isInvalid = event.valid === false;
+
   return (
     <MenuRoot
       ids={{ trigger: triggerId }}
@@ -136,9 +145,54 @@ export default function Edit({ agenda, contextBarRef }) {
             justifyContent="center"
           >
             {isMobile ? (
-              <FaIcon icon={faPencil} size="lg" />
+              <Flex alignItems="center" gap="2">
+                {isInvalid ? (
+                  <Tag
+                    borderRadius="full"
+                    variant="solid"
+                    bg="danger.500"
+                    borderWidth="1px"
+                    borderColor="white"
+                    w="5"
+                    h="5"
+                    display="flex"
+                    alignItems="center"
+                    justifyContent="center"
+                    fontSize="xs"
+                    fontWeight="bold"
+                  >
+                    !
+                  </Tag>
+                ) : null}
+                <FaIcon icon={faPencil} size="lg" />
+              </Flex>
             ) : (
-              <chakra.p flex="1">{intl.formatMessage(messages.edit)}</chakra.p>
+              <Flex direction="column" flex="1">
+                <chakra.span>{intl.formatMessage(messages.edit)}</chakra.span>
+                {isInvalid ? (
+                  <Flex alignItems="center" gap="2" mt="1">
+                    <Tag
+                      borderRadius="full"
+                      variant="solid"
+                      bg="danger.500"
+                      borderWidth="1px"
+                      borderColor="white"
+                      w="5"
+                      h="5"
+                      display="flex"
+                      alignItems="center"
+                      justifyContent="center"
+                      fontSize="xs"
+                      fontWeight="bold"
+                    >
+                      !
+                    </Tag>
+                    <Text fontSize="xs">
+                      {intl.formatMessage(messages.nonCompliant)}
+                    </Text>
+                  </Flex>
+                ) : null}
+              </Flex>
             )}
             {isMobile ? null : <FaIcon icon={faChevronDown} />}
           </ContextBarButton>
@@ -146,9 +200,41 @@ export default function Edit({ agenda, contextBarRef }) {
       </Tooltip>
 
       <MenuContent borderTopRadius="0" maxW="var(--available-width)">
-        <LinkMenuItem value="edit-event" href={editLink}>
-          {intl.formatMessage(messages.editEvent)}
-        </LinkMenuItem>
+        <MenuItem
+          value="edit-event"
+          asChild
+          fontWeight="bold"
+          height="50px"
+          title={
+            isInvalid
+              ? intl.formatMessage(messages.invalidEventInfo)
+              : undefined
+          }
+        >
+          <Link unstyled href={editLink}>
+            <Flex alignItems="center" gap="2">
+              {intl.formatMessage(messages.editEvent)}
+              {isInvalid ? (
+                <Tag
+                  borderRadius="full"
+                  variant="solid"
+                  bg="danger.500"
+                  borderWidth="1px"
+                  borderColor="white"
+                  w="5"
+                  h="5"
+                  display="flex"
+                  alignItems="center"
+                  justifyContent="center"
+                  fontSize="xs"
+                  fontWeight="bold"
+                >
+                  !
+                </Tag>
+              ) : null}
+            </Flex>
+          </Link>
+        </MenuItem>
         <LocationMenuItem
           agenda={agenda}
           event={event}
