b4e1f1d22390180f8e19c625ee6ae408790f91ca
Kaoré <kaore@openagenda.com>
2025-12-09 17:18:44 +0100

    feat(next): handle og metas on strapi pages

--- packages/next/src/views/StrapiPage/components/Metas.tsx
+++ packages/next/src/views/StrapiPage/components/Metas.tsx
@@ -1,10 +1,24 @@
 import Head from 'next/head';
 import { useRouter } from 'next/router';
 import { useIntl } from 'react-intl';
+import { videoPoster } from 'utils/strapi';
 
-export default function Metas({ title, description = null, keywords = null }) {
+const extractImage = ({ video, image }) => {
+  if (image?.formats.large) {
+    return image.formats.large;
+  }
+
+  if (video) {
+    return { url: videoPoster };
+  }
+  return undefined;
+};
+
+export default function Metas({ title, description = null, segment }) {
   const intl = useIntl();
   const router = useRouter();
+
+  const ogImage = extractImage(segment);
 
   const absUrl = new URL(router.asPath, process.env.NEXT_PUBLIC_ROOT);
   const canonicalUrl = `${absUrl.origin}/${intl.locale === 'io' ? 'en' : intl.locale}${absUrl.pathname}`;
@@ -15,11 +29,22 @@ export default function Metas({ title, description = null, keywords = null }) {
     <Head>
       <title>{pageTitle}</title>
       <meta name="robots" content="index, follow" />
-
       {description && <meta name="description" content={description} />}
-      {keywords && <meta name="keywords" content={keywords} />}
-
       <link rel="canonical" href={canonicalUrl} />
+
+      {/* Open Graph meta tags */}
+      <meta property="og:title" content={pageTitle} />
+      <meta property="og:type" content="website" />
+      <meta property="og:url" content={canonicalUrl} />
+      {description && <meta property="og:description" content={description} />}
+      <meta property="og:site_name" content="OpenAgenda" />
+      {ogImage?.url && <meta property="og:image" content={ogImage.url} />}
+      {ogImage?.width && (
+        <meta property="og:image:width" content={ogImage.width} />
+      )}
+      {ogImage?.height && (
+        <meta property="og:image:height" content={ogImage.height} />
+      )}
     </Head>
   );
 }
