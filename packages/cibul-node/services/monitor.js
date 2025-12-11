c57b6f69531c264e1d4fbe455517471ba534485a
Kaoré <kaore@openagenda.com>
2025-12-11 17:16:32 +0100

    fix(cibul-node): monitor log adjustments

--- packages/cibul-node/services/monitor.js
+++ packages/cibul-node/services/monitor.js
@@ -5,15 +5,23 @@ const period = 60 * 1000;
 
 export async function init() {
   const log = logs('monitor');
-  const { MASTER_HOST: host, MASTER_ID: ID, MASTER_IP: IP } = process.env;
+  const {
+    MASTER_HOST: host,
+    MASTER_ID: ID,
+    MASTER_IP: IP,
+    NODE_APP_INSTANCE: appInstance,
+  } = process.env;
   setInterval(() => {
     const memoryUsage = process.memoryUsage();
+
+    const [_nodePath, _processContainer, ...argv] = process.argv;
 
     log.info({
       host,
       ID,
       IP,
-      argv: process.argv,
+      appInstance,
+      argv,
       rss: Math.ceil(memoryUsage.rss / divideBy),
       heapTotal: Math.ceil(memoryUsage.heapTotal / divideBy),
       heapUsed: Math.ceil(memoryUsage.heapUsed / divideBy),
