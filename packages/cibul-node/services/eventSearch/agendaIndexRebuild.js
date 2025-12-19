6d2fbdd2e164aee2f56873a0323b03b199b19774
Kaoré <kaore@openagenda.com>
2025-12-19 16:52:58 +0100

    refactor(cibul-node): simplify agendaIndexRebuild

--- packages/cibul-node/services/eventSearch/agendaIndexRebuild.js
+++ packages/cibul-node/services/eventSearch/agendaIndexRebuild.js
@@ -3,36 +3,6 @@ import getAgendaSearchIndex from './lib/getAgendaSearchIndex.js';
 
 const log = logs('services/eventSearch/agendaIndexRebuild');
 
-function eventsList(core, agenda, logBundle) {
-  let count = 0;
-  return (lastId, limit) =>
-    core
-      .agendas(agenda.uid)
-      .events.list(
-        {},
-        {
-          lastId,
-          limit,
-        },
-        {
-          returnPayload: true,
-          detailed: true,
-          access: 'internal',
-          removed: null,
-          load: { valid: true },
-        },
-      )
-      .then(({ events, lastId: nextLastId }) => {
-        log.info('listed events', {
-          ...logBundle,
-          chunk: events.length,
-          nextLastId,
-          count: count += events.length,
-        });
-        return { lastId: nextLastId, events };
-      });
-}
-
 export default async (services, eventSearch, agenda) => {
   const { core } = services;
 
@@ -47,8 +17,24 @@ export default async (services, eventSearch, agenda) => {
   });
 
   const result = await searchIndex.rebuild({
-    eventsList: eventsList(core, agenda, logBundle),
+    eventsList: (lastId, limit) =>
+      core.agendas(agenda.uid).events.list(
+        {},
+        { lastId, limit },
+        {
+          returnPayload: true,
+          detailed: true,
+          access: 'internal',
+          removed: null,
+          load: { valid: true },
+        },
+      ),
     formSchema,
+    ons: {
+      bulk: (data) => {
+        log.info('bulk', { ...logBundle, data });
+      },
+    },
   });
 
   log.info('done', logBundle);
