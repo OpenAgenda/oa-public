79883762ab74ac4ae923e595c3c5441372368bd1
clemLecro <clement.lecroart@openagenda.com>
2026-01-13 10:01:11 +0100

    fix(validators): text validators rejectEmojis options and convert ©

--- packages/validators/src/lib/emojireg.js
+++ packages/validators/src/lib/emojireg.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const emojiReg = /[\uD83C-\uDBFF][\uDC00-\uDFFF]/g;
+const emojiReg = /[\uD83C-\uDBFF][\uDC00-\uDFFF]|[\u2600-\u26FF]|[\u2700-\u27BF]|[\u231A-\u231B]/g;
 
 module.exports = {
   emojiReg,
