b1af0b6646268c0f88d2292d8540373c61291bcb
Kévin Berthommier <kevin.bertho@gmail.com>
2026-01-29 16:07:13 +0100

    fix(next): remove console.log

--- packages/next/src/views/AgendaShow/components/Metas.tsx
+++ packages/next/src/views/AgendaShow/components/Metas.tsx
@@ -7,8 +7,6 @@ export default function Metas({ agenda, query, preload }) {
   const intl = useIntl();
   const router = useRouter();
 
-  console.log(router.asPath, process.env.NEXT_PUBLIC_ROOT);
-
   const absUrl = new URL(router.asPath, process.env.NEXT_PUBLIC_ROOT);
   const canonicalUrl = `${absUrl.origin}/${intl.locale === 'io' ? 'en' : intl.locale}${absUrl.pathname}`;
 
