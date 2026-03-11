849d88abb9311c6c0a32e731a19243fcadfa495c
clemLecro <clement.lecroart@openagenda.com>
2026-03-11 15:32:15 +0100

    fix(agenda-locations): multilingualTagInfo

--- packages/agenda-locations-app/src/flattenTagSetLabels.js
+++ packages/agenda-locations-app/src/flattenTagSetLabels.js
@@ -1,13 +1,14 @@
-const flatLabel = (label, preferredLang) =>
-  (typeof label === 'string'
-    ? label
-    : label[preferredLang] || label[Object.keys(label)]);
+const flatLabel = (label, preferredLang) => {
+  if (!label || typeof label === 'string') return label;
+  return label[preferredLang] || label[Object.keys(label)[0]];
+};
 
 export default (tagSet, lang) => ({
   ...tagSet,
   groups: tagSet.groups.map((g) => ({
     ...g,
     name: flatLabel(g.name, lang.toLowerCase()),
+    info: flatLabel(g.info, lang.toLowerCase()),
     tags: g.tags.map((t) => ({
       ...t,
       label: flatLabel(t.label, lang.toLowerCase()),
