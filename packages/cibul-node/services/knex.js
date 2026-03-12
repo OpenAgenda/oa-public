353b99f2c7f15d9aed22e1f3c2c027797d0c6dea
Kaoré <kaore@openagenda.com>
2026-03-12 12:48:56 +0100

    feat(cibul-node): log slow >2s querise

--- packages/cibul-node/services/knex.js
+++ packages/cibul-node/services/knex.js
@@ -15,12 +15,46 @@ export function init(config) {
 
   config.knex = knex;
 
-  knex.on('query-error', (error) => {
+  const QUERY_TTL = 60_000;
+  const queryStartTimes = new Map();
+
+  const sweepInterval = setInterval(() => {
+    const now = Date.now();
+    for (const [uid, startTime] of queryStartTimes) {
+      if (now - startTime > QUERY_TTL) {
+        queryStartTimes.delete(uid);
+      }
+    }
+  }, QUERY_TTL);
+  sweepInterval.unref();
+
+  knex.on('query', (query) => {
+    queryStartTimes.set(query.__knexQueryUid, Date.now());
+  });
+
+  knex.on('query-response', (_, query) => {
+    const startTime = queryStartTimes.get(query.__knexQueryUid);
+    queryStartTimes.delete(query.__knexQueryUid);
+    if (startTime !== undefined) {
+      const duration = Date.now() - startTime;
+      if (duration > 2000) {
+        log.warn('slow query (ms)', {
+          sql: query.sql,
+          bindings: query.bindings,
+          duration,
+        });
+      }
+    }
+  });
+
+  knex.on('query-error', (error, query) => {
+    queryStartTimes.delete(query.__knexQueryUid);
     log.error('Knex query error:', { error });
   });
 
   return Object.assign(knex, {
     shutdown: async () => {
+      clearInterval(sweepInterval);
       await knex.destroy();
     },
   });
