d28efed35bc5b3992293d2cbbf7647e10295e7fa
clemLecro <clement.lecroart@openagenda.com>
2026-03-13 18:06:58 +0100

    refactor(utils): moved stopwatch from cibul-node

---
+++ packages/utils/Stopwatch.js
@@ -0,0 +1,21 @@
+module.exports = function Stopwatch() {
+  const times = {};
+  let now = new Date();
+
+  function stopwatch(label, subTimes) {
+    const ms = new Date().getTime() - now.getTime();
+    now = new Date();
+
+    times[label] = ms;
+
+    if (subTimes) {
+      for (const [key, value] of Object.entries(subTimes)) {
+        times[`${label}.${key}`] = value;
+      }
+    }
+  }
+
+  stopwatch.getTimes = () => times;
+
+  return stopwatch;
+}
