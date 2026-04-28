8abd1441c2d9dda3279bc55889bbf11ef8b61f7a
clemLecro <clement.lecroart@openagenda.com>
2026-04-28 17:43:30 +0200

    fix(cibul-node): apply location additional fields conversion in resyncEvent

--- packages/cibul-node/core/agendas/events/search.js
+++ packages/cibul-node/core/agendas/events/search.js
@@ -2,6 +2,7 @@ import logs from '@openagenda/logs';
 import { NotFound, Forbidden } from '@openagenda/verror';
 import Stopwatch from '@openagenda/utils/Stopwatch.js';
 import preCleanSearchQuery from '../utils/preCleanSearchQuery.js';
+import convertLocationAdditionalFields from '../utils/convertLocationAdditionalFields.js';
 import * as convertLongDescription from './lib/convertLongDescription.js';
 import convertToDateHoursMinutesTimings from './lib/convertToDateHoursMinutesFormat.js';
 import loadSearchAccess from './lib/loadSearchAccess.js';
@@ -313,10 +314,21 @@ export async function resyncEvent(core, agendaUid, eventUid, options = {}) {
 
     log('resyncing event %s on index of agenda %s', eventUid, agendaUid);
 
-    const result = await core.services.eventSearch.update(eventPayload, {
-      updateOtherIndices: false,
-      batch,
-    });
+    const result = await core.services.eventSearch.update(
+      {
+        ...eventPayload,
+        event: eventPayload.event.location
+          ? convertLocationAdditionalFields(
+            eventPayload.formSchema,
+            eventPayload.event,
+          )
+          : eventPayload.event,
+      },
+      {
+        updateOtherIndices: false,
+        batch,
+      },
+    );
 
     return result;
   } catch (err) {
