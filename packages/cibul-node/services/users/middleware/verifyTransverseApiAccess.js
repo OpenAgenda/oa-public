a6c728fc594b769dbe5a30f98b768c42efbc15ea
Kaoré <kaore@openagenda.com>
2026-05-07 15:34:59 +0200

    fix: typo

--- packages/cibul-node/services/users/middleware/verifyTransverseApiAccess.js
+++ packages/cibul-node/services/users/middleware/verifyTransverseApiAccess.js
@@ -1,7 +1,8 @@
 export default async (req, res, next) => {
   if (!req?.user?.transverseApiAccess) {
     return res.status(403).json({
-      error: 'Not authorized, contact support@openagena.com to request access.',
+      error:
+        'Not authorized, contact support@openagenda.com to request access.',
     });
   }
   next();
