6b3c3a8b3ae64b759ebc22b4e0901ca1a84a18e9
Kévin Berthommier <kevin.bertho@gmail.com>
2026-05-14 12:12:44 +0200

    refactor(form-schemas): migrate client/ build to tsdown
    Replace babel-cli with tsdown for the client/ subpackage build. Server,
    iso/ and migrations/ stay source-exposed via exports map (passthrough,
    unchanged).
    
    - Add tsdown.config.ts (esm-only, JSX automatic, Emotion importSource)
    - Drop `clean` script and rimraf usage (tsdown wipes outDir natively)
    - Drop babel-cli + related deps from devDependencies

--- packages/form-schemas/.eslintrc.cjs
+++ packages/form-schemas/.eslintrc.cjs
@@ -18,6 +18,7 @@ module.exports = {
           `${__dirname}/testconfig.js`,
           `${__dirname}/stories/**/*.js`,
           `${__dirname}/server.dev.js`,
+          `${__dirname}/tsdown.config.ts`,
         ],
       },
     ],
--- packages/form-schemas/jest.config.js
+++ packages/form-schemas/jest.config.js
@@ -4,7 +4,5 @@ export default {
     url: 'http://localhost/',
   },
 
-  transform: {
-    '\\.[jt]sx?$': ['babel-jest', { rootMode: 'upward' }],
-  },
+  transform: {},
 };
